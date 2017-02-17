8d477f10bf429a24ee504d209708a90762cc3585
Sergiusz Urbaniak <sur@coreos.com>
2017-02-17 14:22:00 +0100

    etcd: use etcd gateway, use etcd fqdn as provisioned by tf/route53

--- openstack/userdata-worker.tf
+++ openstack/userdata-worker.tf
@@ -4,5 +4,6 @@ data "template_file" "userdata-worker" {
   vars {
     kube_config      = "${base64encode(file("${path.root}/../assets/auth/kubeconfig"))}"
     tectonic_version = "${var.tectonic_version}"
+    etcd_fqdn        = "${aws_route53_record.etcd.fqdn}"
   }
 }
