ed33de81c95b5b2028177d74dfc7c78dfaa13664
Ed Rooth <ed.rooth@coreos.com>
2017-03-13 17:15:42 +0100

    add some more top-level config vars

--- Documentation/variables/config.md
+++ Documentation/variables/config.md
@@ -5,10 +5,29 @@ All the common Tectonic SDK variables used for *all* platforms.
 
 | Name | Description | Default | Required |
 |------|-------------|:-----:|:-----:|
-| tectonic_base_domain | The base DNS domain of the cluster. Example: `openstack.dev.coreos.systems` | - | yes |
-| tectonic_cluster_name | The name of the cluster. This will be prepended to `tectonic_base_domain` resulting in the URL to the Tectonic console. Example: `demo` | - | yes |
-| tectonic_etcd_count | The amount of etcd nodes to be created. Example: `1` | `1` | no |
-| tectonic_kube_version | The hyperkube "quay.io/coreos/hyperkube" image version. | - | yes |
-| tectonic_master_count | The amount of master nodes to be created. Example: `1` | - | yes |
-| tectonic_worker_count | The amount of worker nodes to be created. Example: `3` | - | yes |
+| tectonic_admin_email | e-mail address used to login to Tectonic | - | yes |
+| tectonic_admin_password_hash | bcrypt hash of admin password to use with Tectonic Console | - | yes |
+| tectonic_base_domain | Base address used to access the Tectonic Console, without protocol nor trailing forward slash | - | yes |
+| tectonic_ca_cert | PEM-encoded CA certificate, used to generate Tectonic Console's server certificate. Optional, if left blank, a CA certificate will be automatically generated. | - | yes |
+| tectonic_ca_key | PEM-encoded CA key, used to generate Tectonic Console's server certificate. Optional if tectonic_ca_cert is left blank | - | yes |
+| tectonic_ca_key_alg | Algorithm used to generate tectonic_ca_key. Optional if tectonic_ca_cert is left blank. | `RSA` | no |
+| tectonic_cl_channel |  | `stable` | no |
+| tectonic_cluster_cidr | A CIDR notation IP range from which to assign pod IPs | `10.2.0.0/16` | no |
+| tectonic_cluster_name | The name of the cluster. This will be prepended to `tectonic_base_domain` resulting in the URL to the Tectonic console. | - | yes |
+| tectonic_container_images | Container images to use | `<map>` | no |
+| tectonic_etcd_count | The number of etcd nodes to be created. | `1` | no |
+| tectonic_etcd_servers | List of extenral etcd v3 servers to connect with (scheme://ip:port). Optionally use if providing external etcd. | - | yes |
+| tectonic_ingress_type | Type of Ingress mapping to use (e.g. HostPort, NodePort) | `HostPort` | no |
+| tectonic_kube_apiserver_service_ip | Service IP used to reach kube-apiserver inside the cluster | `10.3.0.1` | no |
+| tectonic_kube_apiserver_url | URL used to reach kube-apiserver | `https://10.1.1.1:443` | no |
+| tectonic_kube_dns_service_ip | Service IP used to reach kube-dns | `10.3.0.10` | no |
+| tectonic_license |  | - | yes |
+| tectonic_master_count | The number of master nodes to be created. | `1` | no |
+| tectonic_pull_secret |  | - | yes |
+| tectonic_service_cidr | A CIDR notation IP range from which to assign service cluster IPs | `10.3.0.0/16` | no |
+| tectonic_update_app_id |  | `6bc7b986-4654-4a0f-94b3-84ce6feb1db4` | no |
+| tectonic_update_channel |  | `tectonic-1.5` | no |
+| tectonic_update_server |  | `https://public.update.core-os.net` | no |
+| tectonic_versions | Versions of the components to use | `<map>` | no |
+| tectonic_worker_count | The number of worker nodes to be created. | `3` | no |
 
--- config.tf
+++ config.tf
@@ -1,89 +1,160 @@
-// The flavor ID as given in `openstack flavor list`.
-// Specifies the size (CPU/Memory/Drive) of the VM.
-variable "tectonic_openstack_flavor_id" {
-  type    = "string"
-  default = "5cf64088-893b-46b5-9bb1-ee020277635d"
+variable "tectonic_container_images" {
+  description = "Container images to use"
+  type        = "map"
+
+  default = {
+    hyperkube                 = "quay.io/coreos/hyperkube:v1.5.4_coreos.0"
+    pod_checkpointer          = "quay.io/coreos/pod-checkpointer:5b585a2d731173713fa6871c436f6c53fa17f754"
+    bootkube                  = "quay.io/coreos/bootkube:v0.3.11"
+    console                   = "quay.io/coreos/tectonic-console:v1.0.3"
+    identity                  = "quay.io/coreos/dex:v2.2.4"
+    kube_version_operator     = "quay.io/coreos/kube-version-operator:79aa49c6b225f27e4479a502efeaae4195e1070e"
+    tectonic_channel_operator = "quay.io/coreos/tectonic-channel-operator:3d36eba4ecd9cbc50e1f3773d07e6bc358b00096"
+    node_agent                = "quay.io/coreos/node-agent:96db3df24c2ddf0b82c38111c71179c5b10de0c9"
+    prometheus_operator       = "quay.io/coreos/prometheus-operator:v0.6.0"
+    node_exporter             = "quay.io/prometheus/node-exporter:v0.13.0"
+    config_reload             = "quay.io/coreos/configmap-reload:v0.0.1"
+    heapster                  = "gcr.io/google_containers/heapster:v1.3.0-beta.0"
+    addon_resizer             = "gcr.io/google_containers/addon-resizer:1.7"
+    stats_emitter             = "quay.io/coreos/tectonic-stats:6e882361357fe4b773adbf279cddf48cb50164c1"
+    stats_extender            = "quay.io/coreos/tectonic-stats-extender:487b3da4e175da96dabfb44fba65cdb8b823db2e"
+    error_server              = "quay.io/coreos/tectonic-error-server:1.0"
+    ingress_controller        = "gcr.io/google_containers/nginx-ingress-controller:0.8.3"
+  }
 }
 
-// The image ID as given in `openstack image list`.
-// Specifies the OS image of the VM.
-variable "tectonic_openstack_image_id" {
-  type    = "string"
-  default = "acdcd535-5408-40f3-8e88-ad8ebb6507e6"
+variable "tectonic_versions" {
+  description = "Versions of the components to use"
+  type        = "map"
+
+  default = {
+    prometheus = "v1.5.2"
+    kubernetes = "1.5.3+tectonic.1"
+    tectonic   = "1.5.3-tectonic.1"
+  }
 }
 
-// The ID of the network to be used as the external internet gateway
-// as given in `openstack network list`.
-variable "tectonic_openstack_external_gateway_id" {
-  type    = "string"
-  default = "6d6357ac-0f70-4afa-8bd7-c274cc4ea235"
+variable "tectonic_kube_apiserver_url" {
+  type        = "string"
+  description = "URL used to reach kube-apiserver"
+  default     = "https://10.1.1.1:443"
 }
 
-// The hyperkube "quay.io/coreos/hyperkube" image version.
-variable "tectonic_kube_version" {
-  type = "string"
+variable "tectonic_kube_apiserver_service_ip" {
+  type        = "string"
+  description = "Service IP used to reach kube-apiserver inside the cluster"
+  default     = "10.3.0.1"
 }
 
-// The amount of master nodes to be created.
-// Example: `1`
+variable "tectonic_kube_dns_service_ip" {
+  type        = "string"
+  description = "Service IP used to reach kube-dns"
+  default     = "10.3.0.10"
+}
+
+variable "tectonic_service_cidr" {
+  description = "A CIDR notation IP range from which to assign service cluster IPs"
+  type        = "string"
+  default     = "10.3.0.0/16"
+}
+
+variable "tectonic_cluster_cidr" {
+  description = "A CIDR notation IP range from which to assign pod IPs"
+  type        = "string"
+  default     = "10.2.0.0/16"
+}
+
 variable "tectonic_master_count" {
-  type    = "string"
+  type        = "string"
+  description = "The number of master nodes to be created."
+  default     = "1"
 }
 
-// The amount of worker nodes to be created.
-// Example: `3`
 variable "tectonic_worker_count" {
-  type    = "string"
+  type        = "string"
+  description = "The number of worker nodes to be created."
+  default     = "3"
 }
 
-// The amount of etcd nodes to be created.
 // Example: `1`
 variable "tectonic_etcd_count" {
-  type    = "string"
-  default = "1"
+  type        = "string"
+  default     = "1"
+  description = "The number of etcd nodes to be created."
 }
 
-// The base DNS domain of the cluster.
-// Example: `openstack.dev.coreos.systems`
-variable "tectonic_base_domain" {
-  type    = "string"
+variable "tectonic_etcd_servers" {
+  description = "List of extenral etcd v3 servers to connect with (scheme://ip:port). Optionally use if providing external etcd."
+  type        = "list"
 }
 
-// The name of the cluster.
-// This will be prepended to `tectonic_base_domain` resulting in the URL to the Tectonic console.
 // Example: `demo`
 variable "tectonic_cluster_name" {
-  type    = "string"
+  type        = "string"
+  description = "The name of the cluster. This will be prepended to `tectonic_base_domain` resulting in the URL to the Tectonic console."
 }
 
-// ID of existing VPC to build the cluster into.
-// Example: `vpc-5c73a334`
-variable "tectonic_aws_external_vpc_id" {
-  type    = "string"
-  default = ""
-}
-
-// IP address range to use when creating the cluster VPC.
-// Example: `10.0.0.0/16`
-variable "tectonic_aws_vpc_cidr_block" {
-  type    = "string"
-  default = "10.0.0.0/16"
-}
-
-// Number of availability zones the cluster should span.
-// Example: `3`
-variable "tectonic_aws_az_count" {
+variable "tectonic_pull_secret" {
   type = "string"
 }
 
-// EC2 instance type to use for master nodes.
-// Example: `m4.large`
-variable "tectonic_aws_master_ec2_type" {
+variable "tectonic_license" {
   type = "string"
 }
 
-// EC2 instance type to use for worker nodes. 
-// Example: `m4.large`
-variable "tectonic_aws_worker_ec2_type" {
-  type = "string"
+variable "tectonic_cl_channel" {
+  type    = "string"
+  default = "stable"
+}
+
+variable "tectonic_update_server" {
+  type    = "string"
+  default = "https://public.update.core-os.net"
+}
+
+variable "tectonic_update_channel" {
+  type    = "string"
+  default = "tectonic-1.5"
+}
+
+variable "tectonic_update_app_id" {
+  type    = "string"
+  default = "6bc7b986-4654-4a0f-94b3-84ce6feb1db4"
+}
+
+variable "tectonic_admin_email" {
+  type        = "string"
+  description = "e-mail address used to login to Tectonic"
+}
+
+variable "tectonic_admin_password_hash" {
+  type        = "string"
+  description = "bcrypt hash of admin password to use with Tectonic Console"
+}
+
+variable "tectonic_ingress_type" {
+  description = "Type of Ingress mapping to use (e.g. HostPort, NodePort)"
+  type        = "string"
+  default     = "HostPort"
+}
+
+variable "tectonic_base_domain" {
+  type        = "string"
+  description = "Base address used to access the Tectonic Console, without protocol nor trailing forward slash"
+}
+
+variable "tectonic_ca_cert" {
+  type        = "string"
+  description = "PEM-encoded CA certificate, used to generate Tectonic Console's server certificate. Optional, if left blank, a CA certificate will be automatically generated."
+}
+
+variable "tectonic_ca_key" {
+  type        = "string"
+  description = "PEM-encoded CA key, used to generate Tectonic Console's server certificate. Optional if tectonic_ca_cert is left blank"
+}
+
+variable "tectonic_ca_key_alg" {
+  type        = "string"
+  description = "Algorithm used to generate tectonic_ca_key. Optional if tectonic_ca_cert is left blank."
+  default     = "RSA"
 }
