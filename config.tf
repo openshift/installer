ffd84778f473030ad7891348ba3c375d736993bd
Quentin Machu <me@quentin-machu.fr>
2017-03-17 16:59:08 +0100

    platforms/aws: move AWS specific variables out of the root config file

--- config.tf
+++ config.tf
@@ -164,36 +164,6 @@ variable "tectonic_ca_key_alg" {
   default     = "RSA"
 }
 
-// Name of an EC2 ssh key to use
-// joe-sfo
-variable "tectonic_aws_ssh_key" {
-  type = "string"
-}
-
 variable "tectonic_dns_name" {
-  type = "string"
-}
-
-variable "tectonic_aws_master_ec2_type" {
-  type = "string"
-}
-
-variable "tectonic_aws_worker_ec2_type" {
-  type = "string"
-}
-
-variable "tectonic_aws_etcd_ec2_type" {
-  type = "string"
-}
-
-variable "tectonic_aws_vpc_cidr_block" {
-  type = "string"
-}
-
-variable "tectonic_aws_az_count" {
-  type = "string"
-}
-
-variable "tectonic_aws_external_vpc_id" {
   type = "string"
 }
