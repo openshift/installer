ff7872f5077c87599511e394a5028d1e47efe814
Rob Szumski <rob@robszumski.com>
2017-03-30 10:36:02 -0700

    azure: remove unused variable

--- platforms/azure/variables.tf
+++ platforms/azure/variables.tf
@@ -1,10 +1,3 @@
-// The flavor ID as given in `azure flavor list`.
-// Specifies the size (CPU/Memory/Drive) of the VM.
-variable "tectonic_azure_vm_size" {
-  type    = "string"
-  default = "Standard_DS2"
-}
-
 variable "tectonic_azure_dns_resource_group" {
   type    = "string"
   default = "tectonic-dns-group"
