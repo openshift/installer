e3d359b0201991e86aec704b3b72f75441ef72a0
Rob Szumski <rob@robszumski.com>
2017-03-30 11:50:56 -0700

    tectonic: update variable descriptions

--- modules/tectonic/variables.tf
+++ modules/tectonic/variables.tf
@@ -1,94 +1,98 @@
 variable "container_images" {
-    description = "Container images to use"
+    description = "Container images to use. Leave blank for defaults."
     type = "map"
 }
 
 variable "versions" {
-    description = "Versions of the components to use"
+    description = "Versions of the components to use. Leave blank for defaults."
     type = "map"
 }
 
 variable "platform" {
-    description = "Platform on which Tectonic is being installed (e.g. bare-metal, aws)"
+    description = "Platform on which Tectonic is being installed. Example: bare-metal or aws."
     type        = "string"
 }
 
 variable "ingress_kind" {
-    description = "Type of Ingress mapping to use (e.g. HostPort, NodePort)"
+    description = "Type of Ingress mapping to use. Example: HostPort or NodePort."
     type        = "string"
 }
 
 variable "license_path" {
-    description = "Path to the license issued to run Tectonic"
+    description = "Path on disk to your Tectonic license. Obtain this from your Tectonic Account: https://account.tectonic.com."
     type        = "string"
+    default = "/Users/coreos/Desktop/tectonic-license.txt"
 }
 
 variable "pull_secret_path" {
-    description = "Path to the authentication secret used to pull container images"
     type        = "string"
+    description = "Path on disk to your Tectonic pull secret. Obtain this from your Tectonic Account: https://account.tectonic.com."
+    default = "/Users/coreos/Desktop/config.json"
 }
 
 variable "ca_generated" {
-    description = "Define whether the CA has been generated or user-provided"
+    description = "Define whether the CA has been generated or user-provided."
     type        = "string"
 }
 
 variable "ca_cert" {
-    description = "PEM-encoded CA certificate, used to generate Tectonic Console's server certificate"
+    description = "Contents of a PEM-encoded CA certificate, used to generate Tectonic Console's server certificate. Leave blank to generate a new CA."
     type        = "string"
 }
 
 variable "ca_key_alg" {
-    description = "Algorithm used to generate ca_key"
+    description = "Algorithm used to generate ca_key. Example: RSA."
     type        = "string"
 }
 
 variable "ca_key" {
-    description = "PEM-encoded CA key, used to generate Tectonic Console's server certificate"
+    description = "Contents of a PEM-encoded CA key, used to generate Tectonic Console's server certificate. Leave blank to generate a new CA."
     type        = "string"
 }
 
 variable "base_address" {
-    description = "Base address used to access the Tectonic Console, without protocol nor trailing forward slash (may contain a port)"
+    description = "Base address used to access the Tectonic Console, without protocol nor trailing forward slash (may contain a port). Example: console.example.com:30000."
     type        = "string"
 }
 
 variable "admin_email" {
-    description = "E-mail address used to login to the Tectonic Console"
+    description = "E-mail address used to log in to the Tectonic Console."
     type        = "string"
+    default = "admin@example.com"
 }
 
 variable "admin_password_hash" {
-    description = "Password used to login to the Tectonic Console, hashed by bcrypt"
+    description = "Hashed password used to by the cluster admin to login to the Tectonic Console. Generate with the bcrypt-hash tool (https://github.com/coreos/bcrypt-tool/releases/tag/v1.0.0)."
     type        = "string"
+    default = "2a$12$k9wa31uE/4uD9aVtT/vNtOZwxXyEJ/9DwXXEYB/eUpb9fvEPsH/kO"
 }
 
 variable "update_server" {
-    description = "Server contacted to pull updates from"
+    description = "Server contacted to request Tectonic software updates. Leave blank for defaults."
     type        = "string"
 }
 
 variable "update_channel" {
-    description = "Channel to pull updates from"
+    description = "Release channel used to request Tectonic software updates. Leave blank for defaults. Example: Tectonic-1.5"
     type        = "string"
 }
 
 variable "update_app_id" {
-    description = "Application identifier to pull updates for"
+    description = "Application identifier used to request Tectonic software updates. Leave blank for defaults."
     type        = "string"
 }
 
 variable "console_client_id" {
-    description = "OIDC identifier for the Tectonic Console"
+    description = "OIDC identifier for the Tectonic Console. Leave blank for defaults."
     type        = "string"
 }
 
 variable "kubectl_client_id" {
-    description = "OIDC identifier for kubectl"
+    description = "OIDC identifier for kubectl. Leave blank for defaults."
     type        = "string"
 }
 
 variable "kube_apiserver_url" {
-    description = "URL used to reach kube-apiserver"
+    description = "URL used to reach kube-apiserver. Leave blank for defaults."
     type        = "string"
 }
