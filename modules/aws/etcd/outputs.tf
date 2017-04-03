f4da2319b7ec00849e73bb8afb46a5ba69d5c202
Alex Somesan <alex.somesan@gmail.com>
2017-04-03 12:59:36 +0200

    Use same conditional format as in rest of module (#159)
    * Use same conditional format as in rest of module

--- modules/aws/etcd/outputs.tf
+++ modules/aws/etcd/outputs.tf
@@ -1,4 +1,4 @@
 # We have to do this join() & split() 'trick' because the ternary operator can't output lists.
 output "endpoints" {
-  value = ["${split(",", join(",", var.external_endpoints) == "" ? join(",", aws_route53_record.etc_a_nodes.*.fqdn) :  join(",", var.external_endpoints))}"]
+  value = ["${split(",", length(var.external_endpoints) == 0 ? join(",", aws_route53_record.etc_a_nodes.*.fqdn) : join(",", var.external_endpoints))}"]
 }
