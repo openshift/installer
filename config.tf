3266fce3bcbc1a9dd97f16fc34b6b1bde338deb4
Lucas Servén <lserven@gmail.com>
2017-04-10 07:46:34 -0700

    config.tf: bump to k8s 1.5.6 (#197)

--- config.tf
+++ config.tf
@@ -3,9 +3,9 @@ variable "tectonic_container_images" {
   type        = "map"
 
   default = {
-    hyperkube                    = "quay.io/coreos/hyperkube:v1.5.5_coreos.0"
+    hyperkube                    = "quay.io/coreos/hyperkube:v1.5.6_coreos.0"
     pod_checkpointer             = "quay.io/coreos/pod-checkpointer:5b585a2d731173713fa6871c436f6c53fa17f754"
-    bootkube                     = "quay.io/coreos/bootkube:v0.3.12"
+    bootkube                     = "quay.io/coreos/bootkube:v0.3.13"
     console                      = "quay.io/coreos/tectonic-console:v1.3.0"
     identity                     = "quay.io/coreos/dex:v2.2.4"
     kube_version_operator        = "quay.io/coreos/kube-version-operator:7da46d189c36092f43d07ca381a61897402fa13c"
@@ -22,8 +22,8 @@ variable "tectonic_container_images" {
     error_server                 = "quay.io/coreos/tectonic-error-server:1.0"
     ingress_controller           = "gcr.io/google_containers/nginx-ingress-controller:0.9.0-beta.3"
     kubedns                      = "gcr.io/google_containers/kubedns-amd64:1.9"
-    kubednsmasq                  = "gcr.io/google_containers/kube-dnsmasq-amd64:1.4"
-    dnsmasq_metrics              = "gcr.io/google_containers/dnsmasq-metrics-amd64:1.0"
+    kubednsmasq                  = "gcr.io/google_containers/kube-dnsmasq-amd64:1.4.1"
+    dnsmasq_metrics              = "gcr.io/google_containers/dnsmasq-metrics-amd64:1.0.1"
     exechealthz                  = "gcr.io/google_containers/exechealthz-amd64:1.2"
     flannel                      = "quay.io/coreos/flannel:v0.7.0-amd64"
     etcd                         = "quay.io/coreos/etcd:v3.1.2"
@@ -39,8 +39,8 @@ variable "tectonic_versions" {
     etcd       = "v3.1.2"
     prometheus = "v1.5.2"
     monitoring = "1.0.0"
-    kubernetes = "1.5.5+tectonic.2"
-    tectonic   = "1.5.5-tectonic.2"
+    kubernetes = "1.5.6+tectonic.0"
+    tectonic   = "1.5.6-tectonic.0"
   }
 }
 
