fde17208754e32889e08c0ab8d33a82e3b465769
Dalton Hubble <dghubble@gmail.com>
2017-04-19 15:02:04 -0700

    platforms/bare-metal: Prefix variable names with tectonic

--- platforms/bare-metal/profiles.tf
+++ platforms/bare-metal/profiles.tf
@@ -1,20 +1,12 @@
-// Configure the matchbox provider
-provider "matchbox" {
-  endpoint = "${var.matchbox_rpc_endpoint}"
-  client_cert = "${var.matchbox_client_cert}"
-  client_key = "${var.matchbox_client_key}"
-  ca         = "${var.matchbox_ca}"
-}
-
 // CoreOS Install Profile
 resource "matchbox_profile" "coreos-install" {
   name = "coreos-install"
-  kernel = "/assets/coreos/${var.coreos_version}/coreos_production_pxe.vmlinuz"
+  kernel = "/assets/coreos/${var.tectonic_coreos_version}/coreos_production_pxe.vmlinuz"
   initrd = [
-    "/assets/coreos/${var.coreos_version}/coreos_production_pxe_image.cpio.gz"
+    "/assets/coreos/${var.tectonic_coreos_version}/coreos_production_pxe_image.cpio.gz"
   ]
   args = [
-    "coreos.config.url=${var.matchbox_http_endpoint}/ignition?uuid=$${uuid}&mac=$${mac:hexhyp}",
+    "coreos.config.url=${var.tectonic_matchbox_http_endpoint}/ignition?uuid=$${uuid}&mac=$${mac:hexhyp}",
     "coreos.first_boot=yes",
     "console=tty0",
     "console=ttyS0"
--- platforms/bare-metal/variables.tf
+++ platforms/bare-metal/variables.tf
@@ -1,29 +1,29 @@
-variable "matchbox_http_endpoint" {
+variable "tectonic_matchbox_http_endpoint" {
   type = "string"
   description = "Matchbox HTTP read-only endpoint (e.g. http://matchbox.example.com:8080)"
 }
 
-variable "matchbox_rpc_endpoint" {
+variable "tectonic_matchbox_rpc_endpoint" {
   type = "string"
   description = "Matchbox gRPC API endpoint (e.g. matchbox.example.com:8081)"
 }
 
-variable "matchbox_client_cert" {
+variable "tectonic_matchbox_client_cert" {
   type = "string"
   description = "Matchbox client TLS certificate"
 }
 
-variable "matchbox_client_key" {
+variable "tectonic_matchbox_client_key" {
   type = "string"
   description = "Matchbox client TLS key"
 }
 
-variable "matchbox_ca" {
+variable "tectonic_matchbox_ca" {
   type = "string"
   description = "Matchbox CA certificate to trust"
 }
 
-variable "coreos_version" {
+variable "tectonic_coreos_version" {
   type = "string"
   description = "CoreOS kernel/initrd version to PXE boot. Must be present in matchbox assets."
 }
