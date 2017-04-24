aacd6cc770b67e9a593a03f8945dce8594282ab6
Dalton Hubble <dghubble@gmail.com>
2017-04-24 10:35:53 -0700

    platforms/metal: Prefix matchbox vars with metal

--- platforms/metal/variables.tf
+++ platforms/metal/variables.tf
@@ -1,24 +1,24 @@
-variable "tectonic_matchbox_http_endpoint" {
+variable "tectonic_metal_matchbox_http_endpoint" {
   type        = "string"
   description = "Matchbox HTTP read-only endpoint (e.g. http://matchbox.example.com:8080)"
 }
 
-variable "tectonic_matchbox_rpc_endpoint" {
+variable "tectonic_metal_matchbox_rpc_endpoint" {
   type        = "string"
   description = "Matchbox gRPC API endpoint (e.g. matchbox.example.com:8081)"
 }
 
-variable "tectonic_matchbox_client_cert" {
+variable "tectonic_metal_matchbox_client_cert" {
   type        = "string"
   description = "Matchbox client TLS certificate"
 }
 
-variable "tectonic_matchbox_client_key" {
+variable "tectonic_metal_matchbox_client_key" {
   type        = "string"
   description = "Matchbox client TLS key"
 }
 
-variable "tectonic_matchbox_ca" {
+variable "tectonic_metal_matchbox_ca" {
   type        = "string"
   description = "Matchbox CA certificate to trust"
 }
