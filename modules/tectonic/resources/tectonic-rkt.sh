4a09b6530d953f092141a1c7fc9e45fb2c2950e6
Aaron Levy <aaronjlevy@gmail.com>
2017-04-25 13:46:32 -0700

    modules/bootkube: disable insecure apiserver port

--- modules/tectonic/resources/tectonic-rkt.sh
+++ modules/tectonic/resources/tectonic-rkt.sh
@@ -7,4 +7,4 @@
   ${hyperkube_image} \
   --net=host \
   --dns=host \
-  --exec=/bin/bash -- /assets/tectonic.sh /assets/kubeconfig /assets ${experimental}
+  --exec=/bin/bash -- /assets/tectonic.sh /assets/auth/kubeconfig /assets ${experimental}
