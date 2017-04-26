648db4538fc14c202fe57bbf1f7b55e1fbde2b7b
Amruta Chitnis <amruta.chitnis@coreos.com>
2017-04-26 00:40:24 -0700

    frontend: Updates console to v1.4.2 (#349)

--- config.tf
+++ config.tf
@@ -19,7 +19,7 @@ variable "tectonic_container_images" {
     hyperkube                    = "quay.io/coreos/hyperkube:v1.6.2_coreos.0"
     pod_checkpointer             = "quay.io/coreos/pod-checkpointer:20cf8b9a6018731a0770192f30dfa7a1941521e3"
     bootkube                     = "quay.io/coreos/bootkube:v0.4.1"
-    console                      = "quay.io/coreos/tectonic-console:v1.4.1"
+    console                      = "quay.io/coreos/tectonic-console:v1.4.2"
     identity                     = "quay.io/coreos/dex:v2.3.0"
     kube_version_operator        = "quay.io/coreos/kube-version-operator:7da46d189c36092f43d07ca381a61897402fa13c"
     tectonic_channel_operator    = "quay.io/coreos/tectonic-channel-operator:15c001bd7c008a04394390d08ac71046e723ac48"
