d925abef8d33c2ad296461a0ce8288f62aa965da
Chance Zibolski <chance.zibolski@coreos.com>
2017-04-26 11:00:07 -0700

    Ignore AMI changing on worker launch config
    Prevents terraform from recreating the resource when a new CoreOS
    release comes out.
    
    This an extension of the same changes done to etcd and masters in #367

--- modules/aws/worker-asg/worker.tf
+++ modules/aws/worker-asg/worker.tf
@@ -33,6 +33,10 @@ resource "aws_launch_configuration" "worker_conf" {
 
   lifecycle {
     create_before_destroy = true
+    # Ignore changes in the AMI which force recreation of the resource. This
+    # avoids accidental deletion of nodes whenever a new CoreOS Release comes
+    # out.
+    ignore_changes        = ["image_id"]
   }
 
   root_block_device {
