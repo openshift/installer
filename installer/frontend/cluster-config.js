95647ff27a21894658892c7cc6bda10dd6145ffb
Geoff Greer <geoff@greer.fm>
2017-04-27 12:09:55 -0700

    frontend: Closer to correct TFvars for baremetal.

--- installer/frontend/cluster-config.js
+++ installer/frontend/cluster-config.js
@@ -446,38 +446,35 @@ export const toBaremetal_TF = (cc) => {
     pullSecret: cc[PULL_SECRET],
     adminPassword: window.btoa(cc[ADMIN_PASSWORD]),
     variables: {
-      tectonic_metal_matchbox_http_endpoint: cc[BM_MATCHBOX_HTTP],
-      tectonic_metal_matchbox_rpc_endpoint: cc[BM_MATCHBOX_RPC],
       tectonic_cluster_name: cc[CLUSTER_NAME],
       tectonic_cl_channel: cc[CHANNEL_TO_USE],
       tectonic_admin_email: cc[ADMIN_EMAIL],
       tectonic_metal_cl_version: cc[BM_OS_TO_USE],
-      tectonic_metal_controller_names: cc[BM_MASTERS].map(({name}) => name),
-      tectonic_metal_controller_macs: cc[BM_MASTERS].map(({mac}) => mac),
-      tectonic_metal_controller_domains: ["node1.example.com"],
-
-      tectonic_metal_worker_names: cc[BM_WORKERS].map(({name}) => name),
-      tectonic_metal_worker_macs: cc[BM_WORKERS].map(({mac}) => mac),
-      tectonic_metal_worker_domains: ["node2.example.com", "node3.example.com"],
-
-      tectonic_base_domain: getTectonicDomain(cc),
-      tectonic_metal_controller_domain: getControllerDomain(cc),
       tectonic_metal_ingress_domain: getTectonicDomain(cc),
-      tectonic_ssh_authorized_key: cc[SSH_AUTHORIZED_KEYS].map(k => k.key).filter(k => k && k.length).join('\n'),
-
+      tectonic_metal_controller_domain: getControllerDomain(cc),
+      tectonic_metal_controller_domains: cc[BM_MASTERS].map(({name}) => name),
+      tectonic_metal_controller_names: cc[BM_MASTERS].map(({name}) => name.split('.')[0]),
+      tectonic_metal_controller_macs: cc[BM_MASTERS].map(({mac}) => mac),
+      tectonic_metal_worker_domains: cc[BM_WORKERS].map(({name}) => name),
+      tectonic_metal_worker_names: cc[BM_WORKERS].map(({name}) => name.split('.')[0]),
+      tectonic_metal_worker_macs: cc[BM_WORKERS].map(({mac}) => mac),
+      tectonic_metal_matchbox_http_endpoint: cc[BM_MATCHBOX_HTTP],
+      tectonic_metal_matchbox_rpc_endpoint: cc[BM_MATCHBOX_RPC],
       tectonic_metal_matchbox_ca: cc[BM_MATCHBOX_CA],
       tectonic_metal_matchbox_client_cert: cc[BM_MATCHBOX_CLIENT_CERT],
       tectonic_metal_matchbox_client_key: cc[BM_MATCHBOX_CLIENT_KEY],
-
+      tectonic_ssh_authorized_key: cc[SSH_AUTHORIZED_KEYS].map(k => k.key).filter(k => k && k.length).join('\n'),
       tectonic_cluster_cidr: cc[POD_CIDR],
-      tectonic_kube_etcd_service_ip: cc[EXTERNAL_ETCD_ENABLED] ? cc[EXTERNAL_ETCD_CLIENT] : '',
-      tectonic_kube_self_hosted_etcd: cc[EXTERNAL_ETCD_ENABLED],
-      etcd_servers: [],
       tectonic_service_cidr: cc[SERVICE_CIDR],
       tectonic_dns_name: cc[CLUSTER_SUBDOMAIN],
       tectonic_experimental: cc[UPDATER_ENABLED],
+      tectonic_base_domain: 'unused',
     },
   };
+
+  if (cc[EXTERNAL_ETCD_ENABLED]) {
+    ret.variables.tectonic_etcd_servers = [cc[EXTERNAL_ETCD_CLIENT]];
+  }
 
   if (cc[CA_TYPE] === 'owned') {
     ret.variables.tectonic_ca_cert = cc[CA_CERTIFICATE];
