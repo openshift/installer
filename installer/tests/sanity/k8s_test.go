1e87d9ea60cebcae4f2e053bc4b390575d44bb75
Dan Gillespie <dan.gillespie@coreos.com>
2017-05-08 14:30:40 -0700

    installer/tests/sanity: enable Pod logging test

--- installer/tests/sanity/k8s_test.go
+++ installer/tests/sanity/k8s_test.go
@@ -59,8 +59,8 @@ func TestCluster(t *testing.T) {
 
 	// wait for all nodes to become available
 	t.Run("AllNodesRunning", testAllNodesRunning)
-	t.Run("AllPodsRunning", testAllPodsRunning)
 	t.Run("GetLogs", testLogs)
+	t.Run("AllPodsRunning", testAllPodsRunning)
 	t.Run("KillAPIServer", testKillAPIServer)
 }
 
@@ -113,8 +113,6 @@ func testAllPodsRunning(t *testing.T) {
 }
 
 func testLogs(t *testing.T) {
-	// TODO: Diagnose why this fails.
-	t.SkipNow()
 	c := newClient(t)
 
 	namespace := "tectonic-system"
