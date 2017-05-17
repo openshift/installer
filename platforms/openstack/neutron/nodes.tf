6405c9faddfa371660304c4c8748aacc3d3d2394
Corey O'Brien <coreypobrien@gmail.com>
2017-05-17 08:24:27 -0400

    openstack/neutron: Fix worker floating IP association (#715)
    openstack_compute_floatingip_associate_v2.worker was incorrrectly using the
    tectonic_master_count instead of the tectonic_worker_count

--- platforms/openstack/neutron/nodes.tf
+++ platforms/openstack/neutron/nodes.tf
@@ -66,7 +66,7 @@ resource "openstack_compute_instance_v2" "worker_node" {
 }
 
 resource "openstack_compute_floatingip_associate_v2" "worker" {
-  count = "${var.tectonic_master_count}"
+  count = "${var.tectonic_worker_count}"
 
   floating_ip = "${openstack_networking_floatingip_v2.worker.*.address[count.index]}"
   instance_id = "${openstack_compute_instance_v2.worker_node.*.id[count.index]}"
