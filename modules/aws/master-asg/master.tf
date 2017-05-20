a0305c9f1b048b49a136cddb9dda50cafbe46421
Chance Zibolski <chance.zibolski@coreos.com>
2017-05-19 18:38:05 -0700

    Give master nodes access to s3:HeadObject
    This should allow aws s3 cp to give a better error than 403 when the key
    doesn't exist in the bucket, as mentioned in:
    https://github.com/aws/aws-cli/issues/1689

--- modules/aws/master-asg/master.tf
+++ modules/aws/master-asg/master.tf
@@ -141,7 +141,8 @@ resource "aws_iam_role_policy" "master_policy" {
     },
     {
       "Action" : [
-        "s3:GetObject"
+        "s3:GetObject",
+        "s3:HeadObject"
       ],
       "Resource": "arn:aws:s3:::*",
       "Effect": "Allow"
