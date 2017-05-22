4f0e902383aeeb5f8edecfb707eff0fcb83eda99
Amruta Chitnis <amruta.chitnis@coreos.com>
2017-05-22 16:50:51 -0700

    frontend: Updates text for experimental operator

--- installer/frontend/components/experimental-features.jsx
+++ installer/frontend/components/experimental-features.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { CheckBox, WithClusterConfig } from './ui';
 
 import { UPDATER_ENABLED } from '../cluster-config';
-import { Alert } from './alert';
-import { TectonicGA } from '../tectonic-ga';
 
-export const ExperimentalFeatures = connect(
-  state => ({updater: state.clusterConfig[UPDATER_ENABLED]})
-)(props => <div className="row form-group">
+export const ExperimentalFeatures = () => <div className="row form-group">
   <div className="col-xs-3">
-    <label htmlFor="tectonicOperator">Experimental Features</label>
+    <label htmlFor="tectonicOperator">Automated Updates</label>
   </div>
   <div className="col-xs-9">
     <WithClusterConfig field={UPDATER_ENABLED}>
       <CheckBox id="tectonicOperator" suffix={
-        <label htmlFor="tectonicOperator" className="text-muted wiz-help-text">
-          Install experimental <a href="https://coreos.com/blog/introducing-operators.html" onClick={TectonicGA.sendDocsEvent} target="_blank">Tectonic Operators</a>.
+        <label htmlFor="tectonicOperator">
+          Enable one-click updates for Tectonic, etcd, Prometheus.
         </label>
       } />
     </WithClusterConfig>
-    { props.updater &&
-      <Alert noIcon>
-        Only use on clusters that can be easily replaced.
-      </Alert>
-    }
+    <p className="text-muted checkbox-helper-text">Only use with non-production clusters.</p>
   </div>
-</div>);
+</div>;
