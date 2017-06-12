5c3e83db7b37957932ef02b5ddf9846636fde84a
Geoff Greer <geoff@greer.fm>
2017-06-12 15:27:36 -0700

    frontend: Cleanup GUI test stuff.
    - Don't read & parse aws.json a half-dozen times.
    - Don't export some things that aren't being used outside of installer.js
    - Add log handler. Log various pages.
    - Move cluster function into buildExpectedJson.
    - Don't override aws region grabbed from aws.json. It's already the value we want.

--- installer/frontend/ui-tests/tests/installer.js
+++ installer/frontend/ui-tests/tests/installer.js
@@ -1,16 +1,28 @@
 const installerInput = require('../utils/installerInput');
 const tfvarsUtil = require('../utils/terraformTfvars');
 
+const logger = logs => logs.forEach(log => {
+  const { message, level } = log;
+  switch (level) {
+  case `DEBUG`:
+    console.log("browser:", level, message);
+    break;
+  case `SEVERE`:
+    console.warn("browser:", level, message);
+    break;
+  case `INFO`:
+  default:
+    console.info("browser:", level, message);
+  }
+});
+
 module.exports = {
   after (client) {
     client.end();
   },
 
   'Tectonic Installer Aws Test': (client) => {
-    const options = {
-      "tectonic_aws_region": "us-west-1",
-    };
-    const expectedJson = installerInput.buildExpectedJson(options);
+    const expectedJson = installerInput.buildExpectedJson();
     const platformPage = client.page.platformPage();
     const awsCredentialsPage = client.page.awsCredentialsPage();
     const clusterInfoPage = client.page.clusterInfoPage();
@@ -21,21 +33,25 @@ module.exports = {
     const consoleLoginPage = client.page.consoleLoginPage();
     const submitPage = client.page.submitPage();
 
-    platformPage.navigate(client.launch_url)
-      .selectPlatform();
+    platformPage.navigate(client.launch_url).selectPlatform();
+    client.getLog('browser', logger);
+
     awsCredentialsPage.enterAwsCredentials()
       .waitForElementPresent(awsCredentialsPage.el('@region', expectedJson.tectonic_aws_region), 60000)
       .click(awsCredentialsPage.el('@region', expectedJson.tectonic_aws_region))
       .nextStep();
+    client.getLog('browser', logger);
 
     clusterInfoPage.enterClusterInfo(expectedJson.tectonic_cluster_name);
     certificateAuthorityPage.click('@nextStep');
+    client.getLog('browser', logger);
 
     keysPage.selectSshKeys();
     nodesPage.waitForElementVisible('@nextStep', 10000).click('@nextStep');
     networkingPage.provideNetworkingDetails();
     consoleLoginPage.enterLoginCredentails();
     submitPage.click('@manuallyBoot').click('@manuallyBoot');
+    client.getLog('browser', logger);
     client.pause(10000);
     client.getCookie('tectonic-installer', result => {
       tfvarsUtil.returnTerraformTfvars(client.launch_url, result.value, (err, actualJson) => {
--- installer/frontend/ui-tests/utils/installerInput.js
+++ installer/frontend/ui-tests/utils/installerInput.js
@@ -1,63 +1,53 @@
 const fs = require('fs');
 const path = require('path');
 
-const readAwsTestDataJson = () => {
-  const fileName = path.join(__dirname, '..', '..', '__tests__', 'examples', 'aws.json');
-  //eslint-disable-next-line no-sync
-  const awsTestDatajson = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-  return awsTestDatajson.variables;
-};
-
-const cluster = name => {
-  return name + new Date().getTime().toString();
-};
+const awsJsonPath = path.join(__dirname, '..', '..', '__tests__', 'examples', 'aws.json');
+// eslint-disable-next-line no-sync
+const awsTestDatajson = JSON.parse(fs.readFileSync(awsJsonPath, 'utf8'));
 
 /** Returns expected json. This json is used to prep the data required for the test */
-const buildExpectedJson = (opt) => {
-  const options = {} || opt;
-  const json = readAwsTestDataJson();
+const buildExpectedJson = () => {
+  const tfvars = awsTestDatajson.variables;
+  const clusterName = `aws-test-${new Date().getTime().toString()}`;
+  tfvars.tectonic_cluster_name = clusterName;
+  tfvars.tectonic_dns_name = clusterName;
+  delete tfvars.tectonic_admin_password_hash;
+  delete tfvars.tectonic_aws_extra_tags;
+  delete tfvars.tectonic_aws_external_private_zone;
 
-  json.tectonic_cluster_name = cluster("aws-test-");
-  json.tectonic_dns_name = cluster("aws-test-");
-  delete json.tectonic_admin_password_hash;
-  delete json.tectonic_aws_extra_tags;
-  delete json.tectonic_aws_external_private_zone;
-
-  Object.keys(options).map(key => {
-    json[key] = options[key];
-  });
-  return json;
+  return tfvars;
 };
 
 const clusterTagKey = () => {
   const json = buildExpectedJson();
   return json.tectonic_aws_extra_tags[0] + (new Date().getTime().toString());
 };
+
 const sshKeys = () => {
   const json = buildExpectedJson();
   json.tectonic_aws_ssh_key = 'tectonic-jenkins';
   return json.tectonic_aws_ssh_key;
 };
+
 const clusterSubdomainName = () => {
   const json = buildExpectedJson();
   return json.tectonic_dns_name;
 };
+
 const clusterBaseDomainName = () => {
   const json = buildExpectedJson();
   return json.tectonic_base_domain;
 };
+
 const adminEmail = () => {
   const json = buildExpectedJson();
   return json.tectonic_admin_email;
 };
+
 const adminPassword = () => {
   const json = buildExpectedJson();
   json.tectonic_admin_password_hash = 'password';
   return json.tectonic_admin_password_hash;
-};
-const createCoreosCredentials = (Path, env_var) => {
-  //eslint-disable-next-line no-sync
-  fs.writeFileSync(Path, env_var);
 };
 
 module.exports = {
@@ -68,5 +58,4 @@ module.exports = {
   clusterBaseDomainName,
   adminEmail,
   adminPassword,
-  createCoreosCredentials,
 };
