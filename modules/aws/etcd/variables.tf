alekssaul/tectonic-installer

*/etcd: enable tls for self-provisioned etcd
Fixes #459

modules/aws/etcd/variables.tf
@@ -74,3 +74,19 @@ variable "dns_enabled" {
   type        = "string"
   default     = "false"
 }
+
+variable "tls_enabled" {
+  default = false
+}
+
+variable "tls_ca_crt_pem" {
+  default = ""
+}
+
+variable "tls_key_pem" {
+  default = ""
+}
+
+variable "tls_crt_pem" {
+  default = ""
+}

modules/bootkube/outputs.tf
@@ -51,3 +51,15 @@ output "systemd_service" {
 output "kube_dns_service_ip" {
   value = "${cidrhost(var.service_cidr, 10)}"
 }
+
+output "etcd_ca_crt_pem" {
+  value = "${join("", tls_self_signed_cert.etcd-ca.*.cert_pem)}"
+}
+
+output "etcd_crt_pem" {
+  value = "${join("", tls_locally_signed_cert.etcd.*.cert_pem)}"
+}
+
+output "etcd_key_pem" {
+  value = "${join("", tls_private_key.etcd.*.private_key_pem)}"
+}

modules/bootkube/tls_etcd.tf
@@ -1,0 +1,67 @@
+resource "tls_private_key" "etcd-ca" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_self_signed_cert" "etcd-ca" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  key_algorithm   = "${tls_private_key.etcd-ca.algorithm}"
+  private_key_pem = "${tls_private_key.etcd-ca.private_key_pem}"
+
+  subject {
+    common_name  = "etcd-ca"
+    organization = "etcd"
+  }
+
+  is_ca_certificate     = true
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "cert_signing",
+  ]
+}
+
+resource "tls_private_key" "etcd" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "etcd" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  key_algorithm   = "${tls_private_key.etcd.algorithm}"
+  private_key_pem = "${tls_private_key.etcd.private_key_pem}"
+
+  subject {
+    common_name  = "etcd"
+    organization = "etcd"
+  }
+
+  dns_names = ["${var.etcd_cert_dns_names}"]
+}
+
+resource "tls_locally_signed_cert" "etcd" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  cert_request_pem = "${tls_cert_request.etcd.cert_request_pem}"
+
+  ca_key_algorithm   = "${join(" ", tls_self_signed_cert.etcd-ca.*.key_algorithm)}"
+  ca_private_key_pem = "${join(" ", tls_private_key.etcd-ca.*.private_key_pem)}"
+  ca_cert_pem        = "${join(" ", tls_self_signed_cert.etcd-ca.*.cert_pem)}"
+
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+    "client_auth",
+  ]
+}

openshift/installer

modules/aws/etcd/variables.tf
@@ -74,3 +74,27 @@ variable "dns_enabled" {
   type        = "string"
   default     = "false"
 }
+
+variable "tls_enabled" {
+  default = false
+}
+
+variable "tls_ca_crt_pem" {
+  default = ""
+}
+
+variable "tls_client_key_pem" {
+  default = ""
+}
+
+variable "tls_client_crt_pem" {
+  default = ""
+}
+
+variable "tls_peer_key_pem" {
+  default = ""
+}
+
+variable "tls_peer_crt_pem" {
+  default = ""
+}

modules/bootkube/outputs.tf
@@ -21,6 +21,7 @@ output "id" {
   ${local_file.bootkube-sh.id}
   ${template_dir.bootkube.id} ${template_dir.bootkube-bootstrap.id}
   ${join(" ",
+    local_file.etcd_ca_crt.*.id,
     template_dir.experimental.*.id,
     template_dir.bootstrap-experimental.*.id,
     template_dir.etcd-experimental.*.id,
@@ -50,4 +51,24 @@ output "systemd_service" {
 
 output "kube_dns_service_ip" {
   value = "${cidrhost(var.service_cidr, 10)}"
+}
+
+output "etcd_ca_crt_pem" {
+  value = "${join("", tls_self_signed_cert.etcd-ca.*.cert_pem)}"
+}
+
+output "etcd_client_crt_pem" {
+  value = "${join("", tls_locally_signed_cert.etcd_client.*.cert_pem)}"
+}
+
+output "etcd_client_key_pem" {
+  value = "${join("", tls_private_key.etcd_client.*.private_key_pem)}"
+}
+
+output "etcd_peer_crt_pem" {
+  value = "${join("", tls_locally_signed_cert.etcd_peer.*.cert_pem)}"
+}
+
+output "etcd_peer_key_pem" {
+  value = "${join("", tls_private_key.etcd_peer.*.private_key_pem)}"
 }

modules/bootkube/tls_etcd.tf
@@ -1,0 +1,107 @@
+resource "tls_private_key" "etcd-ca" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_self_signed_cert" "etcd-ca" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  key_algorithm   = "${tls_private_key.etcd-ca.algorithm}"
+  private_key_pem = "${tls_private_key.etcd-ca.private_key_pem}"
+
+  subject {
+    common_name  = "etcd-ca"
+    organization = "etcd"
+  }
+
+  is_ca_certificate     = true
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "cert_signing",
+  ]
+}
+
+resource "tls_private_key" "etcd_client" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "etcd_client" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  key_algorithm   = "${tls_private_key.etcd_client.algorithm}"
+  private_key_pem = "${tls_private_key.etcd_client.private_key_pem}"
+
+  subject {
+    common_name  = "etcd"
+    organization = "etcd"
+  }
+
+  dns_names = ["${var.etcd_cert_dns_names}"]
+}
+
+resource "tls_locally_signed_cert" "etcd_client" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  cert_request_pem = "${tls_cert_request.etcd_client.cert_request_pem}"
+
+  ca_key_algorithm   = "${join(" ", tls_self_signed_cert.etcd-ca.*.key_algorithm)}"
+  ca_private_key_pem = "${join(" ", tls_private_key.etcd-ca.*.private_key_pem)}"
+  ca_cert_pem        = "${join(" ", tls_self_signed_cert.etcd-ca.*.cert_pem)}"
+
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+    "client_auth",
+  ]
+}
+
+resource "tls_private_key" "etcd_peer" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "etcd_peer" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  key_algorithm   = "${tls_private_key.etcd_peer.algorithm}"
+  private_key_pem = "${tls_private_key.etcd_peer.private_key_pem}"
+
+  subject {
+    common_name  = "etcd"
+    organization = "etcd"
+  }
+
+  dns_names = ["${var.etcd_cert_dns_names}"]
+}
+
+resource "tls_locally_signed_cert" "etcd_peer" {
+  count = "${!var.experimental_enabled && var.etcd_tls_enabled ? 1 : 0}"
+
+  cert_request_pem = "${tls_cert_request.etcd_peer.cert_request_pem}"
+
+  ca_key_algorithm   = "${join(" ", tls_self_signed_cert.etcd-ca.*.key_algorithm)}"
+  ca_private_key_pem = "${join(" ", tls_private_key.etcd-ca.*.private_key_pem)}"
+  ca_cert_pem        = "${join(" ", tls_self_signed_cert.etcd-ca.*.cert_pem)}"
+
+  validity_period_hours = 8760
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+    "client_auth",
+  ]
+}