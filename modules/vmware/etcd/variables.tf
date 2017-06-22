alekssaul/tectonic-installer

*/etcd: vmware enable tls for self-provisioned etcd
Fixes #459

modules/vmware/etcd/variables.tf
@@ -89,3 +89,15 @@ variable hostname {
   type        = "map"
   description = "Hostname of the node"
 }
+
+variable "tls_ca_crt_pem" {
+  default = ""
+}
+
+variable "tls_key_pem" {
+  default = ""
+}
+
+variable "tls_crt_pem" {
+  default = ""
+}

platforms/vmware/main.tf
@@ -8,6 +8,10 @@ module "etcd" {
   base_domain        = "${var.tectonic_base_domain}"
   external_endpoints = ["${compact(var.tectonic_etcd_servers)}"]
 
+  tls_ca_crt_pem = "${module.bootkube.etcd_ca_crt_pem}"
+  tls_crt_pem    = "${module.bootkube.etcd_crt_pem}"
+  tls_key_pem    = "${module.bootkube.etcd_key_pem}"
+
   hostname   = "${var.tectonic_vmware_etcd_hostnames}"
   dns_server = "${var.tectonic_vmware_node_dns}"
   ip_address = "${var.tectonic_vmware_etcd_ip}"

openshift/installer

modules/vmware/etcd/variables.tf
@@ -89,3 +89,23 @@ variable hostname {
   type        = "map"
   description = "Hostname of the node"
 }
+
+variable "tls_ca_crt_pem" {
+  default = ""
+}
+
+variable "tls_client_key_pem" {
+  default = ""
+}
+
+variable "tls_client_crt_pem" {
+  default = ""
+}
+
+variable "tls_peer_key_pem" {
+  default = ""
+}
+
+variable "tls_peer_crt_pem" {
+  default = ""
+}

platforms/vmware/main.tf
@@ -8,6 +8,12 @@ module "etcd" {
   base_domain        = "${var.tectonic_base_domain}"
   external_endpoints = ["${compact(var.tectonic_etcd_servers)}"]
 
+  tls_ca_crt_pem     = "${module.bootkube.etcd_ca_crt_pem}"
+  tls_client_crt_pem = "${module.bootkube.etcd_client_crt_pem}"
+  tls_client_key_pem = "${module.bootkube.etcd_client_key_pem}"
+  tls_peer_key_pem   = "${module.bootkube.etcd_peer_crt_pem}"
+  tls_peer_crt_pem   = "${module.bootkube.etcd_peer_key_pem}"
+
   hostname   = "${var.tectonic_vmware_etcd_hostnames}"
   dns_server = "${var.tectonic_vmware_node_dns}"
   ip_address = "${var.tectonic_vmware_etcd_ip}"