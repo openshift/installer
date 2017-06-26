d15e9468f3c50c7ba98493db7e30c1af9918bb2f
Quentin Machu <me@quentin-machu.fr>
2017-06-26 15:15:59 -0700

    release.groovy: make it to use ec2 workers and consistent w/ Jenkinsfile

--- release.groovy
+++ release.groovy
@@ -1,10 +1,32 @@
-// Update https://jenkins-tectonic.prod.coreos.systems/job/tectonic-release
+// Associated job: https://jenkins-tectonic-installer.prod.coreos.systems/job/tectonic-release/
+
+/* Tips
+1. Keep stages focused on producing one artifact or achieving one goal. This makes stages easier to parallelize or re-structure later.
+1. Stages should simply invoke a make target or a self-contained script. Do not write testing logic in this Jenkinsfile.
+3. CoreOS does not ship with `make`, so Docker builds still have to use small scripts.
+*/
+
+def creds = [
+  [
+    $class: 'UsernamePasswordMultiBinding',
+    credentialsId: 'tectonic-release-s3-upload',
+    usernameVariable: 'AWS_ACCESS_KEY_ID',
+    passwordVariable: 'AWS_SECRET_ACCESS_KEY'
+  ],
+  [
+    $class: 'StringBinding',
+    credentialsId: 'github-coreosbot',
+    variable: 'GITHUB_CREDENTIALS'
+  ]
+]
+
+def builder_image = 'quay.io/coreos/tectonic-builder:v1.20'
+
 pipeline {
   agent none
 
   options {
     timeout(time:25, unit:'MINUTES')
-    buildDiscarder(logRotator(numToKeepStr:'20'))
   }
 
   parameters {
@@ -17,51 +39,38 @@ pipeline {
       agent none
       environment {
         GO_PROJECT = '/go/src/github.com/coreos/tectonic-installer'
+        MAKEFLAGS = '-j4'
       }
       steps {
-        script {
-          podTemplate(
-            label: 'webapp-pod',
-            containers: [
-              containerTemplate(
-                name: 'webapp-agent',
-                image: 'quay.io/coreos/tectonic-builder:v1.20',
-                ttyEnabled: true,
-                command: 'cat',
-              )
-            ],
-            volumes: []
-          ) {
-            node('webapp-pod') {
-              withCredentials([[
-                $class: 'UsernamePasswordMultiBinding',
-                credentialsId: 'tectonic-release-s3-upload',
-                usernameVariable: 'AWS_ACCESS_KEY_ID',
-                passwordVariable: 'AWS_SECRET_ACCESS_KEY'
-              ]]) {
-                withCredentials([[
-                  $class: 'UsernamePasswordBinding',
-                  credentialsId: 'github-coreosbot',
-                  variable: 'GITHUB_CREDENTIALS'
-                ]]) {
-                  container('webapp-agent') {
-                    checkout([$class: 'GitSCM', branches: [[name: "refs/tags/${params.releaseTag}"]], userRemoteConfigs: [[credentialsId: 'github-coreosbot', url: 'https://github.com/coreos/tectonic-installer.git']]])
-                    sh "mkdir -p \$(dirname $GO_PROJECT) && ln -sf $WORKSPACE $GO_PROJECT"
-                    sh "go get github.com/golang/lint/golint"
-                    sh """#!/bin/bash -ex
-                    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-                    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-                    export GITHUB_CREDENTIALS=$GITHUB_CREDENTIALS
-                    export PRE_RELEASE=${params.preRelease}
-                    go version
-                    cd $GO_PROJECT/installer
-                    make build
-                    make test
-                    make release
-                    """
-                  }
-                }
-              }
+        node('worker && ec2') {
+          withCredentials(creds) {
+            withDockerContainer(builder_image) {
+              checkout([$class: 'GitSCM', branches: [[name: "refs/tags/${params.releaseTag}"]], userRemoteConfigs: [[credentialsId: 'github-coreosbot', url: 'https://github.com/coreos/tectonic-installer.git']]])
+              sh """#!/bin/bash -ex
+                mkdir -p \$(dirname $GO_PROJECT) && ln -sf $WORKSPACE $GO_PROJECT
+
+                export VERSION=${params.releaseTag}
+                export PRE_RELEASE=${params.preRelease}
+                go version
+
+                # TODO: Remove me.
+                go get github.com/segmentio/terraform-docs
+                go get github.com/s-urbaniak/terraform-examples
+
+                cd $GO_PROJECT/
+                make structure-check
+
+                cd $GO_PROJECT/installer
+                make clean
+                make tools
+                make build
+
+                make dirtycheck
+                make lint
+                make test
+
+                make release
+              """
             }
           }
         }
