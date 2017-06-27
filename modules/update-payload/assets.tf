4e7f5f4b37b1ae7bc6a64a6863d1ff1668168776
Yifan Gu <yifan.gu@coreos.com>
2017-06-27 12:50:47 -0700

    updater: Move operators and appversions into subdirectories.
    This helps to clean up the 'make payload' scripts.

--- modules/update-payload/assets.tf
+++ modules/update-payload/assets.tf
@@ -1,29 +1,22 @@
 # Kubernetes Deployments and AppVersions TPR used in the payload
-resource "template_dir" "upload_payload" {
-  source_dir      = "../tectonic/resources/manifests/updater/"
-  destination_dir = "./generated"
+resource "template_dir" "payload_operators" {
+  source_dir      = "../tectonic/resources/manifests/updater/operators"
+  destination_dir = "./generated/operators"
 
   vars {
-    # Used variables for generating the payload,
-    # just to make terraform happy.
-    update_server = ""
-
-    update_channel                 = ""
-    update_app_id                  = ""
-    node_agent_image               = ""
-    prometheus_operator_image      = ""
-    prometheus_config_reload_image = ""
-    config_reload_image            = ""
-    prometheus_image               = ""
-    alertmanager_image             = ""
-
-    # Actual variables we care about when generating the payload.
     container_linux_update_operator_image = "${var.tectonic_container_images["container_linux_update_operator"]}"
     kube_version_operator_image           = "${var.tectonic_container_images["kube_version_operator"]}"
     tectonic_channel_operator_image       = "${var.tectonic_container_images["tectonic_channel_operator"]}"
     tectonic_prometheus_operator_image    = "${var.tectonic_container_images["tectonic_prometheus_operator"]}"
     tectonic_etcd_operator_image          = "${var.tectonic_container_images["tectonic_etcd_operator"]}"
+  }
+}
 
+resource "template_dir" "payload_appversions" {
+  source_dir      = "../tectonic/resources/manifests/updater/app_versions"
+  destination_dir = "./generated/app_versions"
+
+  vars {
     kubernetes_version             = "${var.tectonic_versions["kubernetes"]}"
     monitoring_version             = "${var.tectonic_versions["monitoring"]}"
     tectonic_version               = "${var.tectonic_versions["tectonic"]}"
