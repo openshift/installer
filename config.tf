54b19925e0c9c8e6c7e86fbbc503ffe60063f03b
Diego Pontoriero <dpontor@gmail.com>
2017-07-20 16:21:02 -0700

    kenc: bump to 0.0.2

--- config.tf
+++ config.tf
@@ -56,7 +56,7 @@ variable "tectonic_container_images" {
     flannel_cni                     = "quay.io/coreos/flannel-cni:0.1.0"
     etcd                            = "quay.io/coreos/etcd:v3.1.8"
     etcd_operator                   = "quay.io/coreos/etcd-operator:v0.4.2"
-    kenc                            = "quay.io/coreos/kenc:8f6e2e885f790030fbbb0496ea2a2d8830e58b8f"
+    kenc                            = "quay.io/coreos/kenc:0.0.2"
     awscli                          = "quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600"
     kube_version                    = "quay.io/coreos/kube-version:0.1.0"
     tectonic_etcd_operator          = "quay.io/coreos/tectonic-etcd-operator:v0.0.1"
