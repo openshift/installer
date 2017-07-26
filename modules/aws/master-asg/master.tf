81f521f004c62031423a00f4763cb83cf8ab41ff
Jordan Ganoff <jordan@ganoff.com>
2017-07-26 15:22:52 -0700

    modules/aws: Configure ASG minimum size = desired instance count (#1048)
    The AWS Auto Scaling Groups for both master and worker nodes should have their
    minimum sizes set to the desired instance count so instances are replaced if
    the total count drops below the desired count.

--- modules/aws/master-asg/master.tf
+++ modules/aws/master-asg/master.tf
@@ -26,7 +26,7 @@ resource "aws_autoscaling_group" "masters" {
   name                 = "${var.cluster_name}-masters"
   desired_capacity     = "${var.instance_count}"
   max_size             = "${var.instance_count * 3}"
-  min_size             = "1"
+  min_size             = "${var.instance_count}"
   launch_configuration = "${aws_launch_configuration.master_conf.id}"
   vpc_zone_identifier  = ["${var.subnet_ids}"]
 
--- modules/aws/worker-asg/worker.tf
+++ modules/aws/worker-asg/worker.tf
@@ -51,7 +51,7 @@ resource "aws_autoscaling_group" "workers" {
   name                 = "${var.cluster_name}-workers"
   desired_capacity     = "${var.instance_count}"
   max_size             = "${var.instance_count * 3}"
-  min_size             = "1"
+  min_size             = "${var.instance_count}"
   launch_configuration = "${aws_launch_configuration.worker_conf.id}"
   vpc_zone_identifier  = ["${var.subnet_ids}"]
 
