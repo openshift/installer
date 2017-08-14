d3b661e8906708f9481ae9910da4bb587edc776e
Andy Pickering <andy.nihon@gmail.com>
2017-08-15 00:20:10 +0900

    frontend: Add link to Bare Metal requirements doc

--- installer/frontend/components/cluster-type.jsx
+++ installer/frontend/components/cluster-type.jsx
@@ -9,6 +9,7 @@ import { PLATFORM_TYPE, PLATFORM_FORM } from '../cluster-config';
 import { TectonicGA } from '../tectonic-ga';
 import {
   AWS_TF,
+  BARE_METAL_TF,
   DOCS,
   PLATFORM_NAMES,
   SELECTED_PLATFORMS,
@@ -19,19 +20,22 @@ import {
 const ErrorComponent = connect(({clusterConfig}) => ({platform: clusterConfig[PLATFORM_TYPE]}))(
   ({error, platform}) => {
     const platformName = PLATFORM_NAMES[platform];
+    const icon = <i className="fa fa-external-link" />;
     if (error) {
       return <p>
-      Use the documentation and the Terraform CLI to install a cluster with specific infrastructure use-cases.
-      This method is designed for automation and doesn't use the graphical installer.
+        Use the documentation and the Terraform CLI to install a cluster with specific infrastructure use-cases.
+        This method is designed for automation and doesn't use the graphical installer.
         <br />
         <a href={DOCS[platform]} target="_blank">
-          <button className="btn btn-primary" style={{marginTop: 8}}>{platformName && platformName.split('(Alpha)')[0]} Docs&nbsp;&nbsp;<i className="fa fa-external-link" /></button>
+          <button className="btn btn-primary" style={{marginTop: 8}}>{platformName && platformName.split('(Alpha)')[0]} Docs&nbsp;&nbsp;{icon}</button>
         </a>
       </p>;
     }
     return <p className="text-muted">
-    Use the graphical installer to input cluster details, this is best for demos and your first Tectonic cluster.
-    &nbsp;&nbsp;<a href={DOCS[platform]} target="_blank">{platformName} documentation&nbsp;&nbsp;<i className="fa fa-external-link" /></a>
+      Use the graphical installer to input cluster details, this is best for demos and your first Tectonic cluster.
+      &nbsp;&nbsp;{platform === BARE_METAL_TF
+        ? <span><br />{platformName} <a href="https://coreos.com/tectonic/docs/latest/install/bare-metal/requirements.html" target="_blank">requirements&nbsp;&nbsp;{icon}</a> and <a href={DOCS[platform]} target="_blank">install guide&nbsp;&nbsp;{icon}</a>.</span>
+        : <a href={DOCS[platform]} target="_blank">{platformName} documentation&nbsp;&nbsp;{icon}</a>}
     </p>;
   });
 
