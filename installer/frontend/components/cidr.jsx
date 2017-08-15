87f316ff1396efaa3a6e51b22de7744a5b0a4793
Andy Pickering <andy.nihon@gmail.com>
2017-08-15 15:19:45 +0900

    frontend: Fix tooltip preventing editing of CIDR inputs

--- installer/frontend/components/cidr.jsx
+++ installer/frontend/components/cidr.jsx
@@ -3,18 +3,6 @@ import React from 'react';
 import { Deselect, Input, WithClusterConfig } from './ui';
 import { validate } from '../validate';
 import { DESELECTED_FIELDS } from '../cluster-config.js';
-import { WithTooltip } from './tooltip';
-
-const generateTooltipText = ({value}) => {
-  if (validate.CIDR(value)) {
-    return;
-  }
-  const [, bits] = value.split('/');
-  // javascript's bit shifting only works on signed 32bit ints so <<31
-  // would be negative :(
-  const addresses = Math.pow(2, 32 - parseInt(bits, 10));
-  return `${addresses} IP address${addresses === 1 ? '' : 'es'}`;
-};
 
 export const CIDR = ({field, name, disabled, placeholder, autoFocus, validator, selectable, fieldName}) => {
   fieldName = fieldName || field;
@@ -23,11 +11,9 @@ export const CIDR = ({field, name, disabled, placeholder, autoFocus, validator, 
       {selectable && <Deselect field={fieldName} />}
       <label htmlFor={(selectable ? `${DESELECTED_FIELDS}.` : '') + fieldName}>{name}</label>
     </div>
-    <div className="col-xs-5 withtooltip">
+    <div className="col-xs-5">
       <WithClusterConfig field={field} validator={validator || validate.CIDR}>
-        <WithTooltip generateText={generateTooltipText}>
-          <Input placeholder={placeholder} id={field} disabled={disabled} autoFocus={autoFocus} />
-        </WithTooltip>
+        <Input placeholder={placeholder} id={field} disabled={disabled} autoFocus={autoFocus} />
       </WithClusterConfig>
     </div>
   </div>;
