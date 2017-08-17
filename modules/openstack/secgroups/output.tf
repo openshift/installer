a8ab3931fd1354af726b14c7c1d6ec8f485d6301
German Eichberger <German.eichberger@rackspace.com>
2017-08-17 15:12:46 -0700

    Users should be able to use port 80 with their installs.
    We were running a load balancer on port 80 forwarding to 30080
    on each worker but the kube-proxy would load balance traffic to
    port 80 on the other workers. Hence, to support HTTP on port 80
    this port needs to be open inside the cluster.

--- modules/openstack/secgroups/output.tf
+++ modules/openstack/secgroups/output.tf
@@ -25,6 +25,8 @@ output "secgroup_node_ids" {
   value = [
     "${openstack_networking_secgroup_v2.base.id}",
     "${openstack_networking_secgroup_v2.k8s.id}",
+    "${openstack_networking_secgroup_v2.k8s_nodes.id}",
+
   ]
 }
 
