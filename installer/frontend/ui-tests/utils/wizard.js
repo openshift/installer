31e139ce8740ed2f2796a50429a8e1d8f0b17c3c
Andy Pickering <andy.nihon@gmail.com>
2017-08-24 21:23:23 +0900

    frontend/tests: Add some sidebar link tests

--- installer/frontend/ui-tests/utils/wizard.js
+++ installer/frontend/ui-tests/utils/wizard.js
@@ -9,6 +9,18 @@ const testPage = (page, json, nextInitiallyDisabled = true) => {
   if (nextInitiallyDisabled) {
     page.expect.element(nextStep).to.have.attribute('class').which.contains('disabled');
   }
+
+  // Sidebar link for the current page should be highlighted and enabled
+  page.expect.element('.wiz-wizard__nav__step--active button').to.not.have.attribute('disabled');
+
+  // The next sidebar links should be disabled if the next button is disabled
+  const nextNavLink = page.expect.element('.wiz-wizard__nav__step--active + .wiz-wizard__nav__step button');
+  if (nextInitiallyDisabled) {
+    nextNavLink.to.have.attribute('disabled');
+  } else {
+    nextNavLink.to.not.have.attribute('disabled');
+  }
+
   if (page.test) {
     page.test(json);
   }
