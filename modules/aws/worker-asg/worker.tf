a8d800111a6107ce90d851e4bd6fc3be84310cad
Sharif Nassar <mrwacky42@users.noreply.github.com>
2017-09-01 09:09:37 -0700

    terraform: avoid resource churn for EBS volumes
    Possibly related to this issue with the Terraform AWS provider:
    https://github.com/terraform-providers/terraform-provider-aws/issues/1521
    
    Terraform or the AWS API report `0` IOPS for `gp2` volumes after creation.
    If I create a `gp2` volume with these modules, every subsequent run of
    Terraform will attempt to destroy and recreate the LC in order to make
    IOPs match. This change sets IOPS to 0 for non-`io1` type volumes.
    
    Since the number of IOPS for `gp2` volume is based on the size, Terraform
    arguably should only accept the `iops` parameter for `io1` volumes.
    
    Interestingly, for the etcd nodes, this problem does not occur because
    the Terraform state shows 100 IOPS even for `gp2` volumes.

--- modules/aws/worker-asg/worker.tf
+++ modules/aws/worker-asg/worker.tf
@@ -43,7 +43,7 @@ resource "aws_launch_configuration" "worker_conf" {
   root_block_device {
     volume_type = "${var.root_volume_type}"
     volume_size = "${var.root_volume_size}"
-    iops        = "${var.root_volume_type == "io1" ? var.root_volume_iops : 100}"
+    iops        = "${var.root_volume_type == "io1" ? var.root_volume_iops : 0}"
   }
 }
 
