9fbed3bc260b4912e7924fef69594bcbeb8849d4
Levi Blackstone <levi.blackstone@rackspace.com>
2017-09-13 10:14:47 -0600

    modules/dns: Only create public DNS records if public IPs exist

--- modules/dns/designate/worker.tf
+++ modules/dns/designate/worker.tf
@@ -8,7 +8,8 @@ resource "openstack_dns_recordset_v2" "worker_nodes" {
 }
 
 resource "openstack_dns_recordset_v2" "worker_nodes_public" {
-  count   = "${var.worker_count}"
+  // hack: worker_public_ips_enabled is a workaround for https://github.com/hashicorp/terraform/issues/10857
+  count   = "${var.worker_public_ips_enabled ? var.worker_count : 0}"
   zone_id = "${data.openstack_dns_zone_v2.tectonic.id}"
   name    = "${var.cluster_name}-worker-${count.index}-public.${var.base_domain}."
   type    = "A"
--- modules/dns/route53/worker.tf
+++ modules/dns/route53/worker.tf
@@ -8,7 +8,8 @@ resource "aws_route53_record" "worker_nodes" {
 }
 
 resource "aws_route53_record" "worker_nodes_public" {
-  count   = "${var.worker_count}"
+  // hack: worker_public_ips_enabled is a workaround for https://github.com/hashicorp/terraform/issues/10857
+  count   = "${var.worker_public_ips_enabled ? var.worker_count : 0}"
   zone_id = "${data.aws_route53_zone.tectonic.zone_id}"
   name    = "${var.cluster_name}-worker-${count.index}-public"
   type    = "A"
