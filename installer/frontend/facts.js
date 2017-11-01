d054abe3c3ee1ebe623c9b8fa5a62ab3da68a32e
Andy Pickering <andy.nihon@gmail.com>
2017-11-01 20:22:07 +0900

    frontend: Remove unused "vcpus" attribute from instance types list

--- installer/frontend/facts.js
+++ installer/frontend/facts.js
@@ -2,41 +2,33 @@ export const AWS_INSTANCE_TYPES = [
   {
     label: 't2.medium (vCPU 2 / Memory 4 GiB)',
     value: 't2.medium',
-    vcpus: 2,
   },
   {
     label: 't2.large (vCPU 2 / Memory 8 GiB)',
     value: 't2.large',
-    vcpus: 2,
   },
   {
     label: 'm4.large (vCPU 2 / Memory 8 GiB)',
     value: 'm4.large',
-    vcpus: 2,
   },
   {
     label: 'm4.xlarge (vCPU 4 / Memory 16 GiB)',
     value: 'm4.xlarge',
-    vcpus: 4,
   },
   {
     label: 'm4.2xlarge (vCPU 8 / Memory 32 GiB)',
     value: 'm4.2xlarge',
-    vcpus: 8,
   },
   {
     label: 'm3.medium (vCPU 1 / Memory 3.75 GiB)',
     value: 'm3.medium',
-    vcpus: 1,
   },
   {
     label: 'm3.large (vCPU 2 / Memory 7.5 GiB)',
     value: 'm3.large',
-    vcpus: 2,
   },
   {
     label: 'm3.xlarge (vCPU 4 / Memory 15 GiB)',
     value: 'm3.xlarge',
-    vcpus: 4,
   },
 ];
