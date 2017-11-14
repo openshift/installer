eff3cc57c0d620e19b349eeb10d75f0a3c649d9c
Lucas Serven <lserven@gmail.com>
2017-11-14 14:30:12 +0100

    modules: retry init assets
    This commit makees several modifications to ensure the init-assets
    service will retry any network operations, e.g. wget, curl, aws etc.
    Rather than adding individual retries for each of the dozens of
    operations that could fail, this commit adds retries to the docker
    commands that encompass those operations.
    
    Fixes: INST-341, INST-574

--- modules/aws/master-asg/resources/init-assets.sh
+++ modules/aws/master-asg/resources/init-assets.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -e
+set -o pipefail
 
-# Defer cleanup rkt containers and images.
-trap "{ /usr/bin/rkt gc --grace-period=0; /usr/bin/rkt image gc --grace-period 0; } &> /dev/null" EXIT
+detect_master() {
+    mkdir -p /run/metadata
+    # shellcheck disable=SC2086,SC2154
+    /usr/bin/docker run \
+        --volume /run/metadata:/run/metadata \
+        --volume /opt/detect-master.sh:/detect-master.sh:ro \
+        --network=host \
+        --env CLUSTER_NAME=${cluster_name} \
+        --entrypoint=/detect-master.sh \
+        ${awscli_image}
+}
 
-mkdir -p /run/metadata
-# shellcheck disable=SC2086,SC2154
-/usr/bin/docker run \
-    --volume /run/metadata:/run/metadata \
-    --volume /opt/detect-master.sh:/detect-master.sh:ro \
-    --network=host \
-    --env CLUSTER_NAME=${cluster_name} \
-    --entrypoint=/detect-master.sh \
-    ${awscli_image}
+until detect_master; do
+    echo "failed to detect master; retrying in 5 seconds"
+    sleep 5
+done
 
 MASTER=$(cat /run/metadata/master)
 if [ "$MASTER" != "true" ]; then
