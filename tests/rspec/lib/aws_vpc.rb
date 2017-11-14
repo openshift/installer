c7054f4a5d1fa672f4533c7eb1df9efb418d817e
cpanato <ctadeu@gmail.com>
2017-11-14 17:19:52 +0100

    tests: update dns names

--- tests/rspec/lib/aws_vpc.rb
+++ tests/rspec/lib/aws_vpc.rb
@@ -25,7 +25,7 @@ class AWSVPC
       'TF_VAR_vpc_aws_region' => ENV['TF_VAR_tectonic_aws_region'],
       'TF_VAR_vpc_name' => @name,
       'TF_VAR_ovpn_password' => @ovpn_password,
-      'TF_VAR_base_domain' => 'tectonic.dev.coreos.systems'
+      'TF_VAR_base_domain' => 'tectonic-ci.de'
     }
   end
 
