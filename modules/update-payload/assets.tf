931d24cbe0025cc1e62db72e0900ddf8fb142965
Thor <thor.hansen@coreos.com>
2017-11-28 09:55:56 -0800

    Removed addon operator managed manifests

--- modules/update-payload/assets.tf
+++ modules/update-payload/assets.tf
@@ -9,6 +9,7 @@ resource "template_dir" "payload_operators" {
     tectonic_prometheus_operator_image = "${var.tectonic_container_images["tectonic_prometheus_operator"]}"
     tectonic_etcd_operator_image       = "${var.tectonic_container_images["tectonic_etcd_operator"]}"
     tectonic_cluo_operator_image       = "${var.tectonic_container_images["tectonic_cluo_operator"]}"
+    kubernetes_addon_operator_image    = "${var.tectonic_container_images["kubernetes_addon_operator"]}"
     image_re                           = "${var.tectonic_image_re}"
   }
 }
@@ -23,5 +24,6 @@ resource "template_dir" "payload_appversions" {
     tectonic_version               = "${var.tectonic_versions["tectonic"]}"
     tectonic_etcd_operator_version = "${var.tectonic_versions["tectonic-etcd"]}"
     tectonic_cluo_operator_version = "${var.tectonic_versions["cluo"]}"
+    kubernetes_addon_version       = "${var.versions["kubernetes_addon"]}"
   }
 }
