3e5580b439050f1cf756d686c84d3f41cf57707a
Casey Callendrello <casey.callendrello@coreos.com>
2018-01-08 22:51:03 -0800

    Networking: always use the on-disk (plugin provided) CNI plugins

--- modules/ignition/assets.tf
+++ modules/ignition/assets.tf
@@ -49,7 +49,6 @@ data "template_file" "kubelet" {
     cloud_provider        = "${var.cloud_provider}"
     cloud_provider_config = "${var.cloud_provider_config != "" ? "--cloud-config=/etc/kubernetes/cloud/config" : ""}"
     cluster_dns_ip        = "${var.kube_dns_service_ip}"
-    cni_bin_dir_flag      = "${var.kubelet_cni_bin_dir != "" ? "--cni-bin-dir=${var.kubelet_cni_bin_dir}" : ""}"
     debug_config          = "${var.kubelet_debug_config}"
     kubeconfig_fetch_cmd  = "${var.kubeconfig_fetch_cmd != "" ? "ExecStartPre=${var.kubeconfig_fetch_cmd}" : ""}"
     node_label            = "${var.kubelet_node_label}"
