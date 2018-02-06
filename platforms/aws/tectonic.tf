abhinavdahiya/tectonic-installer

tls: add aggregated apiserver certs.
This adds a new CA and certificate pair to support running aggregated
api-servers in the cluster. This is required for deploying
metrics-server (heapster replacement) and other aggregated api-server
components that will start to appear in 1.9+.

In the self-signed scenario these are derived from the kube_ca, which
happens to also be the apiserver serving ca. In practice the kube_ca
should be the parent of both this CA and the one that is used by the
--tls-ca-file flag in the apiserver. I added a TODO to fix that when
we implement the "single CA is the parent of all generated certs" change
for track 2.

platforms/aws/tectonic.tf
@@ -28,19 +28,22 @@ module "bootkube" {
 
   pull_secret_path = "${pathexpand(var.tectonic_pull_secret_path)}"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kube_ca_key_pem      = "${module.kube_certs.ca_key_pem}"
-  admin_cert_pem       = "${module.kube_certs.admin_cert_pem}"
-  admin_key_pem        = "${module.kube_certs.admin_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kube_ca_key_pem          = "${module.kube_certs.ca_key_pem}"
+  admin_cert_pem           = "${module.kube_certs.admin_cert_pem}"
+  admin_key_pem            = "${module.kube_certs.admin_key_pem}"
 
   etcd_endpoints = "${module.dns.etcd_endpoints}"
   master_count   = "${var.tectonic_master_count}"

platforms/gcp/tectonic.tf
@@ -40,19 +40,22 @@ module "bootkube" {
   oidc_groups_claim   = "groups"
   oidc_username_claim = "email"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kube_ca_key_pem      = "${module.kube_certs.ca_key_pem}"
-  admin_cert_pem       = "${module.kube_certs.admin_cert_pem}"
-  admin_key_pem        = "${module.kube_certs.admin_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kube_ca_key_pem          = "${module.kube_certs.ca_key_pem}"
+  admin_cert_pem           = "${module.kube_certs.admin_cert_pem}"
+  admin_key_pem            = "${module.kube_certs.admin_key_pem}"
 
   cloud_config_path = ""
   etcd_endpoints    = "${data.template_file.etcd_hostname_list.*.rendered}"

platforms/metal/tectonic.tf
@@ -25,19 +25,22 @@ module "bootkube" {
 
   pull_secret_path = "${pathexpand(var.tectonic_pull_secret_path)}"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kube_ca_key_pem      = "${module.kube_certs.ca_key_pem}"
-  admin_cert_pem       = "${module.kube_certs.admin_cert_pem}"
-  admin_key_pem        = "${module.kube_certs.admin_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kube_ca_key_pem          = "${module.kube_certs.ca_key_pem}"
+  admin_cert_pem           = "${module.kube_certs.admin_cert_pem}"
+  admin_key_pem            = "${module.kube_certs.admin_key_pem}"
 
   etcd_endpoints = "${split(",",
     length(compact(var.tectonic_etcd_servers)) == 0

platforms/vmware/tectonic.tf
@@ -68,19 +68,22 @@ module "bootkube" {
   oidc_client_id      = "tectonic-kubectl"
   oidc_ca_cert        = "${module.ingress_certs.ca_cert_pem}"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kube_ca_key_pem      = "${module.kube_certs.ca_key_pem}"
-  admin_cert_pem       = "${module.kube_certs.admin_cert_pem}"
-  admin_key_pem        = "${module.kube_certs.admin_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kube_ca_key_pem          = "${module.kube_certs.ca_key_pem}"
+  admin_cert_pem           = "${module.kube_certs.admin_cert_pem}"
+  admin_key_pem            = "${module.kube_certs.admin_key_pem}"
 
   etcd_endpoints = "${formatlist("%s.%s", values(var.tectonic_vmware_etcd_hostnames), var.tectonic_base_domain)}"
 

tests/smoke/user_provided_tls/tls.tf
@@ -30,10 +30,13 @@ module "ingress_certs" {
 module "kube_certs" {
   source = "../../modules/tls/kube/user-provided"
 
-  ca_cert_pem_path        = "../../tests/smoke/user_provided_tls/certs/kube/ca.crt"
-  ca_key_pem_path         = "../../tests/smoke/user_provided_tls/certs/kube/ca.key"
-  admin_cert_pem_path     = "../../tests/smoke/user_provided_tls/certs/kube/admin.crt"
-  admin_key_pem_path      = "../../tests/smoke/user_provided_tls/certs/kube/admin.key"
-  apiserver_cert_pem_path = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.crt"
-  apiserver_key_pem_path  = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.key"
+  aggregator_ca_cert_pem_path   = "../../tests/smoke/user_provided_tls/certs/kube/aggregator-ca.crt"
+  ca_cert_pem_path              = "../../tests/smoke/user_provided_tls/certs/kube/ca.crt"
+  ca_key_pem_path               = "../../tests/smoke/user_provided_tls/certs/kube/ca.key"
+  admin_cert_pem_path           = "../../tests/smoke/user_provided_tls/certs/kube/admin.crt"
+  admin_key_pem_path            = "../../tests/smoke/user_provided_tls/certs/kube/admin.key"
+  apiserver_cert_pem_path       = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.crt"
+  apiserver_key_pem_path        = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.key"
+  apiserver_proxy_cert_pem_path = "../../tests/smoke/user_provided_tls/certs/kube/apiserver-proxy.crt"
+  apiserver_proxy_key_pem_path  = "../../tests/smoke/user_provided_tls/certs/kube/apiserver-proxy.key"
 }

openshift/installer

platforms/aws/tectonic.tf
@@ -28,18 +28,21 @@ module "bootkube" {
 
   pull_secret_path = "${pathexpand(var.tectonic_pull_secret_path)}"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kubelet_cert_pem     = "${module.kube_certs.kubelet_cert_pem}"
-  kubelet_key_pem      = "${module.kube_certs.kubelet_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kubelet_cert_pem         = "${module.kube_certs.kubelet_cert_pem}"
+  kubelet_key_pem          = "${module.kube_certs.kubelet_key_pem}"
 
   etcd_endpoints = "${module.dns.etcd_endpoints}"
   master_count   = "${var.tectonic_master_count}"

platforms/gcp/tectonic.tf
@@ -40,18 +40,21 @@ module "bootkube" {
   oidc_groups_claim   = "groups"
   oidc_username_claim = "email"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kubelet_cert_pem     = "${module.kube_certs.kubelet_cert_pem}"
-  kubelet_key_pem      = "${module.kube_certs.kubelet_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kubelet_cert_pem         = "${module.kube_certs.kubelet_cert_pem}"
+  kubelet_key_pem          = "${module.kube_certs.kubelet_key_pem}"
 
   cloud_config_path = ""
   etcd_endpoints    = "${data.template_file.etcd_hostname_list.*.rendered}"

platforms/metal/tectonic.tf
@@ -25,18 +25,21 @@ module "bootkube" {
 
   pull_secret_path = "${pathexpand(var.tectonic_pull_secret_path)}"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kubelet_cert_pem     = "${module.kube_certs.kubelet_cert_pem}"
-  kubelet_key_pem      = "${module.kube_certs.kubelet_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kubelet_cert_pem         = "${module.kube_certs.kubelet_cert_pem}"
+  kubelet_key_pem          = "${module.kube_certs.kubelet_key_pem}"
 
   etcd_endpoints = "${split(",",
     length(compact(var.tectonic_etcd_servers)) == 0

platforms/vmware/tectonic.tf
@@ -68,18 +68,21 @@ module "bootkube" {
   oidc_client_id      = "tectonic-kubectl"
   oidc_ca_cert        = "${module.ingress_certs.ca_cert_pem}"
 
-  apiserver_cert_pem   = "${module.kube_certs.apiserver_cert_pem}"
-  apiserver_key_pem    = "${module.kube_certs.apiserver_key_pem}"
-  etcd_ca_cert_pem     = "${module.etcd_certs.etcd_ca_crt_pem}"
-  etcd_client_cert_pem = "${module.etcd_certs.etcd_client_crt_pem}"
-  etcd_client_key_pem  = "${module.etcd_certs.etcd_client_key_pem}"
-  etcd_peer_cert_pem   = "${module.etcd_certs.etcd_peer_crt_pem}"
-  etcd_peer_key_pem    = "${module.etcd_certs.etcd_peer_key_pem}"
-  etcd_server_cert_pem = "${module.etcd_certs.etcd_server_crt_pem}"
-  etcd_server_key_pem  = "${module.etcd_certs.etcd_server_key_pem}"
-  kube_ca_cert_pem     = "${module.kube_certs.ca_cert_pem}"
-  kubelet_cert_pem     = "${module.kube_certs.kubelet_cert_pem}"
-  kubelet_key_pem      = "${module.kube_certs.kubelet_key_pem}"
+  aggregator_ca_cert_pem   = "${module.kube_certs.aggregator_ca_cert_pem}"
+  apiserver_cert_pem       = "${module.kube_certs.apiserver_cert_pem}"
+  apiserver_key_pem        = "${module.kube_certs.apiserver_key_pem}"
+  apiserver_proxy_cert_pem = "${module.kube_certs.apiserver_proxy_cert_pem}"
+  apiserver_proxy_key_pem  = "${module.kube_certs.apiserver_proxy_key_pem}"
+  etcd_ca_cert_pem         = "${module.etcd_certs.etcd_ca_crt_pem}"
+  etcd_client_cert_pem     = "${module.etcd_certs.etcd_client_crt_pem}"
+  etcd_client_key_pem      = "${module.etcd_certs.etcd_client_key_pem}"
+  etcd_peer_cert_pem       = "${module.etcd_certs.etcd_peer_crt_pem}"
+  etcd_peer_key_pem        = "${module.etcd_certs.etcd_peer_key_pem}"
+  etcd_server_cert_pem     = "${module.etcd_certs.etcd_server_crt_pem}"
+  etcd_server_key_pem      = "${module.etcd_certs.etcd_server_key_pem}"
+  kube_ca_cert_pem         = "${module.kube_certs.ca_cert_pem}"
+  kubelet_cert_pem         = "${module.kube_certs.kubelet_cert_pem}"
+  kubelet_key_pem          = "${module.kube_certs.kubelet_key_pem}"
 
   etcd_endpoints = "${formatlist("%s.%s", values(var.tectonic_vmware_etcd_hostnames), var.tectonic_base_domain)}"
 

tests/smoke/user_provided_tls/tls.tf
@@ -30,9 +30,12 @@ module "ingress_certs" {
 module "kube_certs" {
   source = "../../modules/tls/kube/user-provided"
 
-  ca_cert_pem_path        = "../../tests/smoke/user_provided_tls/certs/kube/ca.crt"
-  kubelet_cert_pem_path   = "../../tests/smoke/user_provided_tls/certs/kube/kubelet.crt"
-  kubelet_key_pem_path    = "../../tests/smoke/user_provided_tls/certs/kube/kubelet.key"
-  apiserver_cert_pem_path = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.crt"
-  apiserver_key_pem_path  = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.key"
+  aggregator_ca_cert_pem_path   = "../../tests/smoke/user_provided_tls/certs/kube/aggregator-ca.crt"
+  ca_cert_pem_path              = "../../tests/smoke/user_provided_tls/certs/kube/ca.crt"
+  kubelet_cert_pem_path         = "../../tests/smoke/user_provided_tls/certs/kube/kubelet.crt"
+  kubelet_key_pem_path          = "../../tests/smoke/user_provided_tls/certs/kube/kubelet.key"
+  apiserver_cert_pem_path       = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.crt"
+  apiserver_key_pem_path        = "../../tests/smoke/user_provided_tls/certs/kube/apiserver.key"
+  apiserver_proxy_cert_pem_path = "../../tests/smoke/user_provided_tls/certs/kube/apiserver-proxy.crt"
+  apiserver_proxy_key_pem_path  = "../../tests/smoke/user_provided_tls/certs/kube/apiserver-proxy.key"
 }