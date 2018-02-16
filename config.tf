aca2f7090aac688c9e8563bf5597dd2d5632e613
Evan Cordell <cordell.evan@gmail.com>
2018-02-16 07:29:42 -0500

    Bump ALM to 0.3.1 (#2956)
    0.3.1 contains fixes for CRD validation on k8s 1.9

--- config.tf
+++ config.tf
@@ -80,7 +80,7 @@ variable "tectonic_container_images" {
     tectonic_cluo_operator       = "quay.io/coreos/tectonic-cluo-operator:v0.3.1"
     tectonic_torcx               = "quay.io/coreos/tectonic-torcx:v0.2.1"
     kubernetes_addon_operator    = "quay.io/coreos/kubernetes-addon-operator:beryllium-m1"
-    tectonic_alm_operator        = "quay.io/coreos/tectonic-alm-operator:v0.3.0"
+    tectonic_alm_operator        = "quay.io/coreos/tectonic-alm-operator:v0.3.1"
     tectonic_utility_operator    = "quay.io/coreos/tectonic-utility-operator:beryllium-m1"
     tectonic_network_operator    = "quay.io/coreos/tectonic-network-operator:beryllium-m1"
   }
@@ -114,7 +114,7 @@ variable "tectonic_versions" {
     monitoring = "1.9.1"
     tectonic   = "1.8.4-tectonic.2"
     cluo       = "0.3.1"
-    alm        = "0.3.0"
+    alm        = "0.3.1"
   }
 }
 
