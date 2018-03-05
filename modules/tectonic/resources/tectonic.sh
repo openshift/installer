cd833d0969335a169bc1b8f5af5ade26347ac6ce
Abhinav Dahiya <abhinavdtu2012@gmail.com>
2018-03-05 04:14:09 -0800

    move ingress namespace to bootkube (#3058)
    * move ingress namespace to bootkube
    
    * modules/botkube-ut2: copy ingress namespace

--- modules/tectonic/resources/tectonic.sh
+++ modules/tectonic/resources/tectonic.sh
@@ -127,9 +127,6 @@ set -e
 # wait for Kubernetes pods
 wait_for_pods kube-system
 
-echo "creating namespaces"
-kubectl create -f ingress/namespace.yaml
-
 echo "Creating Initial Roles"
 kubectl delete -f rbac/role-admin.yaml
 
