c9e25224b3ae0826e0c3c01bf659817f08dee919
Enxebre <alberto.garcial@hotmail.com>
2018-05-16 15:35:02 +0200

    move vendor to root folder

--- installer/version/version.go
+++
@@ -1,7 +0,0 @@
-package version
-
-// TectonicVersion - defaults to 'dev-build' if TECTONIC_VERSION was not set
-var TectonicVersion = "dev-build"
-
-// BuildTime - set to $(date -u '+%d_%b_%Y_%I:%M:%S%p')
-var BuildTime string
