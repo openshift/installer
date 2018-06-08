openshift/installer

commit 4f8f4fcca04034035b59a691a102646163735432

config.tf: Bump images
- Bump operator images.
- Introduce openshift images.
- Drop prometheus operator and dex.

config.tf
@@ -49,7 +49,7 @@ variable "tectonic_image_re" {
 EOF
 
   type    = "string"
-  default = "/^([^/]+/[^/]+/[^/]+):(.*)$/"
+  default = "/^([^/]+/[^/]+):(.*)$/"
 }
 
 variable "tectonic_container_images" {
@@ -61,20 +61,19 @@ variable "tectonic_container_images" {
     awscli                               = "quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600"
     gcloudsdk                            = "google/cloud-sdk:178.0.0-alpine"
     bootkube                             = "quay.io/coreos/bootkube:v0.10.0"
-    tnc_operator                         = "quay.io/coreos/tectonic-node-controller-operator-dev:ce870e6b3cc17f09a99d46150865e81c0997d224"
+    tnc_operator                         = "quay.io/coreos/tectonic-node-controller-operator-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
     etcd_cert_signer                     = "quay.io/coreos/kube-etcd-signer-server:678cc8e6841e2121ebfdb6e2db568fce290b67d6"
     etcd                                 = "quay.io/coreos/etcd:v3.2.14"
-    hyperkube                            = "quay.io/coreos/hyperkube:v1.9.3_coreos.0"
-    kube_core_renderer                   = "quay.io/coreos/kube-core-renderer:beryllium-m2"
-    kube_core_operator                   = "quay.io/coreos/kube-core-operator:beryllium-m2"
-    tectonic_channel_operator            = "quay.io/coreos/tectonic-channel-operator:beryllium-m2"
-    tectonic_prometheus_operator         = "quay.io/coreos/tectonic-prometheus-operator:v1.9.3"
+    hyperkube                            = "openshift/origin-node:latest"
+    kube_core_renderer                   = "quay.io/coreos/kube-core-renderer-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
+    kube_core_operator                   = "quay.io/coreos/kube-core-operator-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
+    tectonic_channel_operator            = "quay.io/coreos/tectonic-channel-operator-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
     tectonic_torcx                       = "quay.io/coreos/tectonic-torcx:v0.2.1"
-    kube_addon_operator                  = "quay.io/coreos/kube-addon-operator:beryllium-m2"
-    tectonic_alm_operator                = "quay.io/coreos/tectonic-alm-operator:v0.4.0"
-    tectonic_ingress_controller_operator = "quay.io/coreos/tectonic-ingress-controller-operator:d6b0848118e3b7c78d7d1728ee8846d5c6af2412"
-    tectonic_utility_operator            = "quay.io/coreos/tectonic-utility-operator:beryllium-m2"
-    tectonic_network_operator            = "quay.io/coreos/tectonic-network-operator:beryllium-m2"
+    kube_addon_operator                  = "quay.io/coreos/kube-addon-operator-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
+    tectonic_alm_operator                = "quay.io/coreos/tectonic-alm-operator:v0.3.1"
+    tectonic_ingress_controller_operator = "quay.io/coreos/tectonic-ingress-controller-operator-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
+    tectonic_utility_operator            = "quay.io/coreos/tectonic-utility-operator-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
+    tectonic_network_operator            = "quay.io/coreos/tectonic-network-operator-dev:8a9cde8fdb4baef4050b4770c837c151647bb99a"
   }
 }
 
@@ -103,9 +102,8 @@ variable "tectonic_versions" {
   type        = "map"
 
   default = {
-    monitoring = "1.9.3"
-    tectonic   = "1.8.4-tectonic.2"
-    alm        = "0.4.0"
+    tectonic = "1.8.4-tectonic.2"
+    alm      = "0.4.0"
   }
 }
 

commit 45fa0e49a98c9b9cb0fe356e838c9189d9f50ebe

*: remove k8s-node-bootstrap.service
This was broken by 4f8f4fc when the origin image was used instead. The
k8s-node-bootstrap service was failing to parse "latest" as valid
semver. Since we are eventually moving away from Container Linux, this
service is safe to remove in favor of some future mechanism.

config.tf
@@ -323,12 +323,6 @@ variable "tectonic_networking" {
 EOF
 }
 
-variable "tectonic_bootstrap_upgrade_cl" {
-  type        = "string"
-  default     = "true"
-  description = "(internal) Whether to trigger a ContainerLinux upgrade on node bootstrap."
-}
-
 variable "tectonic_kubelet_debug_config" {
   type    = "string"
   default = ""

modules/ignition/assets.tf
@@ -49,23 +49,6 @@ data "ignition_systemd_unit" "kubelet" {
   content = "${data.template_file.kubelet.rendered}"
 }
 
-data "template_file" "k8s_node_bootstrap" {
-  template = "${file("${path.module}/resources/services/k8s-node-bootstrap.service")}"
-
-  vars {
-    bootstrap_upgrade_cl = "${var.bootstrap_upgrade_cl}"
-    tectonic_torcx_image = "${var.container_images["tectonic_torcx"]}"
-    torcx_skip_setup     = "false"
-    torcx_store_url      = "${var.torcx_store_url}"
-  }
-}
-
-data "ignition_systemd_unit" "k8s_node_bootstrap" {
-  name    = "k8s-node-bootstrap.service"
-  enabled = true
-  content = "${data.template_file.k8s_node_bootstrap.rendered}"
-}
-
 data "ignition_systemd_unit" "rm_assets" {
   name    = "rm-assets.service"
   enabled = true

modules/ignition/outputs.import
@@ -24,10 +24,6 @@ variable "ign_installer_kubelet_env_id" {
   type = "string"
 }
 
-variable "ign_k8s_node_bootstrap_service_id" {
-  type = "string"
-}
-
 variable "ign_update_ca_certificates_dropin_id" {
   type = "string"
 }

modules/ignition/outputs.tf
@@ -32,7 +32,6 @@ output "ignition_systemd_id_list" {
     "${data.ignition_systemd_unit.docker_dropin.id}",
     "${data.ignition_systemd_unit.kubelet.id}",
     "${data.ignition_systemd_unit.locksmithd.id}",
-    "${data.ignition_systemd_unit.k8s_node_bootstrap.id}",
     "${data.ignition_systemd_unit.update_ca_certificates_dropin.id}",
     "${data.ignition_systemd_unit.rm_assets.id}",
   ]

modules/ignition/resources/services/k8s-node-bootstrap.service
@@ -1,43 +1,0 @@
-[Unit]
-Description=Bootstrap Kubernetes Node Components
-ConditionPathExists=!/etc/kubernetes/kubelet.env
-Wants=update-ca-certificates.service
-After=update-ca-certificates.service
-Before=kubelet.service
-
-[Service]
-Type=simple
-RemainAfterExit=true
-Restart=on-failure
-RestartSec=10
-TimeoutStartSec=1h
-ExecStartPre=/usr/bin/mkdir -p /etc/kubernetes
-ExecStartPre=/usr/bin/docker run --rm \
-            --env http_proxy \
-            --env HTTP_PROXY \
-            --env https_proxy \
-            --env HTTPS_PROXY \
-            --env no_proxy \
-            --env NO_PROXY \
-            --tmpfs /tmp \
-            -v /usr/share:/usr/share:ro \
-            -v /usr/lib/os-release:/usr/lib/os-release:ro \
-            -v /etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt:ro \
-            -v /var/lib/torcx:/var/lib/torcx \
-            -v /var/run/dbus:/var/run/dbus \
-            -v /run/metadata:/run/metadata:ro \
-            -v /run/torcx:/run/torcx:ro \
-            -v /run/systemd:/run/systemd \
-            -v /etc/coreos:/etc/coreos:ro \
-            -v /etc/torcx:/etc/torcx \
-            -v /etc/kubernetes:/etc/kubernetes \
-            ${tectonic_torcx_image} \
-            /tectonic-torcx-bootstrap \
-            --upgrade-os=${bootstrap_upgrade_cl} \
-            --torcx-manifest-url="${torcx_store_url}" \
-            --torcx-skip-setup=${torcx_skip_setup} \
-            --verbose=debug
-ExecStart=/usr/bin/echo "node components bootstrapped"
-
-[Install]
-WantedBy=multi-user.target kubelet.service

modules/ignition/variables.tf
@@ -45,12 +45,6 @@ variable "cloud_provider_config" {
   default     = ""
 }
 
-variable "bootstrap_upgrade_cl" {
-  type        = "string"
-  description = "(optional) Whether to trigger a ContainerLinux OS upgrade during the bootstrap process."
-  default     = "true"
-}
-
 variable "torcx_store_url" {
   type        = "string"
   description = "(optional) URL template for torcx store. Leave empty to use the default CoreOS endpoint."

steps/assets/aws/main.tf
@@ -21,7 +21,6 @@ module assets_base {
   ingress_kind   = "haproxy-router"
 
   tectonic_base_domain             = "${var.tectonic_base_domain}"
-  tectonic_bootstrap_upgrade_cl    = "${var.tectonic_bootstrap_upgrade_cl}"
   tectonic_cluster_name            = "${var.tectonic_cluster_name}"
   tectonic_container_images        = "${var.tectonic_container_images}"
   tectonic_image_re                = "${var.tectonic_image_re}"

steps/assets/base/ignition-bootstrap.tf
@@ -1,7 +1,6 @@
 module "ignition_bootstrap" {
   source = "../../../modules/ignition"
 
-  bootstrap_upgrade_cl = "${var.tectonic_bootstrap_upgrade_cl}"
   cloud_provider       = "${var.cloud_provider}"
   container_images     = "${var.tectonic_container_images}"
   etcd_ca_cert_pem     = "${local.etcd_ca_cert_pem}"

steps/assets/libvirt/main.tf
@@ -9,7 +9,6 @@ module assets_base {
   ingress_kind = "haproxy-router"
 
   tectonic_base_domain             = "${var.tectonic_base_domain}"
-  tectonic_bootstrap_upgrade_cl    = "${var.tectonic_bootstrap_upgrade_cl}"
   tectonic_cluster_name            = "${var.tectonic_cluster_name}"
   tectonic_container_images        = "${var.tectonic_container_images}"
   tectonic_image_re                = "${var.tectonic_image_re}"