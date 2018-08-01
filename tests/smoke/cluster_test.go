6a9d6ae10f2775d64695a56dd0025d7a7a8f85ca
W. Trevor King <wking@tremily.us>
2018-08-01 13:02:24 -0700

    tests/smoke/cluster_test: Get kubectl resource from genericclioptions
    This change is similar to kubernetes/kubernetes@fd044d15 (fix dynamic
    client name, 2018-05-09, kubernetes/kubernetes#63446, v1.11.0-beta.0)
    and it protects us from the eventual removal of
    k8s.io/kubernetes/pkg/kubectl/resource in
    kubernetes/kubernetes@16d6a6c5 (move resource builder to generic
    options, 2018-05-10, kubernetes/kubernetes#63669, v1.11.0-beta.0).

--- tests/smoke/cluster_test.go
+++ tests/smoke/cluster_test.go
@@ -19,7 +19,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
-	"k8s.io/kubernetes/pkg/kubectl/resource"
+	"k8s.io/kubernetes/pkg/kubectl/genericclioptions/resource"
 )
 
 const (
