0b6ab7dc526d8557f5c464a68229f579eab6ec2c
Alejandro Rosero V <alejandroroserov@gmail.com>
2018-08-02 17:31:03 -0500

    Prow: Add shellcheck. Update shellcheck to work in Traivs CI

--- hack/shellcheck.sh
+++ hack/shellcheck.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-DIR="${1:-.}"
 if [ "$IS_CONTAINER" != "" ]; then
-  find "${DIR}" -type f -name '*.sh' -exec shellcheck --format=gcc {} \+
+  find "${1:-.}" -type f -name '*.sh' -exec shellcheck --format=gcc {} \+
 else
-  docker run -e IS_CONTAINER='TRUE' --rm -v "$(realpath "${DIR}")":/workdir:ro --entrypoint sh quay.io/coreos/shellcheck-alpine:v0.5.0 /workdir/hack/shellcheck.sh /workdir;
+  docker run -e IS_CONTAINER='TRUE' --rm -v "$(pwd)":/workdir:ro --entrypoint sh quay.io/coreos/shellcheck-alpine:v0.5.0 /workdir/hack/shellcheck.sh /workdir;
 fi;
