01339127e818a6514eb111064c5d0a3c736f9bf8
W. Trevor King <wking@tremily.us>
2018-08-24 14:17:55 -0700

    hack: Add :z to --volume mounts
    Like we did in bootkube.sh in 0fa4eb13 (Fix perm errors with selinux
    enabled, 2018-08-15, #134).  This gives us permission to access the
    mounted volume when SELinux is enabled (docs in [1]).
    
    I've also normalized these invocations for consistency between the
    various hack/ scripts:
    
    * Adding slash separators to put each option on its own line,
      excepting the final command being run in the container.  This makes
      the long commands slightly easier to skim.  It will also make it
      easier to track down motivation for an option with 'git blame',
      because commits touching options on other lines won't clutter the
      blame.
    
    * Use long-form options (-v -> --volume, etc.).  This makes the
      options a bit more accessible to newcomers, and now that each option
      is on it's own line we have plenty of space.
    
    * Dropped single quotes from 'TRUE'.  There are no shell-sensitive
      characters in TRUE, so there's no need to quote it.
    
    * Use ${PWD} consistently.  It's in POSIX [2], so there's no need to
      execute a pwd process to get this value.
    
    * Drop -t.  None of these commands should need a pseudoterminal.
    
    * Drop explicit rw --volume options.  They're the default [3].
    
    [1]: https://github.com/containers/libpod/blame/v0.8.3/docs/podman-run.1.md#L628
    [2]: http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html#tag_08_03
    [3]: https://github.com/containers/libpod/blame/v0.8.3/docs/podman-run.1.md#L646

--- hack/shellcheck.sh
+++ hack/shellcheck.sh
@@ -6,5 +6,10 @@ if [ "$IS_CONTAINER" != "" ]; then
     -o -path "${TOP_DIR}/.build" -prune \
     -o -type f -name '*.sh' -exec shellcheck --format=gcc {} \+
 else
-  docker run -e IS_CONTAINER='TRUE' --rm -v "$(pwd)":/workdir:ro --entrypoint sh quay.io/coreos/shellcheck-alpine:v0.5.0 /workdir/hack/shellcheck.sh /workdir;
+  docker run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/workdir:ro,z" \
+    --entrypoint sh \
+    quay.io/coreos/shellcheck-alpine:v0.5.0 \
+    /workdir/hack/shellcheck.sh /workdir
 fi;
--- hack/tf-lint.sh
+++ hack/tf-lint.sh
@@ -2,5 +2,10 @@
 if [ "$IS_CONTAINER" != "" ]; then
   tflint
 else
-  docker run -t --rm -v "$(pwd)":/data --env IS_CONTAINER='TRUE' --entrypoint sh quay.io/coreos/tflint ./hack/tf-lint.sh
+  docker run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/data:z" \
+    --entrypoint sh \
+    quay.io/coreos/tflint \
+    ./hack/tf-lint.sh
 fi;
