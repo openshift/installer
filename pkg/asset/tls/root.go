fc9671e6e92bc2fb60d28f3007fb25ffe7355525
Yifan Gu <guyifan1121@gmail.com>
2018-08-28 17:34:56 -0700

    tls: Add RootCA type to implement the asset.Asset interface.
    RootCA implements asset.Asset interface and generate a pair of
    root ca cert/key.

---
+++ pkg/asset/tls/root.go
@@ -0,0 +1,53 @@
+package tls
+
+import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+
+	"github.com/openshift/installer/pkg/asset"
+)
+
+// RootCA contains the private key and the cert that's
+// self-signed as the root CA.
+type RootCA struct {
+	rootDir string
+}
+
+var _ asset.Asset = (*CertKey)(nil)
+
+func (c *RootCA) Dependencies() []asset.Asset {
+	return []asset.Asset{}
+}
+
+func (c *RootCA) Generate(parents map[asset.Asset]*asset.State) (*asset.State, error) {
+	cfg := &CertCfg{
+		Subject:   pkix.Name{CommonName: "root-ca", OrganizationalUnit: []string{"openshift"}},
+		KeyUsages: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		Validity:  ValidityTenYears,
+		IsCA:      true,
+	}
+
+	key, crt, err := GenerateRootCertKey(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate RootCA %v", err)
+	}
+
+	var st asset.State
+	st.Contents = []asset.Content{
+		{
+			Name: assetFilePath(c.rootDir, "root-ca.key"),
+			Data: []byte(PrivateKeyToPem(key)),
+		},
+		{
+			Name: assetFilePath(c.rootDir, "root-ca.crt"),
+			Data: []byte(CertToPem(crt)),
+		},
+	}
+
+	if err := st.PersistToFile(); err != nil {
+		return nil, err
+	}
+
+	return &st, nil
+}
