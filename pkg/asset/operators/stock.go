b55ca60b0e7e6cf407bf1aca8c69b1a8761e19ad
Rajat Chopra <rchopra@redhat.com>
2018-08-31 15:13:25 -0400

    pkg/assets/operators:
    operator config and manifests for:
    1. tnco (to be replaced by mco)
    2. network-operator
    3. kube-core-operator
    4. kube-addon-operator

---
+++ pkg/asset/operators/stock.go
@@ -0,0 +1,80 @@
+package operators
+
+import (
+	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/asset/installconfig"
+)
+
+// Stock is the stock of operator assets that can be generated.
+type Stock interface {
+	// ClusterVersionOperator returns the cvo asset object
+	ClusterVersionOperator() asset.Asset
+
+	// KubeCoreOperator returns the kco asset object
+	KubeCoreOperator() asset.Asset
+
+	// NetworkOperator returns the network operator asset object
+	NetworkOperator() asset.Asset
+
+	// Tnco returns the TNCO asset object
+	Tnco() asset.Asset
+
+	// KubeAddonOperator returns the addon asset object
+	KubeAddonOperator() asset.Asset
+}
+
+// StockImpl implements the Stock interface for operators
+type StockImpl struct {
+	operators              asset.Asset
+	clusterVersionOperator asset.Asset
+	kubeCoreOperator       asset.Asset
+	networkOperator        asset.Asset
+	tnco                   asset.Asset
+	addon                  asset.Asset
+}
+
+var _ Stock = (*StockImpl)(nil)
+
+// EstablishStock establishes the stock of assets in the specified directory.
+func (s *StockImpl) EstablishStock(rootDir string, stock installconfig.Stock) {
+	s.operators = &operators{
+		assetStock: s,
+		directory:  rootDir,
+	}
+	s.kubeCoreOperator = &kubeCoreOperator{
+		installConfigAsset: stock.InstallConfig(),
+		directory:          rootDir,
+	}
+	s.addon = &kubeAddonOperator{
+		installConfigAsset: stock.InstallConfig(),
+		directory:          rootDir,
+	}
+	s.tnco = &tectonicNodeControllerOperator{
+		installConfigAsset: stock.InstallConfig(),
+		directory:          rootDir,
+	}
+	s.networkOperator = &networkOperator{
+		installConfigAsset: stock.InstallConfig(),
+		directory:          rootDir,
+	}
+	// TODO:
+	//s.clusterVersionOperator = &clusterVersionOperator{}
+}
+
+// Operators returns the operators asset
+func (s *StockImpl) Operators() asset.Asset { return s.operators }
+
+// ClusterVersionOperator returns the cvo asset object
+func (s *StockImpl) ClusterVersionOperator() asset.Asset { return s.clusterVersionOperator }
+
+// KubeCoreOperator returns the kco asset object
+func (s *StockImpl) KubeCoreOperator() asset.Asset { return s.kubeCoreOperator }
+
+// NetworkOperator returns the network operator asset object
+func (s *StockImpl) NetworkOperator() asset.Asset { return s.networkOperator }
+
+// KubeAddonOperator returns the addon operator asset object
+func (s *StockImpl) KubeAddonOperator() asset.Asset { return s.addon }
+
+// Tnco returns the tnc operator asset object
+func (s *StockImpl) Tnco() asset.Asset { return s.tnco }
--- pkg/asset/stock/stock.go
+++ pkg/asset/stock/stock.go
@@ -7,6 +7,7 @@ import (
 	"github.com/openshift/installer/pkg/asset/ignition"
 	"github.com/openshift/installer/pkg/asset/installconfig"
 	"github.com/openshift/installer/pkg/asset/kubeconfig"
+	"github.com/openshift/installer/pkg/asset/operators"
 	"github.com/openshift/installer/pkg/asset/tls"
 )
 
@@ -16,6 +17,7 @@ type Stock struct {
 	kubeconfigStock
 	tlsStock
 	ignitionStock
+	operatorStock
 }
 
 type installConfigStock struct {
@@ -34,6 +36,10 @@ type ignitionStock struct {
 	ignition.StockImpl
 }
 
+type operatorStock struct {
+	operators.StockImpl
+}
+
 var _ installconfig.Stock = (*Stock)(nil)
 
 // EstablishStock establishes the stock of assets in the specified directory.
@@ -44,6 +50,7 @@ func EstablishStock(directory string) *Stock {
 	s.tlsStock.EstablishStock(directory, &s.installConfigStock)
 	s.kubeconfigStock.EstablishStock(directory, &s.installConfigStock, &s.tlsStock)
 	s.ignitionStock.EstablishStock(directory, s, s, s)
+	s.operatorStock.EstablishStock(directory, &s.installConfigStock)
 
 	return s
 }
