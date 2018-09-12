89881af24bc2c542a7b1a3a58411bbb36e471a6e
W. Trevor King <wking@tremily.us>
2018-09-12 01:04:17 -0700

    pkg/ipnet: Add YAML serialization support
    For folks using gopkg.in/yaml.v2.  Folks using github.com/ghodss/yaml
    will use the JSON (un)marshallers.  But supporting yaml.v2 is
    convenient for consumers who are already importing that package
    (e.g. because the want different JSON and YAML serializations).

--- pkg/ipnet/ipnet.go
+++ pkg/ipnet/ipnet.go
@@ -3,8 +3,11 @@ package ipnet
 
 import (
 	"encoding/json"
+	"fmt"
 	"net"
 	"reflect"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 var nullString = "null"
@@ -16,7 +19,7 @@ type IPNet struct {
 	net.IPNet
 }
 
-// MarshalJSON interface for an IPNet
+// MarshalJSON interface for an IPNet.
 func (ipnet IPNet) MarshalJSON() (data []byte, err error) {
 	if reflect.DeepEqual(ipnet.IPNet, emptyIPNet) {
 		return nullBytes, nil
@@ -25,7 +28,7 @@ func (ipnet IPNet) MarshalJSON() (data []byte, err error) {
 	return json.Marshal(ipnet.String())
 }
 
-// UnmarshalJSON interface for an IPNet
+// UnmarshalJSON interface for an IPNet.
 func (ipnet *IPNet) UnmarshalJSON(b []byte) (err error) {
 	if string(b) == nullString {
 		ipnet.IP = net.IP{}
@@ -34,11 +37,15 @@ func (ipnet *IPNet) UnmarshalJSON(b []byte) (err error) {
 	}
 
 	var cidr string
-	err = json.Unmarshal(b, &cidr)
+	err = yaml.Unmarshal(b, &cidr)
 	if err != nil {
 		return err
 	}
 
+	return ipnet.parseCIDR(cidr)
+}
+
+func (ipnet *IPNet) parseCIDR(cidr string) (err error) {
 	ip, net, err := net.ParseCIDR(cidr)
 	if err != nil {
 		return err
@@ -46,4 +53,31 @@ func (ipnet *IPNet) UnmarshalJSON(b []byte) (err error) {
 	ipnet.IP = ip
 	ipnet.Mask = net.Mask
 	return nil
+}
+
+// MarshalYAML interface for an IPNet.
+func (ipnet *IPNet) MarshalYAML() (replacement interface{}, err error) {
+	if ipnet == nil || reflect.DeepEqual(ipnet.IPNet, emptyIPNet) {
+		return nil, nil
+	}
+
+	return ipnet.String(), nil
+}
+
+// UnmarshalYAML interface for an IPNet.
+func (ipnet *IPNet) UnmarshalYAML(unmarshal func(interface{}) error) (err error) {
+	var data interface{}
+	err = unmarshal(&data)
+	if err != nil {
+		return nil
+	}
+
+	switch data.(type) {
+	case nil:
+		return nil
+	case string:
+		return ipnet.parseCIDR(data.(string))
+	default:
+		return fmt.Errorf("cannot unmarshal %v into an IPNet", data)
+	}
 }
