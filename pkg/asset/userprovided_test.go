7c08f6cbf1f328b200e398c084245cd159f272bd
Rajat Chopra <rchopra@redhat.com>
2018-09-17 17:15:00 -0400

    pkg/asset:
        Support for default values for user prompts. User is expected to hit <Enter> to select the default value.
    
    pkg/asset/installconfig:
    
        Default values for aws region, libvirt url etc.

--- pkg/asset/userprovided_test.go
+++ pkg/asset/userprovided_test.go
@@ -12,40 +12,52 @@ func TestQueryUser(t *testing.T) {
 	cases := []struct {
 		name           string
 		input          string
+		defaultValue   string
 		expectedOutput string
 	}{
 		{
 			name:           "single word",
 			input:          "test",
+			defaultValue:   "default",
 			expectedOutput: "test",
 		},
 		{
 			name: "with newline",
 			input: `test
 `,
+			defaultValue:   "default",
 			expectedOutput: "test",
 		},
 		{
 			name:           "multiple words",
 			input:          "this is a test",
+			defaultValue:   "default",
 			expectedOutput: "this is a test",
 		},
 		{
 			name: "multiple lines",
 			input: `first
 second`,
+			defaultValue:   "default",
 			expectedOutput: "first",
 		},
 		{
 			name:           "empty",
 			input:          "",
+			defaultValue:   "",
 			expectedOutput: "",
+		},
+		{
+			name:           "default value",
+			input:          "",
+			defaultValue:   "default",
+			expectedOutput: "default",
 		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 			inputReader := bufio.NewReader(strings.NewReader(tc.input))
-			actual := QueryUser(inputReader, "prompt")
+			actual := QueryUser(inputReader, "prompt", tc.defaultValue)
 			assert.Equal(t, tc.expectedOutput, actual, "unexpected output")
 		})
 	}
