9c12d3c5822944160e452727d76e99650d4deda3
Yifan Gu <yifan.gu@coreos.com>
2018-09-19 17:52:39 -0700

    pkg/asset: Add cluster asset that invokes terraform to launch cluster.
    The cluster asset invokes terraform binary with the terraform templates
    and generated tfvars, ignitions, etc to launch a cluster.

---
+++ pkg/asset/cluster/cluster.go
@@ -0,0 +1,100 @@
+package cluster
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/terraform"
+	"github.com/openshift/installer/pkg/types/config"
+)
+
+// Cluster uses the terraform executable to launch a cluster
+// with the given terraform tfvar and generated templates.
+type Cluster struct {
+	// The root directory of the generated assets.
+	rootDir    string
+	tfvars     asset.Asset
+	kubeconfig asset.Asset
+}
+
+var _ asset.Asset = (*Cluster)(nil)
+
+// Name returns the human-friendly name of the asset.
+func (c *Cluster) Name() string {
+	return "Cluster"
+}
+
+// Dependencies returns the direct dependency for launching
+// the cluster.
+func (c *Cluster) Dependencies() []asset.Asset {
+	return []asset.Asset{c.tfvars, c.kubeconfig}
+}
+
+// Generate launches the cluster and generates the terraform state file on disk.
+func (c *Cluster) Generate(parents map[asset.Asset]*asset.State) (*asset.State, error) {
+	state, ok := parents[c.tfvars]
+	if !ok {
+		return nil, fmt.Errorf("failed to get terraform.tfvar state in the parent asset states")
+	}
+
+	var tfvars config.Cluster
+	if err := json.Unmarshal(state.Contents[0].Data, &tfvars); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal terraform tfvars file: %v", err)
+	}
+
+	steps := []string{
+		terraform.InfraStep,
+		terraform.BootstrapStep,
+	}
+
+	dir, err := baseLocation()
+	if err != nil {
+		return nil, err
+	}
+
+	var result asset.State
+
+	for _, step := range steps {
+		templateDir, err := terraform.FindStepTemplates(dir, step, tfvars.Platform)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := terraform.Init(c.rootDir, templateDir); err != nil {
+			return nil, err
+		}
+
+		stateFile, err := terraform.Apply(c.rootDir, step, templateDir)
+		if err != nil {
+			return nil, err
+		}
+
+		data, err := ioutil.ReadFile(stateFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read tfstate file %q: %v", stateFile, err)
+		}
+
+		result.Contents = append(result.Contents, asset.Content{
+			Name: stateFile,
+			Data: data,
+		})
+	}
+
+	// TODO(yifan): Use the kubeconfig to verify the cluster is up.
+
+	return &result, nil
+}
+
+// baseLocation returns the parent dir of the terraform templates (steps, modules).
+// The manifests should live in the same dir as the installer executable.
+func baseLocation() (string, error) {
+	ex, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("undetermined location of own executable: %s", err)
+	}
+	return filepath.Dir(ex), nil
+}
