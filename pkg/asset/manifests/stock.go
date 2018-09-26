b703510797bc288cb236e84f08f2d000c3c0d257
ramr <smitram@gmail.com>
2018-09-25 22:05:49 -0700

    Add dns operator manifests to installer.

--- pkg/asset/manifests/stock.go
+++ pkg/asset/manifests/stock.go
@@ -21,6 +21,9 @@ type Stock interface {
 	// NetworkOperator returns the network operator asset object
 	NetworkOperator() asset.Asset
 
+	// DNSOperator returns the dns operator asset object
+	DNSOperator() asset.Asset
+
 	// KubeAddonOperator returns the addon asset object
 	KubeAddonOperator() asset.Asset
 
@@ -37,6 +40,7 @@ type StockImpl struct {
 	clusterVersionOperator asset.Asset
 	kubeCoreOperator       asset.Asset
 	networkOperator        asset.Asset
+	dnsOperator            asset.Asset
 	addonOperator          asset.Asset
 	mao                    asset.Asset
 	tectonic               asset.Asset
@@ -74,6 +78,9 @@ func (s *StockImpl) EstablishStock(stock installconfig.Stock, tlsStock tls.Stock
 	s.networkOperator = &networkOperator{
 		installConfigAsset: stock.InstallConfig(),
 	}
+	s.dnsOperator = &dnsOperator{
+		installConfigAsset: stock.InstallConfig(),
+	}
 	s.mao = &machineAPIOperator{
 		installConfigAsset: stock.InstallConfig(),
 		aggregatorCA:       tlsStock.AggregatorCA(),
@@ -98,6 +105,9 @@ func (s *StockImpl) KubeCoreOperator() asset.Asset { return s.kubeCoreOperator }
 
 // NetworkOperator returns the network operator asset object
 func (s *StockImpl) NetworkOperator() asset.Asset { return s.networkOperator }
+
+// DNSOperator returns the dns operator asset object
+func (s *StockImpl) DNSOperator() asset.Asset { return s.dnsOperator }
 
 // KubeAddonOperator returns the addon operator asset object
 func (s *StockImpl) KubeAddonOperator() asset.Asset { return s.addonOperator }
