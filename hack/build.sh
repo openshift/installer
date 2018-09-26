6b2c53e3b82f76a5542652428b6d911e2693f9cb
Alex Crawford <crawford@redhat.com>
2018-09-25 17:59:30 -0700

    asset/cluster: include templates in binary
    This links the templates into the binary so that it may self-extract
    instead of reading the templates from the local filesystem. This allows
    the binary to be distributed as a stand-alone instead of within a
    tarball.

--- hack/build.sh
+++ hack/build.sh
@@ -4,4 +4,7 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-CGO_ENABLED=0 go build -o ./bin/openshift-install ./cmd/openshift-install
+archive=$(tar cz steps modules config.tf | base64 -w 0)
+LDFLAGS="-X github.com/openshift/installer/pkg/asset/cluster.TemplateArchive=${archive}"
+
+CGO_ENABLED=0 go build -ldflags "${LDFLAGS}" -o ./bin/openshift-install ./cmd/openshift-install
