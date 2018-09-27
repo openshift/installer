b8e2138a0f289f861a31b997903771055a77eabb
W. Trevor King <wking@tremily.us>
2018-09-27 13:32:53 -0700

    hack/get-terraform: Use 'go env ...' to support other platforms
    Jeff Ligon is on OS X.  With this change, we use Go to give the
    correct OS and architecture constants for the Terraform URL.  Go,
    because Terraform is a Go project, so Hashicorp is building the
    binaries using Go's cross-compilation with the Go naming.
    
    If you don't have Go, I'm just assuming you're on amd64 Linux, because
    that's what our CI infrastructure is running.

--- hack/get-terraform.sh
+++ hack/get-terraform.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+OS=linux
+ARCH=amd64
+if command -v go >/dev/null 2>/dev/null
+then
+	OS="$(go env GOOS)" &&
+	ARCH="$(go env GOARCH)"
+fi &&
 TERRAFORM_VERSION="0.11.8" &&
-TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" &&
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip" &&
+echo "pulling ${TERRAFORM_URL}" >&2 &&
 cd "$(dirname "$0")/.." &&
 mkdir -p bin &&
 curl -L "${TERRAFORM_URL}" | funzip > bin/terraform &&
