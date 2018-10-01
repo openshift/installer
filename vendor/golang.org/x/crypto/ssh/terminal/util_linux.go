2dbee066ccf49d2007c4ea46852395f1e6a4e472
Alex Crawford <crawford@redhat.com>
2018-10-01 16:44:21 -0700

    vendor: bump github.com/sirupsen/logrus
    This bumps github.com/sirupsen/logrus to v1.1.0, which provides
    the DisableLevelTruncation option in the text formatter.

--- vendor/golang.org/x/crypto/ssh/terminal/util_linux.go
+++ vendor/golang.org/x/crypto/ssh/terminal/util_linux.go
@@ -1,21 +1,10 @@
-// Based on ssh/terminal:
 // Copyright 2013 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !appengine,!js
+package terminal
 
-package logrus
-
-import (
-	"io"
-
-	"golang.org/x/sys/unix"
-)
+import "golang.org/x/sys/unix"
 
 const ioctlReadTermios = unix.TCGETS
-
-type Termios unix.Termios
-
-func initTerminal(w io.Writer) {
-}
+const ioctlWriteTermios = unix.TCSETS
