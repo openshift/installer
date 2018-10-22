7d15fb76ae28996daca1cb31f80b7b7f4ec1b0e2
W. Trevor King <wking@tremily.us>
2018-10-22 00:20:17 -0700

    .yamllint: Put yamllint config in the usual place
    This makes the config a bit easier to read, and also supports folks
    who want to run yamllint directly (without going through
    hack/yaml-lint.sh).
    
    I've added the document-start and indentation rules to avoid
    complaints like:
    
      $ hack/yaml-lint.sh
      ...
      ./data/data/bootstrap/files/opt/tectonic/manifest-overrides/kube-proxy-daemonset.yaml
        3:1       warning  missing document start "---"  (document-start)
        23:7      error    wrong indentation: expected 8 but found 6  (indentation)
        24:9      error    wrong indentation: expected 10 but found 8  (indentation)
        31:9      error    wrong indentation: expected 10 but found 8  (indentation)
        40:9      error    wrong indentation: expected 10 but found 8  (indentation)
        49:7      error    wrong indentation: expected 8 but found 6  (indentation)
        51:7      error    wrong indentation: expected 8 but found 6  (indentation)
      ...

--- hack/yaml-lint.sh
+++ hack/yaml-lint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$IS_CONTAINER" != "" ]; then
-  yamllint --config-data "{extends: default, rules: {line-length: {level: warning, max: 120}}}" .
+  yamllint .
 else
   podman run --rm \
     --env IS_CONTAINER=TRUE \
