4a84a4b6b9bbb33e36fbd218694b09fd495d7e0c
Clayton Coleman <ccoleman@redhat.com>
2018-10-26 12:57:44 -0400

    cvo: Rename the API object for CVOConfig to ClusterVersion
    Update the override object to point to the new name for CVOConfig,
    `config.openshift.io` `ClusterVersion`.

--- pkg/asset/manifests/content/bootkube/cvo-overrides.go
+++ pkg/asset/manifests/content/bootkube/cvo-overrides.go
@@ -9,7 +9,40 @@ var (
 	// This is a gate to prevent CVO from installing these operators which is conflicting
 	// with already owned resources by tectonic-operators.
 	// This files can be dropped when the overrides list becomes empty.
+	// IMPORTANT: You must also edit LegacyCVOOverrides
 	CVOOverrides = template.Must(template.New("cvo-override.yaml").Parse(`
+apiVersion: config.openshift.io/v1
+kind: ClusterVersion
+metadata:
+  namespace: openshift-cluster-version
+  name: cluster-version-operator
+spec:
+  upstream: http://localhost:8080/graph
+  channel: fast
+  clusterID: {{.CVOClusterID}}
+  overrides:
+  - kind: Deployment                    # this conflicts with kube-core-operator
+    namespace: openshift-cluster-network-operator
+    name: cluster-network-operator
+    unmanaged: true
+  - kind: ServiceAccount                # missing run level 0 on the namespace and has 0000_08
+    namespace: openshift-cluster-dns-operator
+    name: cluster-dns-operator
+    unmanaged: true
+  - kind: Deployment                    # this conflicts with kube-core-operator
+    namespace: openshift-cluster-dns-operator
+    name: cluster-dns-operator
+    unmanaged: true
+  - kind: APIService                    # packages.apps.redhat.com fails to start properly
+    name: v1alpha1.packages.apps.redhat.com
+    unmanaged: true
+`))
+
+	// LegacyCVOOverrides is the constant to represent contents of legacy-cvo-override.yaml file
+	// This is a gate to prevent CVO from installing these operators which is conflicting
+	// with already owned resources by tectonic-operators.
+	// This files can be dropped when the overrides list becomes empty.
+	LegacyCVOOverrides = template.Must(template.New("cvo-override.yaml").Parse(`
 apiVersion: clusterversion.openshift.io/v1
 kind: CVOConfig
 metadata:
