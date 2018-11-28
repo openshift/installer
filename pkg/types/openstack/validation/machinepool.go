e2d281ae85f2021b460e68127944ce1563190ff8
staebler <staebler@redhat.com>
2018-11-28 14:05:47 -0500

    asset/installconfig: validate InstallConfig loaded from file
    When an InstallConfig asset is loaded from an on-disk file, validate that
    the fields of the InstallConfig have appropriate values. This uses all the
    same validations that are down on the user-provided assets upon which the
    InstalConfig asset depends as well as addition validations on other fields.
    
    https://github.com/openshift/installer/pull/711

--- pkg/types/openstack/validation/machinepool.go
+++ pkg/types/openstack/validation/machinepool.go
@@ -8,12 +8,5 @@ import (
 
 // ValidateMachinePool checks that the specified machine pool is valid.
 func ValidateMachinePool(p *openstack.MachinePool, fldPath *field.Path) field.ErrorList {
-	allErrs := field.ErrorList{}
-	if p.RootVolume.IOPS < 0 {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("rootVolume").Child("iops"), p.RootVolume.IOPS, "Root volume IOPS must be positive"))
-	}
-	if p.RootVolume.Size < 0 {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("rootVolume").Child("size"), p.RootVolume.IOPS, "Root volume size must be positive"))
-	}
-	return allErrs
+	return field.ErrorList{}
 }
