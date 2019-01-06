fb38af77dcc8706019aff3c6282835a0977f8a84
Artyom Lukianov <alukiano@redhat.com>
2019-01-06 13:54:08 +0200

    pkg/asset/machines: replace ProviderConfig with ProviderSpec
    Change under files:
    - aws/machines.go
    - aws/machinesets.go
    - libvirt/machines.go
    - libvirt/machinesets.go

--- pkg/asset/machines/aws/machines.go
+++ pkg/asset/machines/aws/machines.go
@@ -56,7 +56,7 @@ func Machines(config *types.InstallConfig, pool *types.MachinePool, role, userDa
 				},
 			},
 			Spec: clusterapi.MachineSpec{
-				ProviderConfig: clusterapi.ProviderConfig{
+				ProviderSpec: clusterapi.ProviderSpec{
 					Value: &runtime.RawExtension{Object: provider},
 				},
 				// we don't need to set Versions, because we control those via operators.
@@ -122,7 +122,7 @@ func tagsFromUserTags(clusterID, clusterName string, usertags map[string]string)
 // ConfigMasters sets the PublicIP flag and assigns a set of load balancers to the given machines
 func ConfigMasters(machines []clusterapi.Machine, clusterName string) {
 	for _, machine := range machines {
-		providerConfig := machine.Spec.ProviderConfig.Value.Object.(*awsprovider.AWSMachineProviderConfig)
+		providerConfig := machine.Spec.ProviderSpec.Value.Object.(*awsprovider.AWSMachineProviderConfig)
 		providerConfig.PublicIP = pointer.BoolPtr(true)
 		providerConfig.LoadBalancers = []awsprovider.LoadBalancerReference{
 			{
