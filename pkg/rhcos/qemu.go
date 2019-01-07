b547e648c1362f403c50c6e7fa18e824ffcf4620
Colin Walters <walters@verbum.org>
2019-01-07 14:47:52 +0000

    rhcos/qemu: Readd environment var OPENSHIFT_INSTALL_LIBVIRT_IMAGE
    This is primarily targeted for local development/testing.

--- pkg/rhcos/qemu.go
+++ pkg/rhcos/qemu.go
@@ -1,10 +1,15 @@
 package rhcos
 
 import (
+	"os"
 	"fmt"
 )
 
 // QEMU fetches the URL of the latest Red Hat CoreOS release.
 func QEMU(build Build) string {
+	qcowImage, ok := os.LookupEnv("OPENSHIFT_INSTALL_LIBVIRT_IMAGE")
+	if ok {
+		return qcowImage
+	}
 	return fmt.Sprintf("%s/%s/%s/%s", build.BaseURL, build.Channel, build.Meta.OSTreeVersion, build.Meta.Images.QEMU.Path)
 }
