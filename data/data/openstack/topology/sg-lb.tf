bd9c5f345034d3b0e963479f6067880f4505680e
Ian Main <imain@redhat.com>
2019-01-08 17:50:20 +0000

    Allow ssh to service VM.
    I had to add this in order to get access to the service VM via ssh.
    Very nice for devel purposes and possibly needed to switch DNS
    or LB.

--- data/data/openstack/topology/sg-lb.tf
+++ data/data/openstack/topology/sg-lb.tf
@@ -43,6 +43,16 @@ resource "openstack_networking_secgroup_rule_v2" "api_ingress_dns_tcp" {
   security_group_id = "${openstack_networking_secgroup_v2.api.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "api_ingress_ssh_tcp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.api.id}"
+}
+
 resource "openstack_networking_secgroup_rule_v2" "api_ingress_icmp" {
   direction         = "ingress"
   ethertype         = "IPv4"
