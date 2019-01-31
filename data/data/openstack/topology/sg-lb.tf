9815dd24f4386564b4e7b8b13ef80b0ebcf72908
Tomas Sedovic <tomas@sedovic.cz>
2019-01-31 17:25:00 +0100

    openstack: open port 443 on the load balancer
    This is necessary to provide the access to the openshift-console.

--- data/data/openstack/topology/sg-lb.tf
+++ data/data/openstack/topology/sg-lb.tf
@@ -23,6 +23,16 @@ resource "openstack_networking_secgroup_rule_v2" "api_https" {
   security_group_id = "${openstack_networking_secgroup_v2.api.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "console_https" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.api.id}"
+}
+
 resource "openstack_networking_secgroup_rule_v2" "api_ingress_dns_udp" {
   direction         = "ingress"
   ethertype         = "IPv4"
