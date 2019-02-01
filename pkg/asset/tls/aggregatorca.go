9bd5b07ceee48a41655406975e7a4ce95ce2c1e8
Matt Rogers <mrogers@redhat.com>
2019-02-01 12:31:40 -0500

    pkg/asset/tls: self-sign cluster CAs

--- pkg/asset/tls/aggregatorca.go
+++ pkg/asset/tls/aggregatorca.go
@@ -5,6 +5,7 @@ import (
 	"crypto/x509/pkix"
 
 	"github.com/openshift/installer/pkg/asset"
+	"github.com/pkg/errors"
 )
 
 // AggregatorCA is the asset that generates the aggregator-ca key/cert pair.
@@ -18,16 +19,11 @@ var _ asset.Asset = (*AggregatorCA)(nil)
 // the parent CA, and install config if it depends on the install config for
 // DNS names, etc.
 func (a *AggregatorCA) Dependencies() []asset.Asset {
-	return []asset.Asset{
-		&RootCA{},
-	}
+	return []asset.Asset{}
 }
 
 // Generate generates the cert/key pair based on its dependencies.
 func (a *AggregatorCA) Generate(dependencies asset.Parents) error {
-	rootCA := &RootCA{}
-	dependencies.Get(rootCA)
-
 	cfg := &CertCfg{
 		Subject:   pkix.Name{CommonName: "aggregator", OrganizationalUnit: []string{"bootkube"}},
 		KeyUsages: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
@@ -35,7 +31,17 @@ func (a *AggregatorCA) Generate(dependencies asset.Parents) error {
 		IsCA:      true,
 	}
 
-	return a.CertKey.Generate(cfg, rootCA, "aggregator-ca", DoNotAppendParent)
+	key, crt, err := GenerateRootCertKey(cfg)
+	if err != nil {
+		return errors.Wrap(err, "failed to generate Aggregator CA")
+	}
+
+	a.KeyRaw = PrivateKeyToPem(key)
+	a.CertRaw = CertToPem(crt)
+
+	a.generateFiles("aggregator-ca")
+
+	return nil
 }
 
 // Name returns the human-friendly name of the asset.
--- pkg/asset/tls/etcdca.go
+++ pkg/asset/tls/etcdca.go
@@ -5,6 +5,7 @@ import (
 	"crypto/x509/pkix"
 
 	"github.com/openshift/installer/pkg/asset"
+	"github.com/pkg/errors"
 )
 
 // EtcdCA is the asset that generates the etcd-ca key/cert pair.
@@ -18,16 +19,11 @@ var _ asset.Asset = (*EtcdCA)(nil)
 // the parent CA, and install config if it depends on the install config for
 // DNS names, etc.
 func (a *EtcdCA) Dependencies() []asset.Asset {
-	return []asset.Asset{
-		&RootCA{},
-	}
+	return []asset.Asset{}
 }
 
 // Generate generates the cert/key pair based on its dependencies.
 func (a *EtcdCA) Generate(dependencies asset.Parents) error {
-	rootCA := &RootCA{}
-	dependencies.Get(rootCA)
-
 	cfg := &CertCfg{
 		Subject:   pkix.Name{CommonName: "etcd", OrganizationalUnit: []string{"etcd"}},
 		KeyUsages: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
@@ -35,7 +31,17 @@ func (a *EtcdCA) Generate(dependencies asset.Parents) error {
 		IsCA:      true,
 	}
 
-	return a.CertKey.Generate(cfg, rootCA, "etcd-client-ca", DoNotAppendParent)
+	key, crt, err := GenerateRootCertKey(cfg)
+	if err != nil {
+		return errors.Wrap(err, "failed to generate ETCD client CA")
+	}
+
+	a.KeyRaw = PrivateKeyToPem(key)
+	a.CertRaw = CertToPem(crt)
+
+	a.generateFiles("etcd-client-ca")
+
+	return nil
 }
 
 // Name returns the human-friendly name of the asset.
