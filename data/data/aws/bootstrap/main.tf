1b5134215cac1478cf0e488eaee0958592284547
W. Trevor King <wking@tremily.us>
2019-02-06 14:37:49 -0800

    data/aws/bootstrap: Block public ACLs, etc. for the S3 bucket
    The bucket is already private [1], but when browsing S3 in the AWS web
    console today (e.g. [2]), I noticed these buckets had public access
    settings described as:
    
      Manage public access control lists (ACLs)
      Block new public ACLs and uploading public objects (Recommended)
        False
      Remove public access granted through public ACLs (Recommended)
        False
    
      Manage public bucket policies
      Block new public bucket policies (Recommended)
        False
      Block public and cross-account access if bucket has public policies (Recommended)
        False
    
    and the overview tab [3] had Access warnings like "Objects can be
    public".  We might as well shut all of that down, by using this
    access-block resource [4].
    
    [1]: https://www.terraform.io/docs/providers/aws/r/s3_bucket.html#acl
    [2]: https://s3.console.aws.amazon.com/s3/buckets/terraform-20190206183528155600000001/?region=us-east-1&tab=permissions
    [3]: https://s3.console.aws.amazon.com/s3/home?region=us-east-1
    [4]: https://www.terraform.io/docs/providers/aws/r/s3_bucket_public_access_block.html

--- data/data/aws/bootstrap/main.tf
+++ data/data/aws/bootstrap/main.tf
@@ -8,6 +8,15 @@ resource "aws_s3_bucket" "ignition" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "ignition" {
+  bucket = "${aws_s3_bucket.ignition.id}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_object" "ignition" {
   bucket  = "${aws_s3_bucket.ignition.id}"
   key     = "bootstrap.ign"
