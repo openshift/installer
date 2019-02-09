f9ad5006f3350f245c4eebd5bcf60516bb1e9b53
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-02-08 16:03:29 -0800

    tls: create new chains of trust
    The new chains are based on requirements from the master team
    
    ```
    etcd:
    etcd-signer.crt/key - self-signed, 10 year expiry
    etcd-ca-bundle.crt - contains the etcd-signer.crtEtcd-serving-ca-bundle.crt - contains etcd-signer.crt
    etcd-client.crt/key - signed by etcd-signer.crt, 10 year expiry
    
    kubelet-csr:
    kubelet-csr-signer.crt/key - self-signed 1 day expiry
    kubelet-client-ca-bundle.crt - contains kubelet-csr-signer.crt
    Individual kubelet client certs - signed by kubelet-csr-signer.crt, 1 day expiry
    
    aggregator:
    aggregator-signer.crt/key - self-signed 1 day expiry
    aggregator-ca-bundle.crt - contains aggregator-signer.crt
    aggregator-client.crt/key - signed by aggregator-signer.crt, 1 day expiry
    
    service-ca - removed
    
    admin-kubeconfig:
    admin-kubeconfig-signer.crt/key - self-signed 10 year expiry
    Admin-kubeconfig-ca-bundle.crt - contains admin-kubeconfig-signer.crt
    admin-kubeconfig-client.crt/key - signed by admin-kubeconfig-signer.crt, 10 year expiry
    
    kube-apiserver-to-kubelet:
    kube-apiserver-to-kubelet-signer.crt/key - self-signed X year expiry
    Kube-apiserver-to-kubelet-ca-bundle.crt - contains kube-apiserver-to-kubelet-signer.crt (This goes to the kubelet somehow.  I bet it’s the MCD and painful to rotate)
    kube-apiserver-to-kubelet-client.crt/key - signed by kube-apiserver-to-kubelet-signer.crt, 1 day expiry. (Keep this subject consistent with what you sign for now.)
    
    kube-control-plane:
    kube-control-plane-signer.crt/key - self-signed 1 day expiry
    Kube-control-plane-ca-bundle.crt - contains kube-control-plane-signer.crt
    kube-control-plane-kube-controller-manager-client.crt/key - signed by kube-control-plane-signer.crt, 1 day expiry. (Keep this subject consistent with what you sign for now.)
    kube-control-plane-kube-scheduler-client.crt/key - signed by kube-control-plane-signer.crt, 1 day expiry  (Keep this subject consistent with what you sign for now.)
    
    kube-apiserver-localhost (signs for localhost, 127.0.0.1, and the ipv6 one):
    kube-apiserver-localhost-signer.crt/key - self-signed, 10 year expiry
    kube-apiserver-localhost-ca-bundle.crt - contains kube-apiserver-localhost-signer.crt.  (include this in admin.kubeconfig)
    kube-apiserver-localhost-server.crt/key - signed by kube-apiserver-localhost-signer.crt, 1 day expiry
    
    kube-apiserver-service-network (signs for kubernetes.default.svc, kubernetes.default.svc.<dns-suffix>, service IP):
    kube-apiserver-service-network-signer.crt/key - self-signed, 10 year expiry
    kube-apiserver-service-network-ca-bundle.crt - contains kube-apiserver-service-network-signer.crt.  (include this in admin.kubeconfig)
    kube-apiserver-service-network-server.crt/key - signed by kube-apiserver-service-network-signer.crt, 1 day expiry
    
    kube-apiserver-lb-host-ip:
    kube-apiserver-lb-host-ip-signer.crt/key - self-signed, 10 year expiry
    Kube-apiserver-lb-host-ip-ca-bundle.crt - contains kube-apiserver-lb-host-ip-signer.crt.  (include this in admin.kubeconfig)
    kube-apiserver-lb-host-ip-server.crt/key - signed by kube-apiserver-lb-host-ip-signer.crt, 1 day expiry
    ```

---
+++ pkg/asset/tls/kubecontrolplane.go
@@ -0,0 +1,138 @@
+package tls
+
+import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+
+	"github.com/openshift/installer/pkg/asset"
+)
+
+// KubeControlPlaneSignerCertKey is a key/cert pair that signs the kube control-plane client certs.
+type KubeControlPlaneSignerCertKey struct {
+	SelfSignedCertKey
+}
+
+var _ asset.WritableAsset = (*KubeControlPlaneSignerCertKey)(nil)
+
+// Dependencies returns the dependency of the root-ca, which is empty.
+func (c *KubeControlPlaneSignerCertKey) Dependencies() []asset.Asset {
+	return []asset.Asset{}
+}
+
+// Generate generates the root-ca key and cert pair.
+func (c *KubeControlPlaneSignerCertKey) Generate(parents asset.Parents) error {
+	cfg := &CertCfg{
+		Subject:   pkix.Name{CommonName: "kube-control-plane-signer", OrganizationalUnit: []string{"openshift"}},
+		KeyUsages: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		Validity:  ValidityOneYear,
+		IsCA:      true,
+	}
+
+	return c.SelfSignedCertKey.Generate(cfg, "kube-control-plane-signer")
+}
+
+// Name returns the human-friendly name of the asset.
+func (c *KubeControlPlaneSignerCertKey) Name() string {
+	return "Certificate (kube-control-plane-signer)"
+}
+
+// KubeControlPlaneCABundle is the asset the generates the kube-control-plane-ca-bundle,
+// which contains all the individual client CAs.
+type KubeControlPlaneCABundle struct {
+	CertBundle
+}
+
+var _ asset.Asset = (*KubeControlPlaneCABundle)(nil)
+
+// Dependencies returns the dependency of the cert bundle.
+func (a *KubeControlPlaneCABundle) Dependencies() []asset.Asset {
+	return []asset.Asset{
+		&KubeControlPlaneSignerCertKey{},
+		&KubeAPIServerLBSignerCertKey{},
+		&KubeAPIServerLocalhostSignerCertKey{},
+		&KubeAPIServerServiceNetworkSignerCertKey{},
+	}
+}
+
+// Generate generates the cert bundle based on its dependencies.
+func (a *KubeControlPlaneCABundle) Generate(deps asset.Parents) error {
+	var certs []CertInterface
+	for _, asset := range a.Dependencies() {
+		deps.Get(asset)
+		certs = append(certs, asset.(CertInterface))
+	}
+	return a.CertBundle.Generate("kube-control-plane-ca-bundle", certs...)
+}
+
+// Name returns the human-friendly name of the asset.
+func (a *KubeControlPlaneCABundle) Name() string {
+	return "Certificate (kube-control-plane-ca-bundle)"
+}
+
+// KubeControlPlaneKubeControllerManagerClientCertKey is the asset that generates the kube-controller-manger client key/cert pair.
+type KubeControlPlaneKubeControllerManagerClientCertKey struct {
+	SignedCertKey
+}
+
+var _ asset.Asset = (*KubeControlPlaneKubeControllerManagerClientCertKey)(nil)
+
+// Dependencies returns the dependency of the the cert/key pair
+func (a *KubeControlPlaneKubeControllerManagerClientCertKey) Dependencies() []asset.Asset {
+	return []asset.Asset{
+		&KubeControlPlaneSignerCertKey{},
+	}
+}
+
+// Generate generates the cert/key pair based on its dependencies.
+func (a *KubeControlPlaneKubeControllerManagerClientCertKey) Generate(dependencies asset.Parents) error {
+	ca := &KubeControlPlaneSignerCertKey{}
+	dependencies.Get(ca)
+
+	cfg := &CertCfg{
+		Subject:      pkix.Name{CommonName: "system:admin", Organization: []string{"system:masters"}},
+		KeyUsages:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		Validity:     ValidityOneYear,
+	}
+
+	return a.SignedCertKey.Generate(cfg, ca, "kube-control-plane-kube-controller-manager-client", DoNotAppendParent)
+}
+
+// Name returns the human-friendly name of the asset.
+func (a *KubeControlPlaneKubeControllerManagerClientCertKey) Name() string {
+	return "Certificate (kube-control-plane-kube-controller-manager-client)"
+}
+
+// KubeControlPlaneKubeSchedulerClientCertKey is the asset that generates the kube-scheduler client key/cert pair.
+type KubeControlPlaneKubeSchedulerClientCertKey struct {
+	SignedCertKey
+}
+
+var _ asset.Asset = (*KubeControlPlaneKubeSchedulerClientCertKey)(nil)
+
+// Dependencies returns the dependency of the the cert/key pair
+func (a *KubeControlPlaneKubeSchedulerClientCertKey) Dependencies() []asset.Asset {
+	return []asset.Asset{
+		&KubeControlPlaneSignerCertKey{},
+	}
+}
+
+// Generate generates the cert/key pair based on its dependencies.
+func (a *KubeControlPlaneKubeSchedulerClientCertKey) Generate(dependencies asset.Parents) error {
+	ca := &KubeControlPlaneSignerCertKey{}
+	dependencies.Get(ca)
+
+	cfg := &CertCfg{
+		Subject:      pkix.Name{CommonName: "system:admin", Organization: []string{"system:masters"}},
+		KeyUsages:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsages: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		Validity:     ValidityOneYear,
+	}
+
+	return a.SignedCertKey.Generate(cfg, ca, "kube-control-plane-kube-scheduler-client", DoNotAppendParent)
+}
+
+// Name returns the human-friendly name of the asset.
+func (a *KubeControlPlaneKubeSchedulerClientCertKey) Name() string {
+	return "Certificate (kube-control-plane-kube-scheduler-client)"
+}
