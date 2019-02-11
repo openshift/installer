c1e5e3209207e1c0bb02d8951e5352172590d49d
Flavio Percoco <flaper87@gmail.com>
2019-02-11 14:10:00 +0100

    openstack: Use a service subnet for service-vm
    Move the service VM under its own subnet. This helps creating
    deployment-only resources that can be easily cleaned after the fact.
    
    For example, we can set the nodes' subnet dns nameservers to point to
    the service-vm and then set it to something else after the deployment
    succeeds in scenarios where users bring their own DNS.

--- data/data/openstack/masters/main.tf
+++ data/data/openstack/masters/main.tf
@@ -11,29 +11,6 @@ data "ignition_config" "master_ignition_config" {
   append {
     source = "data:text/plain;charset=utf-8;base64,${base64encode(var.user_data_ign)}"
   }
-
-  files = [
-    "${data.ignition_file.master_ifcfg.id}",
-  ]
-}
-
-data "ignition_file" "master_ifcfg" {
-  filesystem = "root"
-  mode       = "420"                                       // 0644
-  path       = "/etc/sysconfig/network-scripts/ifcfg-eth0"
-
-  content {
-    content = <<EOF
-DEVICE="eth0"
-BOOTPROTO="dhcp"
-ONBOOT="yes"
-TYPE="Ethernet"
-PERSISTENT_DHCLIENT="yes"
-DNS1="${var.service_vm_fixed_ip}"
-PEERDNS="no"
-NM_CONTROLLED="yes"
-EOF
-  }
 }
 
 resource "openstack_compute_instance_v2" "master_conf" {
--- data/data/openstack/topology/private-network.tf
+++ data/data/openstack/topology/private-network.tf
@@ -1,15 +1,29 @@
+locals {
+  nodes_cidr_block   = "${cidrsubnet(var.cidr_block, 1, 0)}"
+  service_cidr_block = "${cidrsubnet(var.cidr_block, 1, 1)}"
+}
+
 resource "openstack_networking_network_v2" "openshift-private" {
   name           = "openshift"
   admin_state_up = "true"
   tags           = ["openshiftClusterID=${var.cluster_id}"]
 }
 
-resource "openstack_networking_subnet_v2" "nodes" {
-  name       = "nodes"
-  cidr       = "${var.cidr_block}"
+resource "openstack_networking_subnet_v2" "service" {
+  name       = "service"
+  cidr       = "${local.service_cidr_block}"
   ip_version = 4
   network_id = "${openstack_networking_network_v2.openshift-private.id}"
   tags       = ["openshiftClusterID=${var.cluster_id}"]
+}
+
+resource "openstack_networking_subnet_v2" "nodes" {
+  name            = "nodes"
+  cidr            = "${local.nodes_cidr_block}"
+  ip_version      = 4
+  network_id      = "${openstack_networking_network_v2.openshift-private.id}"
+  tags            = ["openshiftClusterID=${var.cluster_id}"]
+  dns_nameservers = ["${openstack_networking_port_v2.lb_port.all_fixed_ips[0]}"]
 }
 
 resource "openstack_networking_port_v2" "masters" {
@@ -57,7 +71,7 @@ resource "openstack_networking_port_v2" "lb_port" {
   tags               = ["openshiftClusterID=${var.cluster_id}"]
 
   fixed_ip {
-    "subnet_id" = "${openstack_networking_subnet_v2.nodes.id}"
+    "subnet_id" = "${openstack_networking_subnet_v2.service.id}"
   }
 }
 
@@ -76,6 +90,11 @@ resource "openstack_networking_router_v2" "openshift-external-router" {
   admin_state_up      = true
   external_network_id = "${data.openstack_networking_network_v2.external_network.id}"
   tags                = ["openshiftClusterID=${var.cluster_id}"]
+}
+
+resource "openstack_networking_router_interface_v2" "service_router_interface" {
+  router_id = "${openstack_networking_router_v2.openshift-external-router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.service.id}"
 }
 
 resource "openstack_networking_router_interface_v2" "nodes_router_interface" {
