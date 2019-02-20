a37a4eb35d7d18e44e390ec93a8043f72ac8c537
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-02-19 16:46:45 -0800

    asset/installconfig: add humanID to clusterID asset
    UUID is a gloablly unique identifier that is required to make sure that each cluster pinging home is unique.
    
    For other use cases like tagging cloud resources, HumanID that has sufficient uniqueness to allow multiple clusters in user's cloud account
    without collision is enough.
    
    The length constraints are inspired from [1]
    
    [1]: https://github.com/openshift/installer/pull/1277/commits/954c0624ebf5b63f232fd2734bbb0f0a777ea563

--- pkg/asset/installconfig/clusterid.go
+++ pkg/asset/installconfig/clusterid.go
@@ -1,30 +1,70 @@
 package installconfig
 
 import (
+	"fmt"
+	"regexp"
+
 	"github.com/pborman/uuid"
+	utilrand "k8s.io/apimachinery/pkg/util/rand"
 
 	"github.com/openshift/installer/pkg/asset"
 )
 
+const (
+	// resource using InfraID usually have suffixes like `[-/_][a-z]{3,4}` eg. `_int`, `-ext` or `-ctlp`
+	maxNameLen = 32 - 5
+	randomLen  = 5
+	maxBaseLen = maxNameLen - randomLen - 1
+)
+
 // ClusterID is the unique ID of the cluster, immutable during the cluster's life
 type ClusterID struct {
-	ClusterID string
+	// UUID is a globally unique identifier.
+	UUID string
+
+	// InfraID is an identifier for the cluster that is more human friendly.
+	// This does not have
+	InfraID string
 }
 
 var _ asset.Asset = (*ClusterID)(nil)
 
-// Dependencies returns no dependencies.
+// Dependencies returns install-config.
 func (a *ClusterID) Dependencies() []asset.Asset {
-	return []asset.Asset{}
+	return []asset.Asset{
+		&InstallConfig{},
+	}
 }
 
-// Generate generates a new UUID
-func (a *ClusterID) Generate(asset.Parents) error {
-	a.ClusterID = uuid.New()
+// Generate generates a new ClusterID
+func (a *ClusterID) Generate(dep asset.Parents) error {
+	ica := &InstallConfig{}
+	dep.Get(ica)
+
+	// add random chars to the end to randomize
+	a.InfraID = generateInfraID(ica.Config.ObjectMeta.Name)
+	a.UUID = uuid.New()
 	return nil
 }
 
 // Name returns the human-friendly name of the asset.
 func (a *ClusterID) Name() string {
 	return "Cluster ID"
+}
+
+// generateInfraID take base and returns a ID that
+// - is of length maxNameLen
+// - only contains `alphanum` or `-`
+func generateInfraID(base string) string {
+	// truncate to maxBaseLen
+	if len(base) > maxBaseLen {
+		base = base[:maxBaseLen]
+	}
+
+	// replace all characters that are not `alphanum` or `-` with `-`
+	re := regexp.MustCompile("[^A-Za-z0-9-]")
+	base = re.ReplaceAllString(base, "-")
+
+	// add random chars to the end to randomize
+	return fmt.Sprintf("%s-%s", base, utilrand.String(randomLen))
 }
---
+++ pkg/asset/installconfig/clusterid_test.go
@@ -0,0 +1,36 @@
+package installconfig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_generateInfraID(t *testing.T) {
+	tests := []struct {
+		input string
+
+		expLen     int
+		expNonRand string
+	}{{
+		input:      "qwertyuiop",
+		expLen:     10 + randomLen + 1,
+		expNonRand: "qwertyuiop",
+	}, {
+		input:      "qwertyuiopasdfghjklzxcvbnm",
+		expLen:     maxBaseLen + randomLen + 1,
+		expNonRand: "qwertyuiopasdfghjklzx",
+	}, {
+		input:      "qwe.rty.@iop!",
+		expLen:     13 + randomLen + 1,
+		expNonRand: "qwe-rty--iop-",
+	}}
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			got := generateInfraID(test.input)
+			t.Log("InfraID", got)
+			assert.Equal(t, test.expLen, len(got))
+			assert.Equal(t, test.expNonRand, got[:len(got)-randomLen-1])
+		})
+	}
+}
