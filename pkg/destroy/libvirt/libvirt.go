openshift/installer

commit d137c801ca419b00418348fe652425bae4b71ae3

machines: use cluster_id to ref Libvirt resources
update the `providerSpec` to use `cluster_id` when referencing Libvirt resources like,
base_image and network

update the `metadata.json` to use `HumanID` for its `.clusterID` field. The libvirt destroy now uses this
fields to delete resources.

pkg/destroy/libvirt/libvirt.go
@@ -17,15 +17,15 @@ import (
 // returns true, when the name should be handled.
 type filterFunc func(name string) bool
 
-// ClusterNamePrefixFilter returns true for names
-// that are prefixed with clustername.
-// `clustername` cannot be empty.
-var ClusterNamePrefixFilter = func(clustername string) filterFunc {
-	if clustername == "" {
-		panic("clustername cannot be empty")
+// ClusterIDPrefixFilter returns true for names
+// that are prefixed with clusterid.
+// `clusterid` cannot be empty.
+var ClusterIDPrefixFilter = func(clusterid string) filterFunc {
+	if clusterid == "" {
+		panic("clusterid cannot be empty")
 	}
 	return func(name string) bool {
-		return strings.HasPrefix(name, clustername)
+		return strings.HasPrefix(name, clusterid)
 	}
 }
 
@@ -215,7 +215,7 @@ func deleteNetwork(conn *libvirt.Connect, filter filterFunc, logger logrus.Field
 func New(logger logrus.FieldLogger, metadata *types.ClusterMetadata) (destroy.Destroyer, error) {
 	return &ClusterUninstaller{
 		LibvirtURI: metadata.ClusterPlatformMetadata.Libvirt.URI,
-		Filter:     ClusterNamePrefixFilter(metadata.ClusterName),
+		Filter:     ClusterIDPrefixFilter(metadata.ClusterID),
 		Logger:     logger,
 	}, nil
 }

pkg/asset/cluster/metadata.go
@@ -51,7 +51,7 @@ func (m *Metadata) Generate(parents asset.Parents) (err error) {
 
 	metadata := &types.ClusterMetadata{
 		ClusterName: installConfig.Config.ObjectMeta.Name,
-		ClusterID:   clusterID.UUID,
+		ClusterID:   clusterID.HumanID,
 	}
 
 	switch {

pkg/asset/machines/libvirt/machines.go
@@ -21,14 +21,13 @@ func Machines(clusterID string, config *types.InstallConfig, pool *types.Machine
 	if poolPlatform := pool.Platform.Name(); poolPlatform != libvirt.Name {
 		return nil, fmt.Errorf("non-Libvirt machine-pool: %q", poolPlatform)
 	}
-	clustername := config.ObjectMeta.Name
 	platform := config.Platform.Libvirt
 
 	total := int64(1)
 	if pool.Replicas != nil {
 		total = *pool.Replicas
 	}
-	provider := provider(clustername, config.Networking.MachineCIDR.String(), platform, userDataSecret)
+	provider := provider(clusterID, config.Networking.MachineCIDR.String(), platform, userDataSecret)
 	var machines []machineapi.Machine
 	for idx := int64(0); idx < total; idx++ {
 		machine := machineapi.Machine{
@@ -38,9 +37,9 @@ func Machines(clusterID string, config *types.InstallConfig, pool *types.Machine
 			},
 			ObjectMeta: metav1.ObjectMeta{
 				Namespace: "openshift-machine-api",
-				Name:      fmt.Sprintf("%s-%s-%d", clustername, pool.Name, idx),
+				Name:      fmt.Sprintf("%s-%s-%d", clusterID, pool.Name, idx),
 				Labels: map[string]string{
-					"sigs.k8s.io/cluster-api-cluster":      clustername,
+					"sigs.k8s.io/cluster-api-cluster":      clusterID,
 					"sigs.k8s.io/cluster-api-machine-role": role,
 					"sigs.k8s.io/cluster-api-machine-type": role,
 				},
@@ -58,7 +57,7 @@ func Machines(clusterID string, config *types.InstallConfig, pool *types.Machine
 	return machines, nil
 }
 
-func provider(clusterName string, networkInterfaceAddress string, platform *libvirt.Platform, userDataSecret string) *libvirtprovider.LibvirtMachineProviderConfig {
+func provider(clusterID string, networkInterfaceAddress string, platform *libvirt.Platform, userDataSecret string) *libvirtprovider.LibvirtMachineProviderConfig {
 	return &libvirtprovider.LibvirtMachineProviderConfig{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "libvirtproviderconfig.k8s.io/v1alpha1",
@@ -71,9 +70,9 @@ func provider(clusterName string, networkInterfaceAddress string, platform *libv
 		},
 		Volume: &libvirtprovider.Volume{
 			PoolName:     "default",
-			BaseVolumeID: fmt.Sprintf("/var/lib/libvirt/images/%s-base", clusterName),
+			BaseVolumeID: fmt.Sprintf("/var/lib/libvirt/images/%s-base", clusterID),
 		},
-		NetworkInterfaceName:    clusterName,
+		NetworkInterfaceName:    clusterID,
 		NetworkInterfaceAddress: networkInterfaceAddress,
 		Autostart:               false,
 		URI:                     platform.URI,

pkg/asset/machines/libvirt/machinesets.go
@@ -22,7 +22,6 @@ func MachineSets(clusterID string, config *types.InstallConfig, pool *types.Mach
 	if poolPlatform := pool.Platform.Name(); poolPlatform != "" && poolPlatform != libvirt.Name {
 		return nil, fmt.Errorf("non-Libvirt machine-pool: %q", poolPlatform)
 	}
-	clustername := config.ObjectMeta.Name
 	platform := config.Platform.Libvirt
 	// FIXME: libvirt actuator does not support any options from machinepool.
 	// mpool := pool.Platform.Libvirt
@@ -32,8 +31,8 @@ func MachineSets(clusterID string, config *types.InstallConfig, pool *types.Mach
 		total = *pool.Replicas
 	}
 
-	provider := provider(clustername, config.Networking.MachineCIDR.String(), platform, userDataSecret)
-	name := fmt.Sprintf("%s-%s-%d", clustername, pool.Name, 0)
+	provider := provider(clusterID, config.Networking.MachineCIDR.String(), platform, userDataSecret)
+	name := fmt.Sprintf("%s-%s-%d", clusterID, pool.Name, 0)
 	mset := &machineapi.MachineSet{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "machine.openshift.io/v1beta1",
@@ -43,7 +42,7 @@ func MachineSets(clusterID string, config *types.InstallConfig, pool *types.Mach
 			Namespace: "openshift-machine-api",
 			Name:      name,
 			Labels: map[string]string{
-				"sigs.k8s.io/cluster-api-cluster":      clustername,
+				"sigs.k8s.io/cluster-api-cluster":      clusterID,
 				"sigs.k8s.io/cluster-api-machine-role": role,
 				"sigs.k8s.io/cluster-api-machine-type": role,
 			},
@@ -53,14 +52,14 @@ func MachineSets(clusterID string, config *types.InstallConfig, pool *types.Mach
 			Selector: metav1.LabelSelector{
 				MatchLabels: map[string]string{
 					"sigs.k8s.io/cluster-api-machineset": name,
-					"sigs.k8s.io/cluster-api-cluster":    clustername,
+					"sigs.k8s.io/cluster-api-cluster":    clusterID,
 				},
 			},
 			Template: machineapi.MachineTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
 						"sigs.k8s.io/cluster-api-machineset":   name,
-						"sigs.k8s.io/cluster-api-cluster":      clustername,
+						"sigs.k8s.io/cluster-api-cluster":      clusterID,
 						"sigs.k8s.io/cluster-api-machine-role": role,
 						"sigs.k8s.io/cluster-api-machine-type": role,
 					},

pkg/asset/machines/master.go
@@ -109,7 +109,7 @@ func (m *Master) Generate(dependencies asset.Parents) error {
 		mpool.Set(ic.Platform.Libvirt.DefaultMachinePlatform)
 		mpool.Set(pool.Platform.Libvirt)
 		pool.Platform.Libvirt = &mpool
-		machines, err = libvirt.Machines(clusterID.UUID, ic, pool, "master", "master-user-data")
+		machines, err = libvirt.Machines(clusterID.InfraID, ic, pool, "master", "master-user-data")
 		if err != nil {
 			return errors.Wrap(err, "failed to create master machine objects")
 		}

pkg/asset/machines/worker.go
@@ -122,7 +122,7 @@ func (w *Worker) Generate(dependencies asset.Parents) error {
 			mpool.Set(ic.Platform.Libvirt.DefaultMachinePlatform)
 			mpool.Set(pool.Platform.Libvirt)
 			pool.Platform.Libvirt = &mpool
-			sets, err := libvirt.MachineSets(clusterID.UUID, ic, &pool, "worker", "worker-user-data")
+			sets, err := libvirt.MachineSets(clusterID.InfraID, ic, &pool, "worker", "worker-user-data")
 			if err != nil {
 				return errors.Wrap(err, "failed to create worker machine objects")
 			}

commit 6d98889f557f06b3a12a6d3bf19e5b87dcf04d3f

libvirt: Pass correct InfraID prefix to destroy
Otherwise we don't clean up anything.

Fixes: d137c801ca419b00418348fe652425bae4b71ae3

pkg/destroy/libvirt/libvirt.go
@@ -215,7 +215,7 @@ func deleteNetwork(conn *libvirt.Connect, filter filterFunc, logger logrus.Field
 func New(logger logrus.FieldLogger, metadata *types.ClusterMetadata) (destroy.Destroyer, error) {
 	return &ClusterUninstaller{
 		LibvirtURI: metadata.ClusterPlatformMetadata.Libvirt.URI,
-		Filter:     ClusterIDPrefixFilter(metadata.ClusterID),
+		Filter:     ClusterIDPrefixFilter(metadata.InfraID),
 		Logger:     logger,
 	}, nil
 }