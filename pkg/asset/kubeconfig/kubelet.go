b31f5bf730b1840311e8977498ab271298567a47
David Eads <deads@redhat.com>
2019-02-28 09:09:21 -0500

    tls: remove deprecate kubelet.kubeconfig
    The kube-ca was a signer that allowed for trust domain confusion,
    this removes the bootstrap kubelet kubeconfig from that chain and uses
    a dedicated chain that can be discarded, shortened, or rotated by
    an independent owner.

--- pkg/asset/kubeconfig/kubelet.go
+++ pkg/asset/kubeconfig/kubelet.go
@@ -9,12 +9,10 @@ import (
 )
 
 var (
-	kubeconfigKubeletPath       = filepath.Join("auth", "kubeconfig-kubelet")
-	kubeconfigKubeletClientPath = filepath.Join("auth", "kubeconfig-kubelet-client")
+	kubeconfigKubeletPath = filepath.Join("auth", "kubeconfig-kubelet")
 )
 
 // Kubelet is the asset for the kubelet kubeconfig.
-// [DEPRECATED]
 type Kubelet struct {
 	kubeconfig
 }
@@ -24,22 +22,22 @@ var _ asset.WritableAsset = (*Kubelet)(nil)
 // Dependencies returns the dependency of the kubeconfig.
 func (k *Kubelet) Dependencies() []asset.Asset {
 	return []asset.Asset{
-		&tls.KubeCA{},
-		&tls.KubeletCertKey{},
+		&tls.KubeAPIServerCompleteCABundle{},
+		&tls.KubeletClientCertKey{},
 		&installconfig.InstallConfig{},
 	}
 }
 
 // Generate generates the kubeconfig.
 func (k *Kubelet) Generate(parents asset.Parents) error {
-	kubeCA := &tls.KubeCA{}
-	kubeletCertKey := &tls.KubeletCertKey{}
+	ca := &tls.KubeAPIServerCompleteCABundle{}
+	clientcertkey := &tls.KubeletClientCertKey{}
 	installConfig := &installconfig.InstallConfig{}
-	parents.Get(kubeCA, kubeletCertKey, installConfig)
+	parents.Get(ca, clientcertkey, installConfig)
 
 	return k.kubeconfig.generate(
-		kubeCA,
-		kubeletCertKey,
+		ca,
+		clientcertkey,
 		installConfig.Config,
 		"kubelet",
 		kubeconfigKubeletPath,
@@ -53,47 +51,5 @@ func (k *Kubelet) Name() string {
 
 // Load is a no-op because kubelet kubeconfig is not written to disk.
 func (k *Kubelet) Load(asset.FileFetcher) (bool, error) {
-	return false, nil
-}
-
-// KubeletClient is the asset for the kubelet kubeconfig.
-type KubeletClient struct {
-	kubeconfig
-}
-
-var _ asset.WritableAsset = (*KubeletClient)(nil)
-
-// Dependencies returns the dependency of the kubeconfig.
-func (k *KubeletClient) Dependencies() []asset.Asset {
-	return []asset.Asset{
-		&tls.KubeAPIServerCompleteCABundle{},
-		&tls.KubeletClientCertKey{},
-		&installconfig.InstallConfig{},
-	}
-}
-
-// Generate generates the kubeconfig.
-func (k *KubeletClient) Generate(parents asset.Parents) error {
-	ca := &tls.KubeAPIServerCompleteCABundle{}
-	clientcertkey := &tls.KubeletClientCertKey{}
-	installConfig := &installconfig.InstallConfig{}
-	parents.Get(ca, clientcertkey, installConfig)
-
-	return k.kubeconfig.generate(
-		ca,
-		clientcertkey,
-		installConfig.Config,
-		"kubelet",
-		kubeconfigKubeletClientPath,
-	)
-}
-
-// Name returns the human-friendly name of the asset.
-func (k *KubeletClient) Name() string {
-	return "Kubeconfig Kubelet Client"
-}
-
-// Load is a no-op because kubelet kubeconfig is not written to disk.
-func (k *KubeletClient) Load(asset.FileFetcher) (bool, error) {
 	return false, nil
 }
