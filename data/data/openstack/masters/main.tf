3bdf05a1f422fa80b28f4ca063e2f04a2ce07770
Flavio Percoco <flaper87@gmail.com>
2019-03-06 16:39:02 +0100

    openstack: Set hostnames for nodes
    Now that we're not using neutron DNS anymore, we must set the hostnames
    on the nodes so that it's possible to communicate between VMs

--- data/data/openstack/masters/main.tf
+++ data/data/openstack/masters/main.tf
@@ -7,10 +7,29 @@ data "openstack_compute_flavor_v2" "masters_flavor" {
   name = "${var.flavor_name}"
 }
 
+data "ignition_file" "hostname" {
+  count      = "${var.instance_count}"
+  filesystem = "root"
+  mode       = "420"                   // 0644
+  path       = "/etc/hostname"
+
+  content {
+    content = <<EOF
+${var.cluster_id}-master-${count.index}.${var.cluster_domain}
+EOF
+  }
+}
+
 data "ignition_config" "master_ignition_config" {
+  count = "${var.instance_count}"
+
   append {
     source = "data:text/plain;charset=utf-8;base64,${base64encode(var.user_data_ign)}"
   }
+
+  files = [
+    "${element(data.ignition_file.hostname.*.id, count.index)}",
+  ]
 }
 
 resource "openstack_compute_instance_v2" "master_conf" {
@@ -20,7 +39,7 @@ resource "openstack_compute_instance_v2" "master_conf" {
   flavor_id       = "${data.openstack_compute_flavor_v2.masters_flavor.id}"
   image_id        = "${data.openstack_images_image_v2.masters_img.id}"
   security_groups = ["${var.master_sg_ids}"]
-  user_data       = "${data.ignition_config.master_ignition_config.rendered}"
+  user_data       = "${element(data.ignition_config.master_ignition_config.*.rendered, count.index)}"
 
   network = {
     port = "${var.master_port_ids[count.index]}"
