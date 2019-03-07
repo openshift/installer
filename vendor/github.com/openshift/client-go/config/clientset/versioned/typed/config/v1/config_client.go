161e022ede92b99b4e4942b21ce5f9b1ffb0f8c0
Dan Winship <danw@redhat.com>
2019-03-07 09:11:47 -0500

    vendor: update openshift/api
    Pull in updated config/v1/types_network.go to get the Status fields
    marked as omitempty so that when the installer writes out the network
    config, it outputs:
    
      status: {}
    
    rather than:
    
      status:
        clusterNetwork: null
        ...
    
    which fails CRD validation

--- vendor/github.com/openshift/client-go/config/clientset/versioned/typed/config/v1/config_client.go
+++ vendor/github.com/openshift/client-go/config/clientset/versioned/typed/config/v1/config_client.go
@@ -17,7 +17,7 @@ type ConfigV1Interface interface {
 	ClusterOperatorsGetter
 	ClusterVersionsGetter
 	ConsolesGetter
-	DNSsGetter
+	DNSesGetter
 	FeaturesGetter
 	ImagesGetter
 	InfrastructuresGetter
@@ -26,8 +26,7 @@ type ConfigV1Interface interface {
 	OAuthsGetter
 	ProjectsGetter
 	ProxiesGetter
-	RegistriesGetter
-	SchedulingsGetter
+	SchedulersGetter
 }
 
 // ConfigV1Client is used to interact with features provided by the config.openshift.io group.
@@ -59,8 +58,8 @@ func (c *ConfigV1Client) Consoles() ConsoleInterface {
 	return newConsoles(c)
 }
 
-func (c *ConfigV1Client) DNSs() DNSInterface {
-	return newDNSs(c)
+func (c *ConfigV1Client) DNSes() DNSInterface {
+	return newDNSes(c)
 }
 
 func (c *ConfigV1Client) Features() FeaturesInterface {
@@ -95,12 +94,8 @@ func (c *ConfigV1Client) Proxies() ProxyInterface {
 	return newProxies(c)
 }
 
-func (c *ConfigV1Client) Registries() RegistryInterface {
-	return newRegistries(c)
-}
-
-func (c *ConfigV1Client) Schedulings() SchedulingInterface {
-	return newSchedulings(c)
+func (c *ConfigV1Client) Schedulers() SchedulerInterface {
+	return newSchedulers(c)
 }
 
 // NewForConfig creates a new ConfigV1Client for the given config.
