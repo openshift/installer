3b0b3a2abf3289172a80045b5addd8a1bcb864de
Davis Phillips <davis.phillips@gmail.com>
2019-03-08 17:46:36 -0600

    parameterize cpu and memory count and set applicable defaults

--- upi/vsphere/bootstrap/main.tf
+++ upi/vsphere/bootstrap/main.tf
@@ -1,56 +1,66 @@
-
 data "vsphere_datacenter" "dc" {
   name = "${var.datacenter}"
 }
+
 data "vsphere_compute_cluster" "compute_cluster" {
   name          = "${var.cluster}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
 data "vsphere_datastore" "datastore" {
   name          = "${var.datastore}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
 data "vsphere_network" "network" {
   name          = "${var.vm_private_network}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
 data "vsphere_virtual_machine" "template" {
   name          = "${var.vm_template}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
 resource "vsphere_resource_pool" "resource_pool" {
   name                    = "${var.resource_pool}"
   parent_resource_pool_id = "${data.vsphere_compute_cluster.compute_cluster.resource_pool_id}"
 }
+
 resource "vsphere_virtual_machine" "vm" {
   name             = "bootstrap-0"
   resource_pool_id = "${vsphere_resource_pool.resource_pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
-  num_cpus = 4
-  memory   = 8192
-  guest_id = "rhel7_64Guest"
+  num_cpus         = "${var.num_cpus}"
+  memory           = "${var.memory}"
+  guest_id         = "rhel7_64Guest"
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
+
   disk {
-   label = "disk0"
-   unit_number = 0
-   size             = 40
-   thin_provisioned = false
+    label            = "disk0"
+    unit_number      = 0
+    size             = 40
+    thin_provisioned = false
   }
+
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
+
     customize {
       linux_options {
         host_name = "ocp${count.index + 1}"
         domain    = "vmware.devcluster.openshift.com"
       }
-        network_interface {
-          ipv4_address = "139.178.89.${130 + count.index}"
-          ipv4_netmask = 25
+
+      network_interface {
+        ipv4_address = "139.178.89.${130 + count.index}"
+        ipv4_netmask = 25
       }
-        ipv4_gateway = "139.178.89.129"
+
+      ipv4_gateway = "139.178.89.129"
     }
   }
 }
--- upi/vsphere/main.tf
+++ upi/vsphere/main.tf
@@ -1,27 +1,28 @@
 provider "vsphere" {
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
+  user                 = "${var.vsphere_user}"
+  password             = "${var.vsphere_password}"
+  vsphere_server       = "${var.vsphere_server}"
   allow_unverified_ssl = true
 }
 
 module "haproxy" {
   source = "./haproxy"
-  
-  cluster		   = "${var.cluster}"
-  datacenter		   = "${var.datacenter}"
-  datastore		   = "${var.datastore}"
-  public_ipv4		   = "${var.public_ipv4}"
-  public_ipv4_gw	   = "${var.public_ipv4_gw}"
-  public_netmask	   = "${var.public_netmask}"
-  private_ipv4		   = "${var.private_ipv4}"
-  private_ipv4_gw	   = "${var.private_ipv4_gw}"
-  private_netmask	   = "${var.private_netmask}"
-  resource_pool		   = "${var.resource_pool}"
-  vm_network		   = "${var.vm_network}"
-  vm_private_network	   = "${var.vm_private_network}"
-  vm_template	   	   = "${var.vm_template}"
+
+  cluster            = "${var.cluster}"
+  datacenter         = "${var.datacenter}"
+  datastore          = "${var.datastore}"
+  public_ipv4        = "${var.public_ipv4}"
+  public_ipv4_gw     = "${var.public_ipv4_gw}"
+  public_netmask     = "${var.public_netmask}"
+  private_ipv4       = "${var.private_ipv4}"
+  private_ipv4_gw    = "${var.private_ipv4_gw}"
+  private_netmask    = "${var.private_netmask}"
+  resource_pool      = "${var.resource_pool}"
+  vm_network         = "${var.vm_network}"
+  vm_private_network = "${var.vm_private_network}"
+  vm_template        = "${var.vm_template}"
 }
+
 /*
 module "bootstrap" {
   source = "./bootstrap"
@@ -36,3 +37,4 @@ module "nodes" {
 
 }
 */
+
