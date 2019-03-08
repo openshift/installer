9e6e6585feee6623388bdf629540f843f4e3d608
Davis Phillips <davis.phillips@gmail.com>
2019-03-08 13:56:49 -0600

    fmt tf

--- upi/vsphere/variables.tf
+++ upi/vsphere/variables.tf
@@ -1,93 +1,92 @@
 variable "vsphere_server" {
-  type		= "string"  
-  description	= "This is the vSphere server for the environment"
+  type        = "string"
+  description = "This is the vSphere server for the environment"
 }
 
 variable "vsphere_user" {
-  type		= "string"
-  default 	= "administrator@vcenter.local"
-  description	= "vSphere server user for the environment"
+  type        = "string"
+  default     = "administrator@vcenter.local"
+  description = "vSphere server user for the environment"
 }
 
 variable "vsphere_password" {
-  type		= "string"
-  description 	= "vSphere server password"
+  type        = "string"
+  description = "vSphere server password"
 }
 
 variable "virtual_machine_dns_servers" {
-  type    	= "list"
-  description   = "DNS servers to be added to VMs"
-  default 	= ["8.8.8.8", "4.4.4.4"]
+  type        = "list"
+  description = "DNS servers to be added to VMs"
+  default     = ["8.8.8.8", "4.4.4.4"]
 }
 
 variable "cluster" {
-  type		= "string"
-  description	= "This is the vSphere server for the environment"
+  type        = "string"
+  description = "This is the vSphere server for the environment"
 }
 
 variable "datacenter" {
-  type		= "string"
-  description	= "This is the vSphere server for the environment"
+  type        = "string"
+  description = "This is the vSphere server for the environment"
 }
 
 variable "datastore" {
-  type		= "string"
-  description	= "This is the vSphere server for the environment"
+  type        = "string"
+  description = "This is the vSphere server for the environment"
 }
 
-variable "public_ipv4" {       
-  type          = "string"     
-  description   = "This is the publicly accessibly endpoint."
+variable "public_ipv4" {
+  type        = "string"
+  description = "This is the publicly accessibly endpoint."
 }
 
 variable "public_ipv4_gw" {
-  type          = "string"
-  description   = "This is the public network gateway."
+  type        = "string"
+  description = "This is the public network gateway."
 }
 
 variable "public_netmask" {
-  type          = "string"
-  description   = "This is the public network netmask."
+  type        = "string"
+  description = "This is the public network netmask."
 }
 
-variable "private_ipv4" {       
-  type          = "string"     
-  description   = "This is the publicly accessibly endpoint."
+variable "private_ipv4" {
+  type        = "string"
+  description = "This is the publicly accessibly endpoint."
 }
 
 variable "private_ipv4_gw" {
-  type          = "string"
-  description   = "This is the public network gateway."
+  type        = "string"
+  description = "This is the public network gateway."
 }
 
 variable "private_netmask" {
-  type          = "string"
-  description   = "This is the public network netmask."
+  type        = "string"
+  description = "This is the public network netmask."
 }
 
-
 variable "resource_pool" {
-  type		= "string"
-  description 	= "The resource pool for provisioning VMs."
+  type        = "string"
+  description = "The resource pool for provisioning VMs."
 }
 
 variable "vm_network" {
-  type		= "string"
-  description	= "This is the publicly accessible network for cluster ingress and access."
-  default 	= "VM Network" 
+  type        = "string"
+  description = "This is the publicly accessible network for cluster ingress and access."
+  default     = "VM Network"
 }
 
 variable "vm_private_network" {
-  type		= "string"
-  description	= "This is the private network for cluster communication." 
+  type        = "string"
+  description = "This is the private network for cluster communication."
 }
 
 variable "vm_template" {
-  type		= "string"
-  description	= "This is the RHEL template for environment use."
+  type        = "string"
+  description = "This is the RHEL template for environment use."
 }
 
 variable "rhocs_template" {
-  type		= "string"
-  description	= "This is rhe CoreOS template for environment use."
+  type        = "string"
+  description = "This is rhe CoreOS template for environment use."
 }
