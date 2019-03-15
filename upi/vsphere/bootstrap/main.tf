fa713b41f5b68b94e54e1aeda1bc66b147d13b3b
Davis Phillips <davis.phillips@gmail.com>
2019-03-15 15:34:55 -0500

    mend

--- upi/vsphere/bootstrap/main.tf
+++ upi/vsphere/bootstrap/main.tf
@@ -47,58 +47,7 @@ resource "vsphere_virtual_machine" "vm" {
 
   vapp {
     properties {
-      "guestinfo.coreos.config.data" = <<EOF
-{
-  "ignition": {
-    "config": {
-    },
-    "security": {
-      "tls": {
-      }
-    },
-    "timeouts": {
-    },
-    "version": "2.2.0"
-  },
-  "networkd": {
-  },
-  "passwd": {
-    "users": [
-      {
-        "groups": [
-          "sudo"
-        ],
-        "name": "core",
-        "passwordHash": "$1$sRlwnEsn$T1v1ubQUkyFe2kUzZLMAU.",
-        "sshAuthorizedKeys": [
-          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuY6lBsj95cYTVmzqW2Xci37BJta+ZtNHIee8bFCDskKx/qiH/kbceQGDPpAjREBQBpabhxwd8eUktqdiUI1aQIs7I0Om8XMNUd00Phsz69i8PDzVQyGcLzdn4UPOpS89nFmyto0NJ1V5o4RoR3A7ENbzN7li34g5+zGSBXcVdHFFUErCfOnEtgXw5kltU1byv3+GGKAb+f0CL88BRowp35/NH9sRkP8fzWPS0hl+ofiay5H7xDDd6/nqx6OCd0YHfSEYSTMuRKcM55IFDLLgVNLIumYqwDP6WdCw9dv2aSHPX0bpuLwIEgaMEfGgvTTNi8rZ/rC9Y9OYewfHYOpr9 dphillip@dav1x-m"
-        ]
-      }
-    ]
-  },
-  "storage": {
-    "files": [
-      {
-        "path": "/etc/hostname",
-        "filesystem": "root",
-        "mode": 420,
-        "contents": {
-          "inline": "bootstrap.vmware-demo.acrawford.com"
-        }
-      },
-      {
-        "path": "/etc/sysconfig/network-scripts/ifcfg-eth0",
-        "filesystem": "root",
-        "mode": 420,
-        "contents":
-        "inline": "TYPE=Ethernet\nPROXY_METHOD=none\nBROWSER_ONLY=no\nBOOTPROTO=none\nDEFROUTE=yes\nIPV4_FAILURE_FATAL=no\nIPV6INIT=yes\nIPV6_AUTOCONF=yes\nIPV6_DEFROUTE=yes\nIPV6_FAILURE_FATAL=no\nIPV6_ADDR_GEN_MODE=stable-privacy\nNAME=eth0\nDEVICE=eth0\nONBOOT=yes\nIPADDR=139.178.89.196\nPREFIX=26\nGATEWAY=139.178.89.193\nDNS1=8.8.8.8\n"
-      }
-    ]
-  },
-  "systemd": {
-  }
-}
-EOF
+      "guestinfo.coreos.config.data" = 
     }
   }
 }
