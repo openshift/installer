e080f0494708b2674fe37af02f670c8030c32bf6
Colin Walters <walters@verbum.org>
2019-03-15 18:48:25 +0000

    rhcos: Embed full build metadata in binary
    This way we avoid needing a service available at least for AWS
    installs.  The AMIs now get hardcoded into the binary.
    
    `hack/update-rhcos-bootimage.py` is a small script which accepts
    a URL to build metadata and updates our cached version with just
    the subset of keys we care about (or potentially care about); e.g.
    not the pkgdiff.
    
    From Trevor:
    
    Drop the HTTP stuff in favor of just the local asset or
    `OPENSHIFT_INSTALL_OS_IMAGE_OVERRIDE`.
    
    The codecs business works around the lack of byte-stream support in
    json.load before Python 3.6 [1].
    
    [1]: https://docs.python.org/3/library/json.html#json.load
    
    Co-authored-by: Trevor King <wking@tremily.us>

--- pkg/rhcos/ami.go
+++ pkg/rhcos/ami.go
@@ -6,18 +6,17 @@ import (
 	"github.com/pkg/errors"
 )
 
-// AMI fetches the HVM AMI ID of the latest Red Hat Enterprise Linux CoreOS release.
-func AMI(ctx context.Context, channel, region string) (string, error) {
-	meta, err := fetchLatestMetadata(ctx, channel)
+// AMI fetches the HVM AMI ID of the Red Hat Enterprise Linux CoreOS release.
+func AMI(ctx context.Context, region string) (string, error) {
+	meta, err := fetchRHCOSBuild(ctx)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to fetch RHCOS metadata")
 	}
 
-	for _, ami := range meta.AMIs {
-		if ami.Name == region {
-			return ami.HVM, nil
-		}
+	ami, ok := meta.AMIs[region]
+	if !ok {
+		return "", errors.Errorf("no RHCOS AMIs found in %s", region)
 	}
 
-	return "", errors.Errorf("no RHCOS AMIs found in %s", region)
+	return ami.HVM, nil
 }
