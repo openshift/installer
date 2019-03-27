8181502d475a2cd759978dced9eccdf0822fa2fb
W. Trevor King <wking@tremily.us>
2019-03-27 15:15:12 -0700

    data/aws/vpc/common: Drop data.aws_region
    The final consumer was removed in 361c082064 (govcloud: remove
    terraform configs, 2018-03-23, coreos/tectonic-installer#3134).

--- data/data/aws/vpc/common.tf
+++ data/data/aws/vpc/common.tf
@@ -1,7 +1,5 @@
 # Canonical internal state definitions for this module.
 # read only: only locals and data source definitions allowed. No resources or module blocks in this file
-data "aws_region" "current" {}
-
 // Fetch a list of available AZs
 data "aws_availability_zones" "azs" {
   state = "available"
