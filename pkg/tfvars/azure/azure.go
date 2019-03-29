d6064bc701ddb9e933bb0cbd0f9fe7656c310751
Stephane Erbrech <stephane.erbrech@gmail.com>
2019-03-29 09:12:36 -0700

    change azure tfvar variable name to match terraform

--- pkg/tfvars/azure/azure.go
+++ pkg/tfvars/azure/azure.go
@@ -9,7 +9,7 @@ import (
 type config struct {
 	ExtraTags             map[string]string `json:"azure_extra_tags,omitempty"`
 	BootstrapInstanceType string            `json:"azure_bootstrap_vm_type,omitempty"`
-	MasterInstanceType    string            `json:"azure_master_instance_type,omitempty"`
+	MasterInstanceType    string            `json:"azure_master_vm_type,omitempty"`
 	Size                  int64             `json:"azure_master_root_volume_size,omitempty"`
 	Region                string            `json:"azure_region,omitempty"`
 	ClusterName           string            `json:"cluster_name,omitempty"`
