4a4adfe52c91044afeabb6e77e996ca3a85b1f7f
Dan Williams <dcbw@redhat.com>
2019-04-09 10:12:21 -0500

    aws: allow GENEVE (6081) and OVN database ports (6641 & 6642)
    1) Allow GENEVE (6081) between all nodes (masters & workers)
    2) Allow OVN databases (6641 & 6642) between all masters
    3) Allow OVN databases (6641 & 6642) between masters and
       workers, but not between workers themselves

--- data/data/aws/vpc/sg-worker.tf
+++ data/data/aws/vpc/sg-worker.tf
@@ -63,6 +63,26 @@ resource "aws_security_group_rule" "worker_ingress_vxlan_from_master" {
   to_port   = 4789
 }
 
+resource "aws_security_group_rule" "worker_ingress_geneve" {
+  type              = "ingress"
+  security_group_id = aws_security_group.worker.id
+
+  protocol  = "udp"
+  from_port = 6081
+  to_port   = 6081
+  self      = true
+}
+
+resource "aws_security_group_rule" "worker_ingress_geneve_from_master" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.worker.id
+  source_security_group_id = aws_security_group.master.id
+
+  protocol  = "udp"
+  from_port = 6081
+  to_port   = 6081
+}
+
 resource "aws_security_group_rule" "worker_ingress_internal" {
   type              = "ingress"
   security_group_id = aws_security_group.worker.id
