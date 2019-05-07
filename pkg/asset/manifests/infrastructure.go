8b3b3757188bad5004e02d07820a0b7d883d708f
W. Trevor King <wking@tremily.us>
2019-05-07 12:03:48 -0700

    pkg/asset/manifests/infrastructure: Set status.platformStatus.aws.region
    Using openshift/api@dedfb47b1f (config/v1/types_infrastructure: Add
    AWS Region, 2019-04-26, openshift/api#300) so the registry can use it
    as a default for their config resource [1,2].
    
    [1]: https://github.com/openshift/cluster-image-registry-operator/blob/670539cda460d97e0a3e23f077fdf9dd00d2ccdc/manifests/00-crd.yaml
    [2]: https://github.com/openshift/cluster-image-registry-operator/blob/670539cda460d97e0a3e23f077fdf9dd00d2ccdc/pkg/apis/imageregistry/v1/types.go#L179-L184

--- pkg/asset/manifests/infrastructure.go
+++ pkg/asset/manifests/infrastructure.go
@@ -53,24 +53,6 @@ func (i *Infrastructure) Generate(dependencies asset.Parents) error {
 	cloudproviderconfig := &CloudProviderConfig{}
 	dependencies.Get(clusterID, installConfig, cloudproviderconfig)
 
-	var platform configv1.PlatformType
-	switch installConfig.Config.Platform.Name() {
-	case aws.Name:
-		platform = configv1.AWSPlatformType
-	case none.Name:
-		platform = configv1.NonePlatformType
-	case libvirt.Name:
-		platform = configv1.LibvirtPlatformType
-	case openstack.Name:
-		platform = configv1.OpenStackPlatformType
-	case vsphere.Name:
-		platform = configv1.VSpherePlatformType
-	case azure.Name:
-		platform = configv1.AzurePlatformType
-	default:
-		platform = configv1.NonePlatformType
-	}
-
 	config := &configv1.Infrastructure{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: configv1.SchemeGroupVersion.String(),
@@ -82,12 +64,33 @@ func (i *Infrastructure) Generate(dependencies asset.Parents) error {
 		},
 		Status: configv1.InfrastructureStatus{
 			InfrastructureName:   clusterID.InfraID,
-			Platform:             platform,
 			APIServerURL:         getInternalAPIServerURL(installConfig.Config),
 			APIServerInternalURL: getInternalAPIServerURL(installConfig.Config),
 			EtcdDiscoveryDomain:  getEtcdDiscoveryDomain(installConfig.Config),
+			PlatformStatus:       &configv1.PlatformStatus{},
 		},
 	}
+
+	switch installConfig.Config.Platform.Name() {
+	case aws.Name:
+		config.Status.PlatformStatus.Type = configv1.AWSPlatformType
+		config.Status.PlatformStatus.AWS = &configv1.AWSPlatformStatus{
+			Region: installConfig.Config.Platform.AWS.Region,
+		}
+	case none.Name:
+		config.Status.PlatformStatus.Type = configv1.NonePlatformType
+	case libvirt.Name:
+		config.Status.PlatformStatus.Type = configv1.LibvirtPlatformType
+	case openstack.Name:
+		config.Status.PlatformStatus.Type = configv1.OpenStackPlatformType
+	case vsphere.Name:
+		config.Status.PlatformStatus.Type = configv1.VSpherePlatformType
+	case azure.Name:
+		config.Status.PlatformStatus.Type = configv1.AzurePlatformType
+	default:
+		config.Status.PlatformStatus.Type = configv1.NonePlatformType
+	}
+	config.Status.Platform = config.Status.PlatformStatus.Type
 
 	if cloudproviderconfig.ConfigMap != nil {
 		// set the configmap reference.
