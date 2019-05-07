54d03d2a8650a2b573db84012ced69171eba122c
W. Trevor King <wking@tremily.us>
2019-05-07 11:55:42 -0700

    vendor/github.com/openshift/api: Bump to pick up AWS Region
    Pulling in openshift/api@dedfb47b1f (config/v1/types_infrastructure:
    Add AWS Region, 2019-04-26, openshift/api#300).
    
    Generated with:
    
      $ dep ensure -update github.com/openshift/api
    
    using:
    
      $ dep version
      dep:
       version     : v0.5.1
       build date  : 2019-03-20
       git hash    : faa61893
       go version  : go1.10.3
       go compiler : gc
       platform    : linux/amd64
       features    : ImportDuringSolve=false

--- vendor/github.com/openshift/api/config/v1/types_infrastructure.go
+++ vendor/github.com/openshift/api/config/v1/types_infrastructure.go
@@ -37,15 +37,15 @@ type InfrastructureStatus struct {
 	// alphanumeric or hyphen characters.
 	InfrastructureName string `json:"infrastructureName"`
 
-	// platform is the underlying infrastructure provider for the cluster. This
-	// value controls whether infrastructure automation such as service load
-	// balancers, dynamic volume provisioning, machine creation and deletion, and
-	// other integrations are enabled. If None, no infrastructure automation is
-	// enabled. Allowed values are "AWS", "Azure", "BareMetal", "GCP", "Libvirt",
-	// "OpenStack", "VSphere", and "None". Individual components may not support
-	// all platforms, and must handle unrecognized platforms as None if they do
-	// not support that platform.
+	// platform is the underlying infrastructure provider for the cluster.
+	//
+	// Deprecated: Use platformStatus.type instead.
 	Platform PlatformType `json:"platform,omitempty"`
+
+	// platformStatus holds status information specific to the underlying
+	// infrastructure provider.
+	// +optional
+	PlatformStatus *PlatformStatus `json:"platformStatus,omitempty"`
 
 	// etcdDiscoveryDomain is the domain used to fetch the SRV records for discovering
 	// etcd servers and clients.
@@ -92,6 +92,31 @@ const (
 	// VSpherePlatformType represents VMWare vSphere infrastructure.
 	VSpherePlatformType PlatformType = "VSphere"
 )
+
+// PlatformStatus holds the current status specific to the underlying infrastructure provider
+// of the current cluster. Since these are used at status-level for the underlying cluster, it
+// is supposed that only one of the status structs is set.
+type PlatformStatus struct {
+	// type is the underlying infrastructure provider for the cluster. This
+	// value controls whether infrastructure automation such as service load
+	// balancers, dynamic volume provisioning, machine creation and deletion, and
+	// other integrations are enabled. If None, no infrastructure automation is
+	// enabled. Allowed values are "AWS", "Azure", "BareMetal", "GCP", "Libvirt",
+	// "OpenStack", "VSphere", and "None". Individual components may not support
+	// all platforms, and must handle unrecognized platforms as None if they do
+	// not support that platform.
+	Type PlatformType `json:"type"`
+
+	// AWS contains settings specific to the Amazon Web Services infrastructure provider.
+	// +optional
+	AWS *AWSPlatformStatus `json:"aws,omitempty"`
+}
+
+// AWSPlatformStatus holds the current status of the Amazon Web Services infrastructure provider.
+type AWSPlatformStatus struct {
+	// region holds the default AWS region for new AWS resources created by the cluster.
+	Region string `json:"region"`
+}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
