b6208d9a08db4f5da4e3c32927a32c4945052e08
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-06-07 14:02:31 -0700

    data/azure: update the names subnet created for the cluster based on new scheme

--- data/data/azure/vnet/vnet.tf
+++ data/data/azure/vnet/vnet.tf
@@ -8,13 +8,13 @@ resource "azurerm_subnet" "master_subnet" {
   resource_group_name  = var.resource_group_name
   address_prefix       = var.master_subnet_cidr
   virtual_network_name = var.vnet_name
-  name                 = "${var.cluster_id}-controlplane-subnet"
+  name                 = "${var.cluster_id}-master-subnet"
 }
 
 resource "azurerm_subnet" "node_subnet" {
   resource_group_name  = var.resource_group_name
   address_prefix       = var.node_subnet_cidr
   virtual_network_name = var.vnet_name
-  name                 = "${var.cluster_id}-node-subnet"
+  name                 = "${var.cluster_id}-worker-subnet"
 }
 
