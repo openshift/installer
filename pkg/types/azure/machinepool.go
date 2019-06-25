bd7e54b3c7d9034fdd7b86462a5c13cfb59551c7
John Hixson <jhixson@redhat.com>
2019-06-25 16:52:57 -0700

    pkg/types/azure: remove Zones from machine pool

--- pkg/types/azure/machinepool.go
+++ pkg/types/azure/machinepool.go
@@ -3,9 +3,6 @@ package azure
 // MachinePool stores the configuration for a machine pool installed
 // on Azure.
 type MachinePool struct {
-	// Zones is list of availability zones that can be used.
-	Zones []string `json:"zones,omitempty"`
-
 	// InstanceType defines the azure instance type.
 	// eg. Standard_DS_V2
 	InstanceType string `json:"type"`
@@ -15,10 +12,6 @@ type MachinePool struct {
 func (a *MachinePool) Set(required *MachinePool) {
 	if required == nil || a == nil {
 		return
-	}
-
-	if len(required.Zones) > 0 {
-		a.Zones = required.Zones
 	}
 
 	if required.InstanceType != "" {
