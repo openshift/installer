394dc541fb4f5e1c1a54139af99d7f7d13854df3
Cesar Wong <cewong@redhat.com>
2019-07-11 16:12:25 -0400

    [gcp] data/data: give bootstrap machine an external IP
    Adds an 'access_config' block to the network section of the instance.
    The presence of the block enables an external IP address for the
    instance. For a dynamically assigned IP address, the block can be left
    empty. Otherwise it can contain a specific IP from an Adress resource.

--- data/data/gcp/bootstrap/main.tf
+++ data/data/gcp/bootstrap/main.tf
@@ -1,3 +1,16 @@
+resource "google_compute_firewall" "bootstrap_ingress_ssh" {
+  name    = "${var.cluster_id}-bootstrap-ingress-ssh"
+  network = var.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["${var.cluster_id}-bootstrap"]
+}
+
 resource "google_compute_instance" "bootstrap" {
   name         = "${var.cluster_id}-bootstrap"
   machine_type = var.machine_type
@@ -13,13 +26,16 @@ resource "google_compute_instance" "bootstrap" {
 
   network_interface {
     subnetwork = var.subnet
+
+    access_config {
+    }
   }
 
   metadata = {
     user-data = var.ignition
   }
 
-  tags = ["${var.cluster_id}-master"]
+  tags = ["${var.cluster_id}-master", "${var.cluster_id}-bootstrap"]
 
   labels = var.labels
 }
