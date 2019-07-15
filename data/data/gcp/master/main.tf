29aff0b8fdccc9a09c2be0ca35ffa3c736c306ae
Cesar Wong <cewong@redhat.com>
2019-07-15 15:04:07 -0400

    [gcp] Add service accounts for masters and compute

--- data/data/gcp/master/main.tf
+++ data/data/gcp/master/main.tf
@@ -1,3 +1,28 @@
+resource "google_service_account" "master-node-sa" {
+  account_id   = "${var.cluster_id}-m"
+  display_name = "${var.cluster_id}-master-node"
+}
+
+resource "google_project_iam_member" "master-compute-admin" {
+  role   = "roles/compute.instanceAdmin"
+  member = "serviceAccount:${google_service_account.master-node-sa.email}"
+}
+
+resource "google_project_iam_member" "master-network-admin" {
+  role   = "roles/compute.networkAdmin"
+  member = "serviceAccount:${google_service_account.master-node-sa.email}"
+}
+
+resource "google_project_iam_member" "master-storage-admin" {
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.master-node-sa.email}"
+}
+
+resource "google_project_iam_member" "master-object-storage-admin" {
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.master-node-sa.email}"
+}
+
 resource "google_compute_instance" "master" {
   count = var.instance_count
 
@@ -24,6 +49,11 @@ resource "google_compute_instance" "master" {
   tags = ["${var.cluster_id}-master"]
 
   labels = var.labels
+
+  service_account {
+    email  = google_service_account.master-node-sa.email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
 }
 
 # Not ideal, machine API would need to keep membership up to date
