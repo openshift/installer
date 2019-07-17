e81eba661770401a27d14ffb5f1ca3666ec4cd50
Andrew Beekhof <andrew@beekhof.net>
2019-07-16 22:50:07 -0400

    Conditionally include the bootstrap node in the SRV record
    Define a new tfvar `etcd_pivot` that is set based on the
    OPENSHIFT_INSTALL_ETCD_ENABLE_PIVOT environment variable, and controls
    the addition of the bootstrap node to the etcd srv record.
    
    This work is part of the redesign of how etcd is deployed to masters,
    and the existance of the bootstrap node in this record will initially
    be used by bootkube.sh to determine if this new functionally should be
    enabled, and like on masters is used to validate that the bootstrap node
    is permitted to host etcd.
    
    The patch includes support for AWS and libvirt

--- pkg/tfvars/tfvars.go
+++ pkg/tfvars/tfvars.go
@@ -4,7 +4,10 @@ package tfvars
 import (
 	"encoding/json"
 	"net"
+	"os"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 type config struct {
@@ -13,6 +16,7 @@ type config struct {
 	BaseDomain    string `json:"base_domain,omitempty"`
 	MachineCIDR   string `json:"machine_cidr"`
 	Masters       int    `json:"master_count,omitempty"`
+	EtcdPivot     string `json:"etcd_pivot,omitempty"`
 
 	IgnitionBootstrap string `json:"ignition_bootstrap,omitempty"`
 	IgnitionMaster    string `json:"ignition_master,omitempty"`
@@ -20,6 +24,11 @@ type config struct {
 
 // TFVars generates terraform.tfvar JSON for launching the cluster.
 func TFVars(clusterID string, clusterDomain string, baseDomain string, machineCIDR *net.IPNet, bootstrapIgn string, masterIgn string, masterCount int) ([]byte, error) {
+	etcdPivot := "false"
+	if pivot, ok := os.LookupEnv("OPENSHIFT_INSTALL_ETCD_ENABLE_PIVOT"); ok && pivot != "" {
+		logrus.Warn("Found override for etcd provisioning style. Please be warned, this is still under development")
+		etcdPivot = pivot
+	}
 	config := &config{
 		ClusterID:         clusterID,
 		ClusterDomain:     strings.TrimSuffix(clusterDomain, "."),
@@ -28,6 +37,7 @@ func TFVars(clusterID string, clusterDomain string, baseDomain string, machineCI
 		Masters:           masterCount,
 		IgnitionBootstrap: bootstrapIgn,
 		IgnitionMaster:    masterIgn,
+		EtcdPivot:         etcdPivot,
 	}
 
 	return json.MarshalIndent(config, "", "  ")
