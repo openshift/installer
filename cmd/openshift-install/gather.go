e4c3adb58dda49921d36ff11e1a357f9fc56508b
Patrick Dillon <padillon@redhat.com>
2019-07-17 10:02:36 -0400

    Add gather support for GCP.
    Include GCP platform in gather command. Add logic to lookup bootstrap and master ip addresses from terraform state file according to the gcp schema.

--- cmd/openshift-install/gather.go
+++ cmd/openshift-install/gather.go
@@ -17,11 +17,13 @@ import (
 	"github.com/openshift/installer/pkg/terraform"
 	gatheraws "github.com/openshift/installer/pkg/terraform/gather/aws"
 	gatherazure "github.com/openshift/installer/pkg/terraform/gather/azure"
+	gathergcp "github.com/openshift/installer/pkg/terraform/gather/gcp"
 	gatherlibvirt "github.com/openshift/installer/pkg/terraform/gather/libvirt"
 	gatheropenstack "github.com/openshift/installer/pkg/terraform/gather/openstack"
 	"github.com/openshift/installer/pkg/types"
 	awstypes "github.com/openshift/installer/pkg/types/aws"
 	azuretypes "github.com/openshift/installer/pkg/types/azure"
+	gcptypes "github.com/openshift/installer/pkg/types/gcp"
 	libvirttypes "github.com/openshift/installer/pkg/types/libvirt"
 	openstacktypes "github.com/openshift/installer/pkg/types/openstack"
 )
@@ -142,6 +144,15 @@ func extractHostAddresses(config *types.InstallConfig, tfstate *terraform.State)
 			return bootstrap, port, masters, err
 		}
 		masters, err = gatherazure.ControlPlaneIPs(tfstate)
+		if err != nil {
+			logrus.Error(err)
+		}
+	case gcptypes.Name:
+		bootstrap, err = gathergcp.BootstrapIP(tfstate)
+		if err != nil {
+			return bootstrap, port, masters, err
+		}
+		masters, err = gathergcp.ControlPlaneIPs(tfstate)
 		if err != nil {
 			logrus.Error(err)
 		}
