3406de93a1cc60622251603fbd07636da1a8e2a5
Ben Howard <ben.howard@redhat.com>
2019-07-25 09:52:06 -0600

    pkg/rhcos/build.go: Add support for GCP and Azure in rhcos.json

--- pkg/rhcos/builds.go
+++ pkg/rhcos/builds.go
@@ -13,6 +13,14 @@ type metadata struct {
 	AMIs map[string]struct {
 		HVM string `json:"hvm"`
 	} `json:"amis"`
+	Azure struct {
+		Image string `json:"image"`
+		URL string `json:"url"`
+	}
+	GCP struct {
+		Image string `json:"image"`
+		URL string `json:"url"`
+	}
 	BaseURI string `json:"baseURI"`
 	Images  struct {
 		QEMU struct {
