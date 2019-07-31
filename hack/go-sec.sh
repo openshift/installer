afa8e5bce059675fce6a8bc42fb0d9244387eab4
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2019-07-31 09:43:31 +0300

    Introduce gosec for security checks
    gosec does static code analysis and checks for common security issues in
    golang codebases.
    
    This PR introduces a script that will run gosec similarly to other check
    tools.

---
+++ hack/go-sec.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Example:  ./hack/gosec.sh
+set -x
+
+if [ "$IS_CONTAINER" != "" ]; then
+  if [ ! "$(command -v gosec >/dev/null)" ]; then
+      go get github.com/securego/gosec/cmd/gosec
+  fi
+  gosec -severity high -confidence high -exclude G304 ./cmd/... ./data/... ./pkg/... "${@}"
+else
+  podman run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/go/src/github.com/openshift/installer:z" \
+    --workdir /go/src/github.com/openshift/installer \
+    docker.io/openshift/origin-release:golang-1.12 \
+    ./hack/go-sec.sh "${@}"
+fi
