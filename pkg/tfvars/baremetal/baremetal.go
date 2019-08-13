openshift-metal3/kni-installer

baremetal: replace IronicURI with BootstrapProvisioningIP
This means we can use the IP to configure both the nic on the bootstrap
VM which Ironic uses for provisioning (in a future commit) and construct
the ironic_uri

pkg/tfvars/baremetal/baremetal.go
@@ -11,11 +11,11 @@ import (
 )
 
 type config struct {
-	LibvirtURI         string `json:"libvirt_uri,omitempty"`
-	IronicURI          string `json:"ironic_uri,omitempty"`
-	Image              string `json:"os_image,omitempty"`
-	ExternalBridge     string `json:"external_bridge,omitempty"`
-	ProvisioningBridge string `json:"provisioning_bridge,omitempty"`
+	LibvirtURI              string `json:"libvirt_uri,omitempty"`
+	BootstrapProvisioningIP string `json:"bootstrap_provisioning_ip,omitempty"`
+	Image                   string `json:"os_image,omitempty"`
+	ExternalBridge          string `json:"external_bridge,omitempty"`
+	ProvisioningBridge      string `json:"provisioning_bridge,omitempty"`
 
 	// Data required for control plane deployment - several maps per host, because of terraform's limitations
 	Hosts         []map[string]interface{} `json:"hosts"`
@@ -26,7 +26,7 @@ type config struct {
 }
 
 // TFVars generates bare metal specific Terraform variables.
-func TFVars(libvirtURI, ironicURI, osImage, externalBridge, provisioningBridge string, platformHosts []*baremetal.Host, image baremetal.Image) ([]byte, error) {
+func TFVars(libvirtURI, bootstrapProvisioningIP, osImage, externalBridge, provisioningBridge string, platformHosts []*baremetal.Host, image baremetal.Image) ([]byte, error) {
 	osImage, err := libvirttfvars.CachedImage(osImage)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to use cached libvirt image")
@@ -91,16 +91,16 @@ func TFVars(libvirtURI, ironicURI, osImage, externalBridge, provisioningBridge s
 	}
 
 	cfg := &config{
-		LibvirtURI:         libvirtURI,
-		IronicURI:          ironicURI,
-		Image:              osImage,
-		ExternalBridge:     externalBridge,
-		ProvisioningBridge: provisioningBridge,
-		Hosts:              hosts,
-		Properties:         properties,
-		DriverInfos:        driverInfos,
-		RootDevices:        rootDevices,
-		InstanceInfos:      instanceInfos,
+		LibvirtURI:              libvirtURI,
+		BootstrapProvisioningIP: bootstrapProvisioningIP,
+		Image:                   osImage,
+		ExternalBridge:          externalBridge,
+		ProvisioningBridge:      provisioningBridge,
+		Hosts:                   hosts,
+		Properties:              properties,
+		DriverInfos:             driverInfos,
+		RootDevices:             rootDevices,
+		InstanceInfos:           instanceInfos,
 	}
 
 	return json.MarshalIndent(cfg, "", "  ")

openshift/installer

pkg/tfvars/baremetal/baremetal.go
@@ -11,11 +11,11 @@ import (
 )
 
 type config struct {
-	LibvirtURI         string `json:"libvirt_uri,omitempty"`
-	IronicURI          string `json:"ironic_uri,omitempty"`
-	BootstrapOSImage   string `json:"bootstrap_os_image,omitempty"`
-	ExternalBridge     string `json:"external_bridge,omitempty"`
-	ProvisioningBridge string `json:"provisioning_bridge,omitempty"`
+	LibvirtURI              string `json:"libvirt_uri,omitempty"`
+	BootstrapProvisioningIP string `json:"bootstrap_provisioning_ip,omitempty"`
+	BootstrapOSImage        string `json:"bootstrap_os_image,omitempty"`
+	ExternalBridge          string `json:"external_bridge,omitempty"`
+	ProvisioningBridge      string `json:"provisioning_bridge,omitempty"`
 
 	// Data required for control plane deployment - several maps per host, because of terraform's limitations
 	Hosts         []map[string]interface{} `json:"hosts"`
@@ -26,7 +26,7 @@ type config struct {
 }
 
 // TFVars generates bare metal specific Terraform variables.
-func TFVars(libvirtURI, ironicURI, bootstrapOSImage, externalBridge, provisioningBridge string, platformHosts []*baremetal.Host, image baremetal.Image) ([]byte, error) {
+func TFVars(libvirtURI, bootstrapProvisioningIP, bootstrapOSImage, externalBridge, provisioningBridge string, platformHosts []*baremetal.Host, image baremetal.Image) ([]byte, error) {
 	bootstrapOSImage, err := libvirttfvars.CachedImage(bootstrapOSImage)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to use cached bootstrap libvirt image")
@@ -94,16 +94,16 @@ func TFVars(libvirtURI, ironicURI, bootstrapOSImage, externalBridge, provisionin
 	}
 
 	cfg := &config{
-		LibvirtURI:         libvirtURI,
-		IronicURI:          ironicURI,
-		BootstrapOSImage:   bootstrapOSImage,
-		ExternalBridge:     externalBridge,
-		ProvisioningBridge: provisioningBridge,
-		Hosts:              hosts,
-		Properties:         properties,
-		DriverInfos:        driverInfos,
-		RootDevices:        rootDevices,
-		InstanceInfos:      instanceInfos,
+		LibvirtURI:              libvirtURI,
+		BootstrapProvisioningIP: bootstrapProvisioningIP,
+		BootstrapOSImage:        bootstrapOSImage,
+		ExternalBridge:          externalBridge,
+		ProvisioningBridge:      provisioningBridge,
+		Hosts:                   hosts,
+		Properties:              properties,
+		DriverInfos:             driverInfos,
+		RootDevices:             rootDevices,
+		InstanceInfos:           instanceInfos,
 	}
 
 	return json.MarshalIndent(cfg, "", "  ")