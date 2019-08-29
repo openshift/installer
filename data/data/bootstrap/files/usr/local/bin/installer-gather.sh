5aceea9869bce88efd712189b6989186cb55deb6
Colin Walters <walters@verbum.org>
2019-08-29 21:26:19 +0000

    Add crio-configure.service Before=bootkube.service
    Rather than doing a `systemctl restart crio` after `kubelet.service`
    has started, separate out the bit that changes the pause image
    into a small separate service.

--- data/data/bootstrap/files/usr/local/bin/installer-gather.sh
+++ data/data/bootstrap/files/usr/local/bin/installer-gather.sh
@@ -20,7 +20,7 @@ done
 
 echo "Gathering bootstrap journals ..."
 mkdir -p "${ARTIFACTS}/bootstrap/journals"
-for service in release-image bootkube kubelet crio approve-csr
+for service in release-image crio-configure bootkube kubelet crio approve-csr
 do
     journalctl --boot --no-pager --output=short --unit="${service}" > "${ARTIFACTS}/bootstrap/journals/${service}.log"
 done
