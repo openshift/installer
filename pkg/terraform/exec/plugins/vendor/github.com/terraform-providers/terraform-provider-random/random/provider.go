8d04568695ff880e180b8719f2bde8347eede4e5
Mike Fedosin <mfedosin@redhat.com>
2019-09-10 15:38:15 +0200

    Bump terraform-provider-random
    The new version contains random_password module required by OpenStack

--- pkg/terraform/exec/plugins/vendor/github.com/terraform-providers/terraform-provider-random/random/provider.go
+++ pkg/terraform/exec/plugins/vendor/github.com/terraform-providers/terraform-provider-random/random/provider.go
@@ -11,12 +11,13 @@ func Provider() terraform.ResourceProvider {
 		Schema: map[string]*schema.Schema{},
 
 		ResourcesMap: map[string]*schema.Resource{
-			"random_id":      resourceId(),
-			"random_shuffle": resourceShuffle(),
-			"random_pet":     resourcePet(),
-			"random_string":  resourceString(),
-			"random_integer": resourceInteger(),
-			"random_uuid":    resourceUuid(),
+			"random_id":       resourceId(),
+			"random_shuffle":  resourceShuffle(),
+			"random_pet":      resourcePet(),
+			"random_string":   resourceString(),
+			"random_password": resourcePassword(),
+			"random_integer":  resourceInteger(),
+			"random_uuid":     resourceUuid(),
 		},
 	}
 }
