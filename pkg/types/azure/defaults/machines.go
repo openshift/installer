ff851692d190433eefc9d999ac390b651e8cadfa
Yu Qi Zhang <jerzhang@redhat.com>
2019-09-17 20:20:42 -0400

    Azure: bump disk size to 1TB for control plane
    D4s_v3 does not have enough IOPS to properly support etcd during
    cluster runs, which causes a lot of overloads. Bump to 1TB disk
    and D8s_v3 to improve disk performance.
    
    Signed-off-by: Yu Qi Zhang <jerzhang@redhat.com>

--- pkg/types/azure/defaults/machines.go
+++ pkg/types/azure/defaults/machines.go
@@ -14,10 +14,11 @@ func BootstrapInstanceType(region string) string {
 
 // ControlPlaneInstanceType sets the defaults for control plane instances.
 // Minimum requirements are 4 CPU's, 16GiB of ram, and 120GiB storage.
-// D4s v3 gives us 4 CPU's, 16GiB ram and 32GiB of temporary storage
+// D8s v3 gives us 8 CPU's, 32GiB ram and 64GiB of temporary storage
+// This extra bump is done to prevent etcd from overloading
 func ControlPlaneInstanceType(region string) string {
 	instanceClass := getInstanceClass(region)
-	return fmt.Sprintf("%s_D4s_v3", instanceClass)
+	return fmt.Sprintf("%s_D8s_v3", instanceClass)
 }
 
 // ComputeInstanceType sets the defaults for compute instances.
