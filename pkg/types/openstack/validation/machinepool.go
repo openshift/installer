c0868f2b4c8a4bab0e7f57cb5f1e5a00be52a15f
Martin André <m.andre@redhat.com>
2019-09-23 18:21:17 +0200

    Bug 1753187: OpenStack: Allow setting root volume via installconfig
    The `rootVolume` setting can now be passed via installconfig's
    machinepool struct to let users use a more performant volume type than
    the default ephemeral disk.
    
    Co-Authored-By: Emilio Garcia <egarcia@redhat.com>

--- pkg/types/openstack/validation/machinepool.go
+++ pkg/types/openstack/validation/machinepool.go
@@ -8,5 +8,17 @@ import (
 
 // ValidateMachinePool checks that the specified machine pool is valid.
 func ValidateMachinePool(p *openstack.MachinePool, fldPath *field.Path) field.ErrorList {
-	return field.ErrorList{}
+	allErrs := field.ErrorList{}
+
+	// Validate Root Volumes
+	if p.RootVolume != nil {
+		if p.RootVolume.Type == "" {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("rootVolume").Child("type"), p.RootVolume.Type, "Volume type must be specified to use root volumes"))
+		}
+		if p.RootVolume.Size <= 0 {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("rootVolume").Child("size"), p.RootVolume.Size, "Volume size must be greater than zero to use root volumes"))
+		}
+	}
+
+	return allErrs
 }
