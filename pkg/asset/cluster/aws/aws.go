461a8089d859dc4f1743f81457004b0ee4c58c83
W. Trevor King <wking@tremily.us>
2019-10-03 09:41:31 -0700

    pkg/asset/cluster/aws: Tag shared subnets
    So the kubelet cloud-provider can place load balancers appropriately.
    
    This also helps make it clear to other consumers that we are using
    these resources for the cluster ("hmm, I wonder if it would be a bad
    idea to delete this subnet?  Ah, yeah, I don't want to break cluster
    example-123").  But because AWS only allows 50 tags per resource [1],
    we're not tagging shared resources like the VPC or Route 53 zones
    because those don't need to be tagged to get a working cluster.  Users
    are free to add 'shared' tags to resources like that as they see fit.
    I also considered using the EC2-specific API [2], but went with the
    generic tag API in case we wanted to tag Route 53 zones or other
    non-EC2 resources as well in the future.
    
    Tag before entering Terraform to claim the space before we actually
    put cluster resources inside the subnets.  That keeps folks from
    unknowingly reaping them before we start adding resources, and it
    ensures we have space for the new tags.
    
    The 20-tag limit that leads to the loop is from [3].  We're unlikely
    to exceed 20 with just the VPC and subnets, but it seemed reasonable
    to plan for the future to avoid surprises if we grow this list going
    forward.
    
    [1]: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Using_Tags.html#tag-restrictions
    [2]: https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_CreateTags.html
    [3]: https://docs.aws.amazon.com/sdk-for-go/api/service/resourcegroupstaggingapi/#TagResourcesInput

--- pkg/asset/cluster/aws/aws.go
+++ pkg/asset/cluster/aws/aws.go
@@ -2,15 +2,21 @@
 package aws
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi"
+	"github.com/sirupsen/logrus"
+
+	"github.com/openshift/installer/pkg/asset/installconfig"
 	"github.com/openshift/installer/pkg/types"
-	"github.com/openshift/installer/pkg/types/aws"
+	awstypes "github.com/openshift/installer/pkg/types/aws"
 )
 
 // Metadata converts an install configuration to AWS metadata.
-func Metadata(clusterID, infraID string, config *types.InstallConfig) *aws.Metadata {
-	return &aws.Metadata{
+func Metadata(clusterID, infraID string, config *types.InstallConfig) *awstypes.Metadata {
+	return &awstypes.Metadata{
 		Region: config.Platform.AWS.Region,
 		Identifier: []map[string]string{{
 			fmt.Sprintf("kubernetes.io/cluster/%s", infraID): "owned",
@@ -18,4 +24,53 @@ func Metadata(clusterID, infraID string, config *types.InstallConfig) *aws.Metad
 			"openshiftClusterID": clusterID,
 		}},
 	}
+}
+
+// PreTerraform performs any infrastructure initialization which must
+// happen before Terraform creates the remaining infrastructure.
+func PreTerraform(ctx context.Context, clusterID string, installConfig *installconfig.InstallConfig) error {
+	if len(installConfig.Config.Platform.AWS.Subnets) == 0 {
+		return nil
+	}
+
+	privateSubnets, err := installConfig.AWS.PrivateSubnets(ctx)
+	if err != nil {
+		return err
+	}
+
+	publicSubnets, err := installConfig.AWS.PublicSubnets(ctx)
+
+	arns := make([]string, 0, len(privateSubnets)+len(publicSubnets))
+	for _, subnet := range privateSubnets {
+		arns = append(arns, subnet.ARN)
+	}
+	for _, subnet := range publicSubnets {
+		arns = append(arns, subnet.ARN)
+	}
+
+	session, err := installConfig.AWS.Session(ctx)
+	if err != nil {
+		return err
+	}
+
+	request := &resourcegroupstaggingapi.TagResourcesInput{
+		Tags: map[string]*string{
+			fmt.Sprintf("kubernetes.io/cluster/%s", clusterID): aws.String("shared"),
+		},
+	}
+
+	tagClient := resourcegroupstaggingapi.New(session)
+	for i := 0; i < len(arns); i += 20 {
+		request.ResourceARNList = make([]*string, 0, 20)
+		for j := 0; i+j < len(arns) && j < 20; j++ {
+			logrus.Debugf("Tagging %s with kubernetes.io/cluster/%s: shared", arns[i+j], clusterID)
+			request.ResourceARNList = append(request.ResourceARNList, aws.String(arns[i+j]))
+		}
+		_, err = tagClient.TagResourcesWithContext(ctx, request)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
