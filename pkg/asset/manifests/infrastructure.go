3165e5ff089f9e2ea68c3113b5abf578ffc40bbb
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-10-21 11:11:36 -0700

    asset/manifests/infrastructure.go: Set the network resource group for the Azure cluster
    The network resource group is the same as the cluster resource group, unless the Vnet and subnets were provided by the user to the installer.

--- pkg/asset/manifests/infrastructure.go
+++ pkg/asset/manifests/infrastructure.go
@@ -87,8 +87,14 @@ func (i *Infrastructure) Generate(dependencies asset.Parents) error {
 		}
 	case azure.Name:
 		config.Status.PlatformStatus.Type = configv1.AzurePlatformType
+
+		rg := fmt.Sprintf("%s-rg", clusterID.InfraID)
 		config.Status.PlatformStatus.Azure = &configv1.AzurePlatformStatus{
-			ResourceGroupName: fmt.Sprintf("%s-rg", clusterID.InfraID),
+			ResourceGroupName:        rg,
+			NetworkResourceGroupName: rg,
+		}
+		if nrg := installConfig.Config.Platform.Azure.NetworkResourceGroupName; nrg != "" {
+			config.Status.PlatformStatus.Azure.NetworkResourceGroupName = nrg
 		}
 	case baremetal.Name:
 		config.Status.PlatformStatus.Type = configv1.BareMetalPlatformType
