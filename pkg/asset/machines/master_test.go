3c2f5f918ebc16169c7fbbba5f8c7a41b7cffc40
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-10-25 12:25:25 +0200

    Fixup unittests for Ignition Spec v3

--- pkg/asset/machines/master_test.go
+++ pkg/asset/machines/master_test.go
@@ -23,42 +23,6 @@ func TestMasterGenerateMachineConfigs(t *testing.T) {
 		expectedMachineConfig []string
 	}{
 		{
-			name:           "no key hyperthreading enabled",
-			hyperthreading: types.HyperthreadingEnabled,
-		},
-		{
-			name:           "key present hyperthreading enabled",
-			key:            "ssh-rsa: dummy-key",
-			hyperthreading: types.HyperthreadingEnabled,
-			expectedMachineConfig: []string{`apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfig
-metadata:
-  creationTimestamp: null
-  labels:
-    machineconfiguration.openshift.io/role: master
-  name: 99-master-ssh
-spec:
-  config:
-    ignition:
-      config: {}
-      security:
-        tls: {}
-      timeouts: {}
-      version: 2.2.0
-    networkd: {}
-    passwd:
-      users:
-      - name: core
-        sshAuthorizedKeys:
-        - 'ssh-rsa: dummy-key'
-    storage: {}
-    systemd: {}
-  fips: false
-  kernelArguments: null
-  osImageURL: ""
-`},
-		},
-		{
 			name:           "no key hyperthreading disabled",
 			hyperthreading: types.HyperthreadingDisabled,
 			expectedMachineConfig: []string{`apiVersion: machineconfiguration.openshift.io/v1
@@ -71,19 +35,21 @@ metadata:
 spec:
   config:
     ignition:
-      config: {}
+      config:
+        replace:
+          source: null
+          verification: {}
       security:
         tls: {}
       timeouts: {}
-      version: 2.2.0
-    networkd: {}
+      version: 3.0.0
     passwd: {}
     storage:
       files:
       - contents:
           source: data:text/plain;charset=utf-8;base64,QUREIG5vc210
           verification: {}
-        filesystem: root
+        group: {}
         mode: 384
         path: /etc/pivot/kernel-args
         user:
@@ -108,19 +74,21 @@ metadata:
 spec:
   config:
     ignition:
-      config: {}
+      config:
+        replace:
+          source: null
+          verification: {}
       security:
         tls: {}
       timeouts: {}
-      version: 2.2.0
-    networkd: {}
+      version: 3.0.0
     passwd: {}
     storage:
       files:
       - contents:
           source: data:text/plain;charset=utf-8;base64,QUREIG5vc210
           verification: {}
-        filesystem: root
+        group: {}
         mode: 384
         path: /etc/pivot/kernel-args
         user:
@@ -139,12 +107,14 @@ metadata:
 spec:
   config:
     ignition:
-      config: {}
+      config:
+        replace:
+          source: null
+          verification: {}
       security:
         tls: {}
       timeouts: {}
-      version: 2.2.0
-    networkd: {}
+      version: 3.0.0
     passwd:
       users:
       - name: core
--- pkg/asset/machines/worker_test.go
+++ pkg/asset/machines/worker_test.go
@@ -23,42 +23,6 @@ func TestWorkerGenerate(t *testing.T) {
 		expectedMachineConfig []string
 	}{
 		{
-			name:           "no key hyperthreading enabled",
-			hyperthreading: types.HyperthreadingEnabled,
-		},
-		{
-			name:           "key present hyperthreading enabled",
-			key:            "ssh-rsa: dummy-key",
-			hyperthreading: types.HyperthreadingEnabled,
-			expectedMachineConfig: []string{`apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfig
-metadata:
-  creationTimestamp: null
-  labels:
-    machineconfiguration.openshift.io/role: worker
-  name: 99-worker-ssh
-spec:
-  config:
-    ignition:
-      config: {}
-      security:
-        tls: {}
-      timeouts: {}
-      version: 2.2.0
-    networkd: {}
-    passwd:
-      users:
-      - name: core
-        sshAuthorizedKeys:
-        - 'ssh-rsa: dummy-key'
-    storage: {}
-    systemd: {}
-  fips: false
-  kernelArguments: null
-  osImageURL: ""
-`},
-		},
-		{
 			name:           "no key hyperthreading disabled",
 			hyperthreading: types.HyperthreadingDisabled,
 			expectedMachineConfig: []string{`apiVersion: machineconfiguration.openshift.io/v1
@@ -71,86 +35,25 @@ metadata:
 spec:
   config:
     ignition:
-      config: {}
+      config:
+        replace:
+          source: null
+          verification: {}
       security:
         tls: {}
       timeouts: {}
-      version: 2.2.0
-    networkd: {}
+      version: 3.0.0
     passwd: {}
     storage:
       files:
       - contents:
           source: data:text/plain;charset=utf-8;base64,QUREIG5vc210
           verification: {}
-        filesystem: root
+        group: {}
         mode: 384
         path: /etc/pivot/kernel-args
         user:
           name: root
-    systemd: {}
-  fips: false
-  kernelArguments: null
-  osImageURL: ""
-`},
-		},
-		{
-			name:           "key present hyperthreading disabled",
-			key:            "ssh-rsa: dummy-key",
-			hyperthreading: types.HyperthreadingDisabled,
-			expectedMachineConfig: []string{`apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfig
-metadata:
-  creationTimestamp: null
-  labels:
-    machineconfiguration.openshift.io/role: worker
-  name: 99-worker-disable-hyperthreading
-spec:
-  config:
-    ignition:
-      config: {}
-      security:
-        tls: {}
-      timeouts: {}
-      version: 2.2.0
-    networkd: {}
-    passwd: {}
-    storage:
-      files:
-      - contents:
-          source: data:text/plain;charset=utf-8;base64,QUREIG5vc210
-          verification: {}
-        filesystem: root
-        mode: 384
-        path: /etc/pivot/kernel-args
-        user:
-          name: root
-    systemd: {}
-  fips: false
-  kernelArguments: null
-  osImageURL: ""
-`, `apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfig
-metadata:
-  creationTimestamp: null
-  labels:
-    machineconfiguration.openshift.io/role: worker
-  name: 99-worker-ssh
-spec:
-  config:
-    ignition:
-      config: {}
-      security:
-        tls: {}
-      timeouts: {}
-      version: 2.2.0
-    networkd: {}
-    passwd:
-      users:
-      - name: core
-        sshAuthorizedKeys:
-        - 'ssh-rsa: dummy-key'
-    storage: {}
     systemd: {}
   fips: false
   kernelArguments: null
