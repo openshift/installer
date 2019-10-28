29d4ac45ac233dcf2119ac6dd376b79715eb8d95
Roy Golan <rgolan@redhat.com>
2019-10-28 21:23:35 +0200

    ovirt: Implement gather
    Implement the gather functionality for oVirt platform.
    This implementation currently returns empty results because there is
    no way to report IPs of an RHCOS guest VM, hence oVirt is lacking it.
    
    Signed-off-by: Roy Golan <rgolan@redhat.com>

---
+++ pkg/terraform/gather/ovirt/ip.go
@@ -0,0 +1,18 @@
+// Package ovirt supply utilities to extract information from terraform state
+package ovirt
+
+import (
+	"github.com/openshift/installer/pkg/terraform"
+)
+
+// BootstrapIP returns the ip address for bootstrap host.
+// still unsupported, because qemu-ga is not available - see https://bugzilla.redhat.com/show_bug.cgi?id=1764804
+func BootstrapIP(tfs *terraform.State) (string, error) {
+	return "", nil
+}
+
+// ControlPlaneIPs returns the ip addresses for control plane hosts.
+// still unsupported, because qemu-ga is not available  - see https://bugzilla.redhat.com/show_bug.cgi?id=1764804
+func ControlPlaneIPs(tfs *terraform.State) ([]string, error) {
+	return []string{""}, nil
+}
