f729d9693056f565000273ddad48920deb98e428
gwest <gwest@redhat.com>
2019-10-30 13:26:38 -0500

    Change paths for placement in openshift/installer repo

--- upi/azure/arm/deploy_azarm.sh
+++ upi/azure/arm/deploy_azarm.sh
@@ -1,8 +1,9 @@
 echo "Start Deployment"
+export arm_template_path=`./gitpath.sh upi/azure/arm/azuredeploy.json`
 az group deployment create \
    --name $1 \
    --resource-group $1 \
-   --template-uri "https://raw.githubusercontent.com/glennswest/ocpupi4azure/master/arm/azuredeploy.json" \
+   --template-uri ${arm_template_path} \
      --parameters "runit.parameters.json"
 ./openshift-install --dir=gw wait-for bootstrap-complete --log-level debug
 az vm stop --resource-group $1 --name bootstrap-0
