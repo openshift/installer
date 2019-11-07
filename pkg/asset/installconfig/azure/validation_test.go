c9df1b799b971104b9e6cb3cd4249a7e0e3bfc86
John Hixson <jhixson@redhat.com>
2019-11-06 19:01:34 -0800

    Azure: Add virtual network & subnet validations
    This code validates that all resources within a user provided network resource
    group are correct. The virtual network must be owned by the resource group, the
    subnets must be within the virtual network and contained within the same CIDR
    as the machine CIDR.
    
    Steps to generate the mock code:
    - go get github.com/golang/mock/gomock
    - go get github.com/golang/mock/mockgen
    - cd ${GOPATH}/src/github.com/openshift/installer/pkg/asset/installconfig/azure
    - mkdir mock
    - mockgen -source=./client.go -destination=mock/azureclient_generated.go -package=mock
    
    https://jira.coreos.com/browse/CORS-1210

---
+++ pkg/asset/installconfig/azure/validation_test.go
@@ -0,0 +1,158 @@
+package azure
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	aznetwork "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-12-01/network"
+	"github.com/golang/mock/gomock"
+	"github.com/openshift/installer/pkg/asset/installconfig/azure/mock"
+	"github.com/openshift/installer/pkg/ipnet"
+	"github.com/openshift/installer/pkg/types"
+	"github.com/openshift/installer/pkg/types/azure"
+	"github.com/stretchr/testify/assert"
+)
+
+type editFunctions []func(ic *types.InstallConfig)
+
+var (
+	validVirtualNetwork         = "valid-virtual-network"
+	validNetworkResourceGroup   = "valid-network-resource-group"
+	validRegion                 = "centralus"
+	validComputeSubnet          = "valid-compute-subnet"
+	validControlPlaneSubnet     = "valid-controlplane-subnet"
+	validCIDR                   = "10.0.0.0/16"
+	validComputeSubnetCIDR      = "10.0.0.0/24"
+	validControlPlaneSubnetCIDR = "10.0.32.0/24"
+
+	invalidateMachineCIDR = func(ic *types.InstallConfig) {
+		_, newCidr, _ := net.ParseCIDR("192.168.111.0/24")
+		ic.MachineCIDR = &ipnet.IPNet{IPNet: *newCidr}
+	}
+
+	invalidateNetworkResourceGroup = func(ic *types.InstallConfig) {
+		ic.Azure.NetworkResourceGroupName = "invalid-network-resource-group"
+	}
+	invalidateVirtualNetwork     = func(ic *types.InstallConfig) { ic.Azure.VirtualNetwork = "invalid-virtual-network" }
+	invalidateComputeSubnet      = func(ic *types.InstallConfig) { ic.Azure.ComputeSubnet = "invalid-compute-subnet" }
+	invalidateControlPlaneSubnet = func(ic *types.InstallConfig) { ic.Azure.ControlPlaneSubnet = "invalid-controlplane-subnet" }
+	invalidateRegion             = func(ic *types.InstallConfig) { ic.Azure.Region = "eastus" }
+	removeVirtualNetwork         = func(ic *types.InstallConfig) { ic.Azure.VirtualNetwork = "" }
+	removeSubnets                = func(ic *types.InstallConfig) { ic.Azure.ComputeSubnet, ic.Azure.ControlPlaneSubnet = "", "" }
+
+	virtualNetworkAPIResult = &aznetwork.VirtualNetwork{
+		Name: &validVirtualNetwork,
+	}
+	computeSubnetAPIResult = &aznetwork.Subnet{
+		Name: &validComputeSubnet,
+		SubnetPropertiesFormat: &aznetwork.SubnetPropertiesFormat{
+			AddressPrefix: &validComputeSubnetCIDR,
+		},
+	}
+	controlPlaneSubnetAPIResult = &aznetwork.Subnet{
+		Name: &validControlPlaneSubnet,
+		SubnetPropertiesFormat: &aznetwork.SubnetPropertiesFormat{
+			AddressPrefix: &validControlPlaneSubnetCIDR,
+		},
+	}
+)
+
+func validInstallConfig() *types.InstallConfig {
+	return &types.InstallConfig{
+		Networking: &types.Networking{
+			MachineCIDR: ipnet.MustParseCIDR(validCIDR),
+		},
+		Platform: types.Platform{
+			Azure: &azure.Platform{
+				Region:                   validRegion,
+				NetworkResourceGroupName: validNetworkResourceGroup,
+				VirtualNetwork:           validVirtualNetwork,
+				ComputeSubnet:            validComputeSubnet,
+				ControlPlaneSubnet:       validControlPlaneSubnet,
+			},
+		},
+	}
+}
+
+func TestAzureInstallConfigValidation(t *testing.T) {
+	cases := []struct {
+		name     string
+		edits    editFunctions
+		errorMsg string
+	}{
+		{
+			name:     "Valid virtual network & subnets",
+			edits:    editFunctions{},
+			errorMsg: "",
+		},
+		{
+			name:     "Valid install config without virtual network & subnets",
+			edits:    editFunctions{removeVirtualNetwork, removeSubnets},
+			errorMsg: "",
+		},
+		{
+			name:     "Invalid subnet range",
+			edits:    editFunctions{invalidateMachineCIDR},
+			errorMsg: "subnet .+ has an IP address range .+ outside of the MachineCIDR",
+		},
+		{
+			name:     "Invalid virtual network",
+			edits:    editFunctions{invalidateVirtualNetwork},
+			errorMsg: "invalid virtual network",
+		},
+		{
+			name:     "Invalid compute subnet",
+			edits:    editFunctions{invalidateComputeSubnet},
+			errorMsg: "failed to retrieve compute subnet",
+		},
+		{
+			name:     "Invalid control plane subnet",
+			edits:    editFunctions{invalidateControlPlaneSubnet},
+			errorMsg: "failed to retrieve control plane subnet",
+		},
+		{
+			name:     "Invalid both subnets",
+			edits:    editFunctions{invalidateControlPlaneSubnet, invalidateComputeSubnet},
+			errorMsg: "failed to retrieve compute subnet",
+		},
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	azureClient := mock.NewMockAPI(mockCtrl)
+
+	// VirtualNetwork
+	azureClient.EXPECT().GetVirtualNetwork(gomock.Any(), validNetworkResourceGroup, validVirtualNetwork).Return(virtualNetworkAPIResult, nil).AnyTimes()
+	azureClient.EXPECT().GetVirtualNetwork(gomock.Any(), gomock.Not(validNetworkResourceGroup), gomock.Not(validVirtualNetwork)).Return(&aznetwork.VirtualNetwork{}, fmt.Errorf("invalid network resource group")).AnyTimes()
+	azureClient.EXPECT().GetVirtualNetwork(gomock.Any(), validNetworkResourceGroup, gomock.Not(validVirtualNetwork)).Return(&aznetwork.VirtualNetwork{}, fmt.Errorf("invalid virtual network")).AnyTimes()
+
+	// ComputeSubnet
+	azureClient.EXPECT().GetComputeSubnet(gomock.Any(), validNetworkResourceGroup, validVirtualNetwork, validComputeSubnet).Return(computeSubnetAPIResult, nil).AnyTimes()
+	azureClient.EXPECT().GetComputeSubnet(gomock.Any(), gomock.Not(validNetworkResourceGroup), validVirtualNetwork, validComputeSubnet).Return(&aznetwork.Subnet{}, fmt.Errorf("invalid network resource group")).AnyTimes()
+	azureClient.EXPECT().GetComputeSubnet(gomock.Any(), validNetworkResourceGroup, gomock.Not(validVirtualNetwork), validComputeSubnet).Return(&aznetwork.Subnet{}, fmt.Errorf("invalid virtual network")).AnyTimes()
+	azureClient.EXPECT().GetComputeSubnet(gomock.Any(), validNetworkResourceGroup, validVirtualNetwork, gomock.Not(validComputeSubnet)).Return(&aznetwork.Subnet{}, fmt.Errorf("invalid compute subnet")).AnyTimes()
+
+	// ControlPlaneSubnet
+	azureClient.EXPECT().GetControlPlaneSubnet(gomock.Any(), validNetworkResourceGroup, validVirtualNetwork, validControlPlaneSubnet).Return(controlPlaneSubnetAPIResult, nil).AnyTimes()
+	azureClient.EXPECT().GetControlPlaneSubnet(gomock.Any(), gomock.Not(validNetworkResourceGroup), validVirtualNetwork, validControlPlaneSubnet).Return(&aznetwork.Subnet{}, fmt.Errorf("invalid network resource group")).AnyTimes()
+	azureClient.EXPECT().GetControlPlaneSubnet(gomock.Any(), validNetworkResourceGroup, gomock.Not(validVirtualNetwork), validControlPlaneSubnet).Return(&aznetwork.Subnet{}, fmt.Errorf("invalid virtual network")).AnyTimes()
+	azureClient.EXPECT().GetControlPlaneSubnet(gomock.Any(), validNetworkResourceGroup, validVirtualNetwork, gomock.Not(validControlPlaneSubnet)).Return(&aznetwork.Subnet{}, fmt.Errorf("invalid control plane subnet")).AnyTimes()
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			editedInstallConfig := validInstallConfig()
+			for _, edit := range tc.edits {
+				edit(editedInstallConfig)
+			}
+
+			aggregatedErrors := Validate(azureClient, editedInstallConfig)
+			if tc.errorMsg != "" {
+				assert.Regexp(t, tc.errorMsg, aggregatedErrors)
+			} else {
+				assert.NoError(t, aggregatedErrors)
+			}
+		})
+	}
+}
