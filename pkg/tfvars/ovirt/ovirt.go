30ca930fc728e60bfe42effea9e5999bb48ec102
Roy Golan <rgolan@redhat.com>
2019-11-14 15:13:45 +0200

    ovirt: BaseImage handling
    oVirt needs to upload the image which is pinned to the version
    of the installer, and create an ovirt template from it, specific to the
    installer openshift cluster.
    
    Since oVirt uses the same openstack image(for the ignition via
    cloud-init disk suport), I exposed openstack handling of the image and
    made both platform to invoke it.
    
    The result is that the image info is extracted from the version, the
    image is downloaded if not already under the cache, then tf uploaded an
    image to oVirt and creates a template out of it, and the template ID is
    used in the install config.
    
    Depends-on: https://github.com/oVirt/terraform-provider-ovirt/pull/184
    
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- pkg/tfvars/ovirt/ovirt.go
+++ pkg/tfvars/ovirt/ovirt.go
@@ -3,15 +3,20 @@ package ovirt
 
 import (
 	"encoding/json"
+
+	"github.com/openshift/installer/pkg/rhcos"
+	"github.com/openshift/installer/pkg/tfvars/internal/cache"
 )
 
 type config struct {
-	URL        string `json:"ovirt_url"`
-	Username   string `json:"ovirt_username"`
-	Password   string `json:"ovirt_password"`
-	Cafile     string `json:"ovirt_cafile,omitempty"`
-	ClusterID  string `json:"ovirt_cluster_id"`
-	TemplateID string `json:"ovirt_template_id"`
+	URL                    string `json:"ovirt_url"`
+	Username               string `json:"ovirt_username"`
+	Password               string `json:"ovirt_password"`
+	Cafile                 string `json:"ovirt_cafile,omitempty"`
+	ClusterID              string `json:"ovirt_cluster_id"`
+	TemplateID             string `json:"ovirt_template_id"`
+	BaseImageName          string `json:"openstack_base_image_name,omitempty"`
+	BaseImageLocalFilePath string `json:"openstack_base_image_local_file_path,omitempty"`
 }
 
 // TFVars generates ovirt-specific Terraform variables.
@@ -21,15 +26,28 @@ func TFVars(
 	enginePass string,
 	engineCafile string,
 	clusterID string,
-	templateID string) ([]byte, error) {
+	templateID string,
+	baseImageName string,
+	infraID string) ([]byte, error) {
 
 	cfg := config{
-		URL:        engineURL,
-		Username:   engineUser,
-		Password:   enginePass,
-		Cafile:     engineCafile,
-		ClusterID:  clusterID,
-		TemplateID: templateID,
+		URL:                    engineURL,
+		Username:               engineUser,
+		Password:               enginePass,
+		Cafile:                 engineCafile,
+		ClusterID:              clusterID,
+		TemplateID:             templateID,
+		BaseImageName:          baseImageName,
+	}
+
+	imageName, isURL := rhcos.GenerateOpenStackImageName(baseImageName, infraID)
+	cfg.BaseImageName = imageName
+	if isURL {
+		imageFilePath, err := cache.DownloadImageFile(imageName)
+		if err != nil {
+			return nil, err
+		}
+		cfg.BaseImageLocalFilePath = imageFilePath
 	}
 
 	return json.MarshalIndent(cfg, "", "  ")
