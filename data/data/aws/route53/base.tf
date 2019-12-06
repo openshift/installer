2eb483e42938fd9a8c80a6c7b3c823c968b548d7
Russell Bryant <rbryant@redhat.com>
2019-12-06 17:21:55 -0500

    aws: Drop TODO comments about a terraform upgrade
    These comments were about something to check related to a terraform
    upgrade.  The code is right as it is.  As recommended by the comment
    itself, these TODO comments should just be removed now that it has
    been checked.

--- data/data/aws/route53/base.tf
+++ data/data/aws/route53/base.tf
@@ -74,14 +74,6 @@ resource "aws_route53_record" "etcd_a_nodes" {
   ttl     = "60"
   zone_id = aws_route53_zone.int.zone_id
   name    = "etcd-${count.index}.${var.cluster_domain}"
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   records = [var.etcd_ip_addresses[count.index]]
 }
 
@@ -91,14 +83,6 @@ resource "aws_route53_record" "etcd_aaaa_nodes" {
   ttl     = "60"
   zone_id = aws_route53_zone.int.zone_id
   name    = "etcd-${count.index}.${var.cluster_domain}"
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   records = [var.etcd_ipv6_addresses[count.index]]
 }
 
