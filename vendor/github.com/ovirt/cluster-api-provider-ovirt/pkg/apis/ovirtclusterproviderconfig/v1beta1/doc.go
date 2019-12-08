85b8d1267c7837f5b8532bea694425c45eb7a4b8
Roy Golan <rgolan@redhat.com>
2019-12-08 14:26:27 +0200

    ovirt: update to latest cluster-api-provider
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- vendor/github.com/ovirt/cluster-api-provider-ovirt/pkg/apis/ovirtclusterproviderconfig/v1beta1/doc.go
+++
@@ -1,6 +0,0 @@
-// +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=github.com/rgolangh/cluster-api/cluster-api-provider-ovirt/pkg/apis/ovirtclusterproviderconfig
-// +k8s:openapi-gen=true
-// +k8s:defaulter-gen=TypeMeta
-// +groupName=ovirtclusterproviderconfig.machine.openshift.io
-package v1beta1
--- vendor/github.com/ovirt/cluster-api-provider-ovirt/pkg/apis/ovirtprovider/v1beta1/types.go
+++ vendor/github.com/ovirt/cluster-api-provider-ovirt/pkg/apis/ovirtprovider/v1beta1/types.go
@@ -42,8 +42,9 @@ type OvirtMachineProviderSpec struct {
 
 	// Name is the VM name
 	Name string `json:"name"`
+
 	// The VM template this instance will be created from
-	TemplateId string `json:"template_id"`
+	TemplateName string `json:"template_name"`
 
 	// the oVirt cluster this VM instance belongs too
 	ClusterId string `json:"cluster_id"`
@@ -80,8 +81,60 @@ type OvirtClusterProviderStatus struct {
 	// It includes Subnets and Router.
 }
 
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// OvirtMachineProviderStatus
+type OvirtMachineProviderStatus struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// InstanceID is the ID of the instance in GCP
+	// +optional
+	InstanceID *string `json:"instanceId,omitempty"`
+
+	// InstanceState is the provisioning state of the GCP Instance.
+	// +optional
+	InstanceState *string `json:"instanceState,omitempty"`
+
+	// Conditions is a set of conditions associated with the Machine to indicate
+	// errors or other status
+	Conditions []OvirtMachineProviderCondition `json:"conditions,omitempty"`
+}
+
+// OvirtMachineProviderConditionType is a valid value for OvirtMachineProviderCondition.Type
+type OvirtMachineProviderConditionType string
+
+// Valid conditions for an oVirt machine instance
+const (
+        // MachineCreated indicates whether the machine has been created or not. If not,
+        // it should include a reason and message for the failure.
+        MachineCreated OvirtMachineProviderConditionType = "MachineCreated"
+)
+
+// OvirtMachineProviderCondition is a condition in a OvirtMachineProviderStatus
+type OvirtMachineProviderCondition struct {
+        // Type is the type of the condition.
+        Type OvirtMachineProviderConditionType `json:"type"`
+        // Status is the status of the condition.
+        Status corev1.ConditionStatus `json:"status"`
+        // LastProbeTime is the last time we probed the condition.
+        // +optional
+        LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+        // LastTransitionTime is the last time the condition transitioned from one status to another.
+        // +optional
+        LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+        // Reason is a unique, one-word, CamelCase reason for the condition's last transition.
+        // +optional
+        Reason string `json:"reason,omitempty"`
+        // Message is a human-readable message indicating details about last transition.
+        // +optional
+        Message string `json:"message,omitempty"`
+}
+
 func init() {
 	SchemeBuilder.Register(&OvirtMachineProviderSpec{})
+	SchemeBuilder.Register(&OvirtMachineProviderStatus{})
 	SchemeBuilder.Register(&OvirtClusterProviderSpec{})
 	SchemeBuilder.Register(&OvirtClusterProviderStatus{})
 }
