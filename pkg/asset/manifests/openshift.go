bddc6e43534ee42a28a0f2ef6f3fc95993c4d14a
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-12-10 16:26:48 +0100

    Inject a setting to use community operators only

--- pkg/asset/manifests/openshift.go
+++ pkg/asset/manifests/openshift.go
@@ -60,6 +60,7 @@ func (o *Openshift) Dependencies() []asset.Asset {
 		&openshift.KubeadminPasswordSecret{},
 		&openshift.RoleCloudCredsSecretReader{},
 		&openshift.PrivateClusterOutbound{},
+		&openshift.CommunityOperators{},
 	}
 }
 
@@ -179,13 +180,16 @@ func (o *Openshift) Generate(dependencies asset.Parents) error {
 	cloudCredsSecret := &openshift.CloudCredsSecret{}
 	kubeadminPasswordSecret := &openshift.KubeadminPasswordSecret{}
 	roleCloudCredsSecretReader := &openshift.RoleCloudCredsSecretReader{}
+	communityOperatorsSetting := &openshift.CommunityOperators{}
 	dependencies.Get(
 		cloudCredsSecret,
 		kubeadminPasswordSecret,
+		communityOperatorsSetting,
 		roleCloudCredsSecretReader)
 
 	assetData := map[string][]byte{
 		"99_kubeadmin-password-secret.yaml": applyTemplateData(kubeadminPasswordSecret.Files()[0].Data, templateData),
+		"99_community_operators.yaml":       applyTemplateData(communityOperatorsSetting.Files()[0].Data, templateData),
 	}
 
 	switch platform {
