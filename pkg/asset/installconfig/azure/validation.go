bbbe3407bf641a31b0eec143f95836c1860e1e98
Clayton Coleman <ccoleman@redhat.com>
2019-12-16 20:04:11 -0500

    asset: Update assets to tolerate multiple machine networks
    Like the change to the core, update validation messages to
    indicate the specified IPs must be within one of the networks
    in preparation for dual-stack support.
    
    Update the noproxy settings to include all machine and service
    networks, not just the first ones.

--- pkg/asset/installconfig/azure/validation.go
+++ pkg/asset/installconfig/azure/validation.go
@@ -6,7 +6,6 @@ import (
 	"net"
 
 	aznetwork "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-12-01/network"
-	"github.com/openshift/installer/pkg/ipnet"
 	aztypes "github.com/openshift/installer/pkg/types/azure"
 
 	"github.com/openshift/installer/pkg/types"
@@ -16,12 +15,13 @@ import (
 // Validate executes platform-specific validation.
 func Validate(client API, ic *types.InstallConfig) error {
 	allErrs := field.ErrorList{}
-	allErrs = append(allErrs, validateNetworks(client, ic.Azure, ic.Networking.MachineCIDR, field.NewPath("platform").Child("azure"))...)
+
+	allErrs = append(allErrs, validateNetworks(client, ic.Azure, ic.Networking.MachineNetwork, field.NewPath("platform").Child("azure"))...)
 	return allErrs.ToAggregate()
 }
 
 // validateNetworks checks that the user-provided VNet and subnets are valid.
-func validateNetworks(client API, p *aztypes.Platform, machineCIDR *ipnet.IPNet, fieldPath *field.Path) field.ErrorList {
+func validateNetworks(client API, p *aztypes.Platform, machineNetworks []types.MachineNetworkEntry, fieldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
 	if p.VirtualNetwork != "" {
@@ -35,21 +35,21 @@ func validateNetworks(client API, p *aztypes.Platform, machineCIDR *ipnet.IPNet,
 			return append(allErrs, field.Invalid(fieldPath.Child("computeSubnet"), p.ComputeSubnet, "failed to retrieve compute subnet"))
 		}
 
-		allErrs = append(allErrs, validateSubnet(client, machineCIDR, fieldPath.Child("computeSubnet"), computeSubnet, p.ComputeSubnet)...)
+		allErrs = append(allErrs, validateSubnet(client, fieldPath.Child("computeSubnet"), computeSubnet, p.ComputeSubnet, machineNetworks)...)
 
 		controlPlaneSubnet, err := client.GetControlPlaneSubnet(context.TODO(), p.NetworkResourceGroupName, p.VirtualNetwork, p.ControlPlaneSubnet)
 		if err != nil {
 			return append(allErrs, field.Invalid(fieldPath.Child("controlPlaneSubnet"), p.ControlPlaneSubnet, "failed to retrieve control plane subnet"))
 		}
 
-		allErrs = append(allErrs, validateSubnet(client, machineCIDR, fieldPath.Child("controlPlaneSubnet"), controlPlaneSubnet, p.ControlPlaneSubnet)...)
+		allErrs = append(allErrs, validateSubnet(client, fieldPath.Child("controlPlaneSubnet"), controlPlaneSubnet, p.ControlPlaneSubnet, machineNetworks)...)
 	}
 
 	return allErrs
 }
 
 // validateSubnet checks that the subnet is in the same network as the machine CIDR
-func validateSubnet(client API, machineCIDR *ipnet.IPNet, fieldPath *field.Path, subnet *aznetwork.Subnet, subnetName string) field.ErrorList {
+func validateSubnet(client API, fieldPath *field.Path, subnet *aznetwork.Subnet, subnetName string, networks []types.MachineNetworkEntry) field.ErrorList {
 	allErrs := field.ErrorList{}
 
 	subnetIP, _, err := net.ParseCIDR(*subnet.AddressPrefix)
@@ -57,10 +57,15 @@ func validateSubnet(client API, machineCIDR *ipnet.IPNet, fieldPath *field.Path,
 		return append(allErrs, field.Invalid(fieldPath, subnetName, "unable to parse subnet CIDR"))
 	}
 
-	if !machineCIDR.Contains(subnetIP) {
-		errMsg := fmt.Sprintf("subnet %v has an IP address range %v outside of the MachineCIDR %v", subnetName, subnet.AddressPrefix, machineCIDR)
-		return append(allErrs, field.Invalid(fieldPath, subnetName, errMsg))
-	}
+	allErrs = append(allErrs, validateMachineNetworksContainIP(fieldPath, networks, *subnet.Name, subnetIP)...)
+	return allErrs
+}
 
-	return nil
+func validateMachineNetworksContainIP(fldPath *field.Path, networks []types.MachineNetworkEntry, subnetName string, ip net.IP) field.ErrorList {
+	for _, network := range networks {
+		if network.CIDR.Contains(ip) {
+			return nil
+		}
+	}
+	return field.ErrorList{field.Invalid(fldPath, subnetName, fmt.Sprintf("subnet %s address prefix is outside of the specified machine networks", ip))}
 }
