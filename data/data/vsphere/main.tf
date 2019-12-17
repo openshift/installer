60e6212d3d97d77f69c27c95bdaa65178a5e5a72
Joseph Callen <jcallen@redhat.com>
2019-12-17 16:58:44 -0500

    Adding terraform for vSphere IPI; changes to tfvars to support it Added tags to vSphere vCenter objects that will need to be destroyed. Removed the ability to create a resource pool.  Using the cluster default RP inplace.  The customer can move the machines to a properly configured RP if so desired.

---
+++ data/data/vsphere/main.tf
@@ -0,0 +1,97 @@
+provider "vsphere" {
+  user                 = var.vsphere_username
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_url
+  allow_unverified_ssl = false
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_template
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_tag_category" "category" {
+  name        = "openshift-${var.cluster_id}"
+  description = "Added by openshift-install do not remove"
+  cardinality = "SINGLE"
+
+  associable_types = [
+    "VirtualMachine",
+    "ResourcePool",
+    "Folder"
+  ]
+}
+
+resource "vsphere_tag" "tag" {
+  name        = var.cluster_id
+  category_id = vsphere_tag_category.category.id
+  description = "Added by openshift-install do not remove"
+}
+
+resource "vsphere_folder" "folder" {
+  path          = var.vsphere_folder
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  tags          = [vsphere_tag.tag.id]
+}
+
+
+module "bootstrap" {
+  source = "./bootstrap"
+
+  ignition      = var.ignition_bootstrap
+  resource_pool = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore     = data.vsphere_datastore.datastore.id
+  folder        = vsphere_folder.folder.path
+  network       = data.vsphere_network.network.id
+  datacenter    = data.vsphere_datacenter.datacenter.id
+  template      = data.vsphere_virtual_machine.template.id
+  guest_id      = data.vsphere_virtual_machine.template.guest_id
+
+  cluster_id = var.cluster_id
+  tags       = [vsphere_tag.tag.id]
+}
+
+module "master" {
+  source = "./master"
+
+  // limitation of baremetal-runtimecfg.  The hostname must be master
+  name           = "master"
+  instance_count = var.master_count
+  ignition       = var.ignition_master
+
+  resource_pool = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore     = data.vsphere_datastore.datastore.id
+  folder        = vsphere_folder.folder.path
+  network       = data.vsphere_network.network.id
+  datacenter    = data.vsphere_datacenter.datacenter.id
+  template      = data.vsphere_virtual_machine.template.id
+  guest_id      = data.vsphere_virtual_machine.template.guest_id
+  tags          = [vsphere_tag.tag.id]
+
+  cluster_domain = var.cluster_domain
+  cluster_id     = var.cluster_id
+  memory         = var.control_plane_memory_mib
+  num_cpus       = var.control_plane_num_cpus
+  disk_size      = var.control_plane_disk_gib
+}
+
---
+++ pkg/tfvars/vsphere/vsphere.go
@@ -0,0 +1,52 @@
+package vsphere
+
+import (
+	"encoding/json"
+
+	vsphereapis "github.com/openshift/machine-api-operator/pkg/apis/vsphereprovider/v1alpha1"
+)
+
+type config struct {
+	VSphereURL      string `json:"vsphere_url"`
+	VSphereUsername string `json:"vsphere_username"`
+	VSpherePassword string `json:"vsphere_password"`
+	MemoryMiB       int64  `json:"control_plane_memory_mib"`
+	DiskGiB         int32  `json:"control_plane_disk_gib"`
+	NumCPUs         int32  `json:"control_plane_num_cpus"`
+	Cluster         string `json:"vsphere_cluster"`
+	Datacenter      string `json:"vsphere_datacenter"`
+	Datastore       string `json:"vsphere_datastore"`
+	Folder          string `json:"vsphere_folder"`
+	Network         string `json:"vsphere_network"`
+	Template        string `json:"vsphere_template"`
+}
+
+// TFVarsSources contains the parameters to be converted into Terraform variables
+type TFVarsSources struct {
+	ControlPlaneConfigs []*vsphereapis.VSphereMachineProviderSpec
+	Username            string
+	Password            string
+	Cluster             string
+}
+
+//TFVars generate vSphere-specific Terraform variables
+func TFVars(sources TFVarsSources) ([]byte, error) {
+	controlPlaneConfig := sources.ControlPlaneConfigs[0]
+
+	cfg := &config{
+		VSphereURL:      controlPlaneConfig.Workspace.Server,
+		VSphereUsername: sources.Username,
+		VSpherePassword: sources.Password,
+		MemoryMiB:       controlPlaneConfig.MemoryMiB,
+		DiskGiB:         controlPlaneConfig.DiskGiB,
+		NumCPUs:         controlPlaneConfig.NumCPUs,
+		Cluster:         sources.Cluster,
+		Datacenter:      controlPlaneConfig.Workspace.Datacenter,
+		Datastore:       controlPlaneConfig.Workspace.Datastore,
+		Folder:          controlPlaneConfig.Workspace.Folder,
+		Network:         controlPlaneConfig.Network.Devices[0].NetworkName,
+		Template:        controlPlaneConfig.Template,
+	}
+
+	return json.MarshalIndent(cfg, "", "  ")
+}
