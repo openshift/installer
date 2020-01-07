928ab9022d670fc4b87f95376ea5d4fb125a3732
Alex Crawford <crawford@redhat.com>
2020-01-06 18:17:47 -0800

    *: enable s390x bootimage support
    (cherry picked from commit b7d74740800b6f8e754a91d2934ffbfca0dd6b8f)

--- pkg/types/validation/installconfig_test.go
+++ pkg/types/validation/installconfig_test.go
@@ -925,7 +925,15 @@ func TestValidateInstallConfig(t *testing.T) {
 				return c
 			}(),
 		},
-		// TODO(crawford): add a test to validate that homogeneous clusters are enforced once an additional architecture is added
+		{
+			name: "cluster is not heteregenous",
+			installConfig: func() *types.InstallConfig {
+				c := validInstallConfig()
+				c.Compute[0].Architecture = types.ArchitectureS390X
+				return c
+			}(),
+			expectedError: `^compute\[0\].architecture: Invalid value: "s390x": heteregeneous multi-arch is not supported; compute pool architecture must match control plane$`,
+		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
