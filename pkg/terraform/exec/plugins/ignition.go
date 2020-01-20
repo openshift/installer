2b4926045546e65001f5c3fe32ed07c9cfb297b6
Christian Glombek <cglombek@redhat.com>
2020-01-20 17:19:12 +0100

    pkg/terraform/exec/plugins: Use Ignition v2 provider

--- pkg/terraform/exec/plugins/ignition.go
+++ pkg/terraform/exec/plugins/ignition.go
@@ -2,7 +2,7 @@ package plugins
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/terraform-providers/terraform-provider-ignition/ignition"
+	"github.com/terraform-providers/terraform-provider-ignition/v2/ignition"
 )
 
 func init() {
