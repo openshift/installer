bc4e0e53bc1f40da373d8b147ca950745213a383
Mike Fedosin <mfedosin@redhat.com>
2020-01-22 13:46:11 +0100

    Bump Ignition and Gophercloud
    The newest version includes important features, like HTTP headers support,
    that are required for the successful installation of OpenShift on OpenStack
    without Swift.

--- vendor/github.com/coreos/ignition/config/v1/types/config.go
+++ vendor/github.com/coreos/ignition/config/v1/types/config.go
@@ -14,8 +14,16 @@
 
 package types
 
+import "github.com/coreos/go-semver/semver"
+
 const (
 	Version = 1
+)
+
+var (
+	MaxVersion = semver.Version{
+		Major: Version,
+	}
 )
 
 type Config struct {
--- vendor/github.com/coreos/ignition/config/v2_1/append.go
+++ vendor/github.com/coreos/ignition/config/v2_1/append.go
@@ -64,7 +64,11 @@ func appendStruct(vOld, vNew reflect.Value) reflect.Value {
 		case reflect.Slice:
 			vfRes.Set(reflect.AppendSlice(vfOld, vfNew))
 		default:
-			vfRes.Set(vfNew)
+			if vfNew.Kind() == reflect.Ptr && vfNew.IsNil() {
+				vfRes.Set(vfOld)
+			} else {
+				vfRes.Set(vfNew)
+			}
 		}
 	}
 
