2995e59a0b21e1b56d60be41ff11fa235f2dad37
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-02-07 15:58:55 +0100

    bootstrap: remove duplicated report-progress.sh file
    This ensures installer doesn't generate boostrap ignition with
    duplicate entries for this file. Its valid in Ignition v2 but incorrect
    in Ignition v3

--- data/data/bootstrap/files/usr/local/bin/report-progress.sh
+++ data/data/bootstrap/files/usr/local/bin/report-progress.sh
@@ -12,6 +12,9 @@ wait_for_existance() {
 echo "Waiting for bootstrap to complete..."
 wait_for_existance /opt/openshift/.bootkube.done
 
+## remove the routes setup so that we can open up the blackhole
+systemctl stop gcp-routes.service || true
+
 echo "Reporting install progress..."
 while ! oc --config="$KUBECONFIG" create -f - <<-EOF
 	apiVersion: v1
