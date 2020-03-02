81a4cf61eab95cdd8a9b4d511160e1db2abb4416
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-03-02 18:57:57 +0100

    [OKD only] assets: add community operators only setting enabled on OKD

--- pkg/asset/manifests/openshift.go
+++ pkg/asset/manifests/openshift.go
@@ -65,6 +65,7 @@ func (o *Openshift) Dependencies() []asset.Asset {
 		&openshift.RoleCloudCredsSecretReader{},
 		&openshift.PrivateClusterOutbound{},
 		&openshift.BaremetalConfig{},
+		&openshift.CommunityOperators{},
 		new(rhcos.Image),
 	}
 }
@@ -188,6 +189,7 @@ func (o *Openshift) Generate(dependencies asset.Parents) error {
 	kubeadminPasswordSecret := &openshift.KubeadminPasswordSecret{}
 	roleCloudCredsSecretReader := &openshift.RoleCloudCredsSecretReader{}
 	baremetalConfig := &openshift.BaremetalConfig{}
+	communityOperatorsSetting := &openshift.CommunityOperators{}
 	rhcosImage := new(rhcos.Image)
 
 	dependencies.Get(
@@ -195,10 +197,14 @@ func (o *Openshift) Generate(dependencies asset.Parents) error {
 		kubeadminPasswordSecret,
 		roleCloudCredsSecretReader,
 		baremetalConfig,
+		communityOperatorsSetting,
 		rhcosImage)
 
 	assetData := map[string][]byte{
 		"99_kubeadmin-password-secret.yaml": applyTemplateData(kubeadminPasswordSecret.Files()[0].Data, templateData),
+	}
+	if installConfig.Config.IsOKD() {
+		assetData["99_community_operators.yaml"] = applyTemplateData(communityOperatorsSetting.Files()[0].Data, templateData)
 	}
 
 	switch platform {
