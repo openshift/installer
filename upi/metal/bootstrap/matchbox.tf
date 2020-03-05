26b2d2afda1404f6a4914bd3925441671c4964bb
Christian Glombek <cglombek@redhat.com>
2020-03-05 04:59:23 +0100

    upi/metal: Terraform 0.12.x syntax updates

--- upi/metal/bootstrap/matchbox.tf
+++ upi/metal/bootstrap/matchbox.tf
@@ -1,25 +1,25 @@
 resource "matchbox_profile" "bootstrap" {
   name   = "${var.cluster_id}-bootstrap"
-  kernel = "${var.pxe_kernel}"
+  kernel = var.pxe_kernel
 
   initrd = [
-    "${var.pxe_initrd}",
+    var.pxe_initrd,
   ]
 
-  args = [
-    "${var.pxe_kernel_args}",
-    "coreos.inst.ignition_url=${var.matchbox_http_endpoint}/ignition?cluster_id=${var.cluster_id}&role=bootstrap",
-  ]
+  args = concat(
+    var.pxe_kernel_args,
+    ["coreos.inst.ignition_url=${var.matchbox_http_endpoint}/ignition?cluster_id=${var.cluster_id}&role=bootstrap"],
+  )
 
-  raw_ignition = "${var.igntion_config_content}"
+  raw_ignition = var.igntion_config_content
 }
 
 resource "matchbox_group" "bootstrap" {
   name    = "${var.cluster_id}-bootstrap"
-  profile = "${matchbox_profile.bootstrap.name}"
+  profile = matchbox_profile.bootstrap.name
 
-  selector {
-    cluster_id = "${var.cluster_id}"
+  selector = {
+    cluster_id = var.cluster_id
     role       = "bootstrap"
   }
 }
