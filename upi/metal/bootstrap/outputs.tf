4d69ccc8610d41169a9a94acdd8c5133adbb442b
Christian Glombek <cglombek@redhat.com>
2020-03-05 04:59:23 +0100

    upi/metal: Terraform 0.12.x syntax updates

--- upi/metal/bootstrap/outputs.tf
+++ upi/metal/bootstrap/outputs.tf
@@ -1,11 +1,11 @@
 output "device_ip" {
-  value = "${packet_device.bootstrap.network.0.address}"
+  value = packet_device.bootstrap.network[0].address
 }
 
 output "device_hostname" {
-  value = "${packet_device.bootstrap.hostname}"
+  value = packet_device.bootstrap.hostname
 }
 
 output "device_id" {
-  value = "${packet_device.bootstrap.id}"
+  value = packet_device.bootstrap.id
 }
