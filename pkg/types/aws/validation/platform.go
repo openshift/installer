9ca048ea66a0eb9c181bf3840039b38a267012fb
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2020-03-10 13:26:15 -0700

    pkg/types/aws: add service endpoints to aws platform
    Adds a list of service endpoints to the AWS platform. A service endpoint entry/item consists of the service name and the endpoint url.
    
    only one service endpoint for a service can be provided, i.e duplicates are not allowed.
    the endpoint URL must be a hostname or a http(s) URL with no request path or requests queries.

--- pkg/types/aws/validation/platform.go
+++ pkg/types/aws/validation/platform.go
@@ -1,6 +1,9 @@
 package validation
 
 import (
+	"fmt"
+	"net/url"
+	"regexp"
 	"sort"
 
 	"k8s.io/apimachinery/pkg/util/validation/field"
@@ -56,8 +59,55 @@ func ValidatePlatform(p *aws.Platform, fldPath *field.Path) field.ErrorList {
 	if _, ok := Regions[p.Region]; !ok {
 		allErrs = append(allErrs, field.NotSupported(fldPath.Child("region"), p.Region, validRegionValues))
 	}
+
+	allErrs = append(allErrs, validateServiceEndpoints(p.ServiceEndpoints, fldPath.Child("serviceEndpoints"))...)
+
 	if p.DefaultMachinePlatform != nil {
 		allErrs = append(allErrs, ValidateMachinePool(p, p.DefaultMachinePlatform, fldPath.Child("defaultMachinePlatform"))...)
 	}
 	return allErrs
+}
+
+func validateServiceEndpoints(endpoints []aws.ServiceEndpoint, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	tracker := map[string]int{}
+	for idx, e := range endpoints {
+		fldp := fldPath.Index(idx)
+		if eidx, ok := tracker[e.Name]; ok {
+			allErrs = append(allErrs, field.Invalid(fldp.Child("name"), e.Name, fmt.Sprintf("duplicate service endpoint not allowed for %s, service endpoint already defined at %s", e.Name, fldPath.Index(eidx))))
+		} else {
+			tracker[e.Name] = idx
+		}
+
+		if err := validateServiceURL(e.URL); err != nil {
+			allErrs = append(allErrs, field.Invalid(fldp.Child("url"), e.URL, err.Error()))
+		}
+	}
+	return allErrs
+}
+
+var schemeRE = regexp.MustCompile("^([^:]+)://")
+
+func validateServiceURL(uri string) error {
+	endpoint := uri
+	if !schemeRE.MatchString(endpoint) {
+		scheme := "https"
+		endpoint = fmt.Sprintf("%s://%s", scheme, endpoint)
+	}
+
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return err
+	}
+	if u.Hostname() == "" {
+		return fmt.Errorf("host cannot be empty, empty host provided")
+	}
+	if s := u.Scheme; s != "https" {
+		return fmt.Errorf("invalid scheme %s, only https allowed", s)
+	}
+	if r := u.RequestURI(); r != "/" {
+		return fmt.Errorf("no path or request parameters must be provided, %q was provided", r)
+	}
+
+	return nil
 }
