c1329de152b5d38f559b609bc30968478fee8eaa
Mike Fedosin <mfedosin@redhat.com>
2020-04-01 23:34:11 +0200

    Add type validations
    This commit adds validations that all provided network and security
    group ids are UUID v4 strings.

--- pkg/types/openstack/validation/machinepool.go
+++ pkg/types/openstack/validation/machinepool.go
@@ -3,6 +3,7 @@ package validation
 import (
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
+	guuid "github.com/google/uuid"
 	"github.com/openshift/installer/pkg/types/openstack"
 )
 
@@ -20,5 +21,35 @@ func ValidateMachinePool(p *openstack.MachinePool, fldPath *field.Path) field.Er
 		}
 	}
 
+	allErrs = append(allErrs, validateUUIDV4s(p.AdditionalNetworkIDs, fldPath.Child("additionalNetworkIDs"))...)
+	allErrs = append(allErrs, validateUUIDV4s(p.AdditionalSecurityGroupIDs, fldPath.Child("additionalSecurityGroupIDs"))...)
+
 	return allErrs
+}
+
+func validateUUIDV4s(input []string, fldPath *field.Path) field.ErrorList {
+	allErrs := field.ErrorList{}
+	for idx, uuid := range input {
+		if !validUUIDv4(uuid) {
+			allErrs = append(allErrs, field.Invalid(fldPath.Index(idx), uuid, "valid UUID v4 must be specified"))
+		}
+	}
+
+	return allErrs
+}
+
+// validUUIDv4 checks if string is in UUID v4 format
+// For more information: https://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_(random)
+func validUUIDv4(s string) bool {
+	uuid, err := guuid.Parse(s)
+	if err != nil {
+		return false
+	}
+
+	// check that version of the uuid
+	if uuid.Version().String() != "VERSION_4" {
+		return false
+	}
+
+	return true
 }
--- pkg/types/openstack/validation/machinepool_test.go
+++ pkg/types/openstack/validation/machinepool_test.go
@@ -56,6 +56,70 @@ func TestValidateMachinePool(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "valid additional network ids",
+			pool: &openstack.MachinePool{
+				AdditionalNetworkIDs: []string{
+					"51e5fe10-5325-4a32-bce8-7ebe9708c453",
+					"3ade1375-acfd-4eda-90be-3530af4f25ec",
+					"460e993b-e932-43c6-a7a2-e51ca58f4eae",
+				},
+			},
+		},
+		{
+			name: "invalid additional network ids",
+			pool: &openstack.MachinePool{
+				AdditionalNetworkIDs: []string{
+					"51e5fe10-5325-4a32-bce8-7ebe9708c453",
+					"INVALID",
+					"",
+				},
+			},
+			expected: `^\[test-path.additionalNetworkIDs\[1\]: Invalid value: \"INVALID\": valid UUID v4 must be specified, test-path.additionalNetworkIDs\[2\]: Invalid value: \"\": valid UUID v4 must be specified\]$`,
+		},
+		{
+			name: "wrong additional network ids version",
+			pool: &openstack.MachinePool{
+				AdditionalNetworkIDs: []string{
+					"25b91ff0-75c3-11ea-9aff-4fc68ed06d45", // VERSION_1
+					"bd15ec47-a3ec-329b-812b-9c617ca86881", // VERSION_3
+					"39499c61-11eb-5f02-a519-f2e38575cedd", // VERSION_5
+				},
+			},
+			expected: `^\[test-path.additionalNetworkIDs\[0\]: Invalid value: \"25b91ff0-75c3-11ea-9aff-4fc68ed06d45\": valid UUID v4 must be specified, test-path.additionalNetworkIDs\[1\]: Invalid value: \"bd15ec47-a3ec-329b-812b-9c617ca86881\": valid UUID v4 must be specified, test-path.additionalNetworkIDs\[2\]: Invalid value: \"39499c61-11eb-5f02-a519-f2e38575cedd\": valid UUID v4 must be specified\]$`,
+		},
+		{
+			name: "valid additional security group ids",
+			pool: &openstack.MachinePool{
+				AdditionalSecurityGroupIDs: []string{
+					"51e5fe10-5325-4a32-bce8-7ebe9708c453",
+					"3ade1375-acfd-4eda-90be-3530af4f25ec",
+					"460e993b-e932-43c6-a7a2-e51ca58f4eae",
+				},
+			},
+		},
+		{
+			name: "invalid additional security group ids",
+			pool: &openstack.MachinePool{
+				AdditionalSecurityGroupIDs: []string{
+					"51e5fe10-5325-4a32-bce8-7ebe9708c453",
+					"INVALID",
+					"",
+				},
+			},
+			expected: `^\[test-path.additionalSecurityGroupIDs\[1\]: Invalid value: \"INVALID\": valid UUID v4 must be specified, test-path.additionalSecurityGroupIDs\[2\]: Invalid value: \"\": valid UUID v4 must be specified\]$`,
+		},
+		{
+			name: "wrong additional security group ids version",
+			pool: &openstack.MachinePool{
+				AdditionalSecurityGroupIDs: []string{
+					"25b91ff0-75c3-11ea-9aff-4fc68ed06d45", // VERSION_1
+					"bd15ec47-a3ec-329b-812b-9c617ca86881", // VERSION_3
+					"39499c61-11eb-5f02-a519-f2e38575cedd", // VERSION_5
+				},
+			},
+			expected: `^\[test-path.additionalSecurityGroupIDs\[0\]: Invalid value: \"25b91ff0-75c3-11ea-9aff-4fc68ed06d45\": valid UUID v4 must be specified, test-path.additionalSecurityGroupIDs\[1\]: Invalid value: \"bd15ec47-a3ec-329b-812b-9c617ca86881\": valid UUID v4 must be specified, test-path.additionalSecurityGroupIDs\[2\]: Invalid value: \"39499c61-11eb-5f02-a519-f2e38575cedd\": valid UUID v4 must be specified\]$`,
+		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
