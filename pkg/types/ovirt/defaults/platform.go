615242b644953b559dd2c0d09f01b5127d7eaaa9
Roy Golan <rgolan@redhat.com>
2020-04-03 14:02:03 +0300

    ovirt: add vnic profile
    The vnic profile that all the VM shares can now be configurable through
    the platform and is part of the wizard, in case there are multiple for the
    selected network.
    
    ```yaml
    platform:
      ovirt:
        vnicProfileID: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
    ```
    
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- pkg/types/ovirt/defaults/platform.go
+++ pkg/types/ovirt/defaults/platform.go
@@ -4,8 +4,12 @@ import (
 	"github.com/openshift/installer/pkg/types/ovirt"
 )
 
+// DefaultNetworkName is the default network name to use in a cluster
+const DefaultNetworkName = "ovirtmgmt"
+
 // SetPlatformDefaults sets the defaults for the platform.
 func SetPlatformDefaults(p *ovirt.Platform) {
-	// no platform defaults
-	return
+	if p.NetworkName == "" {
+		p.NetworkName = DefaultNetworkName
+	}
 }
