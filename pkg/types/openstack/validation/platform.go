noironetworks/openshift_installer

Changes in error cases

pkg/types/openstack/validation/platform.go
@@ -6,6 +6,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
+	"github.com/openshift/installer/pkg/ipnet"
 	"github.com/openshift/installer/pkg/types"
 	"github.com/openshift/installer/pkg/types/openstack"
 	"github.com/openshift/installer/pkg/validate"
@@ -73,8 +74,7 @@ func ValidatePlatform(p *openstack.Platform, n *types.Networking, fldPath *field
 		if err != nil {
 			ip := net.ParseIP(p.AciNetExt.ClusterSNATSubnet)
 			if ip == nil {
-				// error
-				allErrs = append(allErrs, field.Invalid(fldPath.Child("ClusterSNATSubnet"), p.AciNetExt.ClusterSNATSubnet, err.Error()))
+				allErrs = append(allErrs, field.Invalid(fldPath.Child("clusterSNATPolicyIP"), p.AciNetExt.ClusterSNATSubnet, err.Error()))
 			}	
 		}
 	}
@@ -85,11 +85,35 @@ func ValidatePlatform(p *openstack.Platform, n *types.Networking, fldPath *field
                 if err != nil {
                         ip := net.ParseIP(p.AciNetExt.ClusterSNATDest)
                         if ip == nil {
-                                // error
-                                allErrs = append(allErrs, field.Invalid(fldPath.Child("ClusterSNATDest"), p.AciNetExt.ClusterSNATDest, err.Error()))
+                                allErrs = append(allErrs, field.Invalid(fldPath.Child("clusterSNATPolicyDestIP"), p.AciNetExt.ClusterSNATDest, err.Error()))
                         }
                 }
         }
+
+	machineMask := n.MachineCIDR.Mask
+	if p.AciNetExt.NeutronCIDR.String() != "" {
+                neutronMask := p.AciNetExt.NeutronCIDR.Mask
+                if machineMask.String() != neutronMask.String() {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("neutronCIDR"), p.AciNetExt.NeutronCIDR.String(), "The CIDRs specified in the machineCIDR (" + n.MachineCIDR.String() + ") and neutronCIDR (" + p.AciNetExt.NeutronCIDR.String() + ") configurations in the install-config.yaml have different subnet masks (machineCIDR mask: " + machineMask.String() + ", neutronCIDR mask: " + neutronMask.String()))
+                }
+        } else {
+		// If no neutron CIDR provided, set it to 192.168.0.0 with the machine CIDR mask
+                neutronIP := net.ParseIP("192.168.0.0")
+                p.AciNetExt.NeutronCIDR = &ipnet.IPNet{
+                                        IPNet: net.IPNet{
+                                                IP:   neutronIP,
+                                                Mask: machineMask,
+                                        },
+                                }
+        }
+        n.NeutronCIDR = p.AciNetExt.NeutronCIDR
+
+	_, err = ipnet.ParseCIDR(p.AciNetExt.InstallerHostSubnet)
+	if err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("installerHostSubnet"),
+                		p.AciNetExt.InstallerHostSubnet, "installerHostSubnet has an invalid subnet value (" + p.AciNetExt.InstallerHostSubnet + ")"))
+	}
+
 	return allErrs
 }
 

pkg/types/validation/installconfig.go
@@ -172,24 +172,24 @@ func ValidateInstallConfig(c *types.InstallConfig, openStackValidValuesFetcher o
                         // Validate against values from install config
 
                         if (strconv.Itoa(config.InfraVLAN) != c.Platform.OpenStack.AciNetExt.InfraVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("InfraVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.InfraVLAN, "InfraVLAN values in acc-provision input and install config have to be the same"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("infraVLAN"),
+                                        c.Platform.OpenStack.AciNetExt.InfraVLAN, "infraVLAN values in acc-provision input(" + strconv.Itoa(config.InfraVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.InfraVLAN + ") have to be the same"))
                         }
                         if (strconv.Itoa(config.ServiceVLAN) != c.Platform.OpenStack.AciNetExt.ServiceVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("ServiceVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.ServiceVLAN, "ServiceVLAN values in acc-provision input and install config have to be the same"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("serviceVLAN"),
+                                        c.Platform.OpenStack.AciNetExt.ServiceVLAN, "serviceVLAN values in acc-provision input(" + strconv.Itoa(config.ServiceVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.ServiceVLAN + ") have to be the same"))
                         }
                         if (strconv.Itoa(config.KubeApiVLAN) != c.Platform.OpenStack.AciNetExt.KubeApiVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("KubeApiVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.KubeApiVLAN, "KubeApiVLAN values in acc-provision input and install config have to be the same"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("kubeApiVLAN"),
+                                        c.Platform.OpenStack.AciNetExt.KubeApiVLAN, "kubeApiVLAN values in acc-provision input(" + strconv.Itoa(config.KubeApiVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.KubeApiVLAN + ") have to be the same"))
                         }
                         if DiffSubnets(config.NodeSubnet, machineCIDR) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("MachineCIDR"),
-                                        c.Networking.MachineCIDR, "node_subnet in acc-provision input has to be the same as machineCIDR"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("machineCIDR"),
+                                        c.Networking.MachineCIDR.String(), "node_subnet in acc-provision input(" + config.NodeSubnet + ") has to be the same as machineCIDR in install-config.yaml(" + machineCIDR.String() + ")"))
                         }
                         if DiffSubnets(config.PodSubnet, clusterNetworkCIDR) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("ClusterNetworkCIDR"),
-                                        clusterNetworkCIDR, "pod_subnet in acc-provision input has to be the same as clusterNetwork CIDR"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("clusterNetworkCIDR"),
+                                        clusterNetworkCIDR.String(), "pod_subnet in acc-provision input(" + config.PodSubnet + ") has to be the same as clusterNetwork:cidr in install-config.yaml(" + clusterNetworkCIDR.String() + ")"))
                         }
 		}
 	}

openshift/installer

pkg/types/openstack/validation/platform.go
@@ -6,6 +6,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
+	"github.com/openshift/installer/pkg/ipnet"
 	"github.com/openshift/installer/pkg/types"
 	"github.com/openshift/installer/pkg/types/openstack"
 	"github.com/openshift/installer/pkg/validate"
@@ -73,8 +74,7 @@ func ValidatePlatform(p *openstack.Platform, n *types.Networking, fldPath *field
 		if err != nil {
 			ip := net.ParseIP(p.AciNetExt.ClusterSNATSubnet)
 			if ip == nil {
-				// error
-				allErrs = append(allErrs, field.Invalid(fldPath.Child("ClusterSNATSubnet"), p.AciNetExt.ClusterSNATSubnet, err.Error()))
+				allErrs = append(allErrs, field.Invalid(fldPath.Child("clusterSNATPolicyIP"), p.AciNetExt.ClusterSNATSubnet, err.Error()))
 			}	
 		}
 	}
@@ -85,11 +85,35 @@ func ValidatePlatform(p *openstack.Platform, n *types.Networking, fldPath *field
                 if err != nil {
                         ip := net.ParseIP(p.AciNetExt.ClusterSNATDest)
                         if ip == nil {
-                                // error
-                                allErrs = append(allErrs, field.Invalid(fldPath.Child("ClusterSNATDest"), p.AciNetExt.ClusterSNATDest, err.Error()))
+                                allErrs = append(allErrs, field.Invalid(fldPath.Child("clusterSNATPolicyDestIP"), p.AciNetExt.ClusterSNATDest, err.Error()))
                         }
                 }
         }
+
+	machineMask := n.DeprecatedMachineCIDR.Mask
+	if p.AciNetExt.NeutronCIDR.String() != "" {
+                neutronMask := p.AciNetExt.NeutronCIDR.Mask
+                if machineMask.String() != neutronMask.String() {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("neutronCIDR"), p.AciNetExt.NeutronCIDR.String(), "The CIDRs specified in the machineCIDR (" + n.DeprecatedMachineCIDR.String() + ") and neutronCIDR (" + p.AciNetExt.NeutronCIDR.String() + ") configurations in the install-config.yaml have different subnet masks (machineCIDR mask: " + machineMask.String() + ", neutronCIDR mask: " + neutronMask.String()))
+                }
+        } else {
+		// If no neutron CIDR provided, set it to 192.168.0.0 with the machine CIDR mask
+                neutronIP := net.ParseIP("192.168.0.0")
+                p.AciNetExt.NeutronCIDR = &ipnet.IPNet{
+                                        IPNet: net.IPNet{
+                                                IP:   neutronIP,
+                                                Mask: machineMask,
+                                        },
+                                }
+        }
+        n.NeutronCIDR = p.AciNetExt.NeutronCIDR
+
+	_, err = ipnet.ParseCIDR(p.AciNetExt.InstallerHostSubnet)
+	if err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("installerHostSubnet"),
+                		p.AciNetExt.InstallerHostSubnet, "installerHostSubnet has an invalid subnet value (" + p.AciNetExt.InstallerHostSubnet + ")"))
+	}
+
 	return allErrs
 }
 

pkg/types/validation/installconfig.go
@@ -169,28 +169,28 @@ func ValidateInstallConfig(c *types.InstallConfig, openStackValidValuesFetcher o
                         allErrs = append(allErrs, field.Invalid(tarField.Child("Unmarshal"),
                                 c.Platform.OpenStack.AciNetExt.ProvisionTar, err.Error()))
                 } else {
-			machineCIDR := c.Networking.MachineCIDR
+			machineCIDR := c.Networking.DeprecatedMachineCIDR
                         // Validate against values from install config
 
                         if (strconv.Itoa(config.InfraVLAN) != c.Platform.OpenStack.AciNetExt.InfraVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("InfraVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.InfraVLAN, "InfraVLAN values in acc-provision input and install config have to be the same"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("infraVLAN"),
+                                        c.Platform.OpenStack.AciNetExt.InfraVLAN, "infraVLAN values in acc-provision input(" + strconv.Itoa(config.InfraVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.InfraVLAN + ") have to be the same"))
                         }
                         if (strconv.Itoa(config.ServiceVLAN) != c.Platform.OpenStack.AciNetExt.ServiceVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("ServiceVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.ServiceVLAN, "ServiceVLAN values in acc-provision input and install config have to be the same"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("serviceVLAN"),
+                                        c.Platform.OpenStack.AciNetExt.ServiceVLAN, "serviceVLAN values in acc-provision input(" + strconv.Itoa(config.ServiceVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.ServiceVLAN + ") have to be the same"))
                         }
                         if (strconv.Itoa(config.KubeApiVLAN) != c.Platform.OpenStack.AciNetExt.KubeApiVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("KubeApiVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.KubeApiVLAN, "KubeApiVLAN values in acc-provision input and install config have to be the same"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("kubeApiVLAN"),
+                                        c.Platform.OpenStack.AciNetExt.KubeApiVLAN, "kubeApiVLAN values in acc-provision input(" + strconv.Itoa(config.KubeApiVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.KubeApiVLAN + ") have to be the same"))
                         }
                         if DiffSubnets(config.NodeSubnet, machineCIDR) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("MachineCIDR"),
-                                        c.Networking.MachineCIDR, "node_subnet in acc-provision input has to be the same as machineCIDR"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("machineCIDR"),
+                                        c.Networking.DeprecatedMachineCIDR.String(), "node_subnet in acc-provision input(" + config.NodeSubnet + ") has to be the same as machineCIDR in install-config.yaml(" + machineCIDR.String() + ")"))
                         }
                         if DiffSubnets(config.PodSubnet, clusterNetworkCIDR) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("ClusterNetworkCIDR"),
-                                        clusterNetworkCIDR, "pod_subnet in acc-provision input has to be the same as clusterNetwork CIDR"))
+                                allErrs = append(allErrs, field.Invalid(field.NewPath("clusterNetworkCIDR"),
+                                        clusterNetworkCIDR.String(), "pod_subnet in acc-provision input(" + config.PodSubnet + ") has to be the same as clusterNetwork:cidr in install-config.yaml(" + clusterNetworkCIDR.String() + ")"))
                         }
 		}
 	}