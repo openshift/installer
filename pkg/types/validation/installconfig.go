noironetworks/openshift_installer

Set vlan values using acc-provision tar
kubeAPIVLAN, serviceVLAN and infraVLAN are now set using values from acc-provision generated tar

pkg/types/validation/installconfig.go
@@ -168,21 +168,12 @@ func ValidateInstallConfig(c *types.InstallConfig, openStackValidValuesFetcher o
                         allErrs = append(allErrs, field.Invalid(tarField.Child("Unmarshal"),
                                 c.Platform.OpenStack.AciNetExt.ProvisionTar, err.Error()))
                 } else {
-			machineCIDR := c.Networking.MachineCIDR
-                        // Validate against values from install config
+			c.Platform.OpenStack.AciNetExt.KubeApiVLAN = strconv.Itoa(config.KubeApiVLAN)
+			c.Platform.OpenStack.AciNetExt.InfraVLAN = strconv.Itoa(config.InfraVLAN)
+			c.Platform.OpenStack.AciNetExt.ServiceVLAN = strconv.Itoa(config.ServiceVLAN)
 
-                        if (strconv.Itoa(config.InfraVLAN) != c.Platform.OpenStack.AciNetExt.InfraVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("InfraVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.InfraVLAN, "InfraVLAN values in acc-provision input and install config have to be the same"))
-                        }
-                        if (strconv.Itoa(config.ServiceVLAN) != c.Platform.OpenStack.AciNetExt.ServiceVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("ServiceVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.ServiceVLAN, "ServiceVLAN values in acc-provision input and install config have to be the same"))
-                        }
-                        if (strconv.Itoa(config.KubeApiVLAN) != c.Platform.OpenStack.AciNetExt.KubeApiVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("KubeApiVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.KubeApiVLAN, "KubeApiVLAN values in acc-provision input and install config have to be the same"))
-                        }
+                        // Validate against values from install config
+			machineCIDR := c.Networking.MachineCIDR
                         if DiffSubnets(config.NodeSubnet, machineCIDR) {
                                 allErrs = append(allErrs, field.Invalid(field.NewPath("MachineCIDR"),
                                         c.Networking.MachineCIDR, "node_subnet in acc-provision input has to be the same as machineCIDR"))

openshift/installer

Conflicts:
	pkg/types/validation/installconfig.go

pkg/types/validation/installconfig.go
@@ -169,21 +169,12 @@ func ValidateInstallConfig(c *types.InstallConfig, openStackValidValuesFetcher o
                         allErrs = append(allErrs, field.Invalid(tarField.Child("Unmarshal"),
                                 c.Platform.OpenStack.AciNetExt.ProvisionTar, err.Error()))
                 } else {
-			machineCIDR := c.Networking.DeprecatedMachineCIDR
-                        // Validate against values from install config
+			c.Platform.OpenStack.AciNetExt.KubeApiVLAN = strconv.Itoa(config.KubeApiVLAN)
+			c.Platform.OpenStack.AciNetExt.InfraVLAN = strconv.Itoa(config.InfraVLAN)
+			c.Platform.OpenStack.AciNetExt.ServiceVLAN = strconv.Itoa(config.ServiceVLAN)
 
-                        if (strconv.Itoa(config.InfraVLAN) != c.Platform.OpenStack.AciNetExt.InfraVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("infraVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.InfraVLAN, "infraVLAN values in acc-provision input(" + strconv.Itoa(config.InfraVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.InfraVLAN + ") have to be the same"))
-                        }
-                        if (strconv.Itoa(config.ServiceVLAN) != c.Platform.OpenStack.AciNetExt.ServiceVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("serviceVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.ServiceVLAN, "serviceVLAN values in acc-provision input(" + strconv.Itoa(config.ServiceVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.ServiceVLAN + ") have to be the same"))
-                        }
-                        if (strconv.Itoa(config.KubeApiVLAN) != c.Platform.OpenStack.AciNetExt.KubeApiVLAN) {
-                                allErrs = append(allErrs, field.Invalid(field.NewPath("kubeApiVLAN"),
-                                        c.Platform.OpenStack.AciNetExt.KubeApiVLAN, "kubeApiVLAN values in acc-provision input(" + strconv.Itoa(config.KubeApiVLAN) + ") and install-config.yaml(" + c.Platform.OpenStack.AciNetExt.KubeApiVLAN + ") have to be the same"))
-                        }
+                        // Validate against values from install config
+			machineCIDR := c.Networking.DeprecatedMachineCIDR
                         if DiffSubnets(config.NodeSubnet, machineCIDR) {
                                 allErrs = append(allErrs, field.Invalid(field.NewPath("machineCIDR"),
                                         c.Networking.DeprecatedMachineCIDR.String(), "node_subnet in acc-provision input(" + config.NodeSubnet + ") has to be the same as machineCIDR in install-config.yaml(" + machineCIDR.String() + ")"))