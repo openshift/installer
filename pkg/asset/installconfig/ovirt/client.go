4819be6639935adf926f4b61892cd7b48c1d83b5
Roy Golan <rgolan@redhat.com>
2020-04-24 16:21:12 +0300

    ovirt: a simpler function to get a connection to oVirt API
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- pkg/asset/installconfig/ovirt/client.go
+++ pkg/asset/installconfig/ovirt/client.go
@@ -2,11 +2,12 @@ package ovirt
 
 import (
 	ovirtsdk "github.com/ovirt/go-ovirt"
+	"github.com/pkg/errors"
 )
 
-// GetConnection is a convenience method to get a connection to ovirt api
+// getConnection is a convenience method to get a connection to ovirt api
 // form a Config Object.
-func GetConnection(ovirtConfig Config) (*ovirtsdk.Connection, error) {
+func getConnection(ovirtConfig Config) (*ovirtsdk.Connection, error) {
 	con, err := ovirtsdk.NewConnectionBuilder().
 		URL(ovirtConfig.URL).
 		Username(ovirtConfig.Username).
@@ -16,6 +17,20 @@ func GetConnection(ovirtConfig Config) (*ovirtsdk.Connection, error) {
 		Build()
 	if err != nil {
 		return nil, err
+	}
+	return con, nil
+}
+
+// NewConnection returns a new client connection to oVirt's API endpoint.
+// It is the responsibility of the caller to close the connection.
+func NewConnection() (*ovirtsdk.Connection, error) {
+	ovirtConfig, err := NewConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "getting ovirt configuration")
+	}
+	con, err := getConnection(ovirtConfig)
+	if err != nil {
+		return nil, errors.Wrap(err, "establishing ovirt connection")
 	}
 	return con, nil
 }
