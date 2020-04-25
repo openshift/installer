3f353614e6277984ba87f92c5b54df3873933a39
Peter Larsen <plarsen@redhat.com>
2020-04-24 20:03:06 -0400

    Added debug, less verbosity, fixed parameter reading, remove unnecessary export from bash-script

--- upi/azure/azureGovQuickstart.sh
+++ upi/azure/azureGovQuickstart.sh
@@ -8,7 +8,7 @@ function try()
 
 function throw()
 {   
-    echo $1 
+    echo $1  > /dev/stderr
     exit 1
 }
 
@@ -29,6 +29,30 @@ function ignoreErrors()
     set +e
 }
 
+function cleanupError()
+{
+    set +x
+}
+
+function echoDo()
+# $1: Description
+# $2: Command
+{
+    echo "Task: $1"
+    shift
+    C=''
+    for i in "$@"; do 
+       i="${i//\\/\\\\}"
+       C="$C \"${i//\"/\\\"}\""
+    done
+    echo $C
+    eval $C
+    res=$?
+    echo "--->> Result Code $res"
+}
+
+WORKERODES=3
+
 while test $# -gt 0; do
   case "$1" in
     -h|--help)
@@ -42,14 +66,14 @@ while test $# -gt 0; do
     -w)
       shift
       if test $# -gt 0; then
-        export WORKERNODES=$1
+        WORKERNODES=$1
       else
-        export WORKERNODES=3
+        WORKERNODES=3
       fi
       shift
       ;;
     --worker-nodes*)
-      export PROCESS=`echo $1 | sed -e 's/^[^=]*=//g'`
+      PROCESS=`echo $1 | sed -e 's/^[^=]*=//g'`
       shift
       ;;
     *)
@@ -59,28 +83,35 @@ while test $# -gt 0; do
 done
 
 
+if [ ! -f install-config.yaml ]; then
+  echo Missing "install-config.yaml" >/dev/stderr
+  echo Please create install-config.yaml using template in README.md > /dev/stderr
+  exit 1
+fi 
+
 echo "Making sure all the correct applications are in your path"
 echo "checking az cli"
-az version -o table &>/dev/null || throw "Az cli is not installed, please install. https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest"
+which az &>/dev/null || throw "Az cli is not installed, please install. https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest"
 echo "checking jq"
-jq --version &>/dev/null || throw "jq is not installed, please install. https://stedolan.github.io/jq/download/"
+which jq &>/dev/null || throw "jq is not installed, please install. https://stedolan.github.io/jq/download/"
 echo "checking yq"
-yq --version &>/dev/null || throw "jq is not installed, please install. https://github.com/mikefarah/yq/releases"
+which yq &>/dev/null || throw "jq is not installed, please install. https://github.com/mikefarah/yq/releases"
 echo "checking oc client"
-oc version &>/dev/null || throw "oc is not installed, please install. https://docs.openshift.com/container-platform/4.2/cli_reference/openshift_cli/getting-started-cli.html#cli-installing-cli_cli-developer-commands"
+which oc &>/dev/null || throw "oc is not installed, please install. https://docs.openshift.com/container-platform/4.2/cli_reference/openshift_cli/getting-started-cli.html#cli-installing-cli_cli-developer-commands"
 echo "checking kubectl"
-kubectl version &>/dev/null || throw "kubectl is not installed, please install. https://kubernetes.io/docs/tasks/tools/install-kubectl/"
+which kubectl &>/dev/null || throw "kubectl is not installed, please install. https://kubernetes.io/docs/tasks/tools/install-kubectl/"
 echo "checking openshift-install"
-openshift-install version &>/dev/null || throw "openshift-install is not installed, please install. https://docs.openshift.com/container-platform/4.2/installing/installing_azure/installing-azure-default.html#installation-obtaining-installer_installing-azure-default"
+which openshift-install &>/dev/null || throw "openshift-install is not installed, please install. https://docs.openshift.com/container-platform/4.2/installing/installing_azure/installing-azure-default.html#installation-obtaining-installer_installing-azure-default"
 
 throwErrors
-export CLUSTER_NAME=`yq -r .metadata.name install-config.yaml`
-export AZURE_REGION=`yq -r .platform.azure.region install-config.yaml`
-export SSH_KEY=`yq -r .sshKey install-config.yaml | xargs`
-export BASE_DOMAIN=`yq -r .baseDomain install-config.yaml`
-export BASE_DOMAIN_RESOURCE_GROUP=`yq -r .platform.azure.baseDomainResourceGroupName install-config.yaml`
+CLUSTER_NAME=`yq -r .metadata.name install-config.yaml`
+AZURE_REGION=`yq -r .platform.azure.region install-config.yaml`
+SSH_KEY=`yq -r .sshKey install-config.yaml | xargs`
+BASE_DOMAIN=`yq -r .baseDomain install-config.yaml`
+BASE_DOMAIN_RESOURCE_GROUP=`yq -r .platform.azure.baseDomainResourceGroupName install-config.yaml`
 az cloud set -n AzureUSGovernment
-az login || throw "Unable to login to azure"
+# Assume already logged in
+# az login || throw "Unable to login to azure"
 python3 -c '
 import yaml;
 path = "install-config.yaml";
@@ -103,63 +134,68 @@ data = yaml.full_load(open(path));
 del data["spec"]["publicZone"];
 del data["spec"]["privateZone"];
 open(path, "w").write(yaml.dump(data, default_flow_style=False))'
-export INFRA_ID=`yq -r '.status.infrastructureName' manifests/cluster-infrastructure-02-config.yml`
-export RESOURCE_GROUP=`yq -r '.status.platformStatus.azure.resourceGroupName' manifests/cluster-infrastructure-02-config.yml`
+INFRA_ID=`yq -r '.status.infrastructureName' manifests/cluster-infrastructure-02-config.yml`
+RESOURCE_GROUP=`yq -r '.status.platformStatus.azure.resourceGroupName' manifests/cluster-infrastructure-02-config.yml`
 openshift-install create ignition-configs
-az group create --name $RESOURCE_GROUP --location $AZURE_REGION
-az identity create -g $RESOURCE_GROUP -n ${INFRA_ID}-identity
-az storage account create -g $RESOURCE_GROUP --location $AZURE_REGION --name ${CLUSTER_NAME}sa --kind Storage --sku Standard_LRS
-export ACCOUNT_KEY=`az storage account keys list -g $RESOURCE_GROUP --account-name ${CLUSTER_NAME}sa --query "[0].value" -o tsv`
-export VHD_URL=`curl -s https://raw.githubusercontent.com/openshift/installer/release-4.3/data/data/rhcos.json | jq -r .azure.url`
-az storage container create --name vhd --account-name ${CLUSTER_NAME}sa
-az storage container create --name files --account-name ${CLUSTER_NAME}sa --public-access blob
-az storage blob upload --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY -c "files" -f "bootstrap.ign" -n "bootstrap.ign"
-az storage blob copy start --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY --destination-blob "rhcos.vhd" --destination-container vhd --source-uri "$VHD_URL"
-export PRINCIPAL_ID=`az identity show -g $RESOURCE_GROUP -n ${INFRA_ID}-identity --query principalId --out tsv`
-export RESOURCE_GROUP_ID=`az group show -g $RESOURCE_GROUP --query id --out tsv`
-az role assignment create --assignee "$PRINCIPAL_ID" --role 'Contributor' --scope "$RESOURCE_GROUP_ID"
-az network dns zone create -g $RESOURCE_GROUP -n ${CLUSTER_NAME}.${BASE_DOMAIN}
+
+trap cleanupError 0  # Stop debug when failing
+# set -x
+echoDo "Create Resource Group" az group create --name $RESOURCE_GROUP --location $AZURE_REGION -o none
+echoDo "Create Identity" az identity create -g $RESOURCE_GROUP -n ${INFRA_ID}-identity -o none
+echoDo "Create Storage Account" az storage account create -g $RESOURCE_GROUP --location $AZURE_REGION --name ${CLUSTER_NAME}sa --kind Storage --sku Standard_LRS -o none
+ACCOUNT_KEY=`az storage account keys list -g $RESOURCE_GROUP --account-name ${CLUSTER_NAME}sa --query "[0].value" -o tsv`
+VHD_URL=`curl -s https://raw.githubusercontent.com/openshift/installer/release-4.3/data/data/rhcos.json | jq -r .azure.url`
+echoDo "Create vhd Storage Container" az storage container create --name vhd --account-name ${CLUSTER_NAME}sa -o none
+echoDo "Create file Storage Container" az storage container create --name files --account-name ${CLUSTER_NAME}sa --public-access blob -o none
+echoDo "Upload bootstrap.ign to file storage" az storage blob upload --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY -c "files" -f "bootstrap.ign" -n "bootstrap.ign" -o none
+echoDo "Start VHD to image creation" az storage blob copy start --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY --destination-blob "rhcos.vhd" --destination-container vhd --source-uri "$VHD_URL"
+PRINCIPAL_ID=`az identity show -g $RESOURCE_GROUP -n ${INFRA_ID}-identity --query principalId --out tsv`
+RESOURCE_GROUP_ID=`az group show -g $RESOURCE_GROUP --query id --out tsv`
+echoDo "Create Service Account" az role assignment create --assignee "$PRINCIPAL_ID" --role 'Contributor' --scope "$RESOURCE_GROUP_ID" -o none
+echoDo "Create public DNS Zone" az network dns zone create -g $RESOURCE_GROUP -n ${CLUSTER_NAME}.${BASE_DOMAIN} -o none
 nsServer0=`az network dns zone show --resource-group $RESOURCE_GROUP --name ${CLUSTER_NAME}.${BASE_DOMAIN} --query "nameServers[0]" -o tsv`
 nsServer1=`az network dns zone show --resource-group $RESOURCE_GROUP --name ${CLUSTER_NAME}.${BASE_DOMAIN} --query "nameServers[1]" -o tsv`
-az network dns record-set ns create --name ${CLUSTER_NAME} --resource-group $BASE_DOMAIN_RESOURCE_GROUP --zone-name ${BASE_DOMAIN}
-az network dns record-set ns add-record --nsdname $nsServer0 -n ${CLUSTER_NAME} -g $BASE_DOMAIN_RESOURCE_GROUP -z ${BASE_DOMAIN}
-az network dns record-set ns add-record --nsdname $nsServer1 -n ${CLUSTER_NAME} -g $BASE_DOMAIN_RESOURCE_GROUP -z ${BASE_DOMAIN}
-az network private-dns zone create -g $RESOURCE_GROUP -n ${CLUSTER_NAME}.${BASE_DOMAIN}
+echoDo "Create NS Record Sets" az network dns record-set ns create --name ${CLUSTER_NAME} --resource-group $BASE_DOMAIN_RESOURCE_GROUP --zone-name ${BASE_DOMAIN} -o none
+echoDo "Create NS record" az network dns record-set ns add-record --nsdname $nsServer0 -n ${CLUSTER_NAME} -g $BASE_DOMAIN_RESOURCE_GROUP -z ${BASE_DOMAIN} -o none
+echoDo "Create NS record" az network dns record-set ns add-record --nsdname $nsServer1 -n ${CLUSTER_NAME} -g $BASE_DOMAIN_RESOURCE_GROUP -z ${BASE_DOMAIN} -o none
+echoDo "Create Private DNS zone" az network private-dns zone create -g $RESOURCE_GROUP -n ${CLUSTER_NAME}.${BASE_DOMAIN} -o none
 status="unknown"
 while [ "$status" != "success" ]
 do
   status=`az storage blob show --container-name vhd --name "rhcos.vhd" --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY -o tsv --query properties.copy.status`
   echo $status
 done
-az deployment group create -g $RESOURCE_GROUP \
+echoDo "Create VNET"  az deployment group create -g $RESOURCE_GROUP \
   --template-file "01_vnet.json" \
-  --parameters baseName="$INFRA_ID"
-az network private-dns link vnet create -g $RESOURCE_GROUP -z ${CLUSTER_NAME}.${BASE_DOMAIN} -n ${INFRA_ID}-network-link -v "${INFRA_ID}-vnet" -e false
-export VHD_BLOB_URL=`az storage blob url --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY -c vhd -n "rhcos.vhd" -o tsv`
-az deployment group create -g $RESOURCE_GROUP \
+  --parameters baseName="$INFRA_ID" -o none
+echoDo "Associate private-dns with virtual network" az network private-dns link vnet create -g $RESOURCE_GROUP -z ${CLUSTER_NAME}.${BASE_DOMAIN} -n ${INFRA_ID}-network-link -v "${INFRA_ID}-vnet" -e false -o none
+vhd_blob_url=`az storage blob url --account-name ${cluster_name}sa --account-key $account_key -c vhd -n "rhcos.vhd" -o tsv`
+# looks like we need a short delay here
+sleep 10
+echodo "setup azure VHD storage" az deployment group create -g $resource_group \
   --template-file "02_storage.json" \
   --parameters vhdBlobURL="$VHD_BLOB_URL" \
-  --parameters baseName="$INFRA_ID"
-az deployment group create -g $RESOURCE_GROUP \
+  --parameters baseName="$INFRA_ID" -o none
+echoDo "Setup PrivateDNS records for infrastructure" az deployment group create -g $RESOURCE_GROUP \
   --template-file "03_infra.json" \
   --parameters privateDNSZoneName="${CLUSTER_NAME}.${BASE_DOMAIN}" \
-  --parameters baseName="$INFRA_ID"
-export PUBLIC_IP=`az network public-ip list -g $RESOURCE_GROUP --query "[?name=='${INFRA_ID}-master-pip'] | [0].ipAddress" -o tsv`
-az network dns record-set a add-record -g $RESOURCE_GROUP -z ${CLUSTER_NAME}.${BASE_DOMAIN} -n api -a $PUBLIC_IP --ttl 60
-export BOOTSTRAP_URL=`az storage blob url --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY -c "files" -n "bootstrap.ign" -o tsv`
-export BOOTSTRAP_IGNITION=`jq -rcnM --arg v "2.2.0" --arg url $BOOTSTRAP_URL '{ignition:{version:$v,config:{replace:{source:$url}}}}' | base64 -w0`
-az deployment group create -g $RESOURCE_GROUP \
+  --parameters baseName="$INFRA_ID" -o none
+PUBLIC_IP=`az network public-ip list -g $RESOURCE_GROUP --query "[?name=='${INFRA_ID}-master-pip'] | [0].ipAddress" -o tsv`
+echoDo az network dns record-set a add-record -g $RESOURCE_GROUP -z ${CLUSTER_NAME}.${BASE_DOMAIN} -n api -a $PUBLIC_IP --ttl 60
+BOOTSTRAP_URL=`az storage blob url --account-name ${CLUSTER_NAME}sa --account-key $ACCOUNT_KEY -c "files" -n "bootstrap.ign" -o tsv`
+BOOTSTRAP_IGNITION=`jq -rcnM --arg v "2.2.0" --arg url $BOOTSTRAP_URL '{ignition:{version:$v,config:{replace:{source:$url}}}}' | base64 -w0`
+echoDo "Create Bootstrap" az deployment group create -g $RESOURCE_GROUP \
   --template-file "04_bootstrap.json" \
   --parameters bootstrapIgnition="$BOOTSTRAP_IGNITION" \
   --parameters sshKeyData="$SSH_KEY" \
-  --parameters baseName="$INFRA_ID"
-export MASTER_IGNITION=`cat master.ign | base64`
-az deployment group create -g $RESOURCE_GROUP \
+  --parameters baseName="$INFRA_ID" -o none
+MASTER_IGNITION=$(base64 -w0 master.ign)
+echoDo "Create masters" az deployment group create -g $RESOURCE_GROUP \
   --template-file "05_masters.json" \
   --parameters masterIgnition="$MASTER_IGNITION" \
   --parameters sshKeyData="$SSH_KEY" \
   --parameters privateDNSZoneName="${CLUSTER_NAME}.${BASE_DOMAIN}" \
-  --parameters baseName="$INFRA_ID"
+  --parameters baseName="$INFRA_ID" -o none
 openshift-install wait-for bootstrap-complete --log-level debug
 az network nsg rule delete -g $RESOURCE_GROUP --nsg-name ${INFRA_ID}-controlplane-nsg --name bootstrap_ssh_in
 az vm stop -g $RESOURCE_GROUP --name ${INFRA_ID}-bootstrap
@@ -172,7 +208,7 @@ az network public-ip delete -g $RESOURCE_GROUP --name ${INFRA_ID}-bootstrap-ssh-
 export KUBECONFIG="$PWD/auth/kubeconfig"
 oc get nodes
 oc get clusteroperator
-export WORKER_IGNITION=`cat worker.ign | base64`
+WORKER_IGNITION=$(base64 -w0 worker.ign)
 az deployment group create -g $RESOURCE_GROUP \
   --template-file "06_workers.json" \
   --parameters workerIgnition="$WORKER_IGNITION" \
