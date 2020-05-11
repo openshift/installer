39458867d369d4ade3b6761f2e1919ad392f7887
Vadim Rutkovsky <vrutkovs@redhat.com>
2020-05-12 00:32:13 +0200

    data/data/libvirt/volume/main: set volume to 16 GB

--- data/data/libvirt/volume/main.tf
+++ data/data/libvirt/volume/main.tf
@@ -1,5 +1,12 @@
-resource "libvirt_volume" "coreos_base" {
-  name   = "${var.cluster_id}-base"
+resource "libvirt_volume" "coreos_base_initial" {
+  name   = "${var.cluster_id}-base-initial"
   source = var.image
   pool   = var.pool
+}
+
+resource "libvirt_volume" "coreos_base" {
+  name   = "${var.cluster_id}-base"
+  base_volume_id = "${libvirt_volume.coreos_base_initial.id}"
+  pool   = var.pool
+  size   = 17179869184
 }
