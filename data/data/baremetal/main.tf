cc6f9fa5c6d5477138905448ae263b8a2b6e6d11
Steven Hardy <shardy@redhat.com>
2020-05-12 15:54:36 +0100

    Revert "baremetal: send full ignition to masters"
    This was merged in https://github.com/openshift/installer/pull/3276
    but had the side-effect of breaking an interface some users were
    already reliant on, where additional config is added to the
    pointer ignition output via `create ignition-configs`
    
    Downstream bz https://bugzilla.redhat.com/show_bug.cgi?id=1833483
    reported this issue, and we've taken the decision to revert this
    to resolve that interface regression, and look at other ways to
    satisfy the requirements that led to #3276
    
    This reverts commit f67d61a7142a95063e096857a6c477bef376a3e7.

--- data/data/baremetal/main.tf
+++ data/data/baremetal/main.tf
@@ -21,13 +21,11 @@ module "bootstrap" {
 module "masters" {
   source = "./masters"
 
-  master_count         = var.master_count
-  ignition             = var.ignition_master
-  hosts                = var.hosts
-  properties           = var.properties
-  root_devices         = var.root_devices
-  driver_infos         = var.driver_infos
-  instance_infos       = var.instance_infos
-  ignition_url         = var.ignition_url
-  ignition_url_ca_cert = var.ignition_url_ca_cert
+  master_count   = var.master_count
+  ignition       = var.ignition_master
+  hosts          = var.hosts
+  properties     = var.properties
+  root_devices   = var.root_devices
+  driver_infos   = var.driver_infos
+  instance_infos = var.instance_infos
 }
