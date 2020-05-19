48fe5ed5f63055a8791b51063dc55d9df147306b
Christy Norman <christy@linux.vnet.ibm.com>
2020-05-19 11:42:50 -0500

    maintenance: remove pools and volumes
    This will clean up all volumes under all non-default pools. The
    openshift CI creates a pool for each cluster.
    
    Signed-off-by: Christy Norman <christy@linux.vnet.ibm.com>

--- scripts/maintenance/virsh-cleanup.sh
+++ scripts/maintenance/virsh-cleanup.sh
@@ -9,7 +9,6 @@ then
 fi
 
 CONNECT="${CONNECT:=qemu:///system}"
-POOL="${POOL:=default}"
 
 run()
 {
@@ -23,13 +22,18 @@ do
 	run virsh -c "${CONNECT}" undefine "${DOMAIN}"
 done
 
-virsh -c "${CONNECT}" vol-list "${POOL}" | tail -n +3 | while read -r VOLUME _
+for POOL in $(virsh -c "${CONNECT}" pool-list --all --name)
 do
-	if test -z "${VOLUME}"
-	then
-		continue
-	fi
-	run virsh -c "${CONNECT}" vol-delete --pool "${POOL}" "${VOLUME}"
+	virsh -c "${CONNECT}" vol-list "${POOL}" | tail -n +3 | while read -r VOLUME _
+	do
+		if test -z "${VOLUME}"
+		then
+			continue
+		fi
+		run virsh -c "${CONNECT}" vol-delete --pool "${POOL}" "${VOLUME}"
+	done
+	run virsh -c "${CONNECT}" pool-destroy "${POOL}"
+	run virsh -c "${CONNECT}" pool-undefine "${POOL}"
 done
 
 for NET in $(virsh -c "${CONNECT}" net-list --all --name)
