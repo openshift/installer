fa99646b3226725674a57febd43abcbfa81e8ec4
Aditya Narayanaswamy <anarayan@redhat.com>
2020-07-22 11:00:47 -0400

    Metrics: Add push client to push metrics to endpoint
    Creates a push client that will be used to push metrics to
    Prometheus Aggregation Gateway. The client can be used for
    any type of Gateway and it takes in the URL and the HTTP Client
    that would have all the necessary information(authentication)
    required to push to the gateway.
    
    Configured to take in Prometheus Objects for pushing to gateway.
    https://github.com/prometheus/client_golang/tree/master/prometheus

---
+++ pkg/metrics/pushclient/pushclient.go
@@ -0,0 +1,36 @@
+package pushclient
+
+import (
+	"net/http"
+
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
+	"github.com/prometheus/common/expfmt"
+)
+
+// PushClient is used to send Prometheus metrics objects to any Prometheus
+// Push Gateway. It stores the URL and the client that will be used to push
+// to the desired gateway.
+type PushClient struct {
+	URL     string
+	Client  *http.Client
+	JobName string
+}
+
+// Push uses all the configuration settings from the client and pushes to the prometheus
+// aggregation gateway. It takes in an additional list of collectors and pushes all of
+// them to the previously configured url.
+func (p *PushClient) Push(collectors ...prometheus.Collector) error {
+	pushClient := push.New(p.URL, p.JobName).Client(p.Client).Format(expfmt.FmtText)
+
+	for _, value := range collectors {
+		pushClient.Collector(value)
+	}
+
+	err := pushClient.Push()
+	if err != nil {
+		return errors.Wrap(err, "failed to push metrics")
+	}
+	return nil
+}
