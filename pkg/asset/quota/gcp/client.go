5ab73aa607f3a3d09c3daa97bf9e8bffd97b3a04
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2020-09-11 13:36:27 -0700

    asset/quota/gcp: use GCP api to find CPU count for constraint and guess only on failure
    Previously we were using the machine type name to figure out the CPU count and metric name. But it seems like using the GCP API
    will be more future proof compared to string matching.
    
    So now the constraint calculator will fetch the CPU count from GCP api and only on failure guess the count using string matching.
    Fallback to the string matching allows us to guess the CPU count when API access to not available or other failures. The guess always
    drops the count to 0 when it cannot guess with high confidence.
    
    Also adds unit tests for the machine type -> guess cpu function and use GCP api and fallback to guess function.

---
+++ pkg/asset/quota/gcp/client.go
@@ -0,0 +1,37 @@
+package gcp
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	computev1 "google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
+
+	gcpconfig "github.com/openshift/installer/pkg/asset/installconfig/gcp"
+)
+
+// MachineTypeGetter returns the machine type info for a type in a zone using GCP API.
+type MachineTypeGetter interface {
+	GetMachineType(zone string, machineType string) (*computev1.MachineType, error)
+}
+
+// Client is GCP client for calculating quota constraint.
+type Client struct {
+	computeSvc *computev1.Service
+
+	projectID string
+}
+
+// NewClient returns Client using the context and session.
+func NewClient(ctx context.Context, sess *gcpconfig.Session, projectID string) (*Client, error) {
+	svc, err := computev1.NewService(ctx, option.WithCredentials(sess.Credentials))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create compute service")
+	}
+	return &Client{computeSvc: svc, projectID: projectID}, nil
+}
+
+// GetMachineType returns the machine type info for a type in a zone using the client.
+func (c *Client) GetMachineType(zone string, machineType string) (*computev1.MachineType, error) {
+	return c.computeSvc.MachineTypes.Get(c.projectID, zone, machineType).Context(context.TODO()).Do()
+}
