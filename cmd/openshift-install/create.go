c0219e6370a4f0a9496d3d3bfbee3d83cc265740
Joseph Callen <jcallen@redhat.com>
2020-09-25 16:21:32 -0400

    vsphere test extend install time longer
    not expecting this to be merged

--- cmd/openshift-install/create.go
+++ cmd/openshift-install/create.go
@@ -349,7 +349,7 @@ func waitForBootstrapConfigMap(ctx context.Context, client *kubernetes.Clientset
 // that the cluster has been initialized.
 func waitForInitializedCluster(ctx context.Context, config *rest.Config) error {
 	// TODO revert this value back to 30 minutes.  It's currently at the end of 4.6 and we're trying to see if the
-	timeout := 40 * time.Minute
+	timeout := 50 * time.Minute
 
 	// Wait longer for baremetal, due to length of time it takes to boot
 	if assetStore, err := assetstore.NewStore(rootOpts.dir); err == nil {
