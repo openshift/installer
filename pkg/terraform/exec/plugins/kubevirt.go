e09e99f0e0ee1312d4ee03917ac7f63b350090ff
Nir Argaman <nargaman@redhat.com>
2020-10-19 15:24:42 +0300

    Add terraform plugins for "kubernetes" and "kubevirt" resources

---
+++ pkg/terraform/exec/plugins/kubevirt.go
@@ -0,0 +1,14 @@
+package plugins
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/kubevirt/terraform-provider-kubevirt/kubevirt"
+)
+
+func init() {
+	exec := func() {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: kubevirt.Provider})
+	}
+	KnownPlugins["terraform-provider-kubevirt"] = exec
+}
