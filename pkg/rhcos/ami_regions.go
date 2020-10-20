2b1295d7a6551917ce0c6ba30c4ebefb2458d4de
Micah Abbott <miabbott@redhat.com>
2020-10-20 13:49:27 -0400

    Bug 1889855: additional AWS regions for RHCOS AMIs
    There are RFEs requesting support for additional AWS regions:
    ap-east-1, af-south-1, and eu-south-1
    
    https://issues.redhat.com/browse/RFE-903
    https://issues.redhat.com/browse/RFE-1267
    
    The existing 4.6 RHCOS AMI was manually copied to these regions by the
    ART team.

--- pkg/rhcos/ami_regions.go
+++ pkg/rhcos/ami_regions.go
@@ -4,6 +4,8 @@ package rhcos
 
 // AMIRegoins is a list of regions where the RHEL CoreOS is published.
 var AMIRegions = []string{
+	"af-south-1",
+	"ap-east-1",
 	"ap-northeast-1",
 	"ap-northeast-2",
 	"ap-south-1",
@@ -12,6 +14,7 @@ var AMIRegions = []string{
 	"ca-central-1",
 	"eu-central-1",
 	"eu-north-1",
+	"eu-south-1",
 	"eu-west-1",
 	"eu-west-2",
 	"eu-west-3",
