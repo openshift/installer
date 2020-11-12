9025ff389b933ef12dd560e2d6f7235b67d7d301
retroflexer <skolicha@redhat.com>
2020-11-12 09:57:31 -0500

    Remove the secrets directory recursively

--- data/data/bootstrap/files/usr/local/bin/installer-gather.sh
+++ data/data/bootstrap/files/usr/local/bin/installer-gather.sh
@@ -52,7 +52,7 @@ sudo chown -R "${USER}":"${USER}" "${ARTIFACTS}/rendered-assets"
 sudo find "${ARTIFACTS}/rendered-assets" -type d -print0 | xargs -0 sudo chmod u+x
 # remove sensitive information
 # TODO leave tls.crt inside of secret yaml files
-find "${ARTIFACTS}/rendered-assets" -name "*secret*" -print0 | xargs -0 rm
+find "${ARTIFACTS}/rendered-assets" -name "*secret*" -print0 | xargs -0 rm -rf
 find "${ARTIFACTS}/rendered-assets" -name "*kubeconfig*" -print0 | xargs -0 rm
 find "${ARTIFACTS}/rendered-assets" -name "*.key" -print0 | xargs -0 rm
 find "${ARTIFACTS}/rendered-assets" -name ".kube" -print0 | xargs -0 rm -rf
