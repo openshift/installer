0c004ed8ade589c677601881d9f544ca6b5ec5a7
Eran Cohen <eran@stratoscale.com>
2020-12-06 14:42:11 +0200

    Don't patch the ingresscontroller

--- data/data/bootstrap/files/usr/local/bin/patch.sh
+++ data/data/bootstrap/files/usr/local/bin/patch.sh
@@ -7,9 +7,6 @@ function patchit {
     # allow cluster-authentication-operator to deploy OAuthServer without minimum of 3 master nodes
     oc --kubeconfig ./auth/kubeconfig patch authentications.operator.openshift.io cluster -p='{"spec": {"managementState": "Managed", "unsupportedConfigOverrides": {"useUnsupportedUnsafeNonHANonProductionUnstableOAuthServer": true}}}' --type=merge || return 1
 
-    # patch ingress operator to run a single router pod
-    oc patch --kubeconfig ./auth/kubeconfig -n openshift-ingress-operator ingresscontroller/default --patch '{"spec":{"replicas": 1}}' --type=merge || return 1
-
     # Mark etcd-quorum-guard as unmanaged
     oc --kubeconfig ./auth/kubeconfig patch clusterversion/version --type='merge' -p "$(cat <<- EOF
  spec:
@@ -24,7 +21,6 @@ EOF
 
     # scale down etcd-quorum-guard
     oc --kubeconfig ./auth/kubeconfig scale --replicas=0 deployment/etcd-quorum-guard -n openshift-etcd || return 1
-
 
     return 0
 }
