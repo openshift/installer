74efda534c0eff7778f290f5949be4fb4a09b0d4
John Hixson <jhixson@redhat.com>
2021-02-15 12:31:34 -0800

    upi/metal: add more configuration options for bare metal
    Allow facilities, plan and hardware_reservation_id to be configured
    
    See https://bugzilla.redhat.com/show_bug.cgi?id=1925343

--- upi/metal/bootstrap/packet.tf
+++ upi/metal/bootstrap/packet.tf
@@ -1,11 +1,12 @@
 resource "packet_device" "bootstrap" {
-  hostname         = "${var.cluster_id}-bootstrap"
-  plan             = "c1.small.x86"
-  facilities       = [var.packet_facility]
-  operating_system = "custom_ipxe"
-  ipxe_script_url  = "${var.matchbox_http_endpoint}/ipxe?cluster_id=${var.cluster_id}&role=bootstrap"
-  billing_cycle    = "hourly"
-  project_id       = var.packet_project_id
+  hostname                = "${var.cluster_id}-bootstrap"
+  plan                    = var.packet_plan
+  facilities              = [var.packet_facility]
+  operating_system        = "custom_ipxe"
+  ipxe_script_url         = "${var.matchbox_http_endpoint}/ipxe?cluster_id=${var.cluster_id}&role=bootstrap"
+  billing_cycle           = "hourly"
+  project_id              = var.packet_project_id
+  hardware_reservation_id = var.packet_hardware_reservation_id
 
   depends_on = [matchbox_group.bootstrap]
 }
