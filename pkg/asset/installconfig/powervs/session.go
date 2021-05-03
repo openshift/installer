0de5ca923badb8a335f490ce2cf8b4c9369588f2
Christy Norman <christy@linux.vnet.ibm.com>
2021-05-03 14:05:18 -0500

    session creation options notes
    Signed-off-by: Christy Norman <christy@linux.vnet.ibm.com>

--- pkg/asset/installconfig/powervs/session.go
+++ pkg/asset/installconfig/powervs/session.go
@@ -24,9 +24,23 @@ type Session struct {
 }
 
 // GetSession returns an IBM Cloud session by using credentials found in default locations in order:
-// env IBMID,
-// env IBMID_PASSWORD,
+// env IBMID & env IBMID_PASSWORD,
+// ~/.bluemix/config.json ? (see TODO below)
 // and, if no creds are found, asks for them
+
+/* @TODO: if you do an `ibmcloud login` (or in my case ibmcloud login --sso), you get
+//  a very nice creds file at ~/.bluemix/config.json, with an IAMToken. There's no username,
+//  though (just the account's owner id, but that's not the same). It may be necessary
+//  to use the IAMToken vs the password env var mentioned here:
+//  https://github.com/IBM-Cloud/power-go-client#ibm-cloud-sdk-for-power-cloud
+
+//  Yes, I think we'll need to use the IAMToken. There's a two-factor auth built into the ibmcloud login,
+//  so the password alone isn't enough. The IAMToken is generated as a result. So either:
+     1) require the user has done this already and pull from the file 
+     2) ask the user to paste in their IAMToken.
+     3) let the password env var be the IAMToken? (Going with this atm since it's how I started)
+     4) put it into Platform {userid: , iamtoken: , ...}
+*/
 func GetSession() (*Session, error) {
 	s, err := getPISession()
 	if err != nil {
@@ -36,14 +50,13 @@ func GetSession() (*Session, error) {
     return &Session{session:s}, nil
 }
 
-
+/*
+//  https://github.com/IBM-Cloud/power-go-client/blob/master/ibmpisession/ibmpowersession.go
+*/
 func getPISession() (*ibmpisession.IBMPISession, error) {
 
     var (
         id, passwd, region, zone  string
-        //passwd string
-        //region string
-        //zone   string
     )
 
 	if id = os.Getenv("IBMID"); len(id) == 0 {
@@ -54,6 +67,7 @@ func getPISession() (*ibmpisession.IBMPISession, error) {
     }
 
     region = os.Getenv("IBMCLOUD_REGION")
+    // this can also be pulled from  ~/bluemix/config.json
     if r2 := os.Getenv("IC_REGION"); len(r2) > 0 {
         if len(region) > 0 && region != r2 {
             return nil, errors.New(fmt.Sprintf("conflicting values for IBM Cloud Region: IBMCLOUD_REGION: %s and IC_REGION: %s", region, r2))
