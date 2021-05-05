f8a383b6d41e2858e3e62efa13534345ea14cbc4
Emilien Macchi <emilien@redhat.com>
2021-05-05 12:16:31 -0400

    Bug 1956208: openstack: add validation for volume types
    When an user wants to deploy with rootVolume, they need to specify
    a volume type.
    We already checked that the volume type was set but we didn't check that
    the type was actually existing.
    
    With this patch, we now check that the type exists.
    
    If the Volume Type doesn't exist, it'll show this error message:
    
    ```
    FATAL failed to fetch Metadata: failed to load asset "Install Config": compute[0].platform.openstack.rootVolume.type.type: Invalid value: "wrong": Volume Type either does not exist in this cloud, or is not available
    ```
    
    Note: this patch also adds test coverage when the type is an empty
    string, which wasn't done before.

---
+++ vendor/github.com/gophercloud/gophercloud/openstack/blockstorage/v3/volumetypes/urls.go
@@ -0,0 +1,51 @@
+package volumetypes
+
+import "github.com/gophercloud/gophercloud"
+
+func listURL(c *gophercloud.ServiceClient) string {
+	return c.ServiceURL("types")
+}
+
+func getURL(c *gophercloud.ServiceClient, id string) string {
+	return c.ServiceURL("types", id)
+}
+
+func createURL(c *gophercloud.ServiceClient) string {
+	return c.ServiceURL("types")
+}
+
+func deleteURL(c *gophercloud.ServiceClient, id string) string {
+	return c.ServiceURL("types", id)
+}
+
+func updateURL(c *gophercloud.ServiceClient, id string) string {
+	return c.ServiceURL("types", id)
+}
+
+func extraSpecsListURL(client *gophercloud.ServiceClient, id string) string {
+	return client.ServiceURL("types", id, "extra_specs")
+}
+
+func extraSpecsGetURL(client *gophercloud.ServiceClient, id, key string) string {
+	return client.ServiceURL("types", id, "extra_specs", key)
+}
+
+func extraSpecsCreateURL(client *gophercloud.ServiceClient, id string) string {
+	return client.ServiceURL("types", id, "extra_specs")
+}
+
+func extraSpecUpdateURL(client *gophercloud.ServiceClient, id, key string) string {
+	return client.ServiceURL("types", id, "extra_specs", key)
+}
+
+func extraSpecDeleteURL(client *gophercloud.ServiceClient, id, key string) string {
+	return client.ServiceURL("types", id, "extra_specs", key)
+}
+
+func accessURL(client *gophercloud.ServiceClient, id string) string {
+	return client.ServiceURL("types", id, "os-volume-type-access")
+}
+
+func accessActionURL(client *gophercloud.ServiceClient, id string) string {
+	return client.ServiceURL("types", id, "action")
+}
