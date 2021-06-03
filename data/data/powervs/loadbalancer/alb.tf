18772faf91c10be07467079552714dce57ed7550
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2021-06-03 19:37:05 +0530

    Add internal LB resources
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- data/data/powervs/loadbalancer/alb.tf
+++ data/data/powervs/loadbalancer/alb.tf
@@ -9,7 +9,16 @@ resource "ibm_is_lb" "load_balancer" {
   name            = "${var.cluster_id}-loadbalancer"
   subnets         = [var.vpc_subnet_id]
   security_groups = [ibm_is_security_group.ocp_security_group.id]
-  tags            = [var.cluster_id]
+  tags            = [var.cluster_id, "${var.cluster_id}-loadbalancer"]
+  type            = "public"
+}
+
+resource "ibm_is_lb" "load_balancer_int" {
+  name            = "${var.cluster_id}-loadbalancer-int"
+  subnets         = [var.vpc_subnet_id]
+  security_groups = [ibm_is_security_group.ocp_security_group.id]
+  tags            = [var.cluster_id, "${var.cluster_id}-loadbalancer-int"]
+  type            = "private"
 }
 
 # Using explicit depends_on as otherwise there are issues with updating and adding of pool members
@@ -18,16 +27,16 @@ resource "ibm_is_lb" "load_balancer" {
 ## TODO move this to internal/private LB
 # machine config listener and backend pool
 resource "ibm_is_lb_listener" "machine_config_listener" {
-  lb           = ibm_is_lb.load_balancer.id
+  lb           = ibm_is_lb.load_balancer_int.id
   port         = 22623
   protocol     = "tcp"
   default_pool = ibm_is_lb_pool.machine_config_pool.id
 }
 resource "ibm_is_lb_pool" "machine_config_pool" {
-  depends_on = [ibm_is_lb.load_balancer]
+  depends_on = [ibm_is_lb.load_balancer_int]
 
   name           = "machine-config-server"
-  lb             = ibm_is_lb.load_balancer.id
+  lb             = ibm_is_lb.load_balancer_int.id
   algorithm      = "round_robin"
   protocol       = "tcp"
   health_delay   = 60
@@ -39,14 +48,42 @@ resource "ibm_is_lb_pool_member" "machine_config_member" {
   depends_on = [ibm_is_lb_listener.machine_config_listener]
   count      = local.api_servers_count
 
-  lb             = ibm_is_lb.load_balancer.id
+  lb             = ibm_is_lb.load_balancer_int.id
   pool           = ibm_is_lb_pool.machine_config_pool.id
   port           = 22623
   target_address = local.api_servers[count.index]
 }
 
+# api listener and backend pool (internal)
+resource "ibm_is_lb_listener" "api_listener_int" {
+  lb           = ibm_is_lb.load_balancer_int.id
+  port         = 6443
+  protocol     = "tcp"
+  default_pool = ibm_is_lb_pool.api_pool_int.id
+}
+resource "ibm_is_lb_pool" "api_pool_int" {
+  depends_on = [ibm_is_lb.load_balancer_int]
 
-# api listener and backend pool
+  name           = "openshift-api-server"
+  lb             = ibm_is_lb.load_balancer_int.id
+  algorithm      = "round_robin"
+  protocol       = "tcp"
+  health_delay   = 60
+  health_retries = 5
+  health_timeout = 30
+  health_type    = "tcp"
+}
+resource "ibm_is_lb_pool_member" "api_member_int" {
+  depends_on = [ibm_is_lb_listener.api_listener_int, ibm_is_lb_pool_member.machine_config_member]
+  count      = local.api_servers_count
+
+  lb             = ibm_is_lb.load_balancer_int.id
+  pool           = ibm_is_lb_pool.api_pool_int.id
+  port           = 6443
+  target_address = local.api_servers[count.index]
+}
+
+# api listener and backend pool (external)
 resource "ibm_is_lb_listener" "api_listener" {
   lb           = ibm_is_lb.load_balancer.id
   port         = 6443
