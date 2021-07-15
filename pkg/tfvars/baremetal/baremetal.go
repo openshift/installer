24c86939882851cc6a8d768f86e4a723812efe61
Kiran Thyagaraja <18704819+kirankt@users.noreply.github.com>
2021-07-15 17:17:20 -0400

    Add support for baremetal IPI live-iso boot

--- pkg/tfvars/baremetal/baremetal.go
+++ pkg/tfvars/baremetal/baremetal.go
@@ -5,9 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
-	"net/url"
-	"path"
-	"strings"
 
 	"github.com/metal3-io/baremetal-operator/pkg/bmc"
 	"github.com/metal3-io/baremetal-operator/pkg/hardware"
@@ -26,6 +23,8 @@ type config struct {
 	IronicUsername string `json:"ironic_username"`
 	IronicPassword string `json:"ironic_password"`
 
+	DeploySteps []string `json:"deploy_steps"`
+
 	// Data required for control plane deployment - several maps per host, because of terraform's limitations
 	Hosts         []map[string]interface{} `json:"hosts"`
 	RootDevices   []map[string]interface{} `json:"root_devices"`
@@ -42,6 +41,7 @@ func TFVars(libvirtURI, apiVIP, imageCacheIP, bootstrapOSImage, externalBridge, 
 	}
 
 	var hosts, rootDevices, properties, driverInfos, instanceInfos []map[string]interface{}
+	var deploySteps []string
 
 	for _, host := range platformHosts {
 		// Get hardware profile
@@ -66,6 +66,7 @@ func TFVars(libvirtURI, apiVIP, imageCacheIP, bootstrapOSImage, externalBridge, 
 		driverInfo := accessDetails.DriverInfo(credentials)
 		driverInfo["deploy_kernel"] = fmt.Sprintf("http://%s/images/ironic-python-agent.kernel", net.JoinHostPort(imageCacheIP, "80"))
 		driverInfo["deploy_ramdisk"] = fmt.Sprintf("http://%s/images/ironic-python-agent.initramfs", net.JoinHostPort(imageCacheIP, "80"))
+		driverInfo["deploy_iso"] = fmt.Sprintf("http://%s/images/ironic-python-agent.iso", net.JoinHostPort(imageCacheIP, "80"))
 
 		// Host Details
 		hostMap := map[string]interface{}{
@@ -77,6 +78,7 @@ func TFVars(libvirtURI, apiVIP, imageCacheIP, bootstrapOSImage, externalBridge, 
 			"power_interface":      accessDetails.PowerInterface(),
 			"raid_interface":       accessDetails.RAIDInterface(),
 			"vendor_interface":     accessDetails.VendorInterface(),
+			"deploy_interface":     "custom-agent",
 		}
 
 		// Explicitly set the boot mode to the default "uefi" in case
@@ -115,41 +117,25 @@ func TFVars(libvirtURI, apiVIP, imageCacheIP, bootstrapOSImage, externalBridge, 
 			rootDevice["name"] = profile.RootDeviceHints.DeviceName
 		}
 
-		// Instance Info
-		// The machine-os-downloader container downloads the image, compresses it to speed up deployments
-		// and then makes it available on bootstrapProvisioningIP via http
-		// The image is now formatted with a query string containing the sha256sum, we strip that here
-		// and it will be consumed for validation in https://github.com/openshift/ironic-rhcos-downloader
-		imageURL, err := url.Parse(image)
-		if err != nil {
-			return nil, err
-		}
-		imageURL.RawQuery = ""
-		imageURL.Fragment = ""
-		// We strip any .gz/.xz suffix because ironic-machine-os-downloader unzips the image
-		// ref https://github.com/openshift/ironic-rhcos-downloader/pull/12
-		imageFilename := path.Base(strings.TrimSuffix(imageURL.String(), ".gz"))
-		imageFilename = strings.TrimSuffix(imageFilename, ".xz")
-		cachedImageFilename := "cached-" + imageFilename
-		cacheImageURL := fmt.Sprintf("http://%s/images/%s/%s", net.JoinHostPort(imageCacheIP, "80"), imageFilename, cachedImageFilename)
-		cacheChecksumURL := fmt.Sprintf("%s.md5sum", cacheImageURL)
-		instanceInfo := map[string]interface{}{
-			"image_source":   cacheImageURL,
-			"image_checksum": cacheChecksumURL,
-		}
-
 		// This is the only place where we need to set instance_info capabilities,
 		// if we need to add another capabilitie we need merge the values
 		// and ensure they are in the `key1:value1,key2:value2` format
+		instanceInfo := make(map[string]interface{})
 		if host.BootMode == baremetal.UEFISecureBoot {
 			instanceInfo["capabilities"] = "secure_boot:true"
 		}
+
+		// deploy_steps is set when a custom deployment is desired. We will use ironic's custom deployment
+		// interface to use live ISO based installer. Currently this value is static but may be configurable
+		// in the future.
+		hostDeploySteps := `[{"interface": "deploy", "step": "install_coreos", "priority": 80, "args": {}}]`
 
 		hosts = append(hosts, hostMap)
 		properties = append(properties, propertiesMap)
 		driverInfos = append(driverInfos, driverInfo)
 		rootDevices = append(rootDevices, rootDevice)
 		instanceInfos = append(instanceInfos, instanceInfo)
+		deploySteps = append(deploySteps, hostDeploySteps)
 	}
 
 	var bridges []map[string]string
@@ -182,6 +168,7 @@ func TFVars(libvirtURI, apiVIP, imageCacheIP, bootstrapOSImage, externalBridge, 
 		DriverInfos:      driverInfos,
 		RootDevices:      rootDevices,
 		InstanceInfos:    instanceInfos,
+		DeploySteps:      deploySteps,
 	}
 
 	return json.MarshalIndent(cfg, "", "  ")
