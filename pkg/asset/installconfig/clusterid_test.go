5ecddffe02e198b1c330717e3de89aa6e2a40b97
Miguel Martin <mmartinv@lenovo-p340.mamux.org>
2021-07-22 14:10:04 +0200

    Allow to set the "InfraID" with an environment variable
    This change allows the user to set the "OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE"
    environment variable to modify the "InfraID".
    
    The contents of the "OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE" correspond to
    the random part of the InfraID so it must be formed by 5 random lowercase
    alphanumeric chars with no vowels, otherwise an error will be returned.
    
    An usage example would be:
    
     OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE="xxxxx" openshift-install create manifests --dir=.
    
    This change is needed to implement external provider support in the
    assisted-installer project.

--- pkg/asset/installconfig/clusterid_test.go
+++ pkg/asset/installconfig/clusterid_test.go
@@ -1,6 +1,9 @@
 package installconfig
 
 import (
+	"fmt"
+	"os"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,8 +11,8 @@ import (
 
 func Test_generateInfraID(t *testing.T) {
 	tests := []struct {
-		input string
-
+		input      string
+		envInfraID string
 		expLen     int
 		expNonRand string
 	}{{
@@ -31,10 +34,39 @@ func Test_generateInfraID(t *testing.T) {
 	}}
 	for _, test := range tests {
 		t.Run("", func(t *testing.T) {
-			got := generateInfraID(test.input, 27)
-			t.Log("InfraID", got)
-			assert.Equal(t, test.expLen, len(got))
-			assert.Equal(t, test.expNonRand, got[:len(got)-randomLen-1])
+			for _, expRand := range []string{"", "xxxx", "xxxxx", "xxxxa", "xxXxx", "xxxxxx"} {
+				envInfraID := ""
+				if expRand != "" {
+					envInfraID = fmt.Sprintf("%s", expRand)
+				}
+				os.Setenv("OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE", envInfraID)
+				t.Log("OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE", envInfraID)
+
+				got, err := generateInfraID(test.input, 27)
+				t.Log("InfraID", got)
+
+				randomIDPattern := fmt.Sprintf("^([b-df-hj-np-tv-z0-9]){%d}$", randomLen)
+				randomIDRegexp := regexp.MustCompile(randomIDPattern)
+
+				// Invalid "OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE" configured
+				if envInfraID != "" && !randomIDRegexp.MatchString(envInfraID) {
+					assert.Error(t, err, "Invalid 'OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE': '%s'", envInfraID)
+				} else {
+					infraIDPattern := fmt.Sprintf("^%s-([b-df-hj-np-tv-z0-9]){%d}$", test.expNonRand, randomLen)
+					infraIDRegexp := regexp.MustCompile(infraIDPattern)
+					// No "OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE" env var configured (normal flow)
+					if envInfraID == "" {
+						assert.Regexp(t, infraIDRegexp, got)
+					}
+					// Valid "OPENSHIFT_INSTALL_INFRA_ID_OVERRIDE" configured
+					if infraIDRegexp.MatchString(envInfraID) {
+						assert.Equal(t, test.expNonRand+"-"+envInfraID, got)
+
+					}
+					assert.Equal(t, test.expLen, len(got))
+					assert.Equal(t, test.expNonRand, got[:len(got)-randomLen-1])
+				}
+			}
 		})
 	}
 }
