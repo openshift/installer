c96ce062027520c6d52e7d0aa19e01bae9b69a00
patrickdillon <padillon@redhat.com>
2021-07-23 06:19:34 -0400

    Add Terraform Stages for Azure Stack
    Uses Azure Stack as the Terraform Provider for the Azure Stack platform.
    Follows the same stages as Azure, but uses the Azure Stack provider.

--- pkg/terraform/stages/azure/stages.go
+++ pkg/terraform/stages/azure/stages.go
@@ -5,9 +5,19 @@ import (
 	"github.com/openshift/installer/pkg/terraform/stages"
 )
 
+const azure string = "azure"
+const azurestack string = "azurestack"
+
 // PlatformStages are the stages to run to provision the infrastructure in Azure.
 var PlatformStages = []terraform.Stage{
-	stages.NewStage("azure", "vnet"),
-	stages.NewStage("azure", "bootstrap", stages.WithNormalDestroy()),
-	stages.NewStage("azure", "cluster"),
+	stages.NewStage(azure, "vnet"),
+	stages.NewStage(azure, "bootstrap", stages.WithNormalDestroy()),
+	stages.NewStage(azure, "cluster"),
+}
+
+// StackPlatformStages are the stages to run to provision the infrastructure in Azure Stack.
+var StackPlatformStages = []terraform.Stage{
+	stages.NewStage(azurestack, "vnet"),
+	stages.NewStage(azurestack, "bootstrap", stages.WithNormalDestroy()),
+	stages.NewStage(azurestack, "cluster"),
 }
