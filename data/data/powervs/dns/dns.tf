bb2819aca81acef442ee9692e7c46f4fc285d10a
Manjunath A Kumatagi <mkumatag@in.ibm.com>
2021-07-29 13:41:56 +0530

    Fix the DNS record creation

--- data/data/powervs/dns/dns.tf
+++ data/data/powervs/dns/dns.tf
@@ -1,30 +1,35 @@
+locals {
+  // extracting "cluster_name" from <cluster_domain> := <cluster_name>.<base_domain>
+  cluster_name = replace(var.cluster_domain, ".${var.base_domain}", "")
+}
+
 data "ibm_dns_domain" "domain" {
   name = var.base_domain
 }
 resource "ibm_dns_record" "api" {
   data               = "${var.load_balancer_hostname}."
   domain_id          = data.ibm_dns_domain.domain.id
-  host               = "api.${var.cluster_domain}"
-  responsible_person = "root.${var.cluster_domain}."
+  host               = "api.${local.cluster_name}"
+  responsible_person = "root.${local.cluster_name}."
   ttl                = 900
   type               = "cname"
-  tags               = [var.cluster_id, "${var.cluster_id}-api"]
+  tags               = [var.cluster_id, "${local.cluster_name}-api"]
 }
 resource "ibm_dns_record" "api-int" {
   data               = "${var.load_balancer_int_hostname}."
   domain_id          = data.ibm_dns_domain.domain.id
-  host               = "api-int.${var.cluster_domain}"
-  responsible_person = "root.${var.cluster_domain}."
+  host               = "api-int.${local.cluster_name}"
+  responsible_person = "root.${local.cluster_name}."
   ttl                = 900
   type               = "cname"
-  tags               = [var.cluster_id, "${var.cluster_id}-api-int"]
+  tags               = [var.cluster_id, "${local.cluster_name}-api-int"]
 }
 resource "ibm_dns_record" "apps" {
   data               = "${var.load_balancer_hostname}."
   domain_id          = data.ibm_dns_domain.domain.id
-  host               = "*.apps.${var.cluster_domain}"
-  responsible_person = "root.${var.cluster_domain}."
+  host               = "*.apps.${local.cluster_name}"
+  responsible_person = "root.${local.cluster_name}."
   ttl                = 900
   type               = "cname"
-  tags               = [var.cluster_id, "${var.cluster_id}-apps"]
+  tags               = [var.cluster_id, "${local.cluster_name}-apps"]
 }
