45516eadacb9c740254e179d7c189be5319ca42f
John Hixson <jhixson@redhat.com>
2021-08-04 16:20:41 -0700

    Upgrade to Terraform 0.14.6: update terraform files
    Add terraform.rc and update terraform modules to specify terraform version and
    local provider locations.

--- data/data/openstack/masters/versions.tf
+++ data/data/openstack/masters/versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = ">= 0.14"
   required_providers {
-    azurerm = {
-      source = "openshift/local/azurerm"
-    }
     ignition = {
       source = "openshift/local/ignition"
+    }
+    openstack = {
+      source = "openshift/local/openstack"
     }
   }
 }
