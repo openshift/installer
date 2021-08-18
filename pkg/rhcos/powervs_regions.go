d230f6d2d6ecf6a0ea93acccb44e1d264a7bbfff
Michael Turek <mjturek@linux.vnet.ibm.com>
2021-08-18 19:27:23 -0400

    Flesh out regions for installer survey (#47)
    Based on UPI docs [0] and IBM Cloud docs [1]
    
    [0] https://github.com/ocp-power-automation/ocp4-upi-powervs/blob/master/docs/var.tfvars-doc.md
    [1] https://cloud.ibm.com/docs/power-iaas?topic=power-iaas-creating-power-virtual-server&locale=en#creating-service

--- pkg/rhcos/powervs_regions.go
+++ pkg/rhcos/powervs_regions.go
@@ -5,17 +5,66 @@ package rhcos
 // PowerVSZones holds the zones cooresponding to each region found in PowerVS Regions
 // TODO(cklokman): These are fictional zones for testing
 var PowerVSZones = map[string][]string{
-	"us-south": []string{
-		"us-south-zone1",
-		"us-south-zone2",
+	"eu-de": []string{
+		"eu-de-1",
+		"eu-de-2",
 	},
+	"dal": []string{"dal12"},
+	"lon": []string{
+		"lon04",
+		"lon06",
+	},
+	"mon":     []string{"mon01"},
+	"osa":     []string{"osa21"},
+	"syd":     []string{"syd04"},
+	"sao":     []string{"sao01"},
+	"tor":     []string{"tor01"},
+	"tok":     []string{"tok04"},
+	"us-east": []string{"us-east"},
 }
 
 // PowerVSRegions holds the regions for IBM Power VS, and descriptions used during the survey
 // TODO(cklokman): These are fictional regtions for testing
 var PowerVSRegions = []map[string]string{
 	map[string]string{
-		"name":        "us-south",
-		"description": "This is the us-south test region.",
+		"name":        "dal",
+		"description": "Dallas, USA",
+	},
+	map[string]string{
+		"name":        "eu-de",
+		"description": "Frankfurt, Germany",
+	},
+	map[string]string{
+		"name":        "lon",
+		"description": "London, UK.",
+	},
+	map[string]string{
+		"name":        "mon",
+		"description": "Montreal, Canada",
+	},
+
+	map[string]string{
+		"name":        "osa",
+		"description": "Osaka, Japan",
+	},
+	map[string]string{
+		"name":        "syd",
+		"description": "Sydney, Australia",
+	},
+	map[string]string{
+		"name":        "sao",
+		"description": "São Paulo, Brazil",
+	},
+	map[string]string{
+		"name":        "tor",
+		"description": "Toronto, Canada",
+	},
+	map[string]string{
+		"name":        "tok",
+		"description": "Tokyo, Japan",
+	},
+	map[string]string{
+		"name":        "us-east",
+		"description": "Washington DC, USA",
 	},
 }
