bbe9440416eea737e0fc55039ffa87e75d946b3c
rna-afk <anarayan@redhat.com>
2021-08-20 17:20:02 -0400

    GCP: Update UPI ignition version to 3.2.0
    Updating ignition version from 3.1.0 to 3.2.0 for GCP UPI
    templates.

--- upi/gcp/04_bootstrap.py
+++ upi/gcp/04_bootstrap.py
@@ -22,7 +22,7 @@ def GenerateConfig(context):
             'metadata': {
                 'items': [{
                     'key': 'user-data',
-                    'value': '{"ignition":{"config":{"replace":{"source":"' + context.properties['bootstrap_ign'] + '"}},"version":"3.1.0"}}',
+                    'value': '{"ignition":{"config":{"replace":{"source":"' + context.properties['bootstrap_ign'] + '"}},"version":"3.2.0"}}',
                 }]
             },
             'networkInterfaces': [{
