0ef0d2a7a8f4f93b0b58b1c2612132709edb319e
Evgeny Slutsky <eslutsky@redhat.com>
2021-09-01 11:05:26 +0300

    ovirt: bump cluster-api provider
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- vendor/github.com/openshift/cluster-api-provider-ovirt/pkg/apis/ovirtprovider/v1beta1/register.go
+++ vendor/github.com/openshift/cluster-api-provider-ovirt/pkg/apis/ovirtprovider/v1beta1/register.go
@@ -26,13 +26,12 @@ import (
 	"errors"
 	"fmt"
 
+	machinev1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/klog"
-	"sigs.k8s.io/yaml"
-
-	machinev1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
+	"sigs.k8s.io/yaml"
 )
 
 const GroupName = "ovirtproviderconfig"
@@ -50,7 +49,6 @@ func MachineSpecFromProviderSpec(providerSpec machinev1.ProviderSpec) (*OvirtMac
 	if providerSpec.Value == nil {
 		return nil, errors.New("no such providerSpec found in manifest")
 	}
-
 	var config OvirtMachineProviderSpec
 	if err := yaml.Unmarshal(providerSpec.Value.Raw, &config); err != nil {
 		return nil, err
