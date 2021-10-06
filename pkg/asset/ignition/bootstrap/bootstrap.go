9485b247aad70c4ded6ed58d68455e6915676c8d
staebler <staebler@redhat.com>
2021-10-06 14:44:29 -0400

    Bug 2010665: do not modify cvo ignores for bootstrap-in-place
    When doing a bootstrap-in-place installation, the cluster-version-operator
    does not start until after the bootstrap machine has pivoted to being a
    control-plane machine. Consequently, there is no possibility of the
    cluster-bootstrap racing with the cluster-version-operator to create
    resources. The code that was added to fix Bug 2008119 [1] is not necessary
    for bootstrap-in-place installations. In fact, the code actually breaks
    bootstrap-in-place installations by trying to restore the CVO overrides
    after cluster-bootstrap finishes but before the bootstrap-to-control-plane
    pivot occurs.
    
    The changes in this commit restrict the code for Bug 2008119 to only
    installations that are not bootstrap-in-place.
    
    [1] https://github.com/openshift/installer/pull/5258

--- pkg/asset/ignition/bootstrap/bootstrap.go
+++ pkg/asset/ignition/bootstrap/bootstrap.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/asset/ignition"
 )
 
 const (
@@ -15,12 +16,29 @@ type Bootstrap struct {
 
 var _ asset.WritableAsset = (*Bootstrap)(nil)
 
+// Dependencies returns the assets on which the Bootstrap asset depends.
+func (a *Bootstrap) Dependencies() []asset.Asset {
+	return append(
+		a.Common.Dependencies(),
+		&CVOIgnore{},
+	)
+}
+
 // Generate generates the ignition config for the Bootstrap asset.
 func (a *Bootstrap) Generate(dependencies asset.Parents) error {
 	templateData := a.getTemplateData(dependencies, false)
 	if err := a.generateConfig(dependencies, templateData); err != nil {
 		return err
 	}
+
+	// replace cvo-overrides.yaml so that CVO does not try to create resources for the manifests that the installer
+	// is supplying to cluster-bootstrap
+	cvoIgnore := &CVOIgnore{}
+	dependencies.Get(cvoIgnore)
+	for _, file := range ignition.FilesFromAsset(rootDir, "root", 0644, cvoIgnore) {
+		a.Config.Storage.Files = replaceOrAppend(a.Config.Storage.Files, file)
+	}
+
 	if err := a.generateFile(bootstrapIgnFilename); err != nil {
 		return err
 	}
