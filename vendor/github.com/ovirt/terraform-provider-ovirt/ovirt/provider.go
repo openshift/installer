ccc0ae41bf8f66be238dcf0d272fffc2662235b6
gzaidman <gzaidman@redhat.com>
2021-10-26 10:25:35 +0300

    Bug 2017258: bump oVirt terraform provider version which fix "Disk is locked" bug
    On RHV 4.4.7 the image upload process changed which lead to a bug that affected many customers.
    This change bumps the oVirt terraform version to a version which contains the fix for the Bug.

--- vendor/github.com/ovirt/terraform-provider-ovirt/ovirt/provider.go
+++ vendor/github.com/ovirt/terraform-provider-ovirt/ovirt/provider.go
@@ -48,7 +48,7 @@ func (c *providerContext) Provider() terraform.ResourceProvider {
 				Type:        schema.TypeBool,
 				Required:    false,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("OVIRT_INSECURE", ""),
+				DefaultFunc: schema.EnvDefaultFunc("OVIRT_INSECURE", false),
 				Description: "Skip certificate verification",
 				Sensitive:   false,
 			},
