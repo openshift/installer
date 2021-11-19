468f9590035ae55dd7d3e3332646dfde5b49ce8d
Robert Bost <rbost@redhat.com>
2021-11-19 11:22:45 -0700

    hack: include goimports in go-fmt script

--- hack/go-fmt.sh
+++ hack/go-fmt.sh
@@ -2,6 +2,7 @@
 if [ "$IS_CONTAINER" != "" ]; then
   for TARGET in "${@}"; do
     find "${TARGET}" -name '*.go' ! -path '*/vendor/*' ! -path '*/.build/*' -exec gofmt -s -w {} \+
+    find "${TARGET}" -name '*.go' ! -path '*/vendor/*' ! -path '*/.build/*' -exec goimports -w {} \+
   done
   git diff --exit-code
 else
