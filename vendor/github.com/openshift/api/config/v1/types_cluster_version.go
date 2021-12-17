a829541dcc4ceee1d07fb53743aa93658234a35f
staebler <staebler@redhat.com>
2021-12-17 14:18:12 -0500

    vendor: update openshift/api to include some alibaba infra changes
    * Update openshift/api to 6e0b1eb97188.
    * Update kube modules to v0.23.0.
    * Update controller-runtime to v0.11.0.
    * Remove unused terraform-provider-kubernetes.

--- vendor/github.com/openshift/api/config/v1/types_cluster_version.go
+++ vendor/github.com/openshift/api/config/v1/types_cluster_version.go
@@ -408,7 +408,7 @@ type ClusterCondition struct {
 type PromQLClusterCondition struct {
 	// PromQL is a PromQL query classifying clusters. This query
 	// query should return a 1 in the match case and a 0 in the
-	// does-not-match case case. Queries which return no time
+	// does-not-match case. Queries which return no time
 	// series, or which return values besides 0 or 1, are
 	// evaluation failures.
 	// +kubebuilder:validation:Required
