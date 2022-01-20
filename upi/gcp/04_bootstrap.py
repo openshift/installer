db9a99cc1758fed2c3eef8e37e842b0c1a1d8224
Montana Mendy <montana@linux.com>
2022-01-19 21:57:39 -0800

    Refactor, using git cherry-pick, making it easier to annotate.
    Enforcing range extension, meaning better spots to annotate, flows a bit cleaner.

--- upi/gcp/04_bootstrap.py
+++ upi/gcp/04_bootstrap.py
@@ -1,56 +1,72 @@
 def GenerateConfig(context):
 
-    resources = [{
-        'name': context.properties['infra_id'] + '-bootstrap-public-ip',
-        'type': 'compute.v1.address',
-        'properties': {
-            'region': context.properties['region']
-        }
-    }, {
-        'name': context.properties['infra_id'] + '-bootstrap-in-ssh',
-        'type': 'compute.v1.firewall',
-        'properties': {
-            'network': context.properties['cluster_network'],
-            'allowed': [{
-                'IPProtocol': 'tcp',
-                'ports': ['22']
-            }],
-            'sourceRanges':  ['0.0.0.0/0'],
-            'targetTags': [context.properties['infra_id'] + '-bootstrap']
-        }
-    }, {
-        'name': context.properties['infra_id'] + '-bootstrap',
-        'type': 'compute.v1.instance',
-        'properties': {
-            'disks': [{
-                'autoDelete': True,
-                'boot': True,
-                'initializeParams': {
-                    'diskSizeGb': context.properties['root_volume_size'],
-                    'sourceImage': context.properties['image']
-                }
-            }],
-            'machineType': 'zones/' + context.properties['zone'] + '/machineTypes/' + context.properties['machine_type'],
-            'metadata': {
-                'items': [{
-                    'key': 'user-data',
-                    'value': '{"ignition":{"config":{"replace":{"source":"' + context.properties['bootstrap_ign'] + '","verification":{}}},"timeouts":{},"version":"2.1.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}',
-                }]
+    resources = [
+        {
+            "name": context.properties["infra_id"] + "-bootstrap-public-ip",
+            "type": "compute.v1.address",
+            "properties": {"region": context.properties["region"]},
+        },
+        {
+            "name": context.properties["infra_id"] + "-bootstrap",
+            "type": "compute.v1.instance",
+            "properties": {
+                "disks": [
+                    {
+                        "autoDelete": True,
+                        "boot": True,
+                        "initializeParams": {
+                            "diskSizeGb": context.properties["root_volume_size"],
+                            "sourceImage": context.properties["image"],
+                        },
+                    }
+                ],
+                "machineType": "zones/"
+                + context.properties["zone"]
+                + "/machineTypes/"
+                + context.properties["machine_type"],
+                "metadata": {
+                    "items": [
+                        {
+                            "key": "user-data",
+                            "value": '{"ignition":{"config":{"replace":{"source":"'
+                            + context.properties["bootstrap_ign"]
+                            + '"}},"version":"3.2.0"}}',
+                        }
+                    ]
+                },
+                "networkInterfaces": [
+                    {
+                        "subnetwork": context.properties["control_subnet"],
+                        "accessConfigs": [
+                            {
+                                "natIP": "$(ref."
+                                + context.properties["infra_id"]
+                                + "-bootstrap-public-ip.address)"
+                            }
+                        ],
+                    }
+                ],
+                "tags": {
+                    "items": [
+                        context.properties["infra_id"] + "-master",
+                        context.properties["infra_id"] + "-bootstrap",
+                    ]
+                },
+                "zone": context.properties["zone"],
             },
-            'networkInterfaces': [{
-                'subnetwork': context.properties['control_subnet'],
-                'accessConfigs': [{
-                    'natIP': '$(ref.' + context.properties['infra_id'] + '-bootstrap-public-ip.address)'
-                }]
-            }],
-            'tags': {
-                'items': [
-                    context.properties['infra_id'] + '-master',
-                    context.properties['infra_id'] + '-bootstrap'
-                ]
+        },
+        {
+            "name": context.properties["infra_id"] + "-bootstrap-ig",
+            "type": "compute.v1.instanceGroup",
+            "properties": {
+                "namedPorts": [
+                    {"name": "ignition", "port": 22623},
+                    {"name": "https", "port": 6443},
+                ],
+                "network": context.properties["cluster_network"],
+                "zone": context.properties["zone"],
             },
-            'zone': context.properties['zone']
-        }
-    }]
+        },
+    ]
 
-    return {'resources': resources}
+    return {"resources": resources}
