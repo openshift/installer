914f4633704492cf10ea5dd4305386674bdf8a15
sunhui <wb-sh373163@alibaba-inc.com>
2022-01-23 01:51:28 +0800

    Alibaba: fix VSwitch subnets overlap
    Fix the overlapping problem of the VSwitch subnet of the Nat gateway with
     the master node VSwitch subnets
    
    Signed-off-by: sunhui <wb-sh373163@alibaba-inc.com>

--- data/data/alibabacloud/cluster/vpc/vpc.tf
+++ data/data/alibabacloud/cluster/vpc/vpc.tf
@@ -2,7 +2,6 @@
 locals {
   description = "Created By OpenShift Installer"
   prefix      = var.cluster_id
-  newbits     = max(4, 20 - tonumber(split("/", var.vpc_cidr_block)[1]))
   vpc_id      = var.vpc_id == "" ? alicloud_vpc.vpc.0.id : var.vpc_id
   vswitch_ids = length(var.vswitch_ids) == 0 ? alicloud_vswitch.vswitches.*.id : var.vswitch_ids
 }
@@ -32,7 +31,7 @@ resource "alicloud_vswitch" "vswitches" {
   vswitch_name = "${local.prefix}-vswitch-${var.zone_ids[count.index]}"
   description  = local.description
   vpc_id       = local.vpc_id
-  cidr_block   = cidrsubnet(var.vpc_cidr_block, local.newbits, count.index)
+  cidr_block   = cidrsubnet(var.vpc_cidr_block, ceil(log(length(var.zone_ids) + 1, 2)), count.index + 1)
   zone_id      = var.zone_ids[count.index]
   tags = merge(
     {
@@ -46,7 +45,7 @@ resource "alicloud_vswitch" "vswitch_nat_gateway" {
   vswitch_name = "${local.prefix}-vswitch-nat-gateway"
   description  = local.description
   vpc_id       = local.vpc_id
-  cidr_block   = cidrsubnet(var.vpc_cidr_block, local.newbits, local.newbits)
+  cidr_block   = cidrsubnet(var.vpc_cidr_block, ceil(log(length(var.zone_ids) + 1, 2)), 0)
   zone_id      = var.nat_gateway_zone_id
   tags = merge(
     {
