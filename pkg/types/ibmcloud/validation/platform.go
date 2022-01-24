e5d628dc70cf4df03b3adb80d96e7c63a3511288
Jeff Nowicki <jnowicki@us.ibm.com>
2022-01-24 15:34:49 -0600

    update validation for unsupported options

--- pkg/types/ibmcloud/validation/platform.go
+++ pkg/types/ibmcloud/validation/platform.go
@@ -42,8 +42,6 @@ func ValidatePlatform(p *ibmcloud.Platform, fldPath *field.Path) field.ErrorList
 		allErrs = append(allErrs, field.NotSupported(fldPath.Child("region"), p.Region, regionShortNames))
 	}
 
-	allErrs = append(allErrs, validateVPCConfig(p, fldPath)...)
-
 	if p.DefaultMachinePlatform != nil {
 		allErrs = append(allErrs, ValidateMachinePool(p, p.DefaultMachinePlatform, fldPath.Child("defaultMachinePlatform"))...)
 	}
