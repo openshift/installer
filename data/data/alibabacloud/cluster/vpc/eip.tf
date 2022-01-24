14246b342dde5cd861537079c71abac94886b584
sunhui <wb-sh373163@alibaba-inc.com>
2022-01-25 00:23:04 +0800

    Alibaba: fix resource creation for existing network
    When users use an existing network, no longer create Nat gateways and
    EIPs
    
    Signed-off-by: sunhui <wb-sh373163@alibaba-inc.com>

--- data/data/alibabacloud/cluster/vpc/eip.tf
+++ data/data/alibabacloud/cluster/vpc/eip.tf
@@ -1,4 +1,6 @@
 resource "alicloud_eip_address" "eip" {
+  count = length(var.vswitch_ids) == 0 ? 1 : 0
+
   description          = local.description
   address_name         = "${local.prefix}-eip"
   payment_type         = "PayAsYouGo"
@@ -13,7 +15,9 @@ resource "alicloud_eip_address" "eip" {
 }
 
 resource "alicloud_eip_association" "eip_association" {
-  allocation_id = alicloud_eip_address.eip.id
-  instance_id   = alicloud_nat_gateway.nat_gateway.id
+  count = length(var.vswitch_ids) == 0 ? 1 : 0
+
+  allocation_id = alicloud_eip_address.eip[0].id
+  instance_id   = alicloud_nat_gateway.nat_gateway[0].id
   instance_type = "Nat"
 }
--- data/data/alibabacloud/cluster/vpc/outputs.tf
+++ data/data/alibabacloud/cluster/vpc/outputs.tf
@@ -10,18 +10,6 @@ output "az_to_vswitch_id" {
   value = zipmap(data.alicloud_vswitches.vswitches.vswitches.*.zone_id, data.alicloud_vswitches.vswitches.vswitches.*.id)
 }
 
-output "gw_id" {
-  value = alicloud_nat_gateway.nat_gateway.id
-}
-
-output "eip_id" {
-  value = alicloud_eip_address.eip.id
-}
-
-output "eip_ip" {
-  value = alicloud_eip_address.eip.ip_address
-}
-
 output "slb_ids" {
   value = [alicloud_slb_load_balancer.slb_external.id, alicloud_slb_load_balancer.slb_internal.id]
 }
