701444d177f63f97185e834ccdc9c7be8f893897
Zane Bitter <zbitter@redhat.com>
2022-02-16 15:43:23 -0500

    Handle multiple interfaces
    Always bind assisted service to the IP of the interface of the default
    route.

--- data/data/agent/files/usr/local/bin/start-assisted-service.sh
+++ data/data/agent/files/usr/local/bin/start-assisted-service.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-HOST=$(ip -4 -j address | jq -r '[.[].addr_info]|flatten(1)|map(select(.scope=="global"))|.[0].local')
-echo Using hostname ${HOST} 1>&2
+get_host() {
+    local default_gateway
+    local host_ip
+
+    default_gateway=$(ip -j route show default | jq -r '.[0].gateway')
+    host_ip=$(ip -j route get "${default_gateway}" | jq -r '.[0].prefsrc')
+
+    local host_fmt="%s"
+    if [[ ${host_ip} =~ : ]]; then
+        host_fmt="[%s]"
+    fi
+
+    # shellcheck disable=SC2059
+    printf "${host_fmt}" "${host_ip}"
+}
+
+
+HOST=$(get_host)
+echo Using hostname "${HOST}" 1>&2
 
 podman play kube --configmap <(sed -e "/SERVICE_BASE_URL/ s/127\.0\.0\.1/${HOST}/" /usr/local/share/assisted-service/configmap.yml) /usr/local/share/assisted-service/pod.yml
