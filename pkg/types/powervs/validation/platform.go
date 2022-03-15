b057763021716e3333962323c8daa93ab68f1c4c
Basheer K <basheer.8765@gmail.com>
2022-03-15 11:25:11 +0000

    Added platform validation testcases for IBM PowerVS platform

--- pkg/types/powervs/validation/platform.go
+++ pkg/types/powervs/validation/platform.go
@@ -1,6 +1,7 @@
 package validation
 
 import (
+	"github.com/google/uuid"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
 	"github.com/openshift/installer/pkg/types/powervs"
@@ -10,9 +11,24 @@ import (
 func ValidatePlatform(p *powervs.Platform, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
+	//Validate Region
 	if p.Region == "" {
 		allErrs = append(allErrs, field.Required(fldPath.Child("region"), "region must be specified"))
+	} else if _, ok := powervs.Regions[p.Region]; !ok {
+		allErrs = append(allErrs, field.NotSupported(fldPath.Child("region"), p.Region, powervs.RegionShortNames()))
 	}
 
+	//validate ServiceInstanceID
+	if p.ServiceInstanceID != "" {
+		_, err := uuid.Parse(p.ServiceInstanceID)
+		if err != nil {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("ServiceInstanceID"), p.ServiceInstanceID, "ServiceInstanceID must be a valid UUID"))
+		}
+	}
+
+	//validate DefaultMachinePlatform
+	if p.DefaultMachinePlatform != nil {
+		allErrs = append(allErrs, ValidateMachinePool(p.DefaultMachinePlatform, fldPath.Child("defaultMachinePlatform"))...)
+	}
 	return allErrs
 }
--- pkg/types/powervs/validation/platform_test.go
+++ pkg/types/powervs/validation/platform_test.go
@@ -1,12 +1,94 @@
 package validation
 
 import (
+	"math/rand"
 	"testing"
 
-	_ "github.com/stretchr/testify/assert"
-	_ "k8s.io/apimachinery/pkg/util/validation/field"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 
-	_ "github.com/openshift/installer/pkg/types/powervs"
+	"github.com/openshift/installer/pkg/types/powervs"
 )
 
-func TestValidatePlatform(t *testing.T) {}
+var (
+	validRegion = powervs.RegionShortNames()[rand.Intn(len(powervs.RegionShortNames()))]
+)
+
+func validMinimalPlatform() *powervs.Platform {
+	return &powervs.Platform{
+		Region: validRegion,
+	}
+}
+
+func validMachinePool() *powervs.MachinePool {
+	return &powervs.MachinePool{}
+}
+
+func TestValidatePlatform(t *testing.T) {
+	cases := []struct {
+		name     string
+		platform *powervs.Platform
+		valid    bool
+	}{
+		{
+			name:     "Config: Minimal platform config",
+			platform: validMinimalPlatform(),
+			valid:    true,
+		},
+		{
+			name: "Region: Invalid region",
+			platform: func() *powervs.Platform {
+				p := validMinimalPlatform()
+				p.Region = "invalid"
+				return p
+			}(),
+			valid: false,
+		},
+		{
+			name: "Region: Missing region",
+			platform: func() *powervs.Platform {
+				p := validMinimalPlatform()
+				p.Region = ""
+				return p
+			}(),
+			valid: false,
+		},
+		{
+			name: "Machine Pool: Valid machine pool",
+			platform: func() *powervs.Platform {
+				p := validMinimalPlatform()
+				p.DefaultMachinePlatform = validMachinePool()
+				return p
+			}(),
+			valid: true,
+		},
+		{
+			name: "ServiceInstanceID: Valid Power VS ServiceInstanceID",
+			platform: func() *powervs.Platform {
+				p := validMinimalPlatform()
+				p.ServiceInstanceID = "05d5dbfd-2a62-4d01-b37b-71211be442f6"
+				return p
+			}(),
+			valid: true,
+		},
+		{
+			name: "ServiceInstanceID: Invalid Power VS ServiceInstanceID",
+			platform: func() *powervs.Platform {
+				p := validMinimalPlatform()
+				p.ServiceInstanceID = "abc123"
+				return p
+			}(),
+			valid: false,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := ValidatePlatform(tc.platform, field.NewPath("test-path")).ToAggregate()
+			if tc.valid {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
