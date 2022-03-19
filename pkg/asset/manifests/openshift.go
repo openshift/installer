nutanix-cloud-native/openshift-installer

Add Nutanix-specific customizations to manifests
- Update templates in data/data/manifests
- Add nutanix platform customizations in pkg/asset/manifests

pkg/asset/manifests/openshift.go
@@ -31,6 +31,7 @@ import (
 	baremetaltypes "github.com/openshift/installer/pkg/types/baremetal"
 	gcptypes "github.com/openshift/installer/pkg/types/gcp"
 	ibmcloudtypes "github.com/openshift/installer/pkg/types/ibmcloud"
+	nutanixtypes "github.com/openshift/installer/pkg/types/nutanix"
 	openstacktypes "github.com/openshift/installer/pkg/types/openstack"
 	ovirttypes "github.com/openshift/installer/pkg/types/ovirt"
 	vspheretypes "github.com/openshift/installer/pkg/types/vsphere"
@@ -211,6 +212,16 @@ func (o *Openshift) Generate(dependencies asset.Parents) error {
 				Base64encodeCABundle: base64.StdEncoding.EncodeToString([]byte(conf.CABundle)),
 			},
 		}
+	case nutanixtypes.Name:
+		cloudCreds = cloudCredsSecretData{
+			Nutanix: &NutanixCredsSecretData{
+				Base64encodeNutanixEndpoint: base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.PrismCentral)),
+				Base64encodeNutanixPort:     base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.Port)),
+				Base64encodeNutanixUsername: base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.Username)),
+				Base64encodeNutanixPassword: base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.Password)),
+			},
+		}
+
 	}
 
 	templateData := &openshiftTemplateData{
@@ -236,7 +247,7 @@ func (o *Openshift) Generate(dependencies asset.Parents) error {
 	}
 
 	switch platform {
-	case awstypes.Name, openstacktypes.Name, vspheretypes.Name, azuretypes.Name, gcptypes.Name, ibmcloudtypes.Name, ovirttypes.Name:
+	case awstypes.Name, openstacktypes.Name, vspheretypes.Name, azuretypes.Name, gcptypes.Name, ibmcloudtypes.Name, ovirttypes.Name, nutanixtypes.Name:
 		if installConfig.Config.CredentialsMode != types.ManualCredentialsMode {
 			assetData["99_cloud-creds-secret.yaml"] = applyTemplateData(cloudCredsSecret.Files()[0].Data, templateData)
 		}

pkg/asset/manifests/template.go
@@ -51,6 +51,14 @@ type OvirtCredsSecretData struct {
 	Base64encodeCABundle string
 }
 
+// NutanixCredsSecretData holds encoded credentials and is used to generate cloud-creds secret
+type NutanixCredsSecretData struct {
+	Base64encodeNutanixEndpoint string
+	Base64encodeNutanixPort     string
+	Base64encodeNutanixUsername string
+	Base64encodeNutanixPassword string
+}
+
 type cloudCredsSecretData struct {
 	AWS       *AwsCredsSecretData
 	Azure     *AzureCredsSecretData
@@ -59,6 +67,7 @@ type cloudCredsSecretData struct {
 	OpenStack *OpenStackCredsSecretData
 	VSphere   *VSphereCredsSecretData
 	Ovirt     *OvirtCredsSecretData
+	Nutanix	  *NutanixCredsSecretData
 }
 
 type bootkubeTemplateData struct {

openshift/installer

pkg/asset/manifests/openshift.go
@@ -31,6 +31,7 @@ import (
 	baremetaltypes "github.com/openshift/installer/pkg/types/baremetal"
 	gcptypes "github.com/openshift/installer/pkg/types/gcp"
 	ibmcloudtypes "github.com/openshift/installer/pkg/types/ibmcloud"
+	nutanixtypes "github.com/openshift/installer/pkg/types/nutanix"
 	openstacktypes "github.com/openshift/installer/pkg/types/openstack"
 	ovirttypes "github.com/openshift/installer/pkg/types/ovirt"
 	vspheretypes "github.com/openshift/installer/pkg/types/vsphere"
@@ -211,6 +212,15 @@ func (o *Openshift) Generate(dependencies asset.Parents) error {
 				Base64encodeCABundle: base64.StdEncoding.EncodeToString([]byte(conf.CABundle)),
 			},
 		}
+	case nutanixtypes.Name:
+		cloudCreds = cloudCredsSecretData{
+			Nutanix: &NutanixCredsSecretData{
+				Base64encodeEndpoint: base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.PrismCentral)),
+				Base64encodePort:     base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.Port)),
+				Base64encodeUsername: base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.Username)),
+				Base64encodePassword: base64.StdEncoding.EncodeToString([]byte(installConfig.Config.Nutanix.Password)),
+			},
+		}
 	}
 
 	templateData := &openshiftTemplateData{
@@ -236,7 +246,7 @@ func (o *Openshift) Generate(dependencies asset.Parents) error {
 	}
 
 	switch platform {
-	case awstypes.Name, openstacktypes.Name, vspheretypes.Name, azuretypes.Name, gcptypes.Name, ibmcloudtypes.Name, ovirttypes.Name:
+	case awstypes.Name, openstacktypes.Name, vspheretypes.Name, azuretypes.Name, gcptypes.Name, ibmcloudtypes.Name, ovirttypes.Name, nutanixtypes.Name:
 		if installConfig.Config.CredentialsMode != types.ManualCredentialsMode {
 			assetData["99_cloud-creds-secret.yaml"] = applyTemplateData(cloudCredsSecret.Files()[0].Data, templateData)
 		}

pkg/asset/manifests/template.go
@@ -54,6 +54,14 @@ type OvirtCredsSecretData struct {
 	Base64encodeCABundle string
 }
 
+// NutanixCredsSecretData holds encoded credentials and is used to generate cloud-creds secret
+type NutanixCredsSecretData struct {
+	Base64encodeEndpoint string
+	Base64encodePort     string
+	Base64encodeUsername string
+	Base64encodePassword string
+}
+
 type cloudCredsSecretData struct {
 	AWS       *AwsCredsSecretData
 	Azure     *AzureCredsSecretData
@@ -62,6 +70,7 @@ type cloudCredsSecretData struct {
 	OpenStack *OpenStackCredsSecretData
 	VSphere   *VSphereCredsSecretData
 	Ovirt     *OvirtCredsSecretData
+	Nutanix   *NutanixCredsSecretData
 }
 
 type bootkubeTemplateData struct {