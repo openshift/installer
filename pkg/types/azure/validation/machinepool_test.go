1c5e9f60176cefc1d07d64c01a3451566e7a9c53
staebler <staebler@redhat.com>
2022-04-11 09:30:13 -0400

    azure: unit tests for static validation of marketplace images
    Add unit tests for the static validation of marketplace images
    selected in the install config.

--- pkg/types/azure/validation/machinepool_test.go
+++ pkg/types/azure/validation/machinepool_test.go
@@ -12,79 +12,90 @@ import (
 func TestValidateMachinePool(t *testing.T) {
 	cases := []struct {
 		name     string
-		pool     *azure.MachinePool
-		expected string
-	}{
-		{
-			name: "empty",
-			pool: &azure.MachinePool{},
-		},
-		{
-			name: "valid iops",
-			pool: &azure.MachinePool{
-				OSDisk: azure.OSDisk{
-					DiskSizeGB: 120,
-				},
-			},
-		},
-		{
-			name: "invalid iops",
-			pool: &azure.MachinePool{
-				OSDisk: azure.OSDisk{
-					DiskSizeGB: -120,
-				},
-			},
-			expected: `^test-path\.diskSizeGB: Invalid value: -120: Storage DiskSizeGB must be positive$`,
-		},
-		{
-			name: "valid disk",
-			pool: &azure.MachinePool{
-				OSDisk: azure.OSDisk{
-					DiskType: "Premium_LRS",
-				},
-			},
-		},
-		{
-			name: "invalid disk",
-			pool: &azure.MachinePool{
-				OSDisk: azure.OSDisk{
-					DiskType: "LRS",
-				},
-			},
-			expected: `^test-path\.diskType: Unsupported value: "LRS": supported values: "Premium_LRS", "StandardSSD_LRS", "Standard_LRS"$`,
-		},
-	}
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			azurePlatform := &azure.Platform{CloudName: azure.PublicCloud}
-			err := ValidateMachinePool(tc.pool, "worker", azurePlatform, field.NewPath("test-path")).ToAggregate()
-			if tc.expected == "" {
-				assert.NoError(t, err)
-			} else {
-				assert.Regexp(t, tc.expected, err)
-			}
-		})
-	}
-}
-
-func TestValidateMasterDiskType(t *testing.T) {
-	cases := []struct {
-		name     string
 		pool     *types.MachinePool
 		expected string
 	}{
 		{
 			name: "empty",
 			pool: &types.MachinePool{
+				Name: "worker",
 				Platform: types.MachinePoolPlatform{
 					Azure: &azure.MachinePool{},
 				},
 			},
 		},
 		{
+			name: "valid iops",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSDisk: azure.OSDisk{
+							DiskSizeGB: 120,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "invalid iops",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSDisk: azure.OSDisk{
+							DiskSizeGB: -120,
+						},
+					},
+				},
+			},
+			expected: `^test-path\.diskSizeGB: Invalid value: -120: Storage DiskSizeGB must be positive$`,
+		},
+		{
+			name: "valid disk",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSDisk: azure.OSDisk{
+							DiskType: "Premium_LRS",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "invalid disk",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSDisk: azure.OSDisk{
+							DiskType: "LRS",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.diskType: Unsupported value: "LRS": supported values: "Premium_LRS", "StandardSSD_LRS", "Standard_LRS"$`,
+		},
+		{
 			name: "unsupported disk master",
 			pool: &types.MachinePool{
 				Name: "master",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSDisk: azure.OSDisk{
+							DiskType: "Standard_LRS",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.diskType: Unsupported value: "Standard_LRS": supported values: "Premium_LRS", "StandardSSD_LRS"$`,
+		},
+		{
+			name: "unsupported disk default pool",
+			pool: &types.MachinePool{
+				Name: "",
 				Platform: types.MachinePoolPlatform{
 					Azure: &azure.MachinePool{
 						OSDisk: azure.OSDisk{
@@ -107,6 +118,120 @@ func TestValidateMasterDiskType(t *testing.T) {
 					},
 				},
 			},
+		},
+		{
+			name: "valid OS image",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSImage: azure.OSImage{
+							Publisher: "test-publisher",
+							Offer:     "test-offer",
+							SKU:       "test-sku",
+							Version:   "test-version",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "OS image missing publisher",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSImage: azure.OSImage{
+							Offer:   "test-offer",
+							SKU:     "test-sku",
+							Version: "test-version",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.osImage.publisher: Required value: must specify publisher for the OS image$`,
+		},
+		{
+			name: "OS image missing offer",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSImage: azure.OSImage{
+							Publisher: "test-publisher",
+							SKU:       "test-sku",
+							Version:   "test-version",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.osImage.offer: Required value: must specify offer for the OS image$`,
+		},
+		{
+			name: "OS image missing sku",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSImage: azure.OSImage{
+							Publisher: "test-publisher",
+							Offer:     "test-offer",
+							Version:   "test-version",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.osImage.sku: Required value: must specify SKU for the OS image$`,
+		},
+		{
+			name: "OS image missing version",
+			pool: &types.MachinePool{
+				Name: "worker",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSImage: azure.OSImage{
+							Publisher: "test-publisher",
+							Offer:     "test-offer",
+							SKU:       "test-sku",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.osImage.version: Required value: must specify version for the OS image$`,
+		},
+		{
+			name: "OS image for master",
+			pool: &types.MachinePool{
+				Name: "master",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSImage: azure.OSImage{
+							Publisher: "test-publisher",
+							Offer:     "test-offer",
+							SKU:       "test-sku",
+							Version:   "test-version",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.osImage: Invalid value: .* cannot specify the OS image for the master machines$`,
+		},
+		{
+			name: "OS image for default pool",
+			pool: &types.MachinePool{
+				Name: "",
+				Platform: types.MachinePoolPlatform{
+					Azure: &azure.MachinePool{
+						OSImage: azure.OSImage{
+							Publisher: "test-publisher",
+							Offer:     "test-offer",
+							SKU:       "test-sku",
+							Version:   "test-version",
+						},
+					},
+				},
+			},
+			expected: `^test-path\.osImage: Invalid value: .* cannot specify the OS image for the master machines$`,
 		},
 	}
 	for _, tc := range cases {
