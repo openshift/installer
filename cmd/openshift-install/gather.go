541f076efa592f94b2c4f2bfc9ee0cd368c513ce
John Hixson <jhixson@redhat.com>
2022-05-03 21:41:22 -0700

    gather: gcp specific gather methods

--- cmd/openshift-install/gather.go
+++ cmd/openshift-install/gather.go
@@ -25,6 +25,7 @@ import (
 	"github.com/openshift/installer/pkg/asset/tls"
 	"github.com/openshift/installer/pkg/gather"
 	_ "github.com/openshift/installer/pkg/gather/azure"
+	_ "github.com/openshift/installer/pkg/gather/gcp"
 	"github.com/openshift/installer/pkg/gather/service"
 	"github.com/openshift/installer/pkg/gather/ssh"
 	platformstages "github.com/openshift/installer/pkg/terraform/stages/platform"
