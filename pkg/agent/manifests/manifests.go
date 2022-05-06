d2b5be8524ef1120d6519d87a03bf82251d22e5f
Richard Su <rwsu@redhat.com>
2022-05-06 12:00:09 -0700

    AGENT-56 Remove trailing slash from path.Join

--- pkg/agent/manifests/manifests.go
+++ pkg/agent/manifests/manifests.go
@@ -18,7 +18,7 @@ import (
 // Read a Yaml file and unmarshal the contents
 func GetFileData(fileName string, output interface{}) error {
 
-	path := filepath.Join("./manifests/", fileName)
+	path := filepath.Join("./manifests", fileName)
 
 	contents, err := os.ReadFile(path)
 	if err != nil {
