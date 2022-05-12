e6bbf3e1dab9928c0caf35036fe1451adc8168fb
patrickdillon <padillon@redhat.com>
2022-05-12 07:12:07 -0400

    GCP: Revert Instance Type from N2 to N1
    GCP has separate quotas for N2 CPUs: https://cloud.google.com/compute/quotas#cpu_quota
    We should revert this change until we're not breaking CI.

--- pkg/asset/machines/worker.go
+++ pkg/asset/machines/worker.go
@@ -113,7 +113,7 @@ func defaultAzureMachinePoolPlatform() azuretypes.MachinePool {
 
 func defaultGCPMachinePoolPlatform() gcptypes.MachinePool {
 	return gcptypes.MachinePool{
-		InstanceType: "n2-standard-4",
+		InstanceType: "n1-standard-4",
 		OSDisk: gcptypes.OSDisk{
 			DiskSizeGB: powerOfTwoRootVolumeSize,
 			DiskType:   "pd-ssd",
