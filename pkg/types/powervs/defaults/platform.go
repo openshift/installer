50bb3d01e7230a723d27ad4a7fcc6a41b953bc3f
Basheer K <basheer.8765@gmail.com>
2022-05-24 11:40:03 +0000

    Changed DefaultMachineCIDR of PowerVS to 192.168.0.0/16

--- pkg/types/powervs/defaults/platform.go
+++ pkg/types/powervs/defaults/platform.go
@@ -1,7 +1,15 @@
 package defaults
 
 import (
+	"github.com/openshift/installer/pkg/ipnet"
+
 	"github.com/openshift/installer/pkg/types/powervs"
+)
+
+var (
+	// DefaultMachineCIDR is the PowerVS default IP address space from
+	// which to assign machine IPs.
+	DefaultMachineCIDR = ipnet.MustParseCIDR("192.168.0.0/16")
 )
 
 // SetPlatformDefaults sets the defaults for the platform.
