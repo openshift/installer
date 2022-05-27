f17c1a279b6280a2917da4d090cb928b6cbd3ad4
Zane Bitter <zbitter@redhat.com>
2022-05-27 17:02:05 -0400

    Downgrade INFO logs to DEBUG
    Some of the stuff we were logging at INFO level (visible on the console
    by default) is much finer-grained detail than even what is normally
    logged at DEBUG level.

--- pkg/asset/agent/manifests/nmstateconfig.go
+++ pkg/asset/agent/manifests/nmstateconfig.go
@@ -243,7 +243,7 @@ func buildMacInterfaceMap(nmStateConfig aiv1beta1.NMStateConfig) models.MacInter
 	// TODO - this eventually will move to another asset so the interface definition can be shared with Butane
 	macInterfaceMap := make(models.MacInterfaceMap, 0, len(nmStateConfig.Spec.Interfaces))
 	for _, cfg := range nmStateConfig.Spec.Interfaces {
-		logrus.Println("adding MAC interface map to host static network config - Name: ", cfg.Name, " MacAddress:", cfg.MacAddress)
+		logrus.Debug("adding MAC interface map to host static network config - Name: ", cfg.Name, " MacAddress:", cfg.MacAddress)
 		macInterfaceMap = append(macInterfaceMap, &models.MacInterfaceMapItems0{
 			MacAddress:     cfg.MacAddress,
 			LogicalNicName: cfg.Name,
