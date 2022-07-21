3ebd708743648c4d0c196df951ac87ee29c6d89e
Basheer Khadarsabgar <basheer.8765@gmail.com>
2022-07-21 17:58:29 +0530

    Updated Max and Min machinepool resource limits of Power VS

--- pkg/types/powervs/validation/machinepool_test.go
+++ pkg/types/powervs/validation/machinepool_test.go
@@ -56,14 +56,23 @@ func TestValidateMachinePool(t *testing.T) {
 			pool: &powervs.MachinePool{
 				MemoryGiB: 1,
 			},
-			expected: `^test-path\.memory: Invalid value: 1: memory must be an integer number of GB that is at least 2 and no more than 64$`,
+			expected: `^test-path\.memory: Invalid value: 1: memory must be an integer number of GB that is at least 4$`,
 		},
 		{
-			name: "invalid memory over",
+			name: "invalid memory over of e980 systype",
 			pool: &powervs.MachinePool{
-				MemoryGiB: 65,
+				SysType:   "e980",
+				MemoryGiB: 15308,
 			},
-			expected: `^test-path\.memory: Invalid value: 65: memory must be an integer number of GB that is at least 2 and no more than 64$`,
+			expected: `^test-path\.memory: Invalid value: 15308: maximum memory limit for the e980 SysType is 15307GiB$`,
+		},
+		{
+			name: "invalid memory over of s922 systype",
+			pool: &powervs.MachinePool{
+				SysType:   "s922",
+				MemoryGiB: 943,
+			},
+			expected: `^test-path\.memory: Invalid value: 943: maximum memory limit for the s922 SysType is 942GiB$`,
 		},
 		{
 			name: "valid processors",
@@ -72,18 +81,11 @@ func TestValidateMachinePool(t *testing.T) {
 			},
 		},
 		{
-			name: "invalid processors over",
-			pool: &powervs.MachinePool{
-				Processors: intstr.FromInt(33),
-			},
-			expected: `^test-path\.processors: Invalid value: 33: number of processors must be from \.5 to 32 cores$`,
-		},
-		{
 			name: "invalid processors under",
 			pool: &powervs.MachinePool{
 				Processors: intstr.FromString("0.25"),
 			},
-			expected: `^test-path\.processors: Invalid value: 0.25: number of processors must be from \.5 to 32 cores$`,
+			expected: `^test-path\.processors: Invalid value: 0.25: minimum number of processors must be .5 cores for capped or shared ProcType$`,
 		},
 		{
 			name: "invalid processors string",
@@ -91,6 +93,22 @@ func TestValidateMachinePool(t *testing.T) {
 				Processors: intstr.FromString("all"),
 			},
 			expected: `^test-path\.processors: Invalid value: "all": processors must be a valid floating point number$`,
+		},
+		{
+			name: "invalid processors over for s922 systype",
+			pool: &powervs.MachinePool{
+				SysType:    "s922",
+				Processors: intstr.FromInt(33),
+			},
+			expected: `^test-path\.processors: Invalid value: 33: maximum processors limit for s922 SysType is 15 cores$`,
+		},
+		{
+			name: "invalid processors over for e980 systype",
+			pool: &powervs.MachinePool{
+				SysType:    "e980",
+				Processors: intstr.FromInt(144),
+			},
+			expected: `^test-path\.processors: Invalid value: 144: maximum processors limit for e980 SysType is 143 cores$`,
 		},
 		{
 			name: "invalid processors increment",
