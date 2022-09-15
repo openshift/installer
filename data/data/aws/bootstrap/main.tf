b936f8b8162c82706957cd329890847c125e59aa
Rafael Fonseca <r4f4rfs@gmail.com>
2022-09-15 23:26:06 +0200

    aws: remove deprecated s3_bucket field

--- data/data/aws/bootstrap/main.tf
+++ data/data/aws/bootstrap/main.tf
@@ -34,7 +34,6 @@ data "aws_ebs_default_kms_key" "current" {}
 
 resource "aws_s3_bucket" "ignition" {
   bucket = var.aws_ignition_bucket
-  acl    = "private"
 
   tags = merge(
     {
@@ -46,6 +45,11 @@ resource "aws_s3_bucket" "ignition" {
   lifecycle {
     ignore_changes = all
   }
+}
+
+resource "aws_s3_bucket_acl" ignition {
+  bucket = aws_s3_bucket.ignition.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "ignition" {
