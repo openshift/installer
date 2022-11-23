0506dfc2d7bb4750f52bccb4afcec64e3e1e25b5
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-11-23 14:20:57 +0100

    bootstrap: don't enable gatewayd socket on OKD
    FCOS doesn't come with this service, and now assisted installer fails attempting to enable this socket

--- pkg/asset/ignition/bootstrap/common.go
+++ pkg/asset/ignition/bootstrap/common.go
@@ -50,14 +50,16 @@ var (
 	commonEnabledServices = []string{
 		"progress.service",
 		"kubelet.service",
-		"chown-gatewayd-key.service",
-		"systemd-journal-gatewayd.socket",
 		"approve-csr.service",
 		// baremetal & openstack platform services
 		"keepalived.service",
 		"coredns.service",
 		"ironic.service",
 		"master-bmh-update.service",
+	}
+	ocpEnabledServices = []string{
+		"chown-gatewayd-key.service",
+		"systemd-journal-gatewayd.socket",
 	}
 )
 
@@ -175,6 +177,11 @@ func (a *Common) generateConfig(dependencies asset.Parents, templateData *bootst
 	}
 	if err := AddSystemdUnits(a.Config, "bootstrap/systemd/units", templateData, commonEnabledServices); err != nil {
 		return err
+	}
+	if !templateData.IsOKD {
+		if err := AddSystemdUnits(a.Config, "bootstrap/systemd/units", templateData, ocpEnabledServices); err != nil {
+			return err
+		}
 	}
 
 	// Check for optional platform specific files/units
