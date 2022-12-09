4daff6117fce1d73774b02caf605d9e1b836dd7c
Zane Bitter <zbitter@redhat.com>
2022-12-09 12:31:34 -0500

    Update console debug messages immediately
    When updating the console debug messages, don't wait for the next
    refresh. Issue a reload, which will cause the console to be redisplayed
    unless the user has already started logging in.

---
+++ data/data/agent/files/usr/local/bin/issue_status.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+issue_file() {
+    printf "/etc/issue.d/%s.issue" "$1"
+}
+
+set_issue() {
+    local outfile
+    outfile="$(issue_file "$1")"
+    local tmp
+    tmp="$(mktemp)"
+    {
+        printf '\n'
+        cat -
+        printf '\n'
+    } >"${tmp}"
+    if ! diff "${tmp}" "${outfile}" >/dev/null 2>&1; then
+        mv "${tmp}" "${outfile}"
+        agetty --reload
+    else
+        rm "${tmp}"
+    fi
+}
+
+clear_issue() {
+    local outfile
+    outfile="$(issue_file "$1")"
+    if [ -f "${outfile}" ]; then
+        rm "${outfile}"
+        agetty --reload
+    fi
+}
