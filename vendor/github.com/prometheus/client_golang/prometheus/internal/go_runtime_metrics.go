598fde1dc1f94d7e0f36f446f2ef21c7bccec647
Rafael Fonseca <r4f4rfs@gmail.com>
2023-01-04 20:18:16 +0100

    Bug 2067847: go.mod: update prometheus/client_golang
    A denial of service attack was found in prometheus/client_golang. This flaw allows an attacker to produce a denial of service attack on an HTTP server by exploiting the InstrumentHandlerCounter function in the version below 1.11.1, resulting in a loss of availability.

--- vendor/github.com/prometheus/client_golang/prometheus/internal/go_runtime_metrics.go
+++ vendor/github.com/prometheus/client_golang/prometheus/internal/go_runtime_metrics.go
@@ -62,7 +62,7 @@ func RuntimeMetricsToProm(d *metrics.Description) (string, string, string, bool)
 	// other data.
 	name = strings.ReplaceAll(name, "-", "_")
 	name = name + "_" + unit
-	if d.Cumulative && d.Kind != metrics.KindFloat64Histogram {
+	if d.Cumulative {
 		name = name + "_total"
 	}
 
@@ -84,12 +84,12 @@ func RuntimeMetricsToProm(d *metrics.Description) (string, string, string, bool)
 func RuntimeMetricsBucketsForUnit(buckets []float64, unit string) []float64 {
 	switch unit {
 	case "bytes":
-		// Re-bucket as powers of 2.
-		return reBucketExp(buckets, 2)
+		// Rebucket as powers of 2.
+		return rebucketExp(buckets, 2)
 	case "seconds":
-		// Re-bucket as powers of 10 and then merge all buckets greater
+		// Rebucket as powers of 10 and then merge all buckets greater
 		// than 1 second into the +Inf bucket.
-		b := reBucketExp(buckets, 10)
+		b := rebucketExp(buckets, 10)
 		for i := range b {
 			if b[i] <= 1 {
 				continue
@@ -103,11 +103,11 @@ func RuntimeMetricsBucketsForUnit(buckets []float64, unit string) []float64 {
 	return buckets
 }
 
-// reBucketExp takes a list of bucket boundaries (lower bound inclusive) and
+// rebucketExp takes a list of bucket boundaries (lower bound inclusive) and
 // downsamples the buckets to those a multiple of base apart. The end result
 // is a roughly exponential (in many cases, perfectly exponential) bucketing
 // scheme.
-func reBucketExp(buckets []float64, base float64) []float64 {
+func rebucketExp(buckets []float64, base float64) []float64 {
 	bucket := buckets[0]
 	var newBuckets []float64
 	// We may see a -Inf here, in which case, add it and skip it
