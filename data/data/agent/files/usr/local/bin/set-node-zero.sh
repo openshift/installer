3109dbf6c4d811d2541bf0ec3ddb40f194306d46
Richard Su <rwsu@redhat.com>
2023-01-05 20:55:10 -0500

    AGENT-501 Make it easier to change node zero IP and SERVICE_BASE_URL
    The node zero IP and ServiceBaseURL are hard-coded into scripts and
    systemd service definition files. This makes it difficult to change
    the node zero IP once the agent ISO has booted up. To faciliate
    changing these settings in the interactive network configuration
    use case, the hard-codings have been replaced with sourcing files
    containing environment variables.
    
    In the interactive network configuration scenario, the environment
    files will be updated by the network configuration utility. The
    agent services are blocked by the interactive console service. When
    the interactive console service finishes, the agent services start
    up and read in the environment files picking up any changes.
    
    In the non-interactive scenario, the environment files will be
    prepopulated with values for node zero IP and SERVICE_BASE_URL in
    the Agent ISO. This is how it works today.
    
    Signed-off-by: Richard Su <rwsu@redhat.com>

---
+++ data/data/agent/files/usr/local/bin/set-node-zero.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+source /etc/assisted/agent-installer.env
+echo "NODE_ZERO_IP: $NODE_ZERO_IP"
+
+timeout=$((SECONDS + 30))
+
+while [[ $SECONDS -lt $timeout ]]
+do
+     IS_NODE_ZERO=$(ip -j address | jq "[.[].addr_info] | flatten | map(.local==\"$NODE_ZERO_IP\") | any")
+     if [ "${IS_NODE_ZERO}" = "true" ]; then
+          break
+     fi
+     sleep 5
+done
+
+if [ "${IS_NODE_ZERO}" = "true" ]; then
+    echo "Node 0 IP ${NODE_ZERO_IP} found on this host" 1>&2
+
+    NODE0_PATH=/etc/assisted-service/node0
+    mkdir -p "$(dirname "${NODE0_PATH}")"
+
+    NODE_ZERO_MAC=$(ip -j address | jq -r ".[] | select(.addr_info | map(select(.local == \"$NODE_ZERO_IP\")) | any).address")
+    echo "MAC Address for Node 0: ${NODE_ZERO_MAC}"
+
+    cat >"${NODE0_PATH}" <<EOF
+# This file exists if the agent-based installer has determined the host is node 0.
+# The host is determined to be node 0 when one of its network interfaces has an 
+# IP address matching NODE_ZERO_IP in /etc/assisted/agent-installer.env. 
+# The MAC address of the network interface matching NODE_ZERO_IP is noted below 
+# as BOOTSTRAP_HOST_MAC.
+BOOTSTRAP_HOST_MAC=${NODE_ZERO_MAC}
+EOF
+
+    echo "Created file ${NODE0_PATH}"
+
+    rendezvousHostMessage="This host ${NODE_ZERO_IP} is the rendezvous host."
+
+    cat <<EOF >/etc/motd
+The primary service is assisted-service.service. To watch its status, run:
+
+  journalctl -u assisted-service.service
+EOF
+else
+
+    rendezvousHostMessage="This host is not the rendezvous host. The rendezvous host is at ${NODE_ZERO_IP}."
+fi
+mkdir -p /etc/motd.d/
+echo $rendezvousHostMessage > /etc/motd.d/60-rendezvous-host
+echo $rendezvousHostMessage > /etc/issue.d/60-rendezvous-host.issue
+agetty --reload
--- data/data/agent/files/usr/local/bin/start-agent.sh
+++ data/data/agent/files/usr/local/bin/start-agent.sh
@@ -7,7 +7,7 @@ INFRA_ENV_ID=""
 until [[ $INFRA_ENV_ID != "" && $INFRA_ENV_ID != "null" ]]; do
     sleep 5
     >&2 echo "Querying assisted-service for infra-env-id..."
-    INFRA_ENV_ID=$(curl -s -S '{{.ServiceBaseURL}}/api/assisted-install/v2/infra-envs' | jq -r .[0].id)
+    INFRA_ENV_ID=$(curl -s -S "${SERVICE_BASE_URL}/api/assisted-install/v2/infra-envs" | jq -r .[0].id)
 done
 echo "Fetched infra-env-id and found: $INFRA_ENV_ID"
 
@@ -19,4 +19,4 @@ IMAGE=$(image_for agent-installer-node-agent)
 echo "Using agent image: ${IMAGE} to start agent"
 
 # use infra-env-id to have agent register this host with assisted-service
-exec /usr/local/bin/agent --url '{{.ServiceBaseURL}}' --infra-env-id '{{.InfraEnvID}}' --agent-version ${IMAGE} --insecure=true
+exec /usr/local/bin/agent --url ${SERVICE_BASE_URL} --infra-env-id ${INFRA_ENV_ID} --agent-version ${IMAGE} --insecure=true
