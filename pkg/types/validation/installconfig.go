6fc85b61b83d6d26189c54286d9d638c51d9956d
patrickdillon <padillon@redhat.com>
2023-01-09 14:21:13 -0500

    GCP XPN: remove tech preview feature gate
    GCP XPN, with DNS zones in the service projects, is intended to GA
    in the next release. Let's remove the feature gate requirement,
    especially because enabling the feature gate enables the CCM; so
    cluster behavior is different than what we would be seeing in actual
    deployments.

--- pkg/types/validation/installconfig.go
+++ pkg/types/validation/installconfig.go
@@ -999,10 +999,6 @@ func validateFeatureSet(c *types.InstallConfig) field.ErrorList {
 		errMsg := "the TechPreviewNoUpgrade feature set must be enabled to use this field"
 
 		if c.GCP != nil {
-			if len(c.GCP.NetworkProjectID) > 0 {
-				allErrs = append(allErrs, field.Forbidden(field.NewPath("platform", "gcp", "networkProjectID"), errMsg))
-			}
-
 			if c.GCP.PrivateDNSZone != nil && len(c.GCP.PrivateDNSZone.ProjectID) > 0 {
 				allErrs = append(allErrs, field.Forbidden(field.NewPath("platform", "gcp", "privateDNSZone", "projectID"), errMsg))
 			}
