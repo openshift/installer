053f94c8b2f8ae2156875520aaf219938f5da9a6
Andrea Fasano <afasano@redhat.com>
2023-01-19 13:14:08 -0500

    Embed agent-tui binary in the agent ISO

---
+++ pkg/asset/agent/image/cpio.go
@@ -0,0 +1,109 @@
+package image
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+	"os"
+
+	"github.com/cavaliercoder/go-cpio"
+)
+
+// CpioArchive simplifies the creation of a compressed cpio archive.
+type CpioArchive struct {
+	buffer     *bytes.Buffer
+	gzipWriter *gzip.Writer
+	cpioWriter *cpio.Writer
+}
+
+// NewCpioArchive creates a new CpioArchive instance.
+func NewCpioArchive() *CpioArchive {
+	buf := new(bytes.Buffer)
+	gw := gzip.NewWriter(buf)
+	cw := cpio.NewWriter(gw)
+
+	return &CpioArchive{
+		buffer:     buf,
+		gzipWriter: gw,
+		cpioWriter: cw,
+	}
+}
+
+// StoreBytes appends to the current archive the given content using
+// the specified filename.
+func (ca *CpioArchive) StoreBytes(filename string, content []byte) error {
+	header := cpio.Header{
+		Name: filename,
+		Mode: 0o100_644,
+		Size: int64(len(content)),
+	}
+
+	err := ca.cpioWriter.WriteHeader(&header)
+	if err != nil {
+		return err
+	}
+
+	_, err = ca.cpioWriter.Write(content)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// StoreFile appends to the current archive the specified file.
+func (ca *CpioArchive) StoreFile(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := cpio.FileInfoHeader(fileInfo, "")
+	if err != nil {
+		return err
+	}
+	if err := ca.cpioWriter.WriteHeader(header); err != nil {
+		return err
+	}
+
+	_, err = io.Copy(ca.cpioWriter, f)
+	if err != nil {
+		return err
+
+	}
+
+	return nil
+}
+
+// Save appends persists the archive on the disk.
+func (ca *CpioArchive) Save(archivePath string) error {
+	err := ca.cpioWriter.Close()
+	if err != nil {
+		return err
+	}
+
+	err = ca.gzipWriter.Close()
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create(archivePath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	bs := ca.buffer.Bytes()
+	_, err = out.Write(bs)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
