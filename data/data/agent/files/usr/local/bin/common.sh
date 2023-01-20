157fd0d9df8eb36750a6a8b1dd1cc16c862e7519
Richard Su <rwsu@redhat.com>
2023-01-20 04:35:33 -0500

    Fix shellcheck where variable should be declared in separate line

--- data/data/agent/files/usr/local/bin/common.sh
+++ data/data/agent/files/usr/local/bin/common.sh
@@ -13,7 +13,8 @@ wait_for_assisted_service() {
 }
 
 is_node_zero() {
-    local is_rendezvous_host=$(ip -j address | jq "[.[].addr_info] | flatten | map(.local==\"$NODE_ZERO_IP\") | any")
+    local is_rendezvous_host
+    is_rendezvous_host=$(ip -j address | jq "[.[].addr_info] | flatten | map(.local==\"$NODE_ZERO_IP\") | any")
     if [[ "${is_rendezvous_host}" == "true" ]]; then
         echo 1
     else
