2eb839500df7aea1cb418f573819efe6f1b64220
Sid Shukla <sid.shukla@nutanix.com>
2023-01-24 17:57:59 +0100

    Check-in the vendored changes

--- vendor/github.com/nutanix-cloud-native/prism-go-client/v3/v3.go
+++ vendor/github.com/nutanix-cloud-native/prism-go-client/v3/v3.go
@@ -1,9 +1,12 @@
 package v3
 
 import (
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
-	"strings"
+	"net/http"
 
+	"github.com/go-logr/logr"
 	"github.com/nutanix-cloud-native/prism-go-client"
 	"github.com/nutanix-cloud-native/prism-go-client/internal"
 )
@@ -12,49 +15,90 @@ const (
 	libraryVersion = "v3"
 	absolutePath   = "api/nutanix/" + libraryVersion
 	userAgent      = "nutanix/" + libraryVersion
-	clientName     = "prism_central"
 )
 
 // Client manages the V3 API
 type Client struct {
 	client *internal.Client
 	V3     Service
+
+	clientOpts []internal.ClientOption
+}
+
+// ClientOption is a functional option for the Client
+type ClientOption func(*Client) error
+
+// WithCertificate sets the certificate for the client
+func WithCertificate(certificate *x509.Certificate) ClientOption {
+	return func(c *Client) error {
+		c.clientOpts = append(c.clientOpts, internal.WithCertificate(certificate))
+		return nil
+	}
+}
+
+// WithPEMEncodedCertBundle sets the certificates for the client
+func WithPEMEncodedCertBundle(certBundle []byte) ClientOption {
+	return func(c *Client) error {
+		for block, rest := pem.Decode(certBundle); block != nil; block, rest = pem.Decode(rest) {
+			if block.Type != "CERTIFICATE" {
+				return fmt.Errorf("unexpected PEM block type %q: was expecting CERTIFICATE", block.Type)
+			}
+			certs, err := x509.ParseCertificates(block.Bytes)
+			if err != nil {
+				return err
+			}
+			for _, cert := range certs {
+				c.clientOpts = append(c.clientOpts, internal.WithCertificate(cert))
+			}
+		}
+		return nil
+	}
+}
+
+// WithRoundTripper overrides the transport for the underlying http client
+// Overriding transport is useful for testing against API Mocks
+// This is not recommended for production use
+func WithRoundTripper(transport http.RoundTripper) ClientOption {
+	return func(c *Client) error {
+		c.clientOpts = append(c.clientOpts, internal.WithRoundTripper(transport))
+		return nil
+	}
+}
+
+// WithLogger sets the logger for the client
+func WithLogger(logger *logr.Logger) ClientOption {
+	return func(c *Client) error {
+		c.clientOpts = append(c.clientOpts, internal.WithLogger(logger))
+		return nil
+	}
 }
 
 // NewV3Client return a internal to operate V3 resources
-func NewV3Client(credentials prismgoclient.Credentials) (*Client, error) {
-	var baseClient *internal.Client
-
-	// check if all required fields are present. Else create an empty internal
-	if credentials.Username != "" && credentials.Password != "" && credentials.Endpoint != "" {
-		c, err := internal.NewClient(
-			internal.WithCredentials(&credentials),
-			internal.WithUserAgent(userAgent),
-			internal.WithAbsolutePath(absolutePath))
-		if err != nil {
+func NewV3Client(credentials prismgoclient.Credentials, opts ...ClientOption) (*Client, error) {
+	v3Client := &Client{}
+	for _, opt := range opts {
+		if err := opt(v3Client); err != nil {
 			return nil, err
 		}
-		baseClient = c
-	} else {
-		errorMsg := fmt.Sprintf("Prism Central (PC) Client is missing. "+
-			"Please provide required details - %s in provider configuration.", strings.Join(credentials.RequiredFields[clientName], ", "))
-
-		baseClient = &internal.Client{UserAgent: userAgent, ErrorMsg: errorMsg}
 	}
 
-	f := &Client{
-		client: baseClient,
-		V3: Operations{
-			client: baseClient,
-		},
+	if credentials.Username == "" || credentials.Password == "" || credentials.Endpoint == "" {
+		return nil, fmt.Errorf("username, password and endpoint are required")
 	}
 
-	// f.internal.OnRequestCompleted(func(req *http.Request, resp *http.Response, v interface{}) {
-	// 	if v != nil {
-	// 		utils.PrintToJSON(v, "[Debug] FINISHED REQUEST")
-	// 		// TBD: How to print responses before all requests.
-	// 	}
-	// })
+	v3Client.clientOpts = append(v3Client.clientOpts,
+		internal.WithCredentials(&credentials),
+		internal.WithUserAgent(userAgent),
+		internal.WithAbsolutePath(absolutePath),
+	)
 
-	return f, nil
+	httpClient, err := internal.NewClient(v3Client.clientOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	v3Client.client = httpClient
+	v3Client.V3 = Operations{client: httpClient}
+
+	return v3Client, nil
 }
