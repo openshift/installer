5ef62bf69ebc0e2eb9a35ea9ed752c0f65abe45e
Bob Fournier <bfournie@redhat.com>
2023-01-31 09:16:33 -0500

    OCPBUGS-6840: Get hostname if not configured in manifests
    When using ZTP manifests there is no mechanism for manually configuring
    the hostname, the hostname must be retrieved via address lookup. Run
    the address lookp in the set-hostname script once the interfaces are
    available and configure the hostname if it can be retrieved.

--- data/data/agent/files/usr/local/bin/set-hostname.sh
+++ data/data/agent/files/usr/local/bin/set-hostname.sh
@@ -25,6 +25,34 @@ echo "Waiting for network to determine if this is the rendezvous host." > /etc/m
 # as this host's hostname.
 #
 
+# This is a workaround to handle the case when NetworkManager
+# cannot get the hostname from the IP via reverse DNS
+function lookup_address() {
+
+   ips=$(hostname -I)
+   while [[ "$ips" == "" ]]
+   do
+      echo "Waiting for IPs to appear for reverse DNS" 1>&2
+      sleep 5
+      ips=$(hostname -I)
+   done
+
+   for ip in ${ips}
+   do
+      echo "Requesting hostname lookup for $ip" 1>&2
+      hostname=$(dig -x "$ip" +short)
+      # If hostname returned, configure first entry
+      for hn in ${hostname}; do
+         if [[ "$hn" != "" ]]; then
+            hostnamectl set-hostname "${hn}"
+            echo "Setting hostname to ${hn}" 1>&2
+	    break
+         fi
+      done
+   done
+}
+
+hostname_set=0
 HOSTNAMES_PATH=/etc/assisted/hostnames
 FILES=$(ls $HOSTNAMES_PATH)
 for filename in ${FILES}
@@ -35,7 +63,12 @@ do
         echo "Host has matching MAC address: ${filename}" 1>&2
         echo "Setting hostname to ${HOSTNAME}" 1>&2
         hostnamectl set-hostname "${HOSTNAME}"
+	hostname_set=1
     else
         echo "MAC address, ${filename}, does not exist on this host" 1>&2
     fi
 done
+
+if [[ $hostname_set -eq 0 ]]; then
+   lookup_address
+fi
