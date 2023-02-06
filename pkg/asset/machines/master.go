fee1846d5561e5cddba5c3e162ba345c187c4869
rna-afk <anarayan@redhat.com>
2023-02-06 10:42:06 -0500

    CPMS: Load generated file from directory.
    This loads the CPMS asset from directory if multiple create steps are
    invoked.

--- pkg/asset/machines/master.go
+++ pkg/asset/machines/master.go
@@ -617,6 +617,15 @@ func (m *Master) Load(f asset.FileFetcher) (found bool, err error) {
 	}
 	m.MachineFiles = fileList
 
+	file, err = f.FetchByName(filepath.Join(directory, controlPlaneMachineSetFileName))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	m.ControlPlaneMachineSet = file
+
 	return true, nil
 }
 
@@ -638,6 +647,7 @@ func (m *Master) Machines() ([]machinev1beta1.Machine, error) {
 		&machinev1.AlibabaCloudMachineProviderConfig{},
 		&machinev1.NutanixMachineProviderConfig{},
 		&machinev1.PowerVSMachineProviderConfig{},
+		&machinev1.ControlPlaneMachineSet{},
 	)
 
 	machinev1beta1.AddToScheme(scheme)
