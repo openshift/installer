openshift/installer

commit d3856aa7c6a9630dd7e401efe73e69d0591cd510

Code restructure
Signed-off-by: Pawan Pinjarkar <ppinjark@redhat.com>

pkg/asset/agent/mirror/registriesconf.go
@@ -103,8 +103,11 @@ unqualified-search-registries = []
 
 // RegistriesConf generates the registries.conf file.
 type RegistriesConf struct {
-	File         *asset.File
-	MirrorConfig []RegistriesConfig
+	File             *asset.File
+	Config           *sysregistriesv2.V2RegistriesConf
+	MirrorConfig     []RegistriesConfig
+	Generated        bool
+	ReleaseImagePath string
 }
 
 // RegistriesConfig holds the data extracted from registries.conf
@@ -125,22 +128,33 @@ func (*RegistriesConf) Name() string {
 func (*RegistriesConf) Dependencies() []asset.Asset {
 	return []asset.Asset{
 		&agent.OptionalInstallConfig{},
+		&releaseimage.Image{},
 	}
 }
 
 // Generate generates the registries.conf file from install-config.
 func (i *RegistriesConf) Generate(dependencies asset.Parents) error {
+
 	installConfig := &agent.OptionalInstallConfig{}
-	dependencies.Get(installConfig)
+	releaseImage := &releaseimage.Image{}
+	dependencies.Get(installConfig, releaseImage)
+
+	i.ReleaseImagePath = strings.Split(releaseImage.PullSpec, ":")[0]
+	i.Generated = true
+
 	if !installConfig.Supplied || len(installConfig.Config.ImageContentSources) == 0 {
 		i.File = &asset.File{
-			Filename: RegistriesConfFilename,
-			Data:     []byte(defaultRegistriesConf),
+			Data: []byte(defaultRegistriesConf),
 		}
-		return i.finish("Generated")
+		registriesConf := &sysregistriesv2.V2RegistriesConf{}
+		if err := toml.Unmarshal([]byte(defaultRegistriesConf), registriesConf); err != nil {
+			return errors.Wrapf(err, "failed to unmarshal %s", RegistriesConfFilename)
+		}
+		i.Config = registriesConf
+		return i.finish()
 	}
 
-	registries := sysregistriesv2.V2RegistriesConf{
+	registries := &sysregistriesv2.V2RegistriesConf{
 		Registries: []sysregistriesv2.Registry{},
 	}
 	for _, group := range bootstrap.MergedMirrorSets(installConfig.Config.ImageContentSources) {
@@ -156,18 +170,19 @@ func (i *RegistriesConf) Generate(dependencies asset.Parents) error {
 		}
 		registries.Registries = append(registries.Registries, registry)
 	}
+	i.Config = registries
 
-	data, err := toml.Marshal(registries)
+	registriesData, err := toml.Marshal(registries)
 	if err != nil {
 		return err
 	}
 
 	i.File = &asset.File{
 		Filename: RegistriesConfFilename,
-		Data:     data,
+		Data:     registriesData,
 	}
 
-	return i.finish("Generated")
+	return i.finish()
 }
 
 // Files returns the files generated by the asset.
@@ -181,6 +196,11 @@ func (i *RegistriesConf) Files() []*asset.File {
 // Load returns RegistriesConf asset from the disk.
 func (i *RegistriesConf) Load(f asset.FileFetcher) (bool, error) {
 
+	releaseImage := &releaseimage.Image{}
+	releaseImage.Generate(asset.Parents{})
+
+	i.ReleaseImagePath = strings.Split(releaseImage.PullSpec, ":")[0]
+
 	file, err := f.FetchByName(RegistriesConfFilename)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -189,63 +209,61 @@ func (i *RegistriesConf) Load(f asset.FileFetcher) (bool, error) {
 		return false, errors.Wrap(err, fmt.Sprintf("failed to load %s file", RegistriesConfFilename))
 	}
 
-	i.File = file
+	registriesConf := &sysregistriesv2.V2RegistriesConf{}
+	if err := toml.Unmarshal(file.Data, registriesConf); err != nil {
+		return false, errors.Wrapf(err, "failed to unmarshal %s", RegistriesConfFilename)
+	}
 
-	if err = i.finish("Loaded"); err != nil {
+	i.File, i.Config = file, registriesConf
+
+	mirrorConfig := make([]RegistriesConfig, len(registriesConf.Registries))
+	for i, reg := range registriesConf.Registries {
+		mirrorConfig[i] = RegistriesConfig{
+			Location: reg.Location,
+			Mirror:   reg.Mirrors[0].Location,
+		}
+	}
+	if err != nil {
+		return false, errors.Wrap(err, fmt.Sprintf("failed to parse mirrors in %s", RegistriesConfFilename))
+	}
+
+	i.MirrorConfig = mirrorConfig
+
+	if err = i.finish(); err != nil {
 		return false, err
 	}
 
 	return true, nil
 }
 
-func (i *RegistriesConf) finish(source string) error {
+func (i *RegistriesConf) finish() error {
 
 	if string(i.File.Data) != defaultRegistriesConf {
-		return i.validateReleaseImageIsSameInRegistriesConf(source)
-	}
-
-	config, err := extractLocationMirrorDataFromRegistries(i.File.Data)
-	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("failed to parse mirrors in %s", RegistriesConfFilename))
-	}
-
-	i.MirrorConfig = config
-
-	return nil
-}
-
-func (i *RegistriesConf) validateReleaseImageIsSameInRegistriesConf(source string) error {
-
-	releaseImage := &releaseimage.Image{}
-	releaseImage.Generate(asset.Parents{})
-
-	releaseImagePath := strings.Split(releaseImage.PullSpec, ":")[0]
-
-	if !strings.Contains(string(i.File.Data), releaseImagePath) {
-		if source == "Generated" {
-			return errors.New(fmt.Sprintf("The ImageContentSource configuration in install-config.yaml should have an entry matching the releaseImage %s", releaseImagePath))
-		}
-		return errors.New(fmt.Sprintf("%s should have an entry matching the releaseImage %s", RegistriesConfFilename, releaseImagePath))
+		return i.validateReleaseImageIsSameInRegistriesConf()
 	}
 
 	return nil
 }
 
-// From assisted-service pkg/mirrorregistries/generator.go
-func extractLocationMirrorDataFromRegistries(registriesConfToml []byte) ([]RegistriesConfig, error) {
-	registries := sysregistriesv2.V2RegistriesConf{}
-	err := toml.Unmarshal(registriesConfToml, &registries)
-	if err != nil {
-		return nil, err
-	}
+func (i *RegistriesConf) validateReleaseImageIsSameInRegistriesConf() error {
 
-	registriesConfList := make([]RegistriesConfig, len(registries.Registries))
-	for i, reg := range registries.Registries {
-		registriesConfList[i] = RegistriesConfig{
-			Location: reg.Location,
-			Mirror:   reg.Mirrors[0].Location,
+	var found bool
+
+	for _, registry := range i.Config.Registries {
+		if !i.Generated && registry.Endpoint.Location == "" {
+			return errors.New(fmt.Sprintf("%s should have an entry matching the releaseImage %s. Found \"%s\"", RegistriesConfFilename, i.ReleaseImagePath, registry.Endpoint.Location))
+		}
+		if registry.Endpoint.Location == i.ReleaseImagePath {
+			found = true
+			break
 		}
 	}
 
-	return registriesConfList, nil
+	if !found {
+		if i.Generated {
+			return errors.New(fmt.Sprintf("The ImageContentSource configuration in install-config.yaml should have an entry matching the releaseImage %s", i.ReleaseImagePath))
+		}
+		return errors.New(fmt.Sprintf("%s should have an entry matching the releaseImage %s", RegistriesConfFilename, i.ReleaseImagePath))
+	}
+	return nil
 }

pkg/asset/agent/mirror/registriesconf_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/openshift/installer/pkg/asset/agent"
 	"github.com/openshift/installer/pkg/asset/installconfig"
 	"github.com/openshift/installer/pkg/asset/mock"
+	"github.com/openshift/installer/pkg/asset/releaseimage"
 	"github.com/openshift/installer/pkg/types"
 )
 
@@ -28,6 +29,7 @@ func TestRegistriesConf_Generate(t *testing.T) {
 			name: "missing-config",
 			dependencies: []asset.Asset{
 				&agent.OptionalInstallConfig{},
+				&releaseimage.Image{},
 			},
 			expectedConfig: defaultRegistriesConf,
 		},
@@ -44,41 +46,14 @@ func TestRegistriesConf_Generate(t *testing.T) {
 						},
 					},
 				},
+				&releaseimage.Image{
+					PullSpec: "registry.ci.openshift.org/ocp/release:4.11.0-0.ci-2022-05-16-202609",
+				},
 			},
 			expectedConfig: defaultRegistriesConf,
 		},
 		{
 			name: "invalid-image-content-sources",
-			dependencies: []asset.Asset{
-				&agent.OptionalInstallConfig{
-					Supplied: true,
-					InstallConfig: installconfig.InstallConfig{
-						Config: &types.InstallConfig{
-							ObjectMeta: v1.ObjectMeta{
-								Namespace: "cluster-0",
-							},
-							ImageContentSources: []types.ImageContentSource{
-								{
-									Source: "registry.ci.openshift.org/ocp/release",
-									Mirrors: []string{
-										"virthost.ostest.test.metalkube.org:5000/localimages/local-release-image",
-									},
-								},
-								{
-									Source: "quay.io/openshift-release-dev/ocp-v4.0-art-dev",
-									Mirrors: []string{
-										"virthost.ostest.test.metalkube.org:5000/localimages/local-release-image",
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-			expectedError: "The ImageContentSource configuration in install-config.yaml should have an entry matching the releaseImage registry.ci.openshift.org/origin/release",
-		},
-		{
-			name: "valid-image-content-sources",
 			dependencies: []asset.Asset{
 				&agent.OptionalInstallConfig{
 					Supplied: true,
@@ -104,11 +79,47 @@ func TestRegistriesConf_Generate(t *testing.T) {
 						},
 					},
 				},
+				&releaseimage.Image{
+					PullSpec: "registry.ci.openshift.org/ocp/release:4.11.0-0.ci-2022-05-16-202609",
+				},
+			},
+			expectedError: "The ImageContentSource configuration in install-config.yaml should have an entry matching the releaseImage registry.ci.openshift.org/ocp/release",
+		},
+		{
+			name: "valid-image-content-sources",
+			dependencies: []asset.Asset{
+				&agent.OptionalInstallConfig{
+					Supplied: true,
+					InstallConfig: installconfig.InstallConfig{
+						Config: &types.InstallConfig{
+							ObjectMeta: v1.ObjectMeta{
+								Namespace: "cluster-0",
+							},
+							ImageContentSources: []types.ImageContentSource{
+								{
+									Source: "registry.ci.openshift.org/ocp/release",
+									Mirrors: []string{
+										"virthost.ostest.test.metalkube.org:5000/localimages/local-release-image",
+									},
+								},
+								{
+									Source: "quay.io/openshift-release-dev/ocp-v4.0-art-dev",
+									Mirrors: []string{
+										"virthost.ostest.test.metalkube.org:5000/localimages/local-release-image",
+									},
+								},
+							},
+						},
+					},
+				},
+				&releaseimage.Image{
+					PullSpec: "registry.ci.openshift.org/ocp/release:4.11.0-0.ci-2022-05-16-202609",
+				},
 			},
 			expectedConfig: `unqualified-search-registries = []
 
 [[registry]]
-  location = "registry.ci.openshift.org/origin/release"
+  location = "registry.ci.openshift.org/ocp/release"
   mirror-by-digest-only = true
   prefix = ""
 
@@ -156,6 +167,25 @@ func TestRegistries_LoadedFromDisk(t *testing.T) {
 		expectedFound bool
 		expectedError string
 	}{
+		{
+			name: "invalid-config-file",
+			data: `
+[[registry]]
+wrongLocationkey = "registry.ci.openshift.org/origin/release" 
+mirror-by-digest-only = false
+
+[[registry.mirror]]
+location = "virthost.ostest.test.metalkube.org:5000/localimages/local-release-image"
+
+[[registry]]
+location = "quay.io/openshift-release-dev/ocp-v4.0-art-dev"
+mirror-by-digest-only = false
+
+[[registry.mirror]]
+location = "virthost.ostest.test.metalkube.org:5000/localimages/local-release-image"`,
+			expectedFound: false,
+			expectedError: "mirror/registries.conf should have an entry matching the releaseImage registry.ci.openshift.org/origin/release. Found \"\"",
+		},
 		{
 			name: "valid-config-file",
 			data: `

commit 3d521fa64d003f3cd4d769c2acec83b5fdb683da

OCPBUGS-8258: Specify filename for default registries.conf
This was accidentally broken by
d3856aa7c6a9630dd7e401efe73e69d0591cd510, resulting in errors when
writing the asset to disk (i.e. the 'agent create cluster-manifests'
command would fail).

pkg/asset/agent/mirror/registriesconf.go
@@ -248,7 +248,8 @@ func (i *RegistriesConf) validateReleaseImageIsSameInRegistriesConf(releaseImage
 
 func (i *RegistriesConf) generateDefaultRegistriesConf() error {
 	i.File = &asset.File{
-		Data: []byte(defaultRegistriesConf),
+		Filename: RegistriesConfFilename,
+		Data:     []byte(defaultRegistriesConf),
 	}
 	registriesConf := &sysregistriesv2.V2RegistriesConf{}
 	if err := toml.Unmarshal([]byte(defaultRegistriesConf), registriesConf); err != nil {