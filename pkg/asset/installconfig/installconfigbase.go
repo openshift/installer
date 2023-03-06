0fac273044fc4155ce6c1ca249a2be2cb5a9476c
Eng Zer Jun <engzerjun@gmail.com>
2023-03-07 01:36:20 +0800

    fix(installconfig): use yaml.Unmarshal to ignore unknown keys
    In `sigs.k8s.io/yaml`, yaml.UnmarshalStrict enables
    DisallowUnknownFields automatically.
    
    Fixes: 666feefbd ("installconfig: Move fatal error to warn for strict marshal")
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- pkg/asset/installconfig/installconfigbase.go
+++ pkg/asset/installconfig/installconfigbase.go
@@ -53,7 +53,7 @@ func (a *AssetBase) LoadFromFile(f asset.FileFetcher) (found bool, err error) {
 		err = errors.Wrapf(err, "failed to parse first occurrence of unknown field")
 		logrus.Warnf(err.Error())
 		logrus.Info("Attempting to unmarshal while ignoring unknown keys because strict unmarshaling failed")
-		if err = yaml.UnmarshalStrict(file.Data, config); err != nil {
+		if err = yaml.Unmarshal(file.Data, config); err != nil {
 			err = errors.Wrapf(err, "failed to unmarshal %s", installConfigFilename)
 			return false, errors.Wrap(err, asset.InstallConfigError)
 		}
