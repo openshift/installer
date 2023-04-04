59a4fb35f984951314d7e52848b111227e24ddbe
Zane Bitter <zbitter@redhat.com>
2023-04-04 14:25:14 +1200

    Remove wait_for_assisted_service from common.sh
    We already have a script (that needs to be called from ExecStartPre) to
    wait for assisted-service, so move the implementation into there.

--- data/data/agent/files/usr/local/bin/common.sh
+++ data/data/agent/files/usr/local/bin/common.sh
@@ -3,14 +3,6 @@
 # shellcheck disable=SC1091
 source /etc/assisted/rendezvous-host.env
 
-wait_for_assisted_service() {
-    echo "Waiting for assisted-service to be ready"
-    until curl --output /dev/null --silent --fail "${SERVICE_BASE_URL}/api/assisted-install/v2/infra-envs"; do
-        printf '.'
-        sleep 5
-    done
-}
-
 is_node_zero() {
     local is_rendezvous_host
     is_rendezvous_host=$(ip -j address | jq "[.[].addr_info] | flatten | map(.local==\"$NODE_ZERO_IP\") | any")
--- data/data/agent/files/usr/local/bin/wait-for-assisted-service.sh
+++ data/data/agent/files/usr/local/bin/wait-for-assisted-service.sh
@@ -2,6 +2,10 @@
 set -e
 
 # shellcheck disable=SC1091
-source common.sh
+source /etc/assisted/rendezvous-host.env
 
-wait_for_assisted_service
+echo "Waiting for assisted-service to be ready"
+until curl --output /dev/null --silent --fail "${SERVICE_BASE_URL}/api/assisted-install/v2/infra-envs"; do
+    printf '.'
+    sleep 5
+done
