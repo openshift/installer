178280a7e51bb3978c08702465329e30dd54ba6c
AnnaZivkovic <github@squid.systems>
2023-04-06 16:50:22 -0700

    CORS-2525: Azure: remove storage account with bootstrap destroy
    The work is being done to address the issue of Azure storage accounts being persisted for the life of an OpenShift cluster, which incurs costs and management effort. The goal is to destroy the storage account along with other bootstrap resources during the bootstrap destroy process. Additionally, the default creation of two Azure storage accounts has raised security issues, which the work aims to address by restricting network access using virtual network rules. The end result is a more cost-effective and secure OpenShift cluster deployment on Azure.

--- data/data/azure/bootstrap/variables.tf
+++ data/data/azure/bootstrap/variables.tf
@@ -37,14 +37,24 @@ variable "resource_group_name" {
   description = "The resource group name for the deployment."
 }
 
-variable "storage_account_name" {
+variable "image_version_gallery_name" {
   type        = string
-  description = "the name of the storage account for the cluster. It can be used for boot diagnostics."
+  description = "The name of the image gallery used to set up shared images."
 }
 
-variable "vm_image" {
+variable "image_version_gen2_gallery_name" {
   type        = string
-  description = "The resource id of the vm image used for bootstrap."
+  description = "The name of the gen2 image gallery used to set up shared images."
+}
+
+variable "image_version_name" {
+  type        = string
+  description = "The name of shared image used to set up shared images."
+}
+
+variable "image_version_gen2_name" {
+  type        = string
+  description = "The name of the gen2 shared image used to set up shared images."
 }
 
 variable "identity" {
