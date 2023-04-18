30e46db8696ddd452099080bc5e501ba721be0ce
Rafael Fonseca <r4f4rfs@gmail.com>
2023-04-18 16:40:43 +0200

    asset: machines: gcp: use only zones in which instance is available
    Instead of using all the zones for a region, since some machine types
    are not present in all zones.

--- pkg/asset/machines/gcp/zones.go
+++ pkg/asset/machines/gcp/zones.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pkg/errors"
 	compute "google.golang.org/api/compute/v1"
 	"google.golang.org/api/option"
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	gcpconfig "github.com/openshift/installer/pkg/asset/installconfig/gcp"
 )
@@ -47,4 +48,40 @@ func AvailabilityZones(project, region string) ([]string, error) {
 
 	sort.Strings(zones)
 	return zones, nil
+}
+
+// ZonesForInstanceType retrieves a filtered list of availability zones where
+// the particular instance type is available. This is mainly necessary for
+// arm64, since the instance t2a-standard-* is not available in all
+// availability zones.
+func ZonesForInstanceType(project, region, instanceType string) ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+
+	ssn, err := gcpconfig.GetSession(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get session")
+	}
+
+	svc, err := compute.NewService(ctx, option.WithCredentials(ssn.Credentials))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create compute service")
+	}
+
+	found := sets.New[string]()
+	filter := fmt.Sprintf("name = \"%s\" AND zone : %s-*", instanceType, region)
+	req := svc.MachineTypes.AggregatedList(project).Filter(filter).Fields("items/*/machineTypes(zone),nextPageToken")
+	err = req.Pages(ctx, func(list *compute.MachineTypeAggregatedList) error {
+		for _, scopedList := range list.Items {
+			for _, item := range scopedList.MachineTypes {
+				found.Insert(item.Zone)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get zones for instance type")
+	}
+
+	return sets.List(found), nil
 }
