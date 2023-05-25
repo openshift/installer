5bc9c3bae85ef921ecf62f86ec16fff6d0dede55
Marco Braga <mrbraga@redhat.com>
2023-05-25 00:08:33 -0300

    types/aws/zone: adding the Zone Type for Wavelength
    AWS Wavelength Zone is identified by wavelength-zone by API.
    The same type is created internally to represent that zone
    following the same convention of other zone types.

--- pkg/types/aws/availabilityzones.go
+++ pkg/types/aws/availabilityzones.go
@@ -5,6 +5,8 @@ const (
 	AvailabilityZoneType = "availability-zone"
 	// LocalZoneType is the type of Local zone placed on the metropolitan areas.
 	LocalZoneType = "local-zone"
+	// WavelengtyZoneType is the type of Wavelength zone placed in the Carrier infrastructure closer to areas.
+	WavelengtyZoneType = "wavelength-zone"
 	// ZoneOptInStatusOptedIn is the opt-in status of the zone.
 	// For Availability Zones, this parameter always has the value of opt-in-not-required.
 	// For Local Zones and Wavelength Zones, this parameter is the opt-in status.
