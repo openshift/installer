7f2e890a369b8b7d1d71435ee21cd100a8015391
Marco Braga <mrbraga@redhat.com>
2023-05-24 23:57:15 -0300

    aws tfvars: create vars for Wavelength Zones resources
    This change exports the zone types used to check if the
    deployment contains Wavelength Zones (WLZ) to create
    specific network resources to operate in public subnets.

--- pkg/tfvars/aws/aws.go
+++ pkg/tfvars/aws/aws.go
@@ -27,6 +27,7 @@ type config struct {
 	WorkerAvailabilityZones         []string          `json:"aws_worker_availability_zones"`
 	EdgeLocalZones                  []string          `json:"aws_edge_local_zones,omitempty"`
 	EdgeZonesGatewayIndex           map[string]int    `json:"aws_edge_parent_zones_index,omitempty"`
+	EdgeZonesType                   map[string]string `json:"aws_edge_zones_type,omitempty"`
 	IOPS                            int64             `json:"aws_master_root_volume_iops"`
 	Size                            int64             `json:"aws_master_root_volume_size,omitempty"`
 	Type                            string            `json:"aws_master_root_volume_type,omitempty"`
@@ -110,7 +111,7 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 		if _, ok := sources.AvailabilityZones[zoneName]; !ok {
 			return nil, errors.New(fmt.Sprintf("unable to find the zone when generating terraform vars: %s", zoneName))
 		}
-		if sources.AvailabilityZones[zoneName].Type == typesaws.LocalZoneType {
+		if sources.AvailabilityZones[zoneName].Type == typesaws.LocalZoneType || sources.AvailabilityZones[zoneName].Type == typesaws.WavelengtyZoneType {
 			edgeLocalZoneMap[zoneName] = exists
 			continue
 		}
@@ -142,6 +143,7 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 	sort.Strings(allAvailabilityZones)
 	edgeLocalZones := make([]string, 0, len(edgeLocalZoneMap))
 	edgeZonesGatewayIndexMap := make(map[string]int, len(edgeLocalZoneMap))
+	edgeZonesType := make(map[string]string, len(edgeLocalZoneMap))
 	// new VPC
 	if len(sources.PrivateSubnets) == 0 {
 		for zone := range edgeLocalZoneMap {
@@ -155,6 +157,7 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 			}
 			edgeLocalZones = append(edgeLocalZones, zone)
 			edgeZonesGatewayIndexMap[zone] = gwIndex
+			edgeZonesType[zone] = sources.AvailabilityZones[zone].Type
 		}
 	}
 
@@ -187,6 +190,7 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 		WorkerAvailabilityZones:   workerAvailabilityZones,
 		EdgeLocalZones:            edgeLocalZones,
 		EdgeZonesGatewayIndex:     edgeZonesGatewayIndexMap,
+		EdgeZonesType:             edgeZonesType,
 		BootstrapInstanceType:     masterConfig.InstanceType,
 		MasterInstanceType:        masterConfig.InstanceType,
 		Size:                      *rootVolume.EBS.VolumeSize,
