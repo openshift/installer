openshift/installer

commit 4b0a221641a4efce1b443d2a2fe5240f66acd6e8

Instead of conditioning on the number of nodes, we can condition on whether the api vip was set in the AgentClusterInstall.
Co-authored-by: Richard Su <rwsu@redhat.com>

data/data/agent/files/usr/local/bin/start-cluster-installation.sh.template
@@ -39,7 +39,7 @@ done
 
 echo "All ${total_required_nodes} hosts are ready."
 
-if [[ "$total_required_nodes" > "1" ]]; then
+if [[ "${APIVIP}" != "" ]]; then
     api_vip=$(curl -s -S "${BASE_URL}/clusters" | jq -r .[].api_vip)
     if [ "${api_vip}" == null ]; then
         echo "Setting api vip"

commit a596719e2a1c217f7c90d250976b1fda02bf3618

Remove dead api_vip setting code
This has not been called since 4b0a221641a4efce1b443d2a2fe5240f66acd6e8
accidentally introduced a check on the (non-existent) environment
variable ${APIVIP} instead of the template variable {{.APIVIP}}. Since
that hasn't seemed to be a problem, we can remove it altogether.

The code to set the API VIP was originally introduced waaay back in
4b0a221641a4efce1b443d2a2fe5240f66acd6e8. It was there to work around an
issue that was solved a couple of days later by
openshift/assisted-service#3258.

data/data/agent/files/usr/local/bin/start-cluster-installation.sh.template
@@ -57,14 +57,6 @@ done
 echo "All ${total_required_nodes} hosts are ready." 1>&2
 clear_issue "${status_issue}"
 
-if [[ "${APIVIP}" != "" ]]; then
-    api_vip=$(curl -s -S "${BASE_URL}/clusters" | jq -r .[].api_vip)
-    if [ "${api_vip}" == null ]; then
-        echo "Setting api vip" 1>&2
-        curl -s -S -X PATCH "${BASE_URL}/clusters/${cluster_id}" -H "Content-Type: application/json" -d '{"api_vip": "{{.APIVIP}}"}'
-    fi
-fi
-
 while [[ "${cluster_status}" != "installed" ]]
 do
     sleep 5

pkg/asset/agent/image/ignition.go
@@ -52,7 +52,6 @@ type Ignition struct {
 type agentTemplateData struct {
 	ServiceProtocol           string
 	PullSecret                string
-	APIVIP                    string
 	ControlPlaneAgents        int
 	WorkerAgents              int
 	ReleaseImages             string
@@ -271,7 +270,6 @@ func getTemplateData(name, pullSecret, releaseImageList, releaseImage,
 	return &agentTemplateData{
 		ServiceProtocol:           "http",
 		PullSecret:                pullSecret,
-		APIVIP:                    agentClusterInstall.Spec.APIVIP,
 		ControlPlaneAgents:        agentClusterInstall.Spec.ProvisionRequirements.ControlPlaneAgents,
 		WorkerAgents:              agentClusterInstall.Spec.ProvisionRequirements.WorkerAgents,
 		ReleaseImages:             releaseImageList,

pkg/asset/agent/image/ignition_test.go
@@ -90,7 +90,6 @@ func TestIgnition_getTemplateData(t *testing.T) {
 	assert.Equal(t, clusterName, templateData.ClusterName)
 	assert.Equal(t, "http", templateData.ServiceProtocol)
 	assert.Equal(t, pullSecret, templateData.PullSecret)
-	assert.Equal(t, agentClusterInstall.Spec.APIVIP, templateData.APIVIP)
 	assert.Equal(t, agentClusterInstall.Spec.ProvisionRequirements.ControlPlaneAgents, templateData.ControlPlaneAgents)
 	assert.Equal(t, agentClusterInstall.Spec.ProvisionRequirements.WorkerAgents, templateData.WorkerAgents)
 	assert.Equal(t, releaseImageList, templateData.ReleaseImages)