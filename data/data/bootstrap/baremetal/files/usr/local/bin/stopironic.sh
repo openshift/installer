4352cc135ce5fdf7da7ea232296f3e2472ad2a40
Zane Bitter <zbitter@redhat.com>
2023-06-12 21:49:30 +1200

    baremetal bootstrap: Move ironic-ramdisk-logs to separate service

--- data/data/bootstrap/baremetal/files/usr/local/bin/stopironic.sh
+++ data/data/bootstrap/baremetal/files/usr/local/bin/stopironic.sh
@@ -2,7 +2,7 @@
 
 set -x 
 
-for name in ironic ironic-inspector ironic-ramdisk-logs httpd; do
+for name in ironic ironic-inspector httpd; do
     podman ps | grep -w "$name$" && podman kill $name
     podman ps --all | grep -w "$name$" && podman rm $name -f
 done
