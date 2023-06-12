c3d004b6f57dbd8e3434182508dcac73188c2275
Zane Bitter <zbitter@redhat.com>
2023-06-12 15:54:40 +1200

    baremetal bootstrap: Move IP options into environment file

--- data/data/bootstrap/baremetal/files/usr/local/bin/build-ironic-env.sh
+++ data/data/bootstrap/baremetal/files/usr/local/bin/build-ironic-env.sh
@@ -27,6 +27,27 @@ build_ironic_env() {
     # The ironic container contains httpd (and thus httpd-tools), so rely on it
     # to supply the htpasswd command
     printf 'IRONIC_HTPASSWD="%s"\n' "$(podman run -i --rm "$(image_for ironic)" htpasswd -niB "${IRONIC_USERNAME}" </opt/metal3/auth/ironic/password)"
+
+    if [ "${EXTERNAL_IP_FAMILY:-}" = "ip4" ]; then
+        dhcp_opt="dhcp"
+    elif [ "${EXTERNAL_IP_FAMILY:-}" = "ip6" ]; then
+        dhcp_opt="dhcp6"
+    else
+        dhcp_opt="dhcp,dhcp6"
+    fi
+    printf 'EXTERNAL_IP_OPTIONS="ip=%s"\n' "${dhcp_opt}"
+
+    if [ "${PROVISIONING_NETWORK_TYPE}" != "Disabled" ]; then
+        if [ "${PROV_IP_FAMILY:-}" = "ip4" ]; then
+            dhcp_opt="dhcp"
+        elif [ "${PROV_IP_FAMILY:-}" = "ip6" ]; then
+            dhcp_opt="dhcp6"
+        fi
+    fi
+    printf 'PROVISIONING_IP_OPTIONS="ip=%s"\n' "${dhcp_opt}"
+    # Allow some extra time for the provisioning interface to come up
+    # https://issues.redhat.com/browse/OCPBUGS-5151
+    printf 'IRONIC_KERNEL_PARAMS="rd.net.timeout.carrier=30 ip=%s"\n' "${dhcp_opt}"
 }
 
 build_ironic_env | tee -a /etc/ironic.env
