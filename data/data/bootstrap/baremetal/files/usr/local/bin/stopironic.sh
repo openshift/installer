e45e76abfaebeb39070c1178400abbad0df0ced1
Zane Bitter <zbitter@redhat.com>
2023-06-13 11:45:32 +1200

    baremetal bootstrap: Move httpd to a separate service

--- data/data/bootstrap/baremetal/files/usr/local/bin/stopironic.sh
+++ data/data/bootstrap/baremetal/files/usr/local/bin/stopironic.sh
@@ -2,7 +2,7 @@
 
 set -x 
 
-for name in ironic ironic-inspector httpd; do
+for name in ironic ironic-inspector; do
     podman ps | grep -w "$name$" && podman kill $name
     podman ps --all | grep -w "$name$" && podman rm $name -f
 done
