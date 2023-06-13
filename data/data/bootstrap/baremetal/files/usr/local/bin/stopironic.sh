170b0ecb2145c00d2b4aadc7c5f5190e0ee4712e
Zane Bitter <zbitter@redhat.com>
2023-06-13 12:20:01 +1200

    baremetal bootstrap: Split ironic-inspector into its own service

--- data/data/bootstrap/baremetal/files/usr/local/bin/stopironic.sh
+++ data/data/bootstrap/baremetal/files/usr/local/bin/stopironic.sh
@@ -2,7 +2,7 @@
 
 set -x 
 
-for name in ironic ironic-inspector; do
+for name in ironic; do
     podman ps | grep -w "$name$" && podman kill $name
     podman ps --all | grep -w "$name$" && podman rm $name -f
 done
