9bc8b6994df32f64c399b522cae6b83202a23183
Brent Barbachem <barbacbd@dukes.jmu.edu>
2023-06-27 11:24:21 -0400

    OCPBUGS-15421: Allow different service account for xpn installs in gcp
    **Add service account field to the install config for control Planes
    **Validation to ensure service account field added is only used for xpn installs
    **Add Validation to ensure that the service account is available in the host project
    
    ** Machine manifests will use the provided service account for control planes.
    
    **Pass the service account data used for creating control plane nodes during a shared vpn install.

--- data/data/gcp/cluster/iam/main.tf
+++ data/data/gcp/cluster/iam/main.tf
@@ -3,22 +3,19 @@ locals {
 }
 
 resource "google_service_account" "worker-node-sa" {
-  count        = var.service_account == "" ? 1 : 0
   account_id   = "${var.cluster_id}-w"
   display_name = "${var.cluster_id}-worker-node"
   description  = local.description
 }
 
 resource "google_project_iam_member" "worker-compute-viewer" {
-  count   = var.service_account == "" ? 1 : 0
   project = var.project_id
   role    = "roles/compute.viewer"
-  member  = "serviceAccount:${google_service_account.worker-node-sa[0].email}"
+  member  = "serviceAccount:${google_service_account.worker-node-sa.email}"
 }
 
 resource "google_project_iam_member" "worker-storage-admin" {
-  count   = var.service_account == "" ? 1 : 0
   project = var.project_id
   role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.worker-node-sa[0].email}"
+  member  = "serviceAccount:${google_service_account.worker-node-sa.email}"
 }
