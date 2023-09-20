c1e8a4bbf0625d69618c72869f80ae84d8184cf5
Pawan Pinjarkar <ppinjark@redhat.com>
2023-09-20 15:52:50 -0400

    OCPBUGS-19092: Enable serial console for external OCI platform
    When creating an Agent ISO for OCI, add the kernel argument
    console=ttyS0 to the ISO/PXE kargs.
    
    CoreOS does not include a console arg by default when using metal
    as the platform because different hardware has different consoles
    and specifying one can cause booting to fail on some,
    but it does on many cloud platforms.
    
    Since we know when the user is definitely using OCI
    and we know the correct settings for OCI,
    we should set them up automatically.
    
    Signed-off-by: Pawan Pinjarkar <ppinjark@redhat.com>

--- pkg/asset/agent/image/kargs.go
+++ pkg/asset/agent/image/kargs.go
@@ -1,35 +1,47 @@
 package image
 
 import (
+	"github.com/sirupsen/logrus"
+
+	hiveext "github.com/openshift/assisted-service/api/hiveextension/v1beta1"
+	"github.com/openshift/assisted-service/models"
 	"github.com/openshift/installer/pkg/asset"
 	"github.com/openshift/installer/pkg/asset/agent/manifests"
 )
 
-// Kargs is an Asset the generates the additional kernel args.
+// Kargs is an Asset that generates the additional kernel args.
 type Kargs struct {
-	fips bool
+	fips        bool
+	consoleArgs string
 }
 
-// Dependencies returns the assets on which the AgentArtifacts asset depends.
+// Dependencies returns the assets on which the Kargs asset depends.
 func (a *Kargs) Dependencies() []asset.Asset {
 	return []asset.Asset{
 		&manifests.AgentClusterInstall{},
+		&manifests.AgentManifests{},
 	}
 }
 
-// Generate generates the configurations for the agent ISO image and PXE assets.
+// Generate generates the kernel args configurations for the agent ISO image and PXE assets.
 func (a *Kargs) Generate(dependencies asset.Parents) error {
 	agentClusterInstall := &manifests.AgentClusterInstall{}
-	dependencies.Get(agentClusterInstall)
+	agentManifests := &manifests.AgentManifests{}
+	dependencies.Get(agentClusterInstall, agentManifests)
 
 	a.fips = agentClusterInstall.FIPSEnabled()
 
+	// Add kernel args for external oci platform
+	if agentManifests.GetExternalPlatformName() == string(models.PlatformTypeOci) {
+		logrus.Debugf("Added kernel args to enable serial console for %s %s platform", hiveext.ExternalPlatformType, string(models.PlatformTypeOci))
+		a.consoleArgs = " console=ttyS0"
+	}
 	return nil
 }
 
 // Name returns the human-friendly name of the asset.
 func (a *Kargs) Name() string {
-	return "Agent ISO Kernel Arguments"
+	return "Agent ISO/PXE files Kernel Arguments"
 }
 
 // KernelCmdLine returns the data to be appended to the kernel arguments.
@@ -37,5 +49,5 @@ func (a *Kargs) KernelCmdLine() []byte {
 	if a.fips {
 		return []byte(" fips=1")
 	}
-	return nil
+	return []byte(a.consoleArgs)
 }
