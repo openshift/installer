fc87c7503a0380e1e79b9fd40678ff39d968ed7f
Patrick Dillon <padillon@redhat.com>
2023-09-27 11:02:34 -0400

    Add Alternate Infrastructure Build Tag
    Implements a build tag strategy to allow producing installer binaries
    using an alternate (to terraform) infrastructure provider.
    
    Using build tags we can select different
    ProviderForPlatform implementations. Because the altinfra implementation
    of that function does not depend on any terraform dependencies, none are
    included in the binary.

---
+++ pkg/infrastructure/platform/platform_altinfra.go
@@ -0,0 +1,26 @@
+//go:build altinfra
+// +build altinfra
+
+package platform
+
+import (
+	"fmt"
+
+	"github.com/openshift/installer/pkg/infrastructure"
+	awstypes "github.com/openshift/installer/pkg/types/aws"
+)
+
+// ProviderForPlatform returns the stages to run to provision the infrastructure for the specified platform.
+func ProviderForPlatform(platform, installDir string) ([]infrastructure.Stage, func() error, error) {
+	var stages []infrastructure.Stage
+	var cleanup func() error
+	var err error
+
+	switch platform {
+	case awstypes.Name:
+		return []infrastructure.Stage{}, nil, nil
+	default:
+		panic(fmt.Sprintf("unsupported platform %q", platform))
+	}
+	return stages, cleanup, err
+}
