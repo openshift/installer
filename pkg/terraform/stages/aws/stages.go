35793b98ed98911ddfbac7a301e7230700f38b55
Sandhya Dasu <sdasu@redhat.com>
2023-10-31 23:07:34 -0400

    Customizing AddLBConfig stage for AWS

--- pkg/terraform/stages/aws/stages.go
+++ pkg/terraform/stages/aws/stages.go
@@ -1,10 +1,15 @@
 package aws
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
 	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 
+	"github.com/openshift/installer/pkg/asset/lbconfig"
 	"github.com/openshift/installer/pkg/terraform"
 	"github.com/openshift/installer/pkg/terraform/providers"
 	"github.com/openshift/installer/pkg/terraform/stages"
@@ -17,6 +22,12 @@ var PlatformStages = []terraform.Stage{
 		"aws",
 		"cluster",
 		[]providers.Provider{providers.AWS},
+	),
+	stages.NewStage(
+		"aws",
+		"dnsConfig",
+		[]providers.Provider{providers.AWS},
+		stages.WithCustomAddLBConfig(addAWSLBConfig),
 	),
 	stages.NewStage(
 		"aws",
@@ -40,4 +51,54 @@ func customBootstrapDestroy(s stages.SplitStage, directory string, terraformDir 
 		terraform.Destroy(directory, awstypes.Name, s, terraformDir, opts...),
 		"failed to destroy bootstrap",
 	)
+}
+
+func addAWSLBConfig(s stages.SplitStage, directory string, terraformDir string, varFiles []string) (string, error) {
+	outputsFilePath := filepath.Join(directory, s.OutputsFilename())
+	if _, err := os.Stat(outputsFilePath); err != nil {
+		return "", errors.Wrapf(err, "could not find outputs file %q", outputsFilePath)
+	}
+
+	outputsFile, err := os.ReadFile(outputsFilePath)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to read outputs file %q", outputsFilePath)
+	}
+
+	outputs := map[string]interface{}{}
+	if err := json.Unmarshal(outputsFile, &outputs); err != nil {
+		return "", errors.Wrapf(err, "could not unmarshal outputs file %q", outputsFilePath)
+	}
+
+	var apiLBDNSNames []string
+	if apiLBDNSNamesRaw, ok := outputs["aws_external_api_lb_dns_name"]; ok {
+		apiLBDNSNames[0], ok = apiLBDNSNamesRaw.(string)
+		if !ok {
+			return "", errors.New("could not read External API LB DNS Name from terraform outputs")
+		}
+	}
+
+	var apiIntLBDNSNames []string
+	if apiIntLBDNSNamesRaw, ok := outputs["aws_internal_api_lb_dns_name"]; ok {
+		apiIntLBDNSNames[0], ok = apiIntLBDNSNamesRaw.(string)
+		if !ok {
+			return "", errors.New("could not read API Internal LB DNS Name from terraform outputs")
+		}
+	}
+
+	// Extract bootstrap ignition from terraform outputs
+	var bootstrapIgnition string
+	if bootstrapIgnitionRaw, ok := outputs["aws_bootstrap_ignition"]; ok {
+		bootstrapIgnition, ok = bootstrapIgnitionRaw.(string)
+		if !ok {
+			return "", errors.New("could not read bootstrap ignition from terraform outputs")
+		}
+	}
+
+	// Create openshift-lbconfigfordns configmap and inject into bootstrap ignition
+	bootstrapIgnWithLBConfig, err := lbconfig.InjectLBInfo([]byte(bootstrapIgnition), apiLBDNSNames, apiIntLBDNSNames)
+	if err != nil {
+		return "", errors.Wrap(err, "unable to inject LB ConfigMap into Bootstrap Ignition")
+	}
+
+	return bootstrapIgnWithLBConfig, nil
 }
