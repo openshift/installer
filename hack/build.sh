da7eb8dffcf99edb18b3a215674154e855be6334
Rafael Fonseca <r4f4rfs@gmail.com>
2023-11-07 00:56:54 +0100

    hack: only rebuild terraform providers if needed
    Look at the Golang module information from the binary to determine
    whether the providers need to be rebuilt. It'll be useful when we use an
    external container image containing pre-built binaries for the providers.

--- hack/build.sh
+++ hack/build.sh
@@ -30,6 +30,46 @@ copy_terraform_to_mirror() {
   cp "${PWD}/terraform/bin/${TARGET_OS_ARCH}/terraform" "${PWD}/pkg/terraform/providers/mirror/terraform/"
 }
 
+# Check if a provider has changed based on the version stored in the binary
+check_module_changes() {
+	binpath="$1"
+	srcpath="$2"
+	build_hash="$(go version -m "${binpath}" | grep 'vcs.revision' | cut -f2 -d'=')"
+	# If it's a locally developed provider
+	if test -n "${build_hash}"; then
+		# Check if a provider has changed based on git changes since the
+		# revision the provider was last built
+		git diff --name-only --exit-code "${build_hash}.." -- "${srcpath}"
+	# If it's an imported module
+	else
+		# Check if a provider has changed based on its go.mod git hash
+		version_info="$(go version -m "${binpath}" | grep -Eo 'main.builtGoModHash=[a-fA-F0-9]+' | cut -f2 -d'=')"
+		test "${version_info}" == "$(git hash-object "${srcpath}/go.mod")"
+	fi
+}
+
+# Build terraform and providers only if needed
+build_terraform_and_providers() {
+	TARGET_OS_ARCH=$(go env GOOS)_$(go env GOARCH)
+	bindir="${PWD}/terraform/bin/${TARGET_OS_ARCH}"
+	find "${PWD}/terraform/providers/" -maxdepth 1 -mindepth 1 -type d | while read -r dir; do
+		provider="$(basename "${dir}")"
+		binpath="${bindir}/terraform-provider-${provider}"
+		if test -s "${binpath}" && test -s "${binpath}.zip" && check_module_changes "${binpath}" "terraform/providers/${provider}"; then
+			echo "${provider} is up-to-date"
+		else
+			echo "Rebuilding ${provider}"
+			make -C terraform "go-build.${provider}"
+		fi
+	done
+	if test -s "${bindir}/terraform" && check_module_changes "${bindir}/terraform" "terraform/terraform"; then
+		echo "terraform is up-to-date"
+	else
+		echo "Rebuilding terraform"
+		make -C terraform go-build-terraform
+	fi
+}
+
 minimum_go_version=1.20
 current_go_version=$(go version | cut -d " " -f 3)
 
@@ -44,8 +84,8 @@ MODE="${MODE:-release}"
 # Build terraform binaries before setting environment variables since it messes up make
 if test "${SKIP_TERRAFORM}" != y && ! (echo "${TAGS}" | grep -q -e 'aro' -e 'altinfra')
 then
-  make -C terraform all
-  copy_terraform_to_mirror # Copy terraform parts to embedded mirror.
+	build_terraform_and_providers
+	copy_terraform_to_mirror # Copy terraform parts to embedded mirror.
 fi
 
 # build cluster-api binaries
