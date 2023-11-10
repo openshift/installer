f7701d1578e64af4931a684452a969b3ec742239
Hiro Miyamoto <miyamotoh@us.ibm.com>
2023-11-10 14:02:59 -0500

    Prepare for varying SysTypes for new datacenters
    Signed-off-by: Hiro Miyamoto <miyamotoh@us.ibm.com>

--- pkg/types/powervs/validation/machinepool.go
+++ pkg/types/powervs/validation/machinepool.go
@@ -72,10 +72,8 @@ func ValidateMachinePool(p *powervs.MachinePool, fldPath *field.Path) field.Erro
 
 	// Validate SysType
 	if p.SysType != "" {
-		const sysTypeRegex = `^(?:e980|s922(-.*|))$`
-		// Allowing for a staging-only pattern of s922-* but not exposing here
-		if !regexp.MustCompile(sysTypeRegex).MatchString(p.SysType) {
-			allErrs = append(allErrs, field.Invalid(fldPath.Child("sysType"), p.SysType, "system type must be one of {e980,s922}"))
+		if !powervs.AllKnownSysTypes().Has(p.SysType) {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("sysType"), p.SysType, "unknown system type specified"))
 		}
 	}
 
