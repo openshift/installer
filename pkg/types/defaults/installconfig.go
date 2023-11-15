1f0ad3bb728bc5c18531174e1b949fe4dd788136
Patrick Dillon <padillon@redhat.com>
2023-11-15 10:45:33 -0500

    pkg/types/defaults: set CustomNoUpgrade
    If users enter feature gates without specifying a feature set,
    automatically set CustomNoUpgrade. Validation will still catch if
    a different feature set is used with individual gates.

--- pkg/types/defaults/installconfig.go
+++ pkg/types/defaults/installconfig.go
@@ -1,6 +1,9 @@
 package defaults
 
 import (
+	"github.com/sirupsen/logrus"
+
+	configv1 "github.com/openshift/api/config/v1"
 	operv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/installer/pkg/ipnet"
 	"github.com/openshift/installer/pkg/types"
@@ -90,6 +93,11 @@ func SetInstallConfigDefaults(c *types.InstallConfig) {
 		} else if c.Platform.PowerVS != nil {
 			c.CredentialsMode = types.ManualCredentialsMode
 		}
+	}
+
+	if len(c.FeatureSet) == 0 && len(c.FeatureGates) > 0 {
+		c.FeatureSet = configv1.CustomNoUpgrade
+		logrus.Warn("Feature gates were provided in the Install Config without a feature set: setting FeatureSet to CustomNoUpgrade")
 	}
 
 	switch {
