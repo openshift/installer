f65ed6cb23cd5fee4e0d0de5e41db477670371e2
Patrick Dillon <padillon@redhat.com>
2023-11-27 17:12:05 -0500

    pkg/asset/target: add capi manifests
    Includes CAPI manifests when executing create manifests.

--- pkg/asset/targets/targets.go
+++ pkg/asset/targets/targets.go
@@ -9,6 +9,7 @@ import (
 	"github.com/openshift/installer/pkg/asset/kubeconfig"
 	"github.com/openshift/installer/pkg/asset/machines"
 	"github.com/openshift/installer/pkg/asset/manifests"
+	"github.com/openshift/installer/pkg/asset/manifests/clusterapi"
 	"github.com/openshift/installer/pkg/asset/password"
 	"github.com/openshift/installer/pkg/asset/templates/content/bootkube"
 	"github.com/openshift/installer/pkg/asset/templates/content/openshift"
@@ -27,6 +28,7 @@ var (
 		&machines.Worker{},
 		&manifests.Manifests{},
 		&manifests.Openshift{},
+		&clusterapi.Cluster{},
 	}
 
 	// ManifestTemplates are the manifest-templates targeted assets.
