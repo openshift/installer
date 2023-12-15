b284ecd2236dc15c6dbcb5284b9f542620afe0c9
Stephen Finucane <stephenfin@redhat.com>
2023-12-15 13:45:32 +0000

    DNM: Generate cluster-api manifests in manifest step
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- pkg/asset/targets/targets.go
+++ pkg/asset/targets/targets.go
@@ -9,6 +9,7 @@ import (
 	"github.com/openshift/installer/pkg/asset/kubeconfig"
 	"github.com/openshift/installer/pkg/asset/machines"
 	"github.com/openshift/installer/pkg/asset/manifests"
+	"github.com/openshift/installer/pkg/asset/manifests/clusterapi"
 	"github.com/openshift/installer/pkg/asset/password"
 	"github.com/openshift/installer/pkg/asset/templates/content/bootkube"
 	"github.com/openshift/installer/pkg/asset/templates/content/openshift"
@@ -25,6 +26,7 @@ var (
 	Manifests = []asset.WritableAsset{
 		&machines.Master{},
 		&machines.Worker{},
+		&clusterapi.Cluster{},
 		&manifests.Manifests{},
 		&manifests.Openshift{},
 	}
