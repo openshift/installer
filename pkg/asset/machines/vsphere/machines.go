4bbbaf541adaac7dcdc2957d2390e179e9ffc1b2
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-02-07 13:20:27 -0500

    clean up generated paths

--- pkg/asset/machines/vsphere/machines.go
+++ pkg/asset/machines/vsphere/machines.go
@@ -316,11 +316,11 @@ func provider(clusterID string, vcenter *vsphere.VCenter, failureDomain vsphere.
 	networkDeviceSpec := make([]machineapi.NetworkDeviceSpec, len(failureDomain.Topology.Networks))
 
 	// If failureDomain.Topology.Folder is empty this will be used
-	folder := fmt.Sprintf("/%s/vm/%s", failureDomain.Topology.Datacenter, clusterID)
+	folder := path.Clean(fmt.Sprintf("/%s/vm/%s", failureDomain.Topology.Datacenter, clusterID))
 
 	// If failureDomain.Topology.ResourcePool is empty this will be used
 	// computeCluster is required to be a path
-	resourcePool := fmt.Sprintf("%s/Resources", failureDomain.Topology.ComputeCluster)
+	resourcePool := path.Clean(fmt.Sprintf("%s/Resources", failureDomain.Topology.ComputeCluster))
 
 	if failureDomain.Topology.Folder != "" {
 		folder = failureDomain.Topology.Folder
