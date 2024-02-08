385220dd65d261035b44c1ba467bbb32a413bbc2
Andrea Fasano <afasano@redhat.com>
2024-02-08 11:09:59 -0500

    added ClusterInfo asset

--- pkg/nodejoiner/addnodes.go
+++ pkg/nodejoiner/addnodes.go
@@ -2,11 +2,24 @@ package nodejoiner
 
 import (
 	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/asset/agent/joiner"
+	"github.com/openshift/installer/pkg/asset/agent/workflow"
 	"github.com/openshift/installer/pkg/asset/store"
 )
 
 // NewAddNodesCommand creates a new command for add nodes.
-func NewAddNodesCommand(directory string) error {
+func NewAddNodesCommand(directory string, kubeConfig string) error {
+
+	// Store the current parameters into the assets folder, so
+	// that they could be retrieved later by the assets
+	params := joiner.Params{
+		Kubeconfig: kubeConfig,
+	}
+	params.Save(directory)
+
 	fetcher := store.NewAssetsFetcher(directory)
-	return fetcher.FetchAndPersist([]asset.WritableAsset{})
+	return fetcher.FetchAndPersist([]asset.WritableAsset{
+		&workflow.AgentWorkflowAddNodes{},
+		// To be completed
+	})
 }
