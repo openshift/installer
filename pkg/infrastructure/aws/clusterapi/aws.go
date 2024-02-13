8365398b2ff6dc6dca812b59aa19c977a86d4198
Patrick Dillon <padillon@redhat.com>
2024-02-13 18:34:43 -0500

    pkg/infrastructure/aws: PreProvision IAM Roles
    Creates the IAM roles that will be used by the control-plane &
    compute nodes.

--- pkg/infrastructure/aws/clusterapi/aws.go
+++ pkg/infrastructure/aws/clusterapi/aws.go
@@ -1,14 +1,25 @@
 package clusterapi
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/openshift/installer/pkg/infrastructure/clusterapi"
 	awstypes "github.com/openshift/installer/pkg/types/aws"
 )
 
 var _ clusterapi.Provider = (*Provider)(nil)
+var _ clusterapi.PreProvider = (*Provider)(nil)
 
 // Provider implements AWS CAPI installation.
 type Provider struct{}
 
 // Name gives the name of the provider, AWS.
 func (*Provider) Name() string { return awstypes.Name }
+
+func (*Provider) PreProvision(ctx context.Context, in clusterapi.PreProvisionInput) error {
+	if err := putIAMRoles(ctx, in.InfraID, in.InstallConfig); err != nil {
+		return fmt.Errorf("failed to create IAM roles: %w", err)
+	}
+	return nil
+}
