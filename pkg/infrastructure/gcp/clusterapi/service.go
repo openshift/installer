defe33452f0b6c28d119071ca684397d59617ad4
Patrick Dillon <padillon@redhat.com>
2024-02-27 21:55:37 -0500

    GCP CAPI Infra: auth with service account
    Adds service-account authentication support in the CAPI
    infrastructure packages, which prior to this utilize
    only default application credentials.

--- pkg/infrastructure/gcp/clusterapi/service.go
+++ pkg/infrastructure/gcp/clusterapi/service.go
@@ -5,13 +5,21 @@ import (
 	"fmt"
 
 	"google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
+
+	"github.com/openshift/installer/pkg/asset/installconfig/gcp"
 )
 
 // NewComputeService wraps the creation of a gcp compute service creation.
 func NewComputeService() (*compute.Service, error) {
 	ctx := context.Background()
 
-	service, err := compute.NewService(ctx)
+	ssn, err := gcp.GetSession(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get session: %w", err)
+	}
+
+	service, err := compute.NewService(ctx, option.WithCredentials(ssn.Credentials))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create compute service: %w", err)
 	}
