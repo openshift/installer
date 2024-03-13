75120c528e3952ef4791df072748afb910828792
Rafael Fonseca <r4f4rfs@gmail.com>
2024-03-13 17:41:44 +0100

    CORS-3050: capi/aws: copy AMI to target region, if needed

--- pkg/infrastructure/aws/clusterapi/aws.go
+++ pkg/infrastructure/aws/clusterapi/aws.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"k8s.io/utils/ptr"
+	capa "sigs.k8s.io/cluster-api-provider-aws/v2/api/v1beta2"
+
 	"github.com/openshift/installer/pkg/infrastructure/clusterapi"
 	awstypes "github.com/openshift/installer/pkg/types/aws"
 )
@@ -21,6 +24,16 @@ func (*Provider) Name() string { return awstypes.Name }
 func (*Provider) PreProvision(ctx context.Context, in clusterapi.PreProvisionInput) error {
 	if err := createIAMRoles(ctx, in.InfraID, in.InstallConfig); err != nil {
 		return fmt.Errorf("failed to create IAM roles: %w", err)
+	}
+
+	amiID, err := copyAMIToRegion(ctx, in.InstallConfig, in.InfraID, string(*in.RhcosImage))
+	if err != nil {
+		return fmt.Errorf("failed to copy AMI: %w", err)
+	}
+	for i := range in.MachineManifests {
+		if awsMachine, ok := in.MachineManifests[i].(*capa.AWSMachine); ok {
+			awsMachine.Spec.AMI = capa.AMIReference{ID: ptr.To(amiID)}
+		}
 	}
 	return nil
 }
