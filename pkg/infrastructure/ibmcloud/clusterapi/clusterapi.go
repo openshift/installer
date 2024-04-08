92684d9a69bd13b5acaa901d6da1438c55067a58
cjschaef <cjschaef@us.ibm.com>
2024-04-08 09:15:21 -0500

    CORS-3284: IBMCloud: CAPI PreProvisioining
    Add PreProvision logic for IBM Cloud CAPI support, which can:
    1. Provision ResourceGroup
    2. Create COS Instance and Bucket
    3. Upload RHCOS image to COS
    4. Create IAM Authorization for VPC to COS
    
    Related: https://issues.redhat.com/browse/CORS-3284

--- pkg/infrastructure/ibmcloud/clusterapi/clusterapi.go
+++ pkg/infrastructure/ibmcloud/clusterapi/clusterapi.go
@@ -2,8 +2,13 @@ package clusterapi
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 
+	ibmcloudic "github.com/openshift/installer/pkg/asset/installconfig/ibmcloud"
 	"github.com/openshift/installer/pkg/infrastructure/clusterapi"
+	"github.com/openshift/installer/pkg/rhcos/cache"
 	ibmcloudtypes "github.com/openshift/installer/pkg/types/ibmcloud"
 )
 
@@ -24,5 +29,86 @@ func (Provider) BootstrapHasPublicIP() bool { return false }
 
 // PreProvision creates the IBM Cloud objects required prior to running capibmcloud.
 func (p Provider) PreProvision(ctx context.Context, in clusterapi.PreProvisionInput) error {
+	// Before Provisioning IBM Cloud Infrastructure for the Cluster, we must perform the following.
+	// 1. Create the Resource Group to house cluster resources, if necessary (BYO RG).
+	// 2. Create a COS Instance and Bucket to host the RHCOS Custom Image file.
+	// 3. Upload the RHCOS image to the COS Bucket.
+	// 4. Add IAM Authorization for VPC Image Service to access the COS Object/Bucket/Instance.
+
+	// Setup IBM Cloud Client.
+	metadata := ibmcloudic.NewMetadata(in.InstallConfig.Config)
+	client, err := metadata.Client()
+	if err != nil {
+		return fmt.Errorf("failed creating IBM Cloud client: %w", err)
+	}
+	region := in.InstallConfig.Config.Platform.IBMCloud.Region
+
+	// Create cluster's Resource Group, if necessary (BYO RG is supported).
+	resourceGroupName := in.InfraID
+	if in.InstallConfig.Config.Platform.IBMCloud.ResourceGroupName != "" {
+		resourceGroupName = in.InstallConfig.Config.Platform.IBMCloud.ResourceGroupName
+	}
+
+	// Check whether the Resource Group already exists.
+	resourceGroup, err := client.GetResourceGroup(ctx, resourceGroupName)
+	if err != nil {
+		// If Resource Group cannot be found, but it was provided in install-config (use existing RG), raise an error.
+		// We could create the Resource Group, defined by user, but that might make resource cleanup more difficult.
+		if in.InstallConfig.Config.Platform.IBMCloud.ResourceGroupName != "" {
+			return fmt.Errorf("provided resource group not found: %w", err)
+		}
+	}
+
+	// Create Resource Group if it wasn't found (and isn't expected to be an existing RG).
+	if resourceGroup == nil {
+		if err := client.CreateResourceGroup(ctx, resourceGroupName); err != nil {
+			return fmt.Errorf("failed creating new resource group: %w", err)
+		}
+		// Retrieve the newly created resource group
+		resourceGroup, err = client.GetResourceGroup(ctx, resourceGroupName)
+		if err != nil {
+			return fmt.Errorf("failed retrieving new resource group: %w", err)
+		}
+	}
+
+	// Create a COS Instance and Bucket to host the RHCOS image file.
+	// NOTE(cjschaef): Support to use an existing COS Object (RHCO image file) or VPC Custom Image could be added to skip this step.
+	cosInstanceName := fmt.Sprintf("%s-cos", in.InfraID)
+	cosInstance, err := client.GetCOSInstanceByName(ctx, cosInstanceName)
+	if err != nil {
+		cosInstance, err = client.CreateCOSInstance(ctx, cosInstanceName, *resourceGroup.ID)
+		if err != nil {
+			return fmt.Errorf("failed creating RHCOS image COS instance: %w", err)
+		}
+	}
+	bucketName := fmt.Sprintf("%s-vsi-image", in.InfraID)
+	_, err = client.GetCOSBucketByName(ctx, *cosInstance.ID, bucketName, region)
+	if err != nil {
+		err = client.CreateCOSBucket(ctx, *cosInstance.ID, bucketName, region)
+		if err != nil {
+			return fmt.Errorf("failed creating RHCOS image COS bucket: %w", err)
+		}
+	}
+
+	// Upload the RHCOS image to the COS Bucket.
+	cachedImage, err := cache.DownloadImageFile(string(*in.RhcosImage), cache.InstallerApplicationName)
+	if err != nil {
+		return fmt.Errorf("failed to use cached ibmcloud image: %w", err)
+	}
+	imageData, err := os.ReadFile(cachedImage)
+	if err != nil {
+		return fmt.Errorf("failed reading RHCOS image data: %w", err)
+	}
+	err = client.CreateCOSObject(ctx, imageData, filepath.Base(cachedImage), *cosInstance.ID, bucketName, region)
+	if err != nil {
+		return fmt.Errorf("failed uploading RHCOS image: %w", err)
+	}
+
+	// Create IAM authorization for VPC to COS access for Custom Image Creation
+	err = client.CreateIAMAuthorizationPolicy(ctx, "is", "image", "cloud-object-storage", *cosInstance.ID, []string{"crn:v1:bluemix:public:iam::::serviceRole:Reader"})
+	if err != nil {
+		return fmt.Errorf("failed creating vpc-cos IAM authorization policy: %w", err)
+	}
+
 	return nil
 }
