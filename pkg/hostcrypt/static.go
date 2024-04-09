a3df94e2e453f9506346cd4eeb535d768815bd3b
Rafael Fonseca <r4f4rfs@gmail.com>
2024-04-09 10:43:00 +0200

    hostcrypt: update fips instructions to rhel9
    Update the warning message to say rhel9 instead of rhel8.

--- pkg/hostcrypt/static.go
+++ pkg/hostcrypt/static.go
@@ -5,13 +5,13 @@ package hostcrypt
 
 import "fmt"
 
-const binaryInstructions = "To obtain a suitable binary, download the openshift-install-rhel8 archive from the client mirror, or extract the openshift-install-fips command from the release payload."
+const binaryInstructions = "To obtain a suitable binary, download the openshift-install-rhel9 archive from the client mirror, or extract the openshift-install-fips command from the release payload."
 
 func allowFIPSCluster() error {
 	hostMsg := ""
 	if fipsEnabled, err := hostFIPSEnabled(); err != nil || !fipsEnabled {
 		hostMsg = " on a host with FIPS enabled"
 	}
-	return fmt.Errorf("use the FIPS-capable installer binary for RHEL 8%s.\n%s",
+	return fmt.Errorf("use the FIPS-capable installer binary for RHEL 9%s.\n%s",
 		hostMsg, binaryInstructions)
 }
