440a9bc7c9e91cdb34062b8957edc763eff34f88
Richard Su <rwsu@redhat.com>
2024-04-23 14:14:36 -0400

    Check status interatively instead of using a map
    Co-authored-by: Andrea Fasano <afasano@redhat.com>

--- pkg/agent/monitoraddnodes.go
+++ pkg/agent/monitoraddnodes.go
@@ -14,76 +14,44 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-type addNodeState int
-
-// The node states that we observe and report.
 const (
-	InitialState addNodeState = iota
-	AssistedServiceIsUp
-	KubeletIsRunningOnNode
-	FirstCSRPendingApproval
-	SecondCSRPendingApproval
-	NodeJoinedClusterStatusNotReady
-	Finish // terminal state
+	firstCSRSignerName  = "kubernetes.io/kube-apiserver-client-kubelet"
+	secondCSRSignerName = "kubernetes.io/kubelet-serving"
 )
 
-var stateNameMap = map[int]string{
-	int(InitialState):                    "Initial State",
-	int(AssistedServiceIsUp):             "Assisted Service API is available",
-	int(KubeletIsRunningOnNode):          "Kubelet is running",
-	int(FirstCSRPendingApproval):         "First CSR Pending approval",
-	int(SecondCSRPendingApproval):        "Second CSR Pending approval",
-	int(NodeJoinedClusterStatusNotReady): "Node joined cluster and status is NotReady",
-	int(Finish):                          "Node is Ready",
+type addNodeStatusHistory struct {
+	RestAPISeen            bool
+	KubeletIsRunningOnNode bool
+	FirstCSRSeen           bool
+	SecondCSRSeen          bool
+	NodeJoinedCluster      bool
+	NodeIsReady            bool
 }
 
-type checkFunction func(nodeMonitor *addNodeMonitor)
+type addNodeMonitor struct {
+	nodeIPAddress string
+	cluster       *Cluster
+	status        addNodeStatusHistory
+}
 
-type checkFunctions map[int]checkFunction
+func newAddNodeMonitor(nodeIP string, cluster *Cluster) *addNodeMonitor {
+	mon := addNodeMonitor{
+		nodeIPAddress: nodeIP,
+		cluster:       cluster,
+		status: addNodeStatusHistory{
+			RestAPISeen:            false,
+			KubeletIsRunningOnNode: false,
+			FirstCSRSeen:           false,
+			SecondCSRSeen:          false,
+			NodeJoinedCluster:      false,
+			NodeIsReady:            false,
+		},
+	}
+	return &mon
+}
 
-var defaultChecks = checkFunctions{
-	int(AssistedServiceIsUp): func(mon *addNodeMonitor) {
-		if mon.in(InitialState) &&
-			mon.cluster.API.Rest.IsRestAPILive() {
-			mon.setState(AssistedServiceIsUp)
-		}
-	},
-	int(KubeletIsRunningOnNode): func(mon *addNodeMonitor) {
-		if mon.onOrBefore(KubeletIsRunningOnNode) &&
-			mon.isKubeletRunningOnNode() {
-			mon.setState(KubeletIsRunningOnNode)
-		}
-	},
-	int(FirstCSRPendingApproval): func(mon *addNodeMonitor) {
-		if mon.onOrAfter(KubeletIsRunningOnNode) &&
-			mon.clusterHasFirstCSRPending() {
-			mon.setState(FirstCSRPendingApproval)
-		}
-	},
-	int(SecondCSRPendingApproval): func(mon *addNodeMonitor) {
-		if mon.onOrAfter(KubeletIsRunningOnNode) &&
-			mon.clusterHasSecondCSRPending() {
-			mon.setState(SecondCSRPendingApproval)
-		}
-	},
-	int(NodeJoinedClusterStatusNotReady): func(mon *addNodeMonitor) {
-		hasJoined, isReady, err := mon.nodeHasJoinedClusterAndIsReady()
-		if err != nil {
-			logrus.Debugf("HasNodeJoinedClusterAndIsReady returned err: %v", err)
-		}
-		if hasJoined && !isReady {
-			mon.setState(NodeJoinedClusterStatusNotReady)
-		}
-	},
-	int(Finish): func(mon *addNodeMonitor) {
-		hasJoined, isReady, err := mon.nodeHasJoinedClusterAndIsReady()
-		if err != nil {
-			logrus.Debugf("HasNodeJoinedClusterAndIsReady returned err: %v", err)
-		}
-		if hasJoined && isReady {
-			mon.setState(Finish)
-		}
-	},
+func (mon *addNodeMonitor) logStatus(status string) {
+	logrus.Infof("Node %s: %s", mon.nodeIPAddress, status)
 }
 
 // MonitorAddNodes waits for the a node to be added to the cluster
@@ -101,36 +69,60 @@ func MonitorAddNodes(cluster *Cluster, nodeIPAddress string) error {
 	mon := newAddNodeMonitor(nodeIPAddress, cluster)
 
 	wait.Until(func() {
-		if mon.cluster.API.Rest.IsRestAPILive() {
-			_, err := cluster.MonitorStatusFromAssistedService()
-			if err != nil {
-				logrus.Warnf("Node %s: %s", nodeIPAddress, err)
-			}
+		if !mon.status.RestAPISeen &&
+			mon.cluster.API.Rest.IsRestAPILive() {
+			mon.status.RestAPISeen = true
+			mon.logStatus("Assisted Service API is available")
 		}
 
-		for _, checkFunc := range defaultChecks {
-			lastState := mon.currentState
-			checkFunc(mon)
-			if !mon.in(lastState) {
-				// log state change
-				logrus.Infof("Node %s: %s", nodeIPAddress, mon.currentStateName())
-			}
+		if !mon.status.KubeletIsRunningOnNode &&
+			mon.isKubeletRunningOnNode() {
+			mon.status.KubeletIsRunningOnNode = true
+			mon.logStatus("Kubelet is running")
 		}
 
-		if mon.clusterHasFirstCSRPending() || mon.clusterHasSecondCSRPending() {
-			// TODO? The CSRs have no IP address to identify for which
-			// node it is for, so it is possible to log CSRs pending for
-			// other nodes.
-			mon.logCSRsPendingApproval()
+		if mon.status.KubeletIsRunningOnNode &&
+			!mon.status.FirstCSRSeen &&
+			mon.clusterHasFirstCSRPending() {
+			mon.status.FirstCSRSeen = true
+			mon.logStatus("First CSR Pending approval")
+			mon.logCSRsPendingApproval(firstCSRSignerName)
 		}
 
-		if mon.in(Finish) {
+		if mon.status.KubeletIsRunningOnNode &&
+			!mon.status.SecondCSRSeen &&
+			mon.clusterHasSecondCSRPending() {
+			mon.status.SecondCSRSeen = true
+			mon.logStatus("Second CSR Pending approval")
+			mon.logCSRsPendingApproval(secondCSRSignerName)
+		}
+
+		hasJoined, isReady, err := mon.nodeHasJoinedClusterAndIsReady()
+		if err != nil {
+			logrus.Debugf("nodeHasJoinedClusterAndIsReady returned err: %v", err)
+		}
+
+		if !mon.status.NodeJoinedCluster && hasJoined {
+			mon.status.NodeJoinedCluster = true
+			mon.logStatus("Node joined cluster")
+		}
+
+		if !mon.status.NodeIsReady && isReady {
+			mon.status.NodeIsReady = true
+			mon.logStatus("Node is Ready")
 			// TODO: There appears to be a bug where the node becomes Ready
 			// before second CSR is approved. Log Pending CSRs for now, so users
 			// are aware there are still some waiting their approval even
 			// though the node status is Ready.
-			mon.logCSRsPendingApproval()
+			mon.logCSRsPendingApproval(secondCSRSignerName)
 			cancel()
+		}
+
+		if mon.cluster.API.Rest.IsRestAPILive() {
+			_, err = cluster.MonitorStatusFromAssistedService()
+			if err != nil {
+				logrus.Warnf("Node %s: %s", nodeIPAddress, err)
+			}
 		}
 	}, 5*time.Second, waitContext.Done())
 
@@ -145,41 +137,6 @@ func MonitorAddNodes(cluster *Cluster, nodeIPAddress string) error {
 	}
 
 	return nil
-}
-
-type addNodeMonitor struct {
-	nodeIPAddress string
-	cluster       *Cluster
-	currentState  addNodeState
-}
-
-func newAddNodeMonitor(nodeIP string, cluster *Cluster) *addNodeMonitor {
-	mon := addNodeMonitor{
-		nodeIPAddress: nodeIP,
-		cluster:       cluster,
-		currentState:  InitialState,
-	}
-	return &mon
-}
-
-func (mon *addNodeMonitor) setState(state addNodeState) {
-	mon.currentState = state
-}
-
-func (mon *addNodeMonitor) in(state addNodeState) bool {
-	return mon.currentState == state
-}
-
-func (mon *addNodeMonitor) onOrBefore(state addNodeState) bool {
-	return mon.currentState <= state
-}
-
-func (mon *addNodeMonitor) onOrAfter(state addNodeState) bool {
-	return mon.currentState >= state
-}
-
-func (mon *addNodeMonitor) currentStateName() string {
-	return stateNameMap[int(mon.currentState)]
 }
 
 func (mon *addNodeMonitor) nodeHasJoinedClusterAndIsReady() (bool, bool, error) {
@@ -222,7 +179,10 @@ func (mon *addNodeMonitor) nodeHasJoinedClusterAndIsReady() (bool, bool, error) 
 	return hasJoined, isReady, nil
 }
 
-func (mon *addNodeMonitor) logCSRsPendingApproval() {
+func (mon *addNodeMonitor) logCSRsPendingApproval(signerName string) {
+	// TODO? The CSRs have no IP address to identify for which
+	// node it is for, so it is possible to log CSRs pending for
+	// other nodes.
 	csrs, err := mon.cluster.API.Kube.ListCSRs()
 	if err != nil {
 		logrus.Debugf("error calling listCSRs(): %v ", err)
@@ -231,6 +191,10 @@ func (mon *addNodeMonitor) logCSRsPendingApproval() {
 	for _, csr := range csrs.Items {
 		if len(csr.Status.Conditions) == 0 {
 			// CSR is Pending and awaiting approval
+			if signerName != "" && signerName != csr.Spec.SignerName {
+				continue
+			}
+
 			logrus.Infof("CSR %s with signerName %s and username %s is Pending and awaiting approval",
 				csr.Name, csr.Spec.SignerName, csr.Spec.Username)
 		}
@@ -238,11 +202,14 @@ func (mon *addNodeMonitor) logCSRsPendingApproval() {
 }
 
 func (mon *addNodeMonitor) clusterHasFirstCSRPending() bool {
-	return len(mon.cluster.API.Kube.getCSRsPendingApproval("kubernetes.io/kube-apiserver-client-kubelet")) > 0
+	return len(mon.cluster.API.Kube.getCSRsPendingApproval(firstCSRSignerName)) > 0
 }
 
 func (mon *addNodeMonitor) clusterHasSecondCSRPending() bool {
-	return len(mon.cluster.API.Kube.getCSRsPendingApproval("kubernetes.io/kubelet-serving")) > 0
+	// TODO: the csr.Spec.Username contains the node name
+	// can we use it as an additional filter to only show
+	// those matching mon.nodeIPAddress?
+	return len(mon.cluster.API.Kube.getCSRsPendingApproval(secondCSRSignerName)) > 0
 }
 
 // isKubeletRunningOnNode checks if kubelet responds
