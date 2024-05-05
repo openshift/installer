3f88401165bff7f35356ff5462fc1a479390a815
Rafael Fonseca <r4f4rfs@gmail.com>
2024-05-05 18:24:35 +0200

    aws: terraform: add spot instance support for masters
    Spot instances can result on savings for short-lived clusters.

--- data/data/aws/cluster/main.tf
+++ data/data/aws/cluster/main.tf
@@ -55,6 +55,7 @@ module "masters" {
   user_data_ign                    = var.ignition_master
   publish_strategy                 = var.aws_publish_strategy
   iam_role_name                    = var.aws_master_iam_role_name
+  use_spot_instance                = var.aws_master_use_spot_instance
 }
 
 module "iam" {
