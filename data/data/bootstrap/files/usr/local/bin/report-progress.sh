241ec4efe4cc1e4d90c1a8d5d8b2200701e53a9f
Abu Kashem <akashem@redhat.com>
2024-05-10 18:08:54 -0400

    improve bootstrap logic to wait for at least 2 apiserver instances

--- data/data/bootstrap/files/usr/local/bin/report-progress.sh
+++ data/data/bootstrap/files/usr/local/bin/report-progress.sh
@@ -3,6 +3,9 @@
 # shellcheck disable=SC1091  # using path on bootstrap machine
 . /usr/local/bin/bootstrap-service-record.sh
 
+# shellcheck disable=SC1091  # using path on bootstrap machine
+. /usr/local/bin/wait-for-ha-api.sh
+
 KUBECONFIG="${1}"
 
 wait_for_existence() {
@@ -16,6 +19,9 @@ record_service_stage_start "wait-for-bootstrap-complete"
 echo "Waiting for bootstrap to complete..."
 wait_for_existence /opt/openshift/.bootkube.done
 record_service_stage_success
+
+## wait for API to be available
+wait_for_ha_api
 
 record_service_stage_start "report-bootstrap-complete"
 echo "Reporting install progress..."
