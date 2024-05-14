1a9292bd370e8ca31fc3d7f4ebefb0db2b3a6296
Rafael Fonseca <r4f4rfs@gmail.com>
2024-05-14 14:29:23 -0400

    OCPBUGS-33662: aws: don't always require s3:Delete* permissions
    They shouldn't be required when `preserveBootstrapIgnition` is set in
    the install-config.yaml. Otherwise, the install fails with:
    
    ```
    INFO Credentials loaded from the "denys3" profile in file "/home/cloud-user/.aws/credentials"
    INFO Consuming Install Config from target directory
    WARNING Action not allowed with tested creds          action=s3:DeleteBucket
    WARNING Action not allowed with tested creds          action=s3:DeleteObject
    WARNING Action not allowed with tested creds          action=s3:DeleteObject
    WARNING Tested creds not able to perform all requested actions
    FATAL failed to fetch Cluster: failed to fetch dependency of "Cluster": failed to generate asset "Platform Permissions Check": validate AWS credentials: current credentials insufficient for performing cluster installation
    ```

--- pkg/asset/installconfig/platformpermscheck.go
+++ pkg/asset/installconfig/platformpermscheck.go
@@ -102,6 +102,10 @@ func (a *PlatformPermsCheck) Generate(dependencies asset.Parents) error {
 			permissionGroups = append(permissionGroups, awsconfig.PermissionPublicIpv4Pool)
 		}
 
+		if !ic.Config.AWS.PreserveBootstrapIgnition {
+			permissionGroups = append(permissionGroups, awsconfig.PermissionDeleteIgnitionObjects)
+		}
+
 		ssn, err := ic.AWS.Session(ctx)
 		if err != nil {
 			return err
