ff930bd0f2cb95a78590fc2faf8f34ac7dd20f3c
Patrick Dillon <padillon@redhat.com>
2024-05-16 10:49:58 -0400

    terraform/providers/openstack: remove openstack provider
    Initial step of removing OpenStack Terraform installs. Remove the provider
    to reduce repo size and build time.

--- terraform/providers/openstack/vendor/github.com/terraform-provider-openstack/terraform-provider-openstack/openstack/sharedfilesystem_shared_v2.go
+++
@@ -1,9 +0,0 @@
-package openstack
-
-const (
-	sharedFilesystemV2MinMicroversion = "2.7"
-
-	sharedFilesystemV2SecurityServiceOUMicroversion = "2.44"
-	sharedFilesystemV2SharedAccessCephXMicroversion = "2.13"
-	sharedFilesystemV2SharedAccessMinMicroversion   = "2.21"
-)
