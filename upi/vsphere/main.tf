ee7b66ce31dc331196a0b1165a8d95b798c5ca1e
Joseph Callen <jcallen@redhat.com>
2024-05-20 16:06:13 -0400

    vSphere upi - no rp create

--- upi/vsphere/main.tf
+++ upi/vsphere/main.tf
@@ -60,12 +60,18 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.dc[index(data.vsphere_datacenter.dc.*.name, local.datacenters_distinct[count.index])].id
 }
  
-resource "vsphere_resource_pool" "resource_pool" {
-  count                   = length(data.vsphere_compute_cluster.compute_cluster)
-  name                    = var.cluster_id
-  parent_resource_pool_id = data.vsphere_compute_cluster.compute_cluster[count.index].resource_pool_id
+//resource "vsphere_resource_pool" "resource_pool" {
+//  count                   = length(data.vsphere_compute_cluster.compute_cluster)
+//  name                    = var.cluster_id
+//  parent_resource_pool_id = data.vsphere_compute_cluster.compute_cluster[count.index].resource_pool_id
+//}
+
+data "vsphere_resource_pool" "resource_pool" {
+  count = length(local.datacenters_distinct)
+  name = "ipi-ci-clusters"
+  datacenter_id = data.vsphere_datacenter.dc[index(data.vsphere_datacenter.dc.*.name, local.datacenters_distinct[count.index])].id
 }
- 
+
 resource "vsphere_folder" "folder" {
   count = length(local.datacenters_distinct)
   path          = var.cluster_id
@@ -168,7 +174,8 @@ resource "vsphere_folder" "folder" {
    vmname                = element(split(".", local.lb_fqdns[0]), 0)
    ipaddress             = module.ipam_lb.ip_addresses[0]
    ignition               = module.lb.ignition
-   resource_pool_id      = vsphere_resource_pool.resource_pool[0].id
+   //resource_pool_id      = vsphere_resource_pool.resource_pool[0].id
+   resource_pool_id      = data.vsphere_resource_pool.resource_pool[0].id
    datastore_id          = data.vsphere_datastore.datastore[0].id
    datacenter_id         = data.vsphere_datacenter.dc[0].id
    network_id            = data.vsphere_network.network[0].id
@@ -190,7 +197,8 @@ module "bootstrap" {
 
   vmname                = element(split(".", local.bootstrap_fqdns[0]), 0)
   ipaddress             = module.ipam_bootstrap.ip_addresses[0]
-  resource_pool_id      = vsphere_resource_pool.resource_pool[0].id
+  //resource_pool_id      = vsphere_resource_pool.resource_pool[0].id
+  resource_pool_id      = data.vsphere_resource_pool.resource_pool[0].id
   datastore_id          = data.vsphere_datastore.datastore[0].id
   datacenter_id         = data.vsphere_datacenter.dc[0].id
   network_id            = data.vsphere_network.network[0].id
@@ -219,7 +227,8 @@ module "control_plane_vm" {
   vmname = element(split(".", module.control_plane_a_records.fqdns[count.index]), 0)
   ipaddress = module.ipam_control_plane.ip_addresses[count.index]
   ignition = file(var.control_plane_ignition_path)
-  resource_pool_id      = vsphere_resource_pool.resource_pool[count.index % local.failure_domain_count].id
+  //resource_pool_id      = vsphere_resource_pool.resource_pool[count.index % local.failure_domain_count].id
+  resource_pool_id      = data.vsphere_resource_pool.resource_pool[count.index % local.failure_domain_count].id
   datastore_id          = data.vsphere_datastore.datastore[count.index % local.failure_domain_count].id
   datacenter_id         = data.vsphere_datacenter.dc[index(data.vsphere_datacenter.dc.*.name, local.failure_domains[count.index % local.failure_domain_count]["datacenter"])].id
   network_id            = data.vsphere_network.network[count.index % local.failure_domain_count].id
@@ -240,7 +249,8 @@ module "compute_vm" {
   vmname = element(split(".", module.compute_a_records.fqdns[count.index]), 0)
   ipaddress = module.ipam_compute.ip_addresses[count.index]
 
-  resource_pool_id      = vsphere_resource_pool.resource_pool[count.index % local.failure_domain_count].id
+  //resource_pool_id      = vsphere_resource_pool.resource_pool[count.index % local.failure_domain_count].id
+  resource_pool_id      = data.vsphere_resource_pool.resource_pool[count.index % local.failure_domain_count].id
   datastore_id          = data.vsphere_datastore.datastore[count.index % local.failure_domain_count].id
   datacenter_id         = data.vsphere_datacenter.dc[index(data.vsphere_datacenter.dc.*.name, local.failure_domains[count.index % local.failure_domain_count]["datacenter"])].id
   network_id            = data.vsphere_network.network[count.index % local.failure_domain_count].id
