57567b54f8aab9e47079e501cd30edb15ad868a7
Patrick Dillon <padillon@redhat.com>
2024-05-22 10:04:37 -0400

    Revert "Merge pull request #8434 from neisw/trt-1674-4.16-capi-revert-1"
    This reverts commit c593420bc6603fd6e3f6ab8ffd8e6c2317b19875, reversing
    changes made to 3eed76e1400cac88af6638bb097ada1607137f3f.

--- terraform/providers/aws/vendor/github.com/hashicorp/terraform-provider-aws/internal/service/apigateway/consts.go
+++
@@ -1,9 +0,0 @@
-package apigateway
-
-import (
-	"time"
-)
-
-const (
-	propagationTimeout = 2 * time.Minute
-)
--- terraform/providers/ovirt/vendor/github.com/ovirt/go-ovirt-client/v3/const.go
+++
@@ -1,9 +0,0 @@
-package ovirtclient
-
-// DefaultBlankTemplateID returns the ID for the factory-default blank template. This should not be used
-// as the template may be deleted from the oVirt engine. Instead, use the API call to find the blank template.
-const DefaultBlankTemplateID TemplateID = "00000000-0000-0000-0000-000000000000"
-
-// MinDiskSizeOVirt defines the minimum size of 1M for disks in oVirt. Smaller disks can be created, but they
-// lead to bugs in oVirt when creating disks from templates and changing the format.
-const MinDiskSizeOVirt uint64 = 1048576
