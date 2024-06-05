708042df844709415640d6f88e8d19e2f213e627
Brent Barbachem <barbacbd@gmail.com>
2024-06-05 15:12:35 -0400

    OCPBUGS-34915: Add network tags to CAPG control plane machines

--- pkg/asset/machines/gcp/gcpmachines.go
+++ pkg/asset/machines/gcp/gcpmachines.go
@@ -115,9 +115,6 @@ func createGCPMachine(name string, installConfig *installconfig.InstallConfig, i
 		osImage = mpool.OSImage.Name
 	}
 
-	// TODO tags aren't currently being set in GCPMachine which only has
-	// AdditionalNetworkTags []string
-
 	masterSubnet := installConfig.Config.Platform.GCP.ControlPlaneSubnet
 	if masterSubnet == "" {
 		masterSubnet = gcptypes.DefaultSubnetName(infraID, masterRole)
@@ -131,12 +128,13 @@ func createGCPMachine(name string, installConfig *installconfig.InstallConfig, i
 			},
 		},
 		Spec: capg.GCPMachineSpec{
-			InstanceType:     mpool.InstanceType,
-			Subnet:           ptr.To(masterSubnet),
-			AdditionalLabels: getLabelsFromInstallConfig(installConfig, infraID),
-			Image:            ptr.To(osImage),
-			RootDeviceType:   ptr.To(capg.DiskType(mpool.OSDisk.DiskType)),
-			RootDeviceSize:   mpool.OSDisk.DiskSizeGB,
+			InstanceType:          mpool.InstanceType,
+			Subnet:                ptr.To(masterSubnet),
+			AdditionalLabels:      getLabelsFromInstallConfig(installConfig, infraID),
+			Image:                 ptr.To(osImage),
+			RootDeviceType:        ptr.To(capg.DiskType(mpool.OSDisk.DiskType)),
+			RootDeviceSize:        mpool.OSDisk.DiskSizeGB,
+			AdditionalNetworkTags: mpool.Tags,
 		},
 	}
 	gcpMachine.SetGroupVersionKind(capg.GroupVersion.WithKind("GCPMachine"))
