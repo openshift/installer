c5048c102b6969962675e1f7071224da95ffe6a2
Pawan Pinjarkar <ppinjark@redhat.com>
2024-06-18 19:28:45 -0400

    AGENT-919: Authenticate day2 operations
    Creating a Nodes ISO:
    - secret does not exist
       1. Generate a new public key and JWT token with an expiration time of 48 hours
       2. The public key and token gets saved into the asset store
       3. Create a secret named agent-auth-token in the openshift-config namespace with this token and public key from the asset store.
    
    - secret already exists
       1. Retrieve the stored token and check if the secret with JWT token is older than 24 hours.
    
    - secret already exists and the token is older than 24 Hours
       1. Generate a new public key and JWT token with a new expiration time of 48 hours
       2. Update the secret with a new public key and JWT token
    
    - secret already exists and the token is not older than 24 Hours
       1. Retrieve the token and public key from the secret and update the asset store with the values from the secret.
    
    Running monitor-add-nodes Command:
      1. Retrieve the token from the secret.
      2. Register the agent installer client with the retrieved token.
      3. Send the auth token to the assisted service API via HTTP headers

---
+++ pkg/asset/agent/gencrypto/auth_utils_test.go
@@ -0,0 +1,53 @@
+package gencrypto
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseExpirationFromToken(t *testing.T) {
+	publicKey, privateKey, err := keyPairPEM()
+	assert.NotEmpty(t, publicKey)
+	assert.NotEmpty(t, privateKey)
+	assert.NoError(t, err)
+
+	infraEnvID := uuid.New().String()
+
+	tokenNoExp, err := generateToken(infraEnvID, privateKey)
+	assert.NotEmpty(t, tokenNoExp)
+	assert.NoError(t, err)
+
+	expiry := time.Now().UTC().Add(30 * time.Second)
+	tokenWithExp, err := generateToken(infraEnvID, privateKey, expiry)
+	assert.NotEmpty(t, tokenWithExp)
+	assert.NoError(t, err)
+
+	cases := []struct {
+		name, token, errorMessage string
+		expectedErr               bool
+	}{
+		{
+			name:         "exp-claim-not-set",
+			token:        tokenNoExp,
+			expectedErr:  true,
+			errorMessage: "token missing 'exp' claim",
+		},
+		{
+			name:  "exp-claim-set",
+			token: tokenWithExp,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err = ParseExpirationFromToken(tc.token)
+			if tc.expectedErr {
+				assert.EqualError(t, err, tc.errorMessage)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
--- pkg/asset/agent/gencrypto/authconfig.go
+++ pkg/asset/agent/gencrypto/authconfig.go
@@ -9,16 +9,34 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
+	"fmt"
+	"time"
 
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/openshift/installer/pkg/asset"
 	"github.com/openshift/installer/pkg/asset/agent/common"
+	"github.com/openshift/installer/pkg/asset/agent/joiner"
+	"github.com/openshift/installer/pkg/asset/agent/workflow"
+)
+
+var (
+	authTokenSecretNamespace = "openshift-config" //nolint:gosec // no sensitive info
+	authTokenSecretName      = "agent-auth-token" //nolint:gosec // no sensitive info
+	authTokenSecretDataKey   = "agentAuthToken"
+	authTokenPublicDataKey   = "authTokenPublicKey"
 )
 
 // AuthConfig is an asset that generates ECDSA public/private keys, JWT token.
 type AuthConfig struct {
-	PublicKey, Token string
+	PublicKey, AgentAuthToken string
 }
 
 var _ asset.Asset = (*AuthConfig)(nil)
@@ -37,13 +55,16 @@ var _ asset.Asset = (*AuthConfig)(nil)
 func (a *AuthConfig) Dependencies() []asset.Asset {
 	return []asset.Asset{
 		&common.InfraEnvID{},
+		&workflow.AgentWorkflow{},
+		&joiner.AddNodesConfig{},
 	}
 }
 
 // Generate generates the auth config for agent installer APIs.
 func (a *AuthConfig) Generate(_ context.Context, dependencies asset.Parents) error {
 	infraEnvID := &common.InfraEnvID{}
-	dependencies.Get(infraEnvID)
+	agentWorkflow := &workflow.AgentWorkflow{}
+	dependencies.Get(infraEnvID, agentWorkflow)
 
 	publicKey, privateKey, err := keyPairPEM()
 	if err != nil {
@@ -54,12 +75,33 @@ func (a *AuthConfig) Generate(_ context.Context, dependencies asset.Parents) err
 
 	a.PublicKey = encodedPubKeyPEM
 
-	token, err := localJWTForKey(infraEnvID.ID, privateKey)
-	if err != nil {
-		return err
-	}
-	a.Token = token
+	switch agentWorkflow.Workflow {
+	case workflow.AgentWorkflowTypeInstall:
+		// Auth tokens do not expire
+		token, err := generateToken(infraEnvID.ID, privateKey)
+		if err != nil {
+			return err
+		}
+		a.AgentAuthToken = token
+	case workflow.AgentWorkflowTypeAddNodes:
+		addNodesConfig := &joiner.AddNodesConfig{}
+		dependencies.Get(addNodesConfig)
 
+		// Auth tokens expires after 48 hours
+		expiry := time.Now().UTC().Add(48 * time.Hour)
+		token, err := generateToken(infraEnvID.ID, privateKey, expiry)
+		if err != nil {
+			return err
+		}
+		a.AgentAuthToken = token
+
+		err = a.createOrUpdateAuthTokenSecret(addNodesConfig.Params.Kubeconfig)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("AgentWorkflowType value not supported: %s", agentWorkflow.Workflow)
+	}
 	return nil
 }
 
@@ -68,6 +110,7 @@ func (*AuthConfig) Name() string {
 	return "Agent Installer API Auth Config"
 }
 
+// keyPairPEM returns the public, private keys in PEM format.
 func keyPairPEM() (string, string, error) {
 	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
@@ -111,20 +154,171 @@ func keyPairPEM() (string, string, error) {
 	return pubKeyPEM.String(), privKeyPEM.String(), nil
 }
 
-func localJWTForKey(id string, privateKkeyPem string) (string, error) {
+// generateToken returns a JWT token based on the private key.
+func generateToken(id string, privateKkeyPem string, expiry ...time.Time) (string, error) {
+	// Create the JWT claims
+	claims := jwt.MapClaims{
+		string(InfraEnvKey): id,
+	}
+
+	// Set the expiry time if provided
+	if len(expiry) > 0 {
+		claims["exp"] = expiry[0].Unix()
+	}
+
+	// Create the token using the ES256 signing method and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
 	priv, err := jwt.ParseECPrivateKeyFromPEM([]byte(privateKkeyPem))
 	if err != nil {
 		return "", err
 	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
-		string(InfraEnvKey): id,
-	})
-
+	// Sign the token with the provided private key
 	tokenString, err := token.SignedString(priv)
 	if err != nil {
 		return "", err
 	}
-
 	return tokenString, nil
+}
+
+func initClient(kubeconfig string) (*kubernetes.Clientset, error) {
+	var err error
+	var config *rest.Config
+	if kubeconfig != "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	} else {
+		config, err = rest.InClusterConfig()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	k8sclientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return k8sclientset, err
+}
+
+func (a *AuthConfig) createOrUpdateAuthTokenSecret(kubeconfigPath string) error {
+	k8sclientset, err := initClient(kubeconfigPath)
+	if err != nil {
+		return err
+	}
+	// check if secret exists
+	retrievedSecret, err := k8sclientset.CoreV1().Secrets(authTokenSecretNamespace).Get(context.Background(), authTokenSecretName, metav1.GetOptions{})
+	// if the secret does not exist
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return a.createSecret(k8sclientset)
+		}
+		// Other errors while trying to get the secret
+		return fmt.Errorf("unable to retrieve secret %s/%s: %w", authTokenSecretNamespace, authTokenSecretName, err)
+	}
+
+	// if the secret exists in the cluster, get the token
+	retrievedToken, err := extractAuthTokenFromSecret(retrievedSecret)
+	if err != nil {
+		return err
+	}
+	expiryTime, err := ParseExpirationFromToken(retrievedToken)
+	if err != nil {
+		return err
+	}
+	// Calculate 24 hours before the expiration time
+	thresholdTime := expiryTime.Add(-24 * time.Hour)
+	// Check if current time is past the thresholdTime time of 24 hours
+	if time.Now().UTC().After(thresholdTime) {
+		// update the secret in the cluster with a new token from asset store
+		err = a.refreshAuthTokenSecret(k8sclientset, retrievedSecret)
+		if err != nil {
+			return err
+		}
+		logrus.Debug("auth token secret regenerated and updated in the cluster")
+	} else {
+		// Update the token in asset store with the retrieved token from the cluster
+		a.AgentAuthToken = retrievedToken
+
+		retrievedPublicKey, err := extractPublicKeyFromSecret(retrievedSecret)
+		if err != nil {
+			return err
+		}
+		// Update the asset store with the retrieved public key associated with the valid token from the cluster
+		a.PublicKey = retrievedPublicKey
+		logrus.Debugf("reusing existing auth token (valid up to %s)", expiryTime)
+	}
+	return err
+}
+
+func (a *AuthConfig) createSecret(k8sclientset kubernetes.Interface) error {
+	// Create a Secret
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: authTokenSecretName,
+			Annotations: map[string]string{
+				"updatedAt": "", // Initially set to empty
+			},
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			authTokenSecretDataKey: []byte(a.AgentAuthToken),
+			authTokenPublicDataKey: []byte(a.PublicKey),
+		},
+	}
+	_, err := k8sclientset.CoreV1().Secrets(authTokenSecretNamespace).Create(context.Background(), secret, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to create auth token secret: %w", err)
+	}
+	logrus.Debugf("Created auth token secret %s/%s", authTokenSecretNamespace, authTokenSecretName)
+
+	return nil
+}
+
+func (a *AuthConfig) refreshAuthTokenSecret(k8sclientset kubernetes.Interface, retrievedSecret *corev1.Secret) error {
+	retrievedSecret.Data[authTokenSecretDataKey] = []byte(a.AgentAuthToken)
+	retrievedSecret.Data[authTokenPublicDataKey] = []byte(a.PublicKey)
+	// only for informational purposes
+	retrievedSecret.Annotations["updatedAt"] = time.Now().UTC().Format(time.RFC3339)
+
+	_, err := k8sclientset.CoreV1().Secrets(authTokenSecretNamespace).Update(context.TODO(), retrievedSecret, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+	logrus.Debugf("Updated auth token secret %s/%s", authTokenSecretNamespace, authTokenSecretName)
+	return nil
+}
+
+// GetAuthTokenFromCluster returns a token string stored as the secret from the cluster.
+func GetAuthTokenFromCluster(ctx context.Context, kubeconfigPath string) (string, error) {
+	client, err := initClient(kubeconfigPath)
+	if err != nil {
+		return "", err
+	}
+
+	retrievedSecret, err := client.CoreV1().Secrets(authTokenSecretNamespace).Get(ctx, authTokenSecretName, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	authToken, err := extractAuthTokenFromSecret(retrievedSecret)
+	if err != nil {
+		return "", err
+	}
+	return authToken, err
+}
+
+func extractAuthTokenFromSecret(secret *corev1.Secret) (string, error) {
+	existingAgentAuthToken, exists := secret.Data[authTokenSecretDataKey]
+	if !exists || len(existingAgentAuthToken) == 0 {
+		return "", fmt.Errorf("auth token secret %s/%s does not contain the key %s or is empty", authTokenSecretNamespace, authTokenSecretName, authTokenSecretDataKey)
+	}
+	return string(existingAgentAuthToken), nil
+}
+
+func extractPublicKeyFromSecret(secret *corev1.Secret) (string, error) {
+	existingPublicKey, exists := secret.Data[authTokenPublicDataKey]
+	if !exists || len(existingPublicKey) == 0 {
+		return "", fmt.Errorf("auth token secret %s/%s does not contain the key %s or is empty", authTokenSecretNamespace, authTokenSecretName, authTokenPublicDataKey)
+	}
+	return string(existingPublicKey), nil
 }
