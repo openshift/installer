9cdc03a284a37443afe32e556b3d6c4458aa74a3
Joseph Callen <jcallen@redhat.com>
2024-06-27 09:24:03 -0400

    vSphere: If the folder pre-exists do not tag
    In terraform-versions of the installer we did not tag
    pre-existing folders. The change to capi/capv
    we introduced always tagging a folder which causes failures
    on deprovision. This PR checks if the folder pre-exists
    and skips teh folder create and tag attach functions.

--- pkg/asset/installconfig/vsphere/folder.go
+++ pkg/asset/installconfig/vsphere/folder.go
@@ -1,17 +1,17 @@
-//go:build !altinfra
-// +build !altinfra
-
 package vsphere
 
 import (
 	"context"
+	"errors"
 	"time"
 
+	"github.com/vmware/govmomi/find"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 // folderExists returns an error if a folder is specified in the vSphere platform but a folder with that name is not found in the datacenter.
 func folderExists(validationCtx *validationContext, folderPath string, fldPath *field.Path) field.ErrorList {
+	var notFoundError *find.NotFoundError
 	allErrs := field.ErrorList{}
 	finder := validationCtx.Finder
 	// If no folder is specified, skip this check as the folder will be created.
@@ -23,8 +23,13 @@ func folderExists(validationCtx *validationContext, folderPath string, fldPath *
 	defer cancel()
 
 	folder, err := finder.Folder(ctx, folderPath)
-	if err != nil {
+	if err != nil && !errors.As(err, &notFoundError) {
 		return append(allErrs, field.Invalid(fldPath, folderPath, err.Error()))
+	}
+
+	// folder was not found so no privilege check can be performed
+	if folder == nil {
+		return allErrs
 	}
 	permissionGroup := permissions[permissionFolder]
 
