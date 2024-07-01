9f35e4995db34166f9b265c86abbbaa949f6d0ba
Rafael Fonseca <r4f4rfs@gmail.com>
2024-07-01 17:26:34 +0200

    OCPBUGS-36390: aws: do not require create permissions when BYO IAM role
    The Installer is unconditionally requiring permissions needed to create
    IAM roles even when the users use existing roles. They should be
    included only when needed.

---
+++ pkg/asset/installconfig/aws/iam.go
@@ -0,0 +1,60 @@
+package aws
+
+import (
+	"github.com/openshift/installer/pkg/types"
+	"github.com/openshift/installer/pkg/types/aws"
+)
+
+// IncludesExistingInstanceRole checks if at least one BYO instance role is included in the install-config.
+func IncludesExistingInstanceRole(installConfig *types.InstallConfig) bool {
+	{
+		mpool := aws.MachinePool{}
+		mpool.Set(installConfig.AWS.DefaultMachinePlatform)
+		if mp := installConfig.ControlPlane; mp != nil {
+			mpool.Set(mp.Platform.AWS)
+		}
+		if len(mpool.IAMRole) > 0 {
+			return true
+		}
+	}
+
+	for _, compute := range installConfig.Compute {
+		mpool := aws.MachinePool{}
+		mpool.Set(installConfig.AWS.DefaultMachinePlatform)
+		mpool.Set(compute.Platform.AWS)
+		if len(mpool.IAMRole) > 0 {
+			return true
+		}
+	}
+
+	// If compute stanza is not defined, we know it'll inherit the value from DefaultMachinePlatform
+	mpool := installConfig.AWS.DefaultMachinePlatform
+	return mpool != nil && len(mpool.IAMRole) > 0
+}
+
+// IncludesCreateInstanceRole checks if at least one instance role will be created by the installer.
+func IncludesCreateInstanceRole(installConfig *types.InstallConfig) bool {
+	{
+		mpool := aws.MachinePool{}
+		mpool.Set(installConfig.AWS.DefaultMachinePlatform)
+		if mp := installConfig.ControlPlane; mp != nil {
+			mpool.Set(mp.Platform.AWS)
+		}
+		if len(mpool.IAMRole) == 0 {
+			return true
+		}
+	}
+
+	for _, compute := range installConfig.Compute {
+		mpool := aws.MachinePool{}
+		mpool.Set(installConfig.AWS.DefaultMachinePlatform)
+		mpool.Set(compute.Platform.AWS)
+		if len(mpool.IAMRole) == 0 {
+			return true
+		}
+	}
+
+	// If compute stanza is not defined, we know it'll inherit the value from DefaultMachinePlatform
+	mpool := installConfig.AWS.DefaultMachinePlatform
+	return mpool == nil || len(mpool.IAMRole) == 0
+}
