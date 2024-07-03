7288685bccdda90075ddbeb75d4c0508b9a31d2d
Michail Resvanis <mresvani@redhat.com>
2024-07-03 09:48:44 +0200

    Add imagebased installation ISO assets
    Signed-off-by: Michail Resvanis <mresvani@redhat.com>

---
+++ pkg/asset/imagebased/image/imagebased_config_test.go
@@ -0,0 +1,396 @@
+package image
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
+
+	aiv1beta1 "github.com/openshift/assisted-service/api/v1beta1"
+	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/asset/mock"
+	"github.com/openshift/installer/pkg/types"
+	"github.com/openshift/installer/pkg/types/imagebased"
+)
+
+var (
+	rawNMStateConfig = `
+interfaces:
+  - name: eth0
+    type: ethernet
+    state: up
+    mac-address: 00:00:00:00:00:00
+    ipv4:
+      enabled: true
+      address:
+        - ip: 192.168.122.2
+          prefix-length: 23
+      dhcp: false`
+)
+
+func TestImageBasedInstallConfig_LoadedFromDisk(t *testing.T) {
+	skipTestIfnmstatectlIsMissing(t)
+
+	cases := []struct {
+		name       string
+		data       string
+		fetchError error
+
+		expectedError  string
+		expectedFound  bool
+		expectedConfig *ImageBasedInstallationConfigBuilder
+	}{
+		{
+			name: "valid-config-single-node",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+seedVersion: 4.16.0
+installationDisk: /dev/vda
+networkConfig:
+  interfaces:
+    - name: eth0
+      type: ethernet
+      state: up
+      mac-address: 00:00:00:00:00:00
+      ipv4:
+        enabled: true
+        address:
+          - ip: 192.168.122.2
+            prefix-length: 23
+        dhcp: false
+`,
+
+			expectedFound:  true,
+			expectedConfig: ibiConfig(),
+		},
+		{
+			name: "not-yaml",
+			data: `This is not a yaml file`,
+
+			expectedFound: false,
+			expectedError: "failed to unmarshal image-based-installation-config.yaml: error unmarshaling JSON: while decoding JSON: json: cannot unmarshal string into Go value of type imagebased.InstallationConfig",
+		},
+		{
+			name:       "file-not-found",
+			fetchError: &os.PathError{Err: os.ErrNotExist},
+
+			expectedFound: false,
+		},
+		{
+			name:       "error-fetching-file",
+			fetchError: errors.New("fetch failed"),
+
+			expectedFound: false,
+			expectedError: "failed to load image-based-installation-config.yaml file: fetch failed",
+		},
+		{
+			name: "unknown-field",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+wrongField: wrongValue`,
+
+			expectedFound: false,
+			expectedError: "failed to unmarshal image-based-installation-config.yaml: error unmarshaling JSON: while decoding JSON: json: unknown field \"wrongField\"",
+		},
+		{
+			name: "empty-pullSecret",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: pullSecret: Required value: you must specify a pullSecret",
+		},
+		{
+			name: "invalid-pullSecret",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"missing\":\"auths\"}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: pullSecret: Invalid value: \"{\\\"missing\\\":\\\"auths\\\"}\": auths required",
+		},
+		{
+			name: "invalid-sshKey",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda
+sshKey: invalid_ssh_key`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: sshKey: Invalid value: \"invalid_ssh_key\": ssh: no key found",
+		},
+		{
+			name: "empty-seedVersion",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: seedVersion: Required value: you must specify a seedVersion",
+		},
+		{
+			name: "empty-seedImage",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+installationDisk: /dev/vda`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: seedImage: Required value: you must specify a seedImage",
+		},
+		{
+			name: "empty-installationDisk",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedImage: "quay.io/openshift-kni/seed-image:4.16.0"
+seedVersion: "4.16.0"`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: installationDisk: Required value: you must specify an installationDisk",
+		},
+		{
+			name: "invalid-additionalTrustBundle",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda
+additionalTrustBundle: invalid_cert
+`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: additionalTrustBundle: Invalid value: \"invalid_cert\": invalid block",
+		},
+		{
+			name: "invalid-networkConfig",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda
+networkConfig:
+  invalid: config
+`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: networkConfig: Invalid value: invalid: config\n: failed to execute 'nmstatectl gc', error: InvalidArgument: Invalid YAML string: unknown field `invalid`, expected one of `hostname`, `dns-resolver`, `route-rules`, `routes`, `interfaces`, `ovs-db`, `ovn`",
+		},
+		{
+			name: "invalid-imageDigestSources",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda
+imageDigestSources:
+- source: quay.io
+  mirrors:
+  - Registry.lab.redhat.com:5000
+`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: imageDigestSources[0].mirrors[0]: Invalid value: \"Registry.lab.redhat.com:5000\": failed to parse: invalid reference format: repository name must be lowercase",
+		},
+		{
+			name: "invalid-proxy-schemes",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda
+proxy:
+  httpProxy: ""
+  httpsProxy: ""
+  noProxy: ""
+`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: proxy: Required value: must include httpProxy or httpsProxy",
+		},
+		{
+			name: "invalid-proxy-http-uri",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda
+proxy:
+  httpProxy: "invalidscheme://"
+  httpsProxy: ""
+  noProxy: ""
+`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: proxy.httpProxy: Unsupported value: \"invalidscheme\": supported values: \"http\"",
+		},
+		{
+			name: "invalid-proxy-https-uri",
+			data: `
+apiVersion: v1beta1
+metadata:
+  name: image-based-installation-config
+pullSecret: "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}"
+seedVersion: 4.16.0
+seedImage: quay.io/openshift-kni/seed-image:4.16.0
+installationDisk: /dev/vda
+proxy:
+  httpProxy: ""
+  httpsProxy: "invalidscheme://"
+  noProxy: ""
+`,
+
+			expectedFound: false,
+			expectedError: "invalid Image-based Installation ISO Config: proxy.httpsProxy: Unsupported value: \"invalidscheme\": supported values: \"http\", \"https\"",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			fileFetcher := mock.NewMockFileFetcher(mockCtrl)
+			fileFetcher.EXPECT().FetchByName(configFilename).
+				Return(
+					&asset.File{
+						Filename: configFilename,
+						Data:     []byte(tc.data)},
+					tc.fetchError,
+				)
+
+			asset := &ImageBasedInstallationConfig{}
+			found, err := asset.Load(fileFetcher)
+			assert.Equal(t, tc.expectedFound, found)
+			if tc.expectedError != "" {
+				assert.Equal(t, tc.expectedError, err.Error())
+			} else {
+				assert.NoError(t, err)
+				if tc.expectedConfig != nil {
+					assert.Equal(t, tc.expectedConfig.build(), asset.Config, "unexpected Config in ImageBasedInstallConfig")
+				}
+			}
+		})
+	}
+}
+
+// ImageBasedInstallationConfigBuilder it's a builder class to make it easier
+// creating imagebased.InstallationConfig instance used in the test cases.
+type ImageBasedInstallationConfigBuilder struct {
+	imagebased.InstallationConfig
+}
+
+func ibiConfig() *ImageBasedInstallationConfigBuilder {
+	return &ImageBasedInstallationConfigBuilder{
+		InstallationConfig: imagebased.InstallationConfig{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "image-based-installation-config",
+			},
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: imagebased.ImageBasedConfigVersion,
+			},
+			SeedImage:            "quay.io/openshift-kni/seed-image:4.16.0",
+			SeedVersion:          "4.16.0",
+			InstallationDisk:     "/dev/vda",
+			ExtraPartitionStart:  "-40G",
+			ExtraPartitionLabel:  "varlibcontainers",
+			ExtraPartitionNumber: 5,
+			Shutdown:             false,
+			SSHKey:               "",
+			PullSecret:           "{\"auths\":{\"example.com\":{\"auth\":\"c3VwZXItc2VjcmV0Cg==\"}}}",
+			NetworkConfig: aiv1beta1.NetConfig{
+				Raw: unmarshalJSON([]byte(rawNMStateConfig)),
+			},
+		},
+	}
+}
+
+func (icb *ImageBasedInstallationConfigBuilder) build() *imagebased.InstallationConfig {
+	return &icb.InstallationConfig
+}
+
+func (icb *ImageBasedInstallationConfigBuilder) additionalTrustBundle(atb string) *ImageBasedInstallationConfigBuilder {
+	icb.InstallationConfig.AdditionalTrustBundle = atb
+	return icb
+}
+
+func (icb *ImageBasedInstallationConfigBuilder) networkConfig(nc aiv1beta1.NetConfig) *ImageBasedInstallationConfigBuilder {
+	icb.InstallationConfig.NetworkConfig = nc
+	return icb
+}
+
+func (icb *ImageBasedInstallationConfigBuilder) imageDigestSources(ids []types.ImageDigestSource) *ImageBasedInstallationConfigBuilder {
+	icb.InstallationConfig.ImageDigestSources = ids
+	return icb
+}
+
+func (icb *ImageBasedInstallationConfigBuilder) ignitionConfigOverride(ignitionConfigOverride string) *ImageBasedInstallationConfigBuilder {
+	icb.InstallationConfig.IgnitionConfigOverride = ignitionConfigOverride
+	return icb
+}
+
+func unmarshalJSON(b []byte) []byte {
+	output, err := yaml.JSONToYAML(b)
+	if err != nil {
+		return nil
+	}
+	return output
+}
+
+func skipTestIfnmstatectlIsMissing(t *testing.T) {
+	t.Helper()
+
+	_, execErr := exec.LookPath("nmstatectl")
+	if execErr != nil {
+		t.Skip("No nmstatectl binary available")
+	}
+}
---
+++ pkg/asset/imagebased/image/postdeployment_test.go
@@ -0,0 +1,80 @@
+package image
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/asset/mock"
+)
+
+func TestPostDeployment_Load(t *testing.T) {
+	cases := []struct {
+		name       string
+		fetchError error
+
+		expectedFound bool
+		expectedFile  string
+		expectedError string
+	}{
+		{
+			name:       "no-post-deployment-script",
+			fetchError: os.ErrNotExist,
+
+			expectedFound: false,
+		},
+		{
+			name: "post-deployment-script",
+
+			expectedFound: true,
+			expectedFile:  "post.sh",
+		},
+		{
+			name:       "error",
+			fetchError: os.ErrPermission,
+
+			expectedError: "failed to load the post.sh file: permission denied",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assetFile := &asset.File{
+				Filename: postDeploymentFilename,
+				Data:     []byte("test-bash-script"),
+			}
+
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			fileFetcher := mock.NewMockFileFetcher(mockCtrl)
+
+			if tc.fetchError != nil {
+				fileFetcher.EXPECT().FetchByName("post.sh").Return(nil, tc.fetchError)
+			} else {
+				fileFetcher.EXPECT().FetchByName("post.sh").Return(assetFile, nil)
+			}
+
+			postDeploymentAsset := &PostDeployment{}
+			found, err := postDeploymentAsset.Load(fileFetcher)
+
+			assert.Equal(t, tc.expectedFound, found)
+
+			if tc.expectedError != "" {
+				assert.Equal(t, tc.expectedError, err.Error())
+			} else {
+				assert.NoError(t, err)
+
+				if tc.expectedFile != "" {
+					assert.True(t,
+						tc.expectedFile == postDeploymentAsset.File.Filename,
+						fmt.Sprintf("Expected file %s not found", tc.expectedFile))
+				}
+			}
+		})
+	}
+}
