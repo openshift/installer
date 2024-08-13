562fa375ac1369bb8e36bb7877cc3b585478847b
John Hixson <jhixson@redhat.com>
2024-08-13 14:45:42 -0700

    pkg/infrastructure/azure: limit storage account to 24 characters or less
    https://issues.redhat.com/browse/OCPBUGS-37953

--- pkg/infrastructure/azure/azure.go
+++ pkg/infrastructure/azure/azure.go
@@ -281,7 +281,7 @@ func (p *Provider) InfraReady(ctx context.Context, in clusterapi.InfraReadyInput
 	}
 
 	resourceGroupName := p.ResourceGroupName
-	storageAccountName := fmt.Sprintf("%ssa", strings.ReplaceAll(in.InfraID, "-", ""))
+	storageAccountName := getStorageAccountName(in.InfraID)
 	containerName := "vhd"
 	blobName := fmt.Sprintf("rhcos%s.vhd", randomString(5))
 
@@ -761,6 +761,19 @@ func getControlPlaneIDs(cl client.Client, replicas *int64, infraID string) ([]st
 		return nil, fmt.Errorf("%s .Spec.ProviderID is empty", bootstrapName)
 	}
 	return res, nil
+}
+
+// Storage account names can't be more than 24 characters.
+func getStorageAccountName(infraID string) string {
+	storageAccountNameMax := 24
+
+	storageAccountName := strings.ReplaceAll(infraID, "-", "")
+	if len(storageAccountName) > storageAccountNameMax-2 {
+		storageAccountName = storageAccountName[:storageAccountNameMax-2]
+	}
+	storageAccountName = fmt.Sprintf("%ssa", storageAccountName)
+
+	return storageAccountName
 }
 
 func randomString(length int) string {
