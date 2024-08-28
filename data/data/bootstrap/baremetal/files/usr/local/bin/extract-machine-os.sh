7ec3c30ed2e1d6f041f456e740d1e2cddb66ffaf
Zane Bitter <zbitter@redhat.com>
2024-08-28 15:29:39 +1200

    Move extract-machine-os command to a script

---
+++ data/data/bootstrap/baremetal/files/usr/local/bin/extract-machine-os.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eu
+
+CID_FILE="$1"
+
+podman run --rm --name machine-os-extractor --cidfile="${CID_FILE}" --sdnotify=ignore --log-driver=passthrough --env IP_OPTIONS="${PROVISIONING_IP_OPTIONS}" -v systemd-ironic:/shared:z "${MACHINE_OS_IMAGES_IMAGE}" /bin/copy-metal --all /shared/html/images/
