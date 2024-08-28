3e65dd94d5a32fad44074392c698aadf389cc292
Zane Bitter <zbitter@redhat.com>
2024-08-28 15:32:27 +1200

    Handle failures to extract machine OS
    If a oneshot service fails, it will retry but services that depend on it
    will not be restarted. Instead, convert extract-machine-os to a notify
    script so that it will be retried until it succeeds.

--- data/data/bootstrap/baremetal/files/usr/local/bin/extract-machine-os.sh
+++ data/data/bootstrap/baremetal/files/usr/local/bin/extract-machine-os.sh
@@ -4,4 +4,6 @@ set -eu
 
 CID_FILE="$1"
 
-podman run --rm --name machine-os-extractor --cidfile="${CID_FILE}" --sdnotify=ignore --log-driver=passthrough --env IP_OPTIONS="${PROVISIONING_IP_OPTIONS}" -v systemd-ironic:/shared:z "${MACHINE_OS_IMAGES_IMAGE}" /bin/copy-metal --all /shared/html/images/
+podman run --rm --name machine-os-extractor --cidfile="${CID_FILE}" --cgroups=no-conmon --sdnotify=ignore --log-driver=passthrough --env IP_OPTIONS="${PROVISIONING_IP_OPTIONS}" -v systemd-ironic:/shared:z "${MACHINE_OS_IMAGES_IMAGE}" /bin/copy-metal --all /shared/html/images/
+
+systemd-notify --ready
