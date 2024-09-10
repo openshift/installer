8ccea8acbf93b70d42e5199cedf7be3ee9095286
Zane Bitter <zbitter@redhat.com>
2024-09-10 14:46:20 +1200

    Retry extracting Machine OS on failure
    If e.g. a network interruption causes a failure to extract the machine
    OS, try again. (Retries at the systemd level do not help, because
    dependent services will not start, so it must be retried within the
    script.)

--- data/data/bootstrap/baremetal/files/usr/local/bin/extract-machine-os.sh
+++ data/data/bootstrap/baremetal/files/usr/local/bin/extract-machine-os.sh
@@ -4,4 +4,6 @@ set -eu
 
 CID_FILE="$1"
 
-podman run --rm --name machine-os-extractor --cidfile="${CID_FILE}" --cgroups=no-conmon --log-driver=passthrough --env IP_OPTIONS="${PROVISIONING_IP_OPTIONS}" -v systemd-ironic:/shared:z "${MACHINE_OS_IMAGES_IMAGE}" /bin/copy-metal --all /shared/html/images/
+while ! podman run --rm --name machine-os-extractor --cidfile="${CID_FILE}" --cgroups=no-conmon --log-driver=passthrough --env IP_OPTIONS="${PROVISIONING_IP_OPTIONS}" -v systemd-ironic:/shared:z "${MACHINE_OS_IMAGES_IMAGE}" /bin/copy-metal --all /shared/html/images/; do
+    sleep 5
+done
