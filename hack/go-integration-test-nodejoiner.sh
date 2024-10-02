8d5de32688911ce4065e902fab7f58693e2224c4
Andrea Fasano <afasano@redhat.com>
2024-10-02 10:38:51 -0400

    allow node-joiner hack script to run in the CI

--- hack/go-integration-test-nodejoiner.sh
+++ hack/go-integration-test-nodejoiner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Example:  ./hack/go-integration-test-nodejoiner.sh
 
-go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+go install -mod=mod sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 # shellcheck disable=SC2086
 KUBEBUILDER_ASSETS="$($GOPATH/bin/setup-envtest use 1.31.0 -p path --bin-dir /tmp)" go test -timeout 0 -run .Integration ./cmd/node-joiner/... "${@}"
 
