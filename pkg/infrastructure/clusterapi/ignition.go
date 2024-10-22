c7355cb6315557f4cfda882f906ade9bab05761b
Sandhya Dasu <sdasu@redhat.com>
2024-10-22 16:12:04 -0400

    CAPI: Update Master pointer Ignition with API-Int IP
    When userProvisionedDNS(custom-dns) is configured, the Master
    Pointer Ignition needs to be updated with the API-INT IP in place
    of the API-Int URL because at this stage in-cluster DNS is not
    running yet and the full Ignition needs to be downloaded.

--- pkg/infrastructure/clusterapi/ignition.go
+++ pkg/infrastructure/clusterapi/ignition.go
@@ -4,12 +4,17 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"net"
+	"net/url"
 	"strings"
 
+	ignutil "github.com/coreos/ignition/v2/config/util"
 	igntypes "github.com/coreos/ignition/v2/config/v3_2/types"
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/yaml"
 
 	configv1 "github.com/openshift/api/config/v1"
+	mcfgv1 "github.com/openshift/api/machineconfiguration/v1"
 	"github.com/openshift/installer/cmd/openshift-install/command"
 	"github.com/openshift/installer/pkg/asset"
 	"github.com/openshift/installer/pkg/asset/lbconfig"
@@ -18,7 +23,8 @@ import (
 )
 
 const (
-	infrastructureFilepath = "/opt/openshift/manifests/cluster-infrastructure-02-config.yml"
+	infrastructureFilepath      = "/opt/openshift/manifests/cluster-infrastructure-02-config.yml"
+	masterMachineConfigFilePath = "/opt/openshift/openshift/99_openshift-machine-api_master-control-plane-machine-set.yaml"
 
 	// replaceable is the string that precedes the encoded data in the ignition data.
 	// The data must be replaced before decoding the string, and the string must be
@@ -29,16 +35,16 @@ const (
 // EditIgnition attempts to edit the contents of the bootstrap ignition when the user has selected
 // a custom DNS configuration. Find the public and private load balancer addresses and fill in the
 // infrastructure file within the ignition struct.
-func EditIgnition(in IgnitionInput, platform string, publicIPAddresses, privateIPAddresses []string) ([]byte, error) {
+func EditIgnition(in IgnitionInput, platform string, publicIPAddresses, privateIPAddresses []string) ([]byte, []byte, error) {
 	ignData := &igntypes.Config{}
 	err := json.Unmarshal(in.BootstrapIgnData, ignData)
 	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal bootstrap ignition: %w", err)
+		return nil, nil, fmt.Errorf("failed to unmarshal bootstrap ignition: %w", err)
 	}
 
-	err = AddLoadBalancersToInfra(platform, ignData, publicIPAddresses, privateIPAddresses)
+	err = addLoadBalancersToInfra(platform, ignData, publicIPAddresses, privateIPAddresses)
 	if err != nil {
-		return nil, fmt.Errorf("failed to add load balancers to ignition config: %w", err)
+		return nil, nil, fmt.Errorf("failed to add load balancers to ignition config: %w", err)
 	}
 
 	publicIPsSingleStr := strings.Join(publicIPAddresses, ",")
@@ -46,24 +52,34 @@ func EditIgnition(in IgnitionInput, platform string, publicIPAddresses, privateI
 
 	lbConfig, err := lbconfig.GenerateLBConfigOverride(privateIPsSingleStr, publicIPsSingleStr)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	if err := asset.NewDefaultFileWriter(lbConfig).PersistToFile(command.RootOpts.Dir); err != nil {
-		return nil, fmt.Errorf("failed to save %s to state file: %w", lbConfig.Name(), err)
+		return nil, nil, fmt.Errorf("failed to save %s to state file: %w", lbConfig.Name(), err)
+	}
+
+	editedMasterIgn, err := updatePointerIgnition(in, privateIPAddresses, "master")
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to update Master Ignition with API-Int IP: %w", err)
+	}
+
+	err = updatePointerMachineConfig(ignData, editedMasterIgn)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to update Master Machine Config within Bootstrap Ignition: %w", err)
 	}
 
 	editedIgnBytes, err := json.Marshal(ignData)
 	if err != nil {
-		return nil, fmt.Errorf("failed to convert ignition data to json: %w", err)
+		return nil, nil, fmt.Errorf("failed to convert ignition data to json: %w", err)
 	}
 
-	return editedIgnBytes, nil
+	return editedIgnBytes, editedMasterIgn, nil
 }
 
 // AddLoadBalancersToInfra will load the public and private load balancer information into
 // the infrastructure CR. This will occur after the data has already been inserted into the
 // ignition file.
-func AddLoadBalancersToInfra(platform string, config *igntypes.Config, publicLBs []string, privateLBs []string) error {
+func addLoadBalancersToInfra(platform string, config *igntypes.Config, publicLBs []string, privateLBs []string) error {
 	for i, fileData := range config.Storage.Files {
 		// update the contents of this file
 		if fileData.Path == infrastructureFilepath {
@@ -121,5 +137,74 @@ func AddLoadBalancersToInfra(platform string, config *igntypes.Config, publicLBs
 		}
 	}
 
+	return nil
+}
+
+func updatePointerIgnition(in IgnitionInput, privateLBs []string, role string) ([]byte, error) {
+	ignData := &igntypes.Config{}
+	err := json.Unmarshal(in.MasterIgnData, ignData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal master ignition: %w", err)
+	}
+
+	if len(privateLBs) == 0 {
+		return nil, fmt.Errorf("no private load balancer ip address found")
+	}
+	ignitionHost := net.JoinHostPort(privateLBs[0], "22623")
+
+	ignitionConfig := igntypes.IgnitionConfig{
+		Merge: []igntypes.Resource{{
+			Source: ignutil.StrToPtr(func() *url.URL {
+				return &url.URL{
+					Scheme: "https",
+					Host:   ignitionHost,
+					Path:   fmt.Sprintf("/config/%s", role),
+				}
+			}().String()),
+		}},
+	}
+
+	ignData.Ignition.Config = ignitionConfig
+	updatedIgnBytes, err := json.Marshal(ignData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert %s ignition data to json: %w", role, err)
+	}
+	return updatedIgnBytes, nil
+}
+
+func updatePointerMachineConfig(bootstrapIgnConfig *igntypes.Config, pointerIgn []byte) error {
+	for i, fileData := range bootstrapIgnConfig.Storage.Files {
+		// update the contents of this file
+		if fileData.Path == masterMachineConfigFilePath {
+			contents := bootstrapIgnConfig.Storage.Files[i].Contents.Source
+			replaced := strings.Replace(*contents, replaceable, "", 1)
+
+			rawDecodedText, err := base64.StdEncoding.DecodeString(replaced)
+			if err != nil {
+				return fmt.Errorf("failed to decode contents of bootstrap ignition file: %w", err)
+			}
+			machineConfig := &mcfgv1.MachineConfig{}
+			if err := yaml.Unmarshal(rawDecodedText, machineConfig); err != nil {
+				return fmt.Errorf("failed to unmarshal Master Machine Config within Bootstrap Ignition: %w", err)
+			}
+			if pointerIgn != nil {
+				rawExt := runtime.RawExtension{
+					Raw: pointerIgn,
+				}
+				machineConfig.Spec.Config = rawExt
+			}
+			// convert Master Machine Config back to an encoded string
+			masterConfigContents, err := yaml.Marshal(machineConfig)
+			if err != nil {
+				return fmt.Errorf("failed to marshal Master Machine Config: %w", err)
+			}
+
+			encoded := fmt.Sprintf("%s%s", replaceable, base64.StdEncoding.EncodeToString(masterConfigContents))
+			// replace the contents with the edited information
+			bootstrapIgnConfig.Storage.Files[i].Contents.Source = &encoded
+
+			break
+		}
+	}
 	return nil
 }
