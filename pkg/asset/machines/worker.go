ee09b69d8113f2dfcf609ca5369cb26f721b1b3c
Zane Bitter <zbitter@redhat.com>
2024-11-13 20:34:52 +1300

    Used shared session for Machine API Azure platform validations
    Don't create a new client for each validating each machine type, instead
    use the common one that is used to valiate the install-config and cache
    it.
    
    (cherry picked from commit 1f4b8233e6ad08f4eb0deac50221dbb9355f4a32)

--- pkg/asset/machines/worker.go
+++ pkg/asset/machines/worker.go
@@ -31,7 +31,6 @@ import (
 	"github.com/openshift/installer/pkg/asset/ignition/machine"
 	"github.com/openshift/installer/pkg/asset/installconfig"
 	icaws "github.com/openshift/installer/pkg/asset/installconfig/aws"
-	icazure "github.com/openshift/installer/pkg/asset/installconfig/azure"
 	icgcp "github.com/openshift/installer/pkg/asset/installconfig/gcp"
 	"github.com/openshift/installer/pkg/asset/machines/aws"
 	"github.com/openshift/installer/pkg/asset/machines/azure"
@@ -448,12 +447,11 @@ func (w *Worker) Generate(dependencies asset.Parents) error {
 			mpool.Set(ic.Platform.Azure.DefaultMachinePlatform)
 			mpool.Set(pool.Platform.Azure)
 
-			session, err := installConfig.Azure.Session()
+			client, err := installConfig.Azure.Client()
 			if err != nil {
-				return errors.Wrap(err, "failed to fetch session")
+				return err
 			}
 
-			client := icazure.NewClient(session)
 			if len(mpool.Zones) == 0 {
 				azs, err := client.GetAvailabilityZones(context.TODO(), ic.Platform.Azure.Region, mpool.InstanceType)
 				if err != nil {
