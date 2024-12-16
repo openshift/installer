233ea8505c88a6e0252768773fe4f1b270c2f353
Patrick Dillon <padillon@redhat.com>
2024-12-16 14:59:44 -0500

    pkg/rhcos: merge marketplace with coreos stream
    Reworks the logic for fetching the coreos stream to merge a
    marketplace.json file (if it exists) with the (rhcos|fcos).json
    file before producing the results.

--- pkg/rhcos/stream.go
+++ pkg/rhcos/stream.go
@@ -5,3 +5,7 @@ package rhcos
 func getStreamFileName() string {
 	return "coreos/rhcos.json"
 }
+
+func getMarketplaceStreamFileName() string {
+	return "coreos/marketplace-rhcos.json"
+}
--- pkg/rhcos/stream_scos.go
+++ pkg/rhcos/stream_scos.go
@@ -5,3 +5,10 @@ package rhcos
 func getStreamFileName() string {
 	return "coreos/scos.json"
 }
+
+func getMarketplaceStreamFileName() string {
+	// There is no current need for scos marketplace images,
+	// so this file does not currently exist. The calling
+	// functions will gracefully handle the missing file.
+	return "coreos/marketplace-scos.json"
+}
