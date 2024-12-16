bc46191d3b857c4d3c0b797aeff5425e94486e96
Patrick Dillon <padillon@redhat.com>
2024-12-16 12:29:51 -0500

    hack: add marketplace stream to coreos manifest
    Updates the generation of the coreos stream manifest, which runs
    during container building, to merge the contents of the
    marketplace-rhcos.json file with rhcos.json when creating the coreos
    stream configmap.

--- hack/build-coreos-manifest.go
+++ hack/build-coreos-manifest.go
@@ -7,37 +7,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/coreos/stream-metadata-go/stream"
+	"github.com/coreos/stream-metadata-go/stream/rhcos"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
 )
 
 const (
-	streamRHCOSJSON = "data/data/coreos/rhcos.json"
-	streamFCOSJSON  = "data/data/coreos/fcos.json"
-	streamSCOSJSON  = "data/data/coreos/scos.json"
-	fcosTAG         = "okd"
-	scosTAG         = "scos"
-	dest            = "bin/manifests/coreos-bootimages.yaml"
+	streamRHCOSJSON            = "data/data/coreos/rhcos.json"
+	streamFCOSJSON             = "data/data/coreos/fcos.json"
+	streamSCOSJSON             = "data/data/coreos/scos.json"
+	streamMarketplaceRHCOSJSON = "data/data/coreos/marketplace-rhcos.json"
+	fcosTAG                    = "okd"
+	scosTAG                    = "scos"
+	dest                       = "bin/manifests/coreos-bootimages.yaml"
 )
 
 func run() error {
-	var streamJSON string
-	tags, _ := os.LookupEnv("TAGS")
-	switch {
-	case strings.Contains(tags, fcosTAG):
-		streamJSON = streamFCOSJSON
-	case strings.Contains(tags, scosTAG):
-		streamJSON = streamSCOSJSON
-	default:
-		streamJSON = streamRHCOSJSON
-	}
-	bootimages, err := os.ReadFile(streamJSON)
+	bootimages, err := getBootImages()
 	if err != nil {
 		return err
 	}
@@ -77,6 +71,67 @@ func run() error {
 	}
 
 	return nil
+}
+
+func getBootImages() ([]byte, error) {
+	var okd bool
+	var streamJSON string
+	tags, _ := os.LookupEnv("TAGS")
+	switch {
+	case strings.Contains(tags, fcosTAG):
+		streamJSON = streamFCOSJSON
+		okd = true
+	case strings.Contains(tags, scosTAG):
+		streamJSON = streamSCOSJSON
+		okd = true
+	default:
+		streamJSON = streamRHCOSJSON
+	}
+
+	bootimages, err := os.ReadFile(streamJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	if okd {
+		// okd does not yet have marketplace images, so we are done
+		return bootimages, nil
+	}
+
+	return mergeMarketplaceStream(bootimages)
+}
+
+type marketplaceStream map[string]*rhcos.Marketplace
+
+func mergeMarketplaceStream(streamJSON []byte) ([]byte, error) {
+	mktStream := marketplaceStream{}
+	mktJSON, err := os.ReadFile(streamMarketplaceRHCOSJSON)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open marketplace file: %w", err)
+	}
+	if err := json.Unmarshal(mktJSON, &mktStream); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal market stream: %w", err)
+	}
+
+	stream := stream.Stream{}
+	if err := json.Unmarshal(streamJSON, &stream); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal boot image stream: %w", err)
+	}
+
+	for name, arch := range stream.Architectures {
+		if mkt, ok := mktStream[name]; ok {
+			if arch.RHELCoreOSExtensions == nil {
+				arch.RHELCoreOSExtensions = &rhcos.Extensions{}
+			}
+			arch.RHELCoreOSExtensions.Marketplace = mkt
+		}
+	}
+
+	bootImgs, err := json.Marshal(stream)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal merged boot image stream: %w", err)
+	}
+	return bootImgs, nil
 }
 
 func main() {
