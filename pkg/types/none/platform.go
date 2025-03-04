fb01fcc85a92c2aa475d683d4301d918f6668561
Michael Shitrit <mshitrit@redhat.com>
2025-03-04 11:10:46 +0200

    Add fencing credentials to platform none
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- pkg/types/none/platform.go
+++ pkg/types/none/platform.go
@@ -1,5 +1,11 @@
 package none
 
+import "github.com/openshift/installer/pkg/types/common"
+
 // Platform stores any global configuration used for generic
 // platforms.
-type Platform struct{}
+type Platform struct {
+	// FencingCredentials stores the information about a baremetal host's management controller.
+	// +optional
+	FencingCredentials []*common.FencingCredential `json:"fencingCredentials,omitempty"`
+}
