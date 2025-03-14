c4b13bda3ba2d7838439529c9b4e5b050878c43b
Mark Hamzy <hamzy@us.ibm.com>
2025-03-14 16:12:29 -0500

    PowerVS: remove TF code
    Remove the PowerVS Terraform code path as we no longer support it.

--- data/data/powervs/cluster/master/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "master_ips" {
-  value = module.vm.master_ips
-}
-
--- data/data/powervs/cluster/power_network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dhcp_id" {
-  value = data.ibm_pi_dhcp.dhcp_service.dhcp_id
-}
-
-output "dhcp_network_id" {
-  value = data.ibm_pi_dhcp.dhcp_service.network_id
-}
