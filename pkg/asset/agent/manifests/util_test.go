25363f4b4d9a1a7257dfc35d0e515d596ab3690a
Zane Bitter <zbitter@redhat.com>
2025-03-25 11:13:15 +1300

    unit tests: Check return values
    The linter is not happy.

--- pkg/asset/agent/manifests/util_test.go
+++ pkg/asset/agent/manifests/util_test.go
@@ -69,8 +69,14 @@ var (
 
 // GetValidOptionalInstallConfig returns a valid optional install config
 func getValidOptionalInstallConfig() *agent.OptionalInstallConfig {
-	_, newCidr, _ := net.ParseCIDR("192.168.96.0/20")
-	_, machineNetCidr, _ := net.ParseCIDR("10.10.11.0/24")
+	_, newCidr, err := net.ParseCIDR("192.168.96.0/20")
+	if err != nil {
+		panic(err)
+	}
+	_, machineNetCidr, err := net.ParseCIDR("10.10.11.0/24")
+	if err != nil {
+		panic(err)
+	}
 
 	return &agent.OptionalInstallConfig{
 		AssetBase: installconfig.AssetBase{
@@ -128,10 +134,22 @@ func getValidOptionalInstallConfig() *agent.OptionalInstallConfig {
 
 // GetValidOptionalInstallConfigDualStack returns a valid optional install config for dual stack
 func getValidOptionalInstallConfigDualStack() *agent.OptionalInstallConfig {
-	_, newCidr, _ := net.ParseCIDR("192.168.96.0/20")
-	_, newCidrIPv6, _ := net.ParseCIDR("2001:db8:1111::/48")
-	_, machineNetCidr, _ := net.ParseCIDR("10.10.11.0/24")
-	_, machineNetCidrIPv6, _ := net.ParseCIDR("2001:db8:5dd8:c956::/64")
+	_, newCidr, err := net.ParseCIDR("192.168.96.0/20")
+	if err != nil {
+		panic(err)
+	}
+	_, newCidrIPv6, err := net.ParseCIDR("2001:db8:1111::/48")
+	if err != nil {
+		panic(err)
+	}
+	_, machineNetCidr, err := net.ParseCIDR("10.10.11.0/24")
+	if err != nil {
+		panic(err)
+	}
+	_, machineNetCidrIPv6, err := net.ParseCIDR("2001:db8:5dd8:c956::/64")
+	if err != nil {
+		panic(err)
+	}
 
 	return &agent.OptionalInstallConfig{
 		AssetBase: installconfig.AssetBase{
