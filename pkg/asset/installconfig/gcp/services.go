09d86550c36305a6286631f36ee79a25ef29e552
barbacbd <barbacbd@gmail.com>
2025-04-22 13:51:53 -0400

    CORS-3916: Update Installconfig to format and accept service endpoints
    ** Accept service endpoints through the install config
    ** Service Endpoints should be entered in a format such as
    https://compute-exampleendpoint.p.googleapis.com
    and the path will be added by the installer to be something like
    https://compute-exampleendpoint.p.googleapis.com/compute/v1/.
    ** The endpoints are formatted to ensure that the version is correct. If the
    user would provide a version such as v2 when v1 is required, it would be difficult
    for the installer to provide useful errors.

--- pkg/asset/installconfig/gcp/services.go
+++ pkg/asset/installconfig/gcp/services.go
@@ -3,6 +3,7 @@ package gcp
 import (
 	"context"
 	"fmt"
+	"net/url"
 
 	"google.golang.org/api/cloudresourcemanager/v3"
 	"google.golang.org/api/compute/v1"
@@ -15,6 +16,71 @@ import (
 
 	configv1 "github.com/openshift/api/config/v1"
 )
+
+type ServiceNameGCP string
+type ServiceVersionGCP string
+
+const (
+	CloudResourceManagerNameGCP ServiceNameGCP = "cloudresourcemanager"
+	ComputeServiceNameGCP       ServiceNameGCP = "compute"
+	ContainerServiceNameGCP     ServiceNameGCP = "container"
+	DNSServiceNameGCP           ServiceNameGCP = "dns"
+	FileServiceNameGCP          ServiceNameGCP = "file"
+	IAMServiceNameGCP           ServiceNameGCP = "iam"
+	ServiceUsageNameGCP         ServiceNameGCP = "serviceusage"
+	StorageServiceNameGCP       ServiceNameGCP = "storage"
+
+	ServiceVersionGCP1 ServiceVersionGCP = "v1"
+	ServiceVersionGCP3 ServiceVersionGCP = "v3"
+	// TODO: should this be betav1
+	ServiceVersionGCPBeta ServiceVersionGCP = "beta"
+)
+
+// FormatGCPEndpointList will format the list of GCP Service Endpoints to match the expected url
+// for WithEndpoint or BasePath override endpoint options
+func FormatGCPEndpointList(endpoints []configv1.GCPServiceEndpoint) ([]configv1.GCPServiceEndpoint, error) {
+	// The endpoints are modified to include the path
+	modifiedEndpoints := []configv1.GCPServiceEndpoint{}
+	for _, se := range endpoints {
+		var err error
+		var formattedURL string
+		switch se.Name {
+		case configv1.GCPServiceEndpointNameCloudResource:
+			formattedURL, err = FormatGCPEndpoint(se.URL, CloudResourceManagerNameGCP, ServiceVersionGCP3)
+		case configv1.GCPServiceEndpointNameCompute:
+			formattedURL, err = FormatGCPEndpoint(se.URL, ComputeServiceNameGCP, ServiceVersionGCP1)
+		case configv1.GCPServiceEndpointNameDNS:
+			formattedURL, err = FormatGCPEndpoint(se.URL, DNSServiceNameGCP, ServiceVersionGCP1)
+		//case configv1.GCPServiceEndpointNameFile:
+		//	formattedURL, err = FormatGCPEndpoint(se.URL, FileServiceNameGCP, ServiceVersionGCP1)
+		case configv1.GCPServiceEndpointNameIAM:
+			formattedURL, err = FormatGCPEndpoint(se.URL, IAMServiceNameGCP, ServiceVersionGCP1)
+		//case configv1.GCPServiceEndpointNameServiceUsage:
+		//	formattedURL, err = FormatGCPEndpoint(se.URL, ServiceUsageNameGCP, ServiceVersionGCPBeta)
+		case configv1.GCPServiceEndpointNameStorage:
+			formattedURL, err = FormatGCPEndpoint(se.URL, StorageServiceNameGCP, ServiceVersionGCP1)
+		}
+
+		if err != nil || formattedURL == "" {
+			return nil, fmt.Errorf("failed to format GCP Service Endpoint URL %s: %w", se.URL, err)
+		}
+		modifiedEndpoints = append(modifiedEndpoints, configv1.GCPServiceEndpoint{Name: se.Name, URL: formattedURL})
+	}
+	return modifiedEndpoints, nil
+}
+
+// FormatGCPEndpoint will format the endpoint to ensure that the string is in the format that would be
+// accepted by both options (WithEndpoint and BasePath override).
+func FormatGCPEndpoint(endpoint string, service ServiceNameGCP, version ServiceVersionGCP) (string, error) {
+	endpointURL, err := url.Parse(endpoint)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse GCP Service Endpoint URL %s: %w", endpoint, err)
+	}
+
+	endpointURL.Scheme = "https"
+	endpointURL.Path = fmt.Sprintf("/%s/%s/", service, version)
+	return endpointURL.String(), nil
+}
 
 // getOptions creates the options for use during service creation.
 func getOptions(ctx context.Context) ([]option.ClientOption, error) {
