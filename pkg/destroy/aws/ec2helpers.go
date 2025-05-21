7b466d92755c6e3be1390e2314d3dbf8f866c286
barbacbd <barbacbd@gmail.com>
2025-05-21 14:57:59 -0400

    pkg/destroy/aws/iamhelpers.go
    ** Remove/Change all imports from AWS sdk v1 to v2.
    
    pkg/destroy/aws/errors.go
    pkg/destroy/aws/ec2helpers.go
    
    ** Remove the Error checking/formatting function from ec2helpers and put the function
    in the errors.go file.

--- pkg/destroy/aws/ec2helpers.go
+++ pkg/destroy/aws/ec2helpers.go
@@ -13,7 +13,6 @@ import (
 	elbv2 "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
 	iamv2 "github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/smithy-go"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -913,13 +912,4 @@ func deleteEC2VPCEndpointService(ctx context.Context, client *ec2v2.Client, id s
 	}
 	logger.Info("Deleted")
 	return nil
-}
-
-// HandleErrorCode takes the error and extracts the error code if it was successfully cast as an API Error.
-func HandleErrorCode(err error) string {
-	var apiErr smithy.APIError
-	if errors.As(err, &apiErr) {
-		return apiErr.ErrorCode()
-	}
-	return ""
 }
--- pkg/destroy/aws/errors.go
+++ pkg/destroy/aws/errors.go
@@ -1,1 +1,15 @@
 package aws
+
+import (
+	"github.com/aws/smithy-go"
+	"github.com/pkg/errors"
+)
+
+// HandleErrorCode takes the error and extracts the error code if it was successfully cast as an API Error.
+func HandleErrorCode(err error) string {
+	var apiErr smithy.APIError
+	if errors.As(err, &apiErr) {
+		return apiErr.ErrorCode()
+	}
+	return ""
+}
