dc5a636c0fc09d045ce0ad8d6f881fb12dad66f0
barbacbd <barbacbd@gmail.com>
2025-06-12 10:09:02 -0400

    OCPBUGS-51299: Add default values for validation to kms keys
    installconfig/gcp:
    
    ** Add default location and project to the kms key validation. The installer does not
    require these fields to be entered, so the defaults will be selected from the projectID and
    region fields of the GCP Platform.

--- pkg/types/gcp/defaults/platform.go
+++ pkg/types/gcp/defaults/platform.go
@@ -4,4 +4,20 @@ import "github.com/openshift/installer/pkg/types/gcp"
 
 // SetPlatformDefaults sets the defaults for the platform.
 func SetPlatformDefaults(p *gcp.Platform) {
+	if p == nil {
+		return
+	}
+
+	if gcpDmp := p.DefaultMachinePlatform; gcpDmp != nil {
+		if ek := gcpDmp.EncryptionKey; ek != nil {
+			if kms := ek.KMSKey; kms != nil {
+				if kms.ProjectID == "" {
+					kms.ProjectID = p.ProjectID
+				}
+				if kms.Location == "" {
+					kms.Location = p.Region
+				}
+			}
+		}
+	}
 }
