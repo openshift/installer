e26ab802342afa8a64c13cf5b23e89945ec8a7ea
barbacbd <barbacbd@gmail.com>
2025-06-12 10:09:02 -0400

    OCPBUGS-51299: Add default values for validation to kms keys
    installconfig/gcp:
    
    ** Add default location and project to the kms key validation. The installer does not
    require these fields to be entered, so the defaults will be selected from the projectID and
    region fields of the GCP Platform.

--- pkg/asset/installconfig/gcp/validation.go
+++ pkg/asset/installconfig/gcp/validation.go
@@ -804,7 +804,7 @@ func validatePlatformKMSKeys(client API, ic *types.InstallConfig, fieldPath *fie
 	cp := ic.ControlPlane
 	validatedControlPlaneKey := false
 	if cp != nil && cp.Platform.GCP != nil && cp.Platform.GCP.EncryptionKey != nil && cp.Platform.GCP.EncryptionKey.KMSKey != nil {
-		if _, err := client.GetKeyRing(context.TODO(), cp.Platform.GCP.OSDisk.EncryptionKey.KMSKey); err != nil {
+		if _, err := client.GetKeyRing(context.TODO(), ic.GCP.ProjectID, ic.GCP.Region, cp.Platform.GCP.OSDisk.EncryptionKey.KMSKey); err != nil {
 			return append(allErrs, field.Invalid(fieldPath.Child("controlPlane").Child("encryptionKey").Child("kmsKey").Child("keyRing"),
 				cp.Platform.GCP.OSDisk.EncryptionKey.KMSKey.KeyRing,
 				err.Error(),
@@ -816,7 +816,7 @@ func validatePlatformKMSKeys(client API, ic *types.InstallConfig, fieldPath *fie
 	validatedComputeKeys := false
 	for _, mp := range ic.Compute {
 		if mp.Platform.GCP != nil && mp.Platform.GCP.EncryptionKey != nil && mp.Platform.GCP.EncryptionKey.KMSKey != nil {
-			if _, err := client.GetKeyRing(context.TODO(), mp.Platform.GCP.OSDisk.EncryptionKey.KMSKey); err != nil {
+			if _, err := client.GetKeyRing(context.TODO(), ic.GCP.ProjectID, ic.GCP.Region, mp.Platform.GCP.OSDisk.EncryptionKey.KMSKey); err != nil {
 				allErrs = append(allErrs, field.Invalid(fieldPath.Child("compute").Child("encryptionKey").Child("kmsKey").Child("keyRing"),
 					mp.Platform.GCP.OSDisk.EncryptionKey.KMSKey.KeyRing,
 					err.Error(),
@@ -829,7 +829,7 @@ func validatePlatformKMSKeys(client API, ic *types.InstallConfig, fieldPath *fie
 
 	defaultMp := ic.GCP.DefaultMachinePlatform
 	if defaultMp != nil && defaultMp.EncryptionKey != nil && defaultMp.EncryptionKey.KMSKey != nil {
-		if _, err := client.GetKeyRing(context.TODO(), defaultMp.EncryptionKey.KMSKey); err != nil {
+		if _, err := client.GetKeyRing(context.TODO(), ic.GCP.ProjectID, ic.GCP.Region, defaultMp.EncryptionKey.KMSKey); err != nil {
 			if validatedControlPlaneKey && (validatedComputeKeys && len(allErrs) == 0) {
 				logrus.Warn("defaultMachinePool.encryptionKey.KMSKey.KeyRing is not valid, but compute and control plane key rings are valid")
 			} else {
