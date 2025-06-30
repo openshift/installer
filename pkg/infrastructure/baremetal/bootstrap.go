1ddfe7373b2c40ed8075dff7f0fb42731af450f9
Zane Bitter <zbitter@redhat.com>
2025-06-30 17:42:37 +1200

    Create scratch volume for /var
    We have almost 9GiB of data to store in /var (mostly container images in
    the crio storage), and since only 50% of RAM is used for the tmpfs this
    requires an inordinate amount of RAM.
    
    Create a volume for mounting as /var so that none of this data ends up
    in the tmpfs. This allows us to return to the original RAM allocation of
    6GiB. Avoid creating a new tmpfs for node-image-pull.service to pull the
    ostree repo, and instead let it land on disk.
    
    This is added to the ignition during the creation of the VM, so other
    use cases for the baremetal bootstrap ignition (e.g. assisted installer)
    cannot be affected.

--- pkg/infrastructure/baremetal/bootstrap.go
+++ pkg/infrastructure/baremetal/bootstrap.go
@@ -2,6 +2,7 @@ package baremetal
 
 import (
 	"context"
+	"encoding/json"
 	"encoding/xml"
 	"errors"
 	"fmt"
@@ -11,12 +12,14 @@ import (
 	"runtime"
 	"strings"
 
+	igntypes "github.com/coreos/ignition/v2/config/v3_2/types"
 	"github.com/coreos/stream-metadata-go/arch"
 	"github.com/digitalocean/go-libvirt"
 	"github.com/sirupsen/logrus"
 	"libvirt.org/go/libvirtxml"
 
 	"github.com/openshift/assisted-image-service/pkg/isoeditor"
+	"github.com/openshift/installer/pkg/asset/ignition"
 	"github.com/openshift/installer/pkg/asset/rhcos"
 )
 
@@ -59,7 +62,7 @@ func newDomain(name string) libvirtxml.Domain {
 			Mode: "host-passthrough",
 		},
 		Memory: &libvirtxml.DomainMemory{
-			Value: 18,
+			Value: 6,
 			Unit:  "GiB",
 		},
 		VCPU: &libvirtxml.DomainVCPU{
@@ -168,6 +171,60 @@ func getLiveISO(config baremetalConfig) (string, error) {
 	return fetcher.GetBaseISOFilename(context.Background(), arch.RpmArch(runtime.GOARCH))
 }
 
+func bootstrapIgnition(config baremetalConfig) (*isoeditor.IgnitionContent, error) {
+	ign := &igntypes.Config{}
+	// TODO(zaneb): Put swap config into system ignition rather than modifying user ignition
+	if err := json.Unmarshal([]byte(config.IgnitionBootstrap), &ign); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal bootstrap Ignition config: %w", err)
+	}
+
+	fsLabel := "var"
+	partDev := fmt.Sprintf("/dev/disk/by-partlabel/%s", fsLabel)
+	format := "xfs"
+	path := "/var"
+	ign.Storage.Disks = append(ign.Storage.Disks, igntypes.Disk{
+		Device: "/dev/vda",
+		Partitions: []igntypes.Partition{
+			{
+				Number: 1,
+				Label:  &fsLabel,
+			},
+		},
+	})
+	ign.Storage.Filesystems = append(ign.Storage.Filesystems, igntypes.Filesystem{
+		Device: partDev,
+		Label:  &fsLabel,
+		Format: &format,
+		Path:   &path,
+	})
+	systemdPartDev := strings.ReplaceAll(strings.ReplaceAll(strings.TrimLeft(partDev, "/"), "-", "\\x2d"), "/", "-")
+	enabled := true
+	mountUnit := fmt.Sprintf(`[Unit]
+Requires=systemd-fsck@%s.service
+After=systemd-fsck@%s.service
+[Mount]
+Where=%s
+What=%s
+Type=%s
+
+[Install]
+RequiredBy=localfs.target
+`,
+		systemdPartDev, systemdPartDev, path, partDev, format)
+	ign.Systemd.Units = append(ign.Systemd.Units, igntypes.Unit{
+		Name:     fmt.Sprintf("%s.mount", fsLabel),
+		Contents: &mountUnit,
+		Enabled:  &enabled,
+	})
+	ign.Storage.Files = append(ign.Storage.Files, ignition.FileFromString("/etc/no-var-tmpfs", "root", 0o440, ""))
+
+	ignData, err := ignition.Marshal(ign)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal bootstrap Ignition config: %w", err)
+	}
+	return &isoeditor.IgnitionContent{Config: ignData}, nil
+}
+
 func createLiveVolume(virConn *libvirt.Libvirt, config baremetalConfig, pool libvirt.StoragePool) (libvirt.StorageVol, error) {
 	isoFile, err := getLiveISO(config)
 	if err != nil {
@@ -175,13 +232,17 @@ func createLiveVolume(virConn *libvirt.Libvirt, config baremetalConfig, pool lib
 	}
 	defer os.Remove(isoFile)
 
+	ignition, err := bootstrapIgnition(config)
+	if err != nil {
+		return libvirt.StorageVol{}, err
+	}
 	var kargs string
 	if config.FIPS {
 		kargs += " fips=1"
 	}
 	stream, err := isoeditor.NewRHCOSStreamReader(
 		isoFile,
-		&isoeditor.IgnitionContent{Config: []byte(config.IgnitionBootstrap)},
+		ignition,
 		nil,
 		[]byte(kargs),
 	)
@@ -231,6 +292,35 @@ func createLiveVolume(virConn *libvirt.Libvirt, config baremetalConfig, pool lib
 	return liveVolume, nil
 }
 
+func createScratchVolume(virConn *libvirt.Libvirt, clusterID string, pool libvirt.StoragePool) (libvirt.StorageVol, error) {
+	vol := libvirtxml.StorageVolume{
+		Name: fmt.Sprintf("%s-scratch", clusterID),
+		Target: &libvirtxml.StorageVolumeTarget{
+			Format: &libvirtxml.StorageVolumeTargetFormat{
+				Type: "qcow2",
+			},
+			Permissions: &libvirtxml.StorageVolumeTargetPermissions{
+				Mode: "644",
+			},
+		},
+		Capacity: &libvirtxml.StorageVolumeSize{
+			Unit:  "GiB",
+			Value: 20,
+		},
+	}
+	scratchVolumeXML, err := xml.Marshal(vol)
+	if err != nil {
+		return libvirt.StorageVol{}, err
+	}
+
+	scratchVolume, err := virConn.StorageVolCreateXML(pool, string(scratchVolumeXML), 0)
+	if err != nil {
+		return libvirt.StorageVol{}, err
+	}
+
+	return scratchVolume, nil
+}
+
 func getHostCapabilities(virConn *libvirt.Libvirt) (libvirtxml.Caps, error) {
 	var caps libvirtxml.Caps
 
@@ -247,7 +337,7 @@ func getHostCapabilities(virConn *libvirt.Libvirt) (libvirtxml.Caps, error) {
 	return caps, nil
 }
 
-func createBootstrapDomain(virConn *libvirt.Libvirt, config baremetalConfig, pool libvirt.StoragePool, liveCDVolume libvirt.StorageVol) error {
+func createBootstrapDomain(virConn *libvirt.Libvirt, config baremetalConfig, pool libvirt.StoragePool, liveCDVolume, scratchVolume libvirt.StorageVol) error {
 	bootstrapDom := newDomain(fmt.Sprintf("%s-bootstrap", config.ClusterID))
 
 	capabilities, err := getHostCapabilities(virConn)
@@ -303,9 +393,30 @@ func createBootstrapDomain(virConn *libvirt.Libvirt, config baremetalConfig, poo
 				Volume: liveCDVolume.Name,
 			},
 		},
+		Boot: &libvirtxml.DomainDeviceBoot{
+			Order: 1,
+		},
 	}
 
-	bootstrapDom.Devices.Disks = append(bootstrapDom.Devices.Disks, liveCD)
+	scratchDisk := libvirtxml.DomainDisk{
+		Device: "disk",
+		Target: &libvirtxml.DomainDiskTarget{
+			Bus: "virtio",
+			Dev: "vda",
+		},
+		Driver: &libvirtxml.DomainDiskDriver{
+			Name: "qemu",
+			Type: "qcow2",
+		},
+		Source: &libvirtxml.DomainDiskSource{
+			Volume: &libvirtxml.DomainDiskSourceVolume{
+				Pool:   pool.Name,
+				Volume: scratchVolume.Name,
+			},
+		},
+	}
+
+	bootstrapDom.Devices.Disks = append(bootstrapDom.Devices.Disks, liveCD, scratchDisk)
 
 	bootstrapDom.Resource = nil
 
@@ -351,8 +462,14 @@ func createBootstrap(config baremetalConfig) error {
 		return err
 	}
 
+	logrus.Debug("  Creating scratch volume")
+	scratchVolume, err := createScratchVolume(virConn, config.ClusterID, pool)
+	if err != nil {
+		return err
+	}
+
 	logrus.Debug("  Creating bootstrap domain")
-	err = createBootstrapDomain(virConn, config, pool, liveVolume)
+	err = createBootstrapDomain(virConn, config, pool, liveVolume, scratchVolume)
 	if err != nil {
 		return err
 	}
@@ -416,6 +533,17 @@ func destroyBootstrap(config baremetalConfig) error {
 	}
 
 	logrus.Debug("  Deleting live volume")
+	err = virConn.StorageVolDelete(vol, libvirt.StorageVolDeleteNormal)
+	if err != nil {
+		return err
+	}
+
+	vol, err = virConn.StorageVolLookupByName(pool, fmt.Sprintf("%s-scratch", config.ClusterID))
+	if err != nil {
+		return err
+	}
+
+	logrus.Debug("  Deleting scratch volume")
 	err = virConn.StorageVolDelete(vol, libvirt.StorageVolDeleteNormal)
 	if err != nil {
 		return err
