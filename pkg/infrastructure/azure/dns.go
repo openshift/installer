d24e76cb70a9b6ab973b1fdfae3de73da9c2a779
John Hixson <jhixson@redhat.com>
2025-08-08 21:27:13 -0700

    pkg/infrastructure/azure: Create IPv6 DNS records

--- pkg/infrastructure/azure/dns.go
+++ pkg/infrastructure/azure/dns.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dns/armdns"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/privatedns/armprivatedns"
+	"github.com/sirupsen/logrus"
 	"k8s.io/utils/ptr"
 	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -14,14 +16,6 @@ import (
 	"github.com/openshift/installer/pkg/asset/manifests/capiutils"
 	"github.com/openshift/installer/pkg/infrastructure/clusterapi"
 	"github.com/openshift/installer/pkg/types/azure"
-)
-
-type recordListType string
-
-const (
-	cname      recordListType = "Cname"
-	arecord    recordListType = "ARecord"
-	aaaarecord recordListType = "AaaaRecord"
 )
 
 type recordList struct {
@@ -36,34 +30,55 @@ type recordPrivateList struct {
 	RecordSet  armprivatedns.RecordSet
 }
 
-// Create DNS entries for azure.
-func createDNSEntries(ctx context.Context, in clusterapi.InfraReadyInput, extLBFQDN, publicIP, resourceGroup string, opts *arm.ClientOptions) error {
-	baseDomainResourceGroup := in.InstallConfig.Config.Azure.BaseDomainResourceGroupName
-	zone := in.InstallConfig.Config.BaseDomain
-	privatezone := in.InstallConfig.Config.ClusterDomain()
-	apiExternalName := fmt.Sprintf("api.%s", in.InstallConfig.Config.ObjectMeta.Name)
+type createDNSEntriesInput struct {
+	infra                clusterapi.InfraReadyInput
+	extLBFQDNIPv4        string
+	extLBFQDNIPv6        string
+	publicIPv4           string
+	publicIPv6           string
+	resourceGroupName    string
+	networkClientFactory *armnetwork.ClientFactory
+	opts                 *arm.ClientOptions
+}
 
-	if in.InstallConfig.Config.Azure.ResourceGroupName != "" {
-		resourceGroup = in.InstallConfig.Config.Azure.ResourceGroupName
+// Create DNS entries for azure.
+func createDNSEntries(ctx context.Context, in *createDNSEntriesInput) error {
+	baseDomainResourceGroup := in.infra.InstallConfig.Config.Azure.BaseDomainResourceGroupName
+	zone := in.infra.InstallConfig.Config.BaseDomain
+	privatezone := in.infra.InstallConfig.Config.ClusterDomain()
+	apiExternalName := fmt.Sprintf("api.%s", in.infra.InstallConfig.Config.ObjectMeta.Name)
+
+	if in.infra.InstallConfig.Config.Azure.ResourceGroupName != "" {
+		in.resourceGroupName = in.infra.InstallConfig.Config.Azure.ResourceGroupName
 	}
 	azureTags := make(map[string]*string)
-	for k, v := range in.InstallConfig.Config.Azure.UserTags {
+	for k, v := range in.infra.InstallConfig.Config.Azure.UserTags {
 		azureTags[k] = ptr.To(v)
 	}
-	azureTags[fmt.Sprintf("kubernetes.io_cluster.%s", in.InfraID)] = ptr.To("owned")
-	azureCluster := &capz.AzureCluster{}
-	key := client.ObjectKey{
-		Name:      in.InfraID,
-		Namespace: capiutils.Namespace,
+	azureTags[fmt.Sprintf("kubernetes.io_cluster.%s", in.infra.InfraID)] = ptr.To("owned")
+	lb, err := getLoadBalancer(ctx, &lbInput{
+		networkClientFactory: in.networkClientFactory,
+		resourceGroupName:    in.resourceGroupName,
+		loadBalancerName:     fmt.Sprintf("%s-internal", in.infra.InfraID),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to get Azure internal load balancer: %w", err)
 	}
-	if err := in.Client.Get(ctx, key, azureCluster); err != nil && azureCluster != nil {
-		return fmt.Errorf("failed to get Azure cluster: %w", err)
-	}
-
-	if len(azureCluster.Spec.NetworkSpec.APIServerLB.FrontendIPs) == 0 {
+	if len(lb.Properties.FrontendIPConfigurations) == 0 {
 		return fmt.Errorf("failed to get Azure cluster LB frontend IPs")
 	}
-	ipIlb := azureCluster.Spec.NetworkSpec.APIServerLB.FrontendIPs[0].PrivateIPAddress
+
+	var ipv4Addresses, ipv6Addresses []*string
+	for _, frontendIPConfig := range lb.Properties.FrontendIPConfigurations {
+		if *frontendIPConfig.Properties.PrivateIPAddressVersion == armnetwork.IPVersionIPv4 {
+			ipv4Addresses = append(ipv4Addresses, frontendIPConfig.Properties.PrivateIPAddress)
+			logrus.Debugf("XXX: PrivateIPv4Address=%s", *frontendIPConfig.Properties.PrivateIPAddress)
+		} else if *frontendIPConfig.Properties.PrivateIPAddressVersion == armnetwork.IPVersionIPv6 {
+			ipv6Addresses = append(ipv6Addresses, frontendIPConfig.Properties.PrivateIPAddress)
+			logrus.Debugf("XXX: PrivateIPv6Address=%s", *frontendIPConfig.Properties.PrivateIPAddress)
+		}
+	}
+
 	// useIPv6 := false
 	// for _, network := range in.InstallConfig.Config.Networking.ServiceNetwork {
 	// 	if network.IP.To4() == nil {
@@ -73,29 +88,39 @@ func createDNSEntries(ctx context.Context, in clusterapi.InfraReadyInput, extLBF
 
 	privateRecords := []recordPrivateList{}
 	ttl := int64(300)
-	recordType := arecord
 	// if useIPv6 {
 	// 	recordType = aaaarecord
 	// }
-	privateRecords = append(privateRecords, createPrivateRecordSet("api-int", azureTags, ttl, recordType, ipIlb, ""))
-	privateRecords = append(privateRecords, createPrivateRecordSet("api", azureTags, ttl, recordType, ipIlb, ""))
 
-	session, err := in.InstallConfig.Azure.Session()
+	if len(ipv4Addresses) > 0 {
+		for _, ipv4Address := range ipv4Addresses {
+			privateRecords = append(privateRecords, createPrivateRecordSet("api-int", azureTags, ttl, armdns.RecordTypeA, *ipv4Address, ""))
+			privateRecords = append(privateRecords, createPrivateRecordSet("api", azureTags, ttl, armdns.RecordTypeA, *ipv4Address, ""))
+		}
+	}
+	if len(ipv6Addresses) > 0 {
+		for _, ipv6Address := range ipv6Addresses {
+			privateRecords = append(privateRecords, createPrivateRecordSet("api-int", azureTags, ttl, armdns.RecordTypeAAAA, *ipv6Address, ""))
+			privateRecords = append(privateRecords, createPrivateRecordSet("api", azureTags, ttl, armdns.RecordTypeAAAA, *ipv6Address, ""))
+		}
+	}
+
+	session, err := in.infra.InstallConfig.Azure.Session()
 	if err != nil {
 		return fmt.Errorf("failed to create session: %w", err)
 	}
 	subscriptionID := session.Credentials.SubscriptionID
 
-	onAzureStack := in.InstallConfig.Azure.CloudName == azure.StackCloud
+	onAzureStack := in.infra.InstallConfig.Azure.CloudName == azure.StackCloud
 	if onAzureStack {
-		opts.APIVersion = stackDNSAPIVersion
+		in.opts.APIVersion = stackDNSAPIVersion
 	}
 
-	recordSetClient, err := armdns.NewRecordSetsClient(subscriptionID, session.TokenCreds, opts)
+	recordSetClient, err := armdns.NewRecordSetsClient(subscriptionID, session.TokenCreds, in.opts)
 	if err != nil {
 		return fmt.Errorf("failed to create public record client: %w", err)
 	}
-	privateRecordSetClient, err := armprivatedns.NewRecordSetsClient(subscriptionID, session.TokenCreds, opts)
+	privateRecordSetClient, err := armprivatedns.NewRecordSetsClient(subscriptionID, session.TokenCreds, in.opts)
 	if err != nil {
 		return fmt.Errorf("failed to create private record client: %w", err)
 	}
@@ -103,14 +128,27 @@ func createDNSEntries(ctx context.Context, in clusterapi.InfraReadyInput, extLBF
 	// Azure Stack only supports "DNS zones"--there is not a private/public zone distinction,
 	// so we handle Azure Stack differently and create all records in the single zone.
 	if onAzureStack {
-		stackRecords := []recordList{}
-		apiInternalName := fmt.Sprintf("api-int.%s", in.InstallConfig.Config.ObjectMeta.Name)
-		if in.InstallConfig.Config.PublicAPI() {
-			stackRecords = append(stackRecords, createRecordSet(apiExternalName, azureTags, ttl, arecord, publicIP, ""))
-		} else {
-			stackRecords = append(stackRecords, createRecordSet(apiExternalName, azureTags, ttl, arecord, ipIlb, ""))
+		azureCluster := &capz.AzureCluster{}
+		key := client.ObjectKey{
+			Name:      in.infra.InfraID,
+			Namespace: capiutils.Namespace,
 		}
-		stackRecords = append(stackRecords, createRecordSet(apiInternalName, azureTags, ttl, arecord, ipIlb, ""))
+		if err := in.infra.Client.Get(ctx, key, azureCluster); err != nil && azureCluster != nil {
+			return fmt.Errorf("failed to get Azure cluster: %w", err)
+		}
+
+		if len(azureCluster.Spec.NetworkSpec.APIServerLB.FrontendIPs) == 0 {
+			return fmt.Errorf("failed to get Azure cluster LB frontend IPs")
+		}
+		ipIlb := azureCluster.Spec.NetworkSpec.APIServerLB.FrontendIPs[0].PrivateIPAddress
+		stackRecords := []recordList{}
+		apiInternalName := fmt.Sprintf("api-int.%s", in.infra.InstallConfig.Config.ObjectMeta.Name)
+		if in.infra.InstallConfig.Config.PublicAPI() {
+			stackRecords = append(stackRecords, createRecordSet(apiExternalName, azureTags, ttl, armdns.RecordTypeA, in.publicIPv4, ""))
+		} else {
+			stackRecords = append(stackRecords, createRecordSet(apiExternalName, azureTags, ttl, armdns.RecordTypeA, ipIlb, ""))
+		}
+		stackRecords = append(stackRecords, createRecordSet(apiInternalName, azureTags, ttl, armdns.RecordTypeA, ipIlb, ""))
 		for _, record := range stackRecords {
 			_, err = recordSetClient.CreateOrUpdate(ctx, baseDomainResourceGroup, zone, record.Name, record.RecordType, record.RecordSet, nil)
 			if err != nil {
@@ -122,21 +160,33 @@ func createDNSEntries(ctx context.Context, in clusterapi.InfraReadyInput, extLBF
 
 	// Create the records for api and api-int in the private zone and api.<clustername> for public zone.
 	// CAPI currently creates a record called "apiserver" instead of "api" so creating "api" for the installer in the private zone.
-	if in.InstallConfig.Config.PublicAPI() {
-		cnameRecordName := apiExternalName
+	if in.infra.InstallConfig.Config.PublicAPI() {
+		logrus.Debugf("XXX: apiExternalName=%s", apiExternalName)
+		logrus.Debugf("XXX: extLBFQDNIPv4=%s", in.extLBFQDNIPv4)
+		logrus.Debugf("XXX: extLBFQDNIPv6=%s", in.extLBFQDNIPv6)
 		// apiExternalNameV6 := fmt.Sprintf("v6-api.%s", infraID)
 		// if useIPv6 {
 		// 	cnameRecordName = apiExternalNameV6
 		// }
-		publicRecords := createRecordSet(cnameRecordName, azureTags, ttl, cname, "", extLBFQDN)
-		_, err = recordSetClient.CreateOrUpdate(ctx, baseDomainResourceGroup, zone, publicRecords.Name, publicRecords.RecordType, publicRecords.RecordSet, nil)
-		if err != nil {
-			return fmt.Errorf("failed to create public record set: %w", err)
+		if in.publicIPv4 != "" {
+			publicRecords := createRecordSet(apiExternalName, azureTags, ttl, armdns.RecordTypeA, in.publicIPv4, "")
+			_, err = recordSetClient.CreateOrUpdate(ctx, baseDomainResourceGroup, zone, publicRecords.Name, publicRecords.RecordType, publicRecords.RecordSet, nil)
+			if err != nil {
+				return fmt.Errorf("failed to create public IPv4 record set: %w", err)
+			}
+		}
+
+		if in.publicIPv6 != "" {
+			publicRecords := createRecordSet(apiExternalName, azureTags, ttl, armdns.RecordTypeAAAA, in.publicIPv6, "")
+			_, err = recordSetClient.CreateOrUpdate(ctx, baseDomainResourceGroup, zone, publicRecords.Name, publicRecords.RecordType, publicRecords.RecordSet, nil)
+			if err != nil {
+				return fmt.Errorf("failed to create public IPv6 record set: %w", err)
+			}
 		}
 	}
 
 	for _, record := range privateRecords {
-		_, err = privateRecordSetClient.CreateOrUpdate(ctx, resourceGroup, privatezone, record.RecordType, record.Name, record.RecordSet, nil)
+		_, err = privateRecordSetClient.CreateOrUpdate(ctx, in.resourceGroupName, privatezone, record.RecordType, record.Name, record.RecordSet, nil)
 		if err != nil {
 			return fmt.Errorf("failed to create private record set: %w", err)
 		}
@@ -145,7 +195,7 @@ func createDNSEntries(ctx context.Context, in clusterapi.InfraReadyInput, extLBF
 	return nil
 }
 
-func createPrivateRecordSet(lbType string, azureTags map[string]*string, ttl int64, rType recordListType, ipAddress string, recordName string) (record recordPrivateList) {
+func createPrivateRecordSet(lbType string, azureTags map[string]*string, ttl int64, rType armdns.RecordType, ipAddress string, recordName string) (record recordPrivateList) {
 	record = recordPrivateList{
 		Name: lbType,
 		RecordSet: armprivatedns.RecordSet{
@@ -157,19 +207,19 @@ func createPrivateRecordSet(lbType string, azureTags map[string]*string, ttl int
 	}
 
 	switch rType {
-	case cname:
+	case armdns.RecordTypeCNAME:
 		record.RecordType = armprivatedns.RecordTypeCNAME
 		record.RecordSet.Properties.CnameRecord = &armprivatedns.CnameRecord{
 			Cname: &recordName,
 		}
-	case arecord:
+	case armdns.RecordTypeA:
 		record.RecordType = armprivatedns.RecordTypeA
 		record.RecordSet.Properties.ARecords = []*armprivatedns.ARecord{
 			{
 				IPv4Address: &ipAddress,
 			},
 		}
-	case aaaarecord:
+	case armdns.RecordTypeAAAA:
 		record.RecordType = armprivatedns.RecordTypeAAAA
 		record.RecordSet.Properties.AaaaRecords = []*armprivatedns.AaaaRecord{
 			{
@@ -180,7 +230,7 @@ func createPrivateRecordSet(lbType string, azureTags map[string]*string, ttl int
 	return record
 }
 
-func createRecordSet(lbType string, azureTags map[string]*string, ttl int64, rType recordListType, ipAddress string, recordName string) (record recordList) {
+func createRecordSet(lbType string, azureTags map[string]*string, ttl int64, rType armdns.RecordType, ipAddress string, recordName string) (record recordList) {
 	record = recordList{
 		Name: lbType,
 		RecordSet: armdns.RecordSet{
@@ -192,19 +242,19 @@ func createRecordSet(lbType string, azureTags map[string]*string, ttl int64, rTy
 	}
 
 	switch rType {
-	case cname:
+	case armdns.RecordTypeCNAME:
 		record.RecordType = armdns.RecordTypeCNAME
 		record.RecordSet.Properties.CnameRecord = &armdns.CnameRecord{
 			Cname: &recordName,
 		}
-	case arecord:
+	case armdns.RecordTypeA:
 		record.RecordType = armdns.RecordTypeA
 		record.RecordSet.Properties.ARecords = []*armdns.ARecord{
 			{
 				IPv4Address: &ipAddress,
 			},
 		}
-	case aaaarecord:
+	case armdns.RecordTypeAAAA:
 		record.RecordType = armdns.RecordTypeAAAA
 		record.RecordSet.Properties.AaaaRecords = []*armdns.AaaaRecord{
 			{
