cb6fab9811988bcc14d780f1e7e78230a1a4a1eb
John Hixson <jhixson@redhat.com>
2025-08-08 21:17:44 -0700

    pkg/asset/manifests/azure: dual stack support for cluster manifest
    - Support IPv4 and IPv6 CIDRs
    - Set VNet CIDR blocks
    - Set subnet CIDR blocks
    
    https://issues.redhat.com/browse/CORS-3901
    https://issues.redhat.com/browse/CORS-3897

--- pkg/asset/manifests/azure/cluster.go
+++ pkg/asset/manifests/azure/cluster.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
@@ -28,55 +29,27 @@ import (
 
 // GenerateClusterAssets generates the manifests for the cluster-api.
 func GenerateClusterAssets(installConfig *installconfig.InstallConfig, clusterID *installconfig.ClusterID) (*capiutils.GenerateClusterAssetsOutput, error) {
-	manifests := []*asset.RuntimeFile{}
-	mainCIDR := capiutils.CIDRFromInstallConfig(installConfig)
+	logrus.Debugf("XXX: GenerateClusterAssets")
+	resourceGroup := installConfig.Config.Platform.Azure.ClusterResourceGroupName(clusterID.InfraID)
+	controlPlaneSubnet := installConfig.Config.Platform.Azure.ControlPlaneSubnetName(clusterID.InfraID)
+	computeSubnet := installConfig.Config.Platform.Azure.ComputeSubnetName(clusterID.InfraID)
+	networkSecurityGroup := installConfig.Config.Platform.Azure.NetworkSecurityGroupName(clusterID.InfraID)
+	securityGroup := capz.SecurityGroup{Name: networkSecurityGroup}
 
 	session, err := installConfig.Azure.Session()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create Azure session")
 	}
 
-	subnets, err := cidr.SplitIntoSubnetsIPv4(mainCIDR.String(), 2)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to split CIDR into subnets")
-	}
-
 	// CAPZ expects the capz-system to be created.
 	azureNamespace := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "capz-system"}}
 	azureNamespace.SetGroupVersionKind(corev1.SchemeGroupVersion.WithKind("Namespace"))
+
+	manifests := []*asset.RuntimeFile{}
 	manifests = append(manifests, &asset.RuntimeFile{
 		Object: azureNamespace,
 		File:   asset.File{Filename: "00_azure-namespace.yaml"},
 	})
-
-	resourceGroup := installConfig.Config.Platform.Azure.ClusterResourceGroupName(clusterID.InfraID)
-	controlPlaneSubnet := installConfig.Config.Platform.Azure.ControlPlaneSubnetName(clusterID.InfraID)
-	computeSubnet := installConfig.Config.Platform.Azure.ComputeSubnetName(clusterID.InfraID)
-	networkSecurityGroup := installConfig.Config.Platform.Azure.NetworkSecurityGroupName(clusterID.InfraID)
-
-	source := "*"
-	if installConfig.Config.Publish == types.InternalPublishingStrategy {
-		source = mainCIDR.String()
-	}
-
-	securityGroup := capz.SecurityGroup{
-		Name: networkSecurityGroup,
-		SecurityGroupClass: capz.SecurityGroupClass{
-			SecurityRules: []capz.SecurityRule{
-				{
-					Name:             "apiserver_in",
-					Protocol:         capz.SecurityGroupProtocolTCP,
-					Direction:        capz.SecurityRuleDirectionInbound,
-					Priority:         101,
-					SourcePorts:      ptr.To("*"),
-					DestinationPorts: ptr.To("6443"),
-					Source:           ptr.To(source),
-					Destination:      ptr.To("*"),
-					Action:           capz.SecurityRuleActionAllow,
-				},
-			},
-		},
-	}
 
 	// Setting ID on the Subnet disables natgw creation. See:
 	// https://github.com/kubernetes-sigs/cluster-api-provider-azure/blob/21479a9a4c640b43e0bef028487c522c55605d06/api/v1beta1/azurecluster_default.go#L160
@@ -93,25 +66,50 @@ func GenerateClusterAssets(installConfig *installconfig.InstallConfig, clusterID
 		nodeSubnetID = "UNKNOWN"
 	}
 
+	//apiServerLBFrontendConfigName := azure.InternalFrontendIPv4ConfigName
+	apiServerLBFrontendConfigName := fmt.Sprintf("%s-internal-frontEnd", clusterID.InfraID)
+	//apiServerBackendPoolName := azure.InternalBackendAddressPoolIPv4Name
+	apiServerBackendPoolName := fmt.Sprintf("%s-internal", clusterID.InfraID)
+	if installConfig.Config.Networking.IsIPv6() && !installConfig.Config.Networking.IsDualStack() {
+		//apiServerLBFrontendConfigName = azure.InternalFrontendIPv6ConfigName
+		apiServerLBFrontendConfigName = fmt.Sprintf("%s-internal-frontEnd-ipv6", clusterID.InfraID)
+		//apiServerBackendPoolName = azure.InternalBackendAddressPoolIPv6Name
+		apiServerBackendPoolName = fmt.Sprintf("%s-internal-ipv6", clusterID.InfraID)
+	}
 	apiServerLB := capz.LoadBalancerSpec{
 		Name: fmt.Sprintf("%s-internal", clusterID.InfraID),
 		BackendPool: capz.BackendPool{
-			Name: fmt.Sprintf("%s-internal", clusterID.InfraID),
+			Name: apiServerBackendPoolName,
 		},
 		LoadBalancerClassSpec: capz.LoadBalancerClassSpec{
 			Type: capz.Internal,
 		},
 	}
 
+	//controlPlaneBackendPoolName := azure.OutboundBackendAddressPoolIPv4Name
+	controlPlaneBackendPoolName := fmt.Sprintf("%s-outbound-lb-outboundBackendPool", clusterID.InfraID)
+	if installConfig.Config.Networking.IsIPv6() && !installConfig.Config.Networking.IsDualStack() {
+		//controlPlaneBackendPoolName = azure.OutboundBackendAddressPoolIPv6Name
+		controlPlaneBackendPoolName = fmt.Sprintf("%s-outbound-lb-outboundBackendPool-ipv6", clusterID.InfraID)
+	}
 	controlPlaneOutboundLB := &capz.LoadBalancerSpec{
 		Name:             clusterID.InfraID,
 		FrontendIPsCount: to.Ptr(int32(1)),
+		BackendPool: capz.BackendPool{
+			Name: controlPlaneBackendPoolName,
+		},
 	}
 
 	if installConfig.Config.Platform.Azure.OutboundType == azure.UserDefinedRoutingOutboundType {
 		controlPlaneOutboundLB = nil
 	}
 
+	// XXX: keep this in for now to keep the LB code happy, but rewrite
+	// later
+	subnets, err := cidr.SplitIntoSubnetsIPv4(capiutils.CIDRFromInstallConfig(installConfig).String(), 2)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to split CIDR into subnets")
+	}
 	virtualNetworkID := ""
 	lbip := capz.DefaultInternalLBIPAddress
 	lbip = getIPWithinCIDR(subnets, lbip)
@@ -150,11 +148,12 @@ func GenerateClusterAssets(installConfig *installconfig.InstallConfig, clusterID
 	}
 
 	apiServerLB.FrontendIPs = []capz.FrontendIP{{
-		Name: fmt.Sprintf("%s-internal-frontEnd", clusterID.InfraID),
+		Name: apiServerLBFrontendConfigName,
 		FrontendIPClass: capz.FrontendIPClass{
 			PrivateIPAddress: lbip,
 		},
 	}}
+
 	if installConfig.Config.Azure.VirtualNetwork != "" {
 		client, err := installConfig.Azure.Client()
 		if err != nil {
@@ -184,11 +183,13 @@ func GenerateClusterAssets(installConfig *installconfig.InstallConfig, clusterID
 		SubnetClassSpec: capz.SubnetClassSpec{
 			Name: computeSubnet,
 			Role: capz.SubnetNode,
-			CIDRBlocks: []string{
-				subnets[1].String(),
-			},
+			/*
+				CIDRBlocks: []string{
+					subnets[1].String(),
+				},
+			*/
 		},
-		SecurityGroup: securityGroup,
+		//SecurityGroup: securityGroup,
 	}
 
 	if installConfig.Config.Azure.OutboundType == azure.NATGatewaySingleZoneOutboundType {
@@ -228,11 +229,6 @@ func GenerateClusterAssets(installConfig *installconfig.InstallConfig, clusterID
 					// See https://github.com/kubernetes-sigs/cluster-api-provider-azure/blob/main/azure/scope/cluster.go#L585
 					// https://github.com/kubernetes-sigs/cluster-api-provider-azure/commit/0f321e4089a3f4dc37f8420bf2ef6762c398c400
 					ID: virtualNetworkID,
-					VnetClassSpec: capz.VnetClassSpec{
-						CIDRBlocks: []string{
-							mainCIDR.String(),
-						},
-					},
 				},
 				APIServerLB:            &apiServerLB,
 				ControlPlaneOutboundLB: controlPlaneOutboundLB,
@@ -241,16 +237,238 @@ func GenerateClusterAssets(installConfig *installconfig.InstallConfig, clusterID
 						SubnetClassSpec: capz.SubnetClassSpec{
 							Name: controlPlaneSubnet,
 							Role: capz.SubnetControlPlane,
-							CIDRBlocks: []string{
-								subnets[0].String(),
-							},
 						},
-						SecurityGroup: securityGroup,
+						//SecurityGroup: securityGroup,
 					},
 					computeSubnetSpec,
 				},
 			},
 		},
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+	// If a single CIDR is in the install-config:
+	//   - Split it:
+	//     - Use first half for control plane subnet
+	//     - Use second half for compute subnet
+	//
+	// If more than a single CIDR is specified:
+	//   - If multiple of same protocol:
+	//     - Use first for control plane subnet
+	//     - Use second for compute subnet
+	//     - Create additional subnets for any additional
+	//
+	// If dual stack:
+	//   - Same as above, but bind both protocols to each subnet
+	//
+	// Example #1:
+	//
+	// machineNetwork:
+	// - cidr: 10.0.0.0/17     <- control plane subnet
+	// - cidr: 10.0.128.0/17   <- compute subnet
+	// - cidr: 192.168.0.0/16  <- subnet #x
+	// - cidr: 172.16.0.0/16   <- subnet #y
+	// - cidr: ffd0::/48       <- splits into ffd0:0:0:1::/64 and ffd0:0:0:2::/64
+	//                            where ffd0:0:0:1::/64 is control plane subnet
+	//                            and ffd0:0:0:2::/64 is compute subnet
+	//                            ffd0:0:0:1::/64 is added to 10.0.0.0/17 subnet cidr blocks
+	//                            ffd0:0:0:2::/64 is added to 10.0.128.0/17 subnet cidr blocks
+	// Example #2:
+	//
+	// machineNetwork:
+	// - cidr: 10.0.0.0/16
+	//
+	// ^ Splits into 10.0.0.0/17 and 10.0.128.0/17
+	//   10.0.0.0/17 for the control plane subnet
+	//   10.0.128.0/17 for the compute subnet
+	//
+	// Example #3:
+	//
+	// machineNetwork:
+	// - cidr: ffd0::/48
+	//
+	// ^ Splits into ffd0:0:0:1::/64 and ffd0:0:0:2::/64
+	//   ffd0:0:0:1::/64 is for the control plane subnet
+	//   ffd0:0:0:2::/64 is for the compute subnet
+	//
+	// Example #4:
+	//
+	// machineNetwork:
+	// - cidr: 10.0.0.0/16
+	// - cidr: ffd0::/48
+	//
+	// ^ 10.0.0.0/16 splits into 10.0.0.0/17 and 10.0.128.0/17
+	//   ffd0::/48 splits into ffd0:0:0:1::/64 and ffd0:0:0:2::/64
+	//   10.0.0.0/17 and ffd0:0:0:1::/64 and the control plane cidr blocks
+	//   10.0.128.0/17 and ffd0:0:0:1::/64 are the compute cidr blocks
+	///////////////////////////////////////////////////////////////////////////////////////////
+	CIDRs := capiutils.CIDRsFromInstallConfig(installConfig)
+
+	// XXXX
+	// XXX: Get back to this and rewrite using install config networking
+	// IsIP* functions
+	// XXXX
+
+	/*
+	   if installConfig.Networking.IsDualStack() {
+	           p.DualStack = true
+	   } else if installConfig.Networking.IsIPv4() {
+	           p.FrontendIPConfigName = frontendIPv4ConfigName
+	   } else if installConfig.Networking.IsIPv6() {
+	           p.FrontendIPConfigName = frontendIPv6ConfigName
+	   }
+	*/
+	//installConfig.Config.Networking.Is
+
+	// Split CIDRs into IPv4 and IPv6 CIDRs
+	var ipv4CIDRs, ipv6CIDRs []ipnet.IPNet
+	for _, CIDR := range CIDRs {
+		switch len(CIDR.IP) {
+		case net.IPv4len:
+			ipv4CIDRs = append(ipv4CIDRs, CIDR)
+		case net.IPv6len:
+			ipv6CIDRs = append(ipv6CIDRs, CIDR)
+		}
+	}
+
+	// Split IPv4 CIDRs into IPv4 subnets
+	var ipv4Subnets []*net.IPNet
+	logrus.Debugf("XXX: len(ipv4CIDRs)=%d", len(ipv4CIDRs))
+	switch len(ipv4CIDRs) {
+	case 1:
+		ipv4Subnets, err = cidr.SplitIntoSubnetsIPv4(ipv4CIDRs[0].String(), 2)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to split IPv4 CIDR into subnets")
+		}
+	default:
+		for _, ipv4Cidr := range ipv4CIDRs {
+			ipv4Subnets = append(ipv4Subnets, &net.IPNet{
+				IP:   ipv4Cidr.IP,
+				Mask: ipv4Cidr.Mask,
+			})
+		}
+	}
+
+	// Split IPv6 CIDRs into IPv6 subnets
+	var ipv6Subnets []*net.IPNet
+	logrus.Debugf("XXX: len(ipv6CIDRs)=%d", len(ipv6CIDRs))
+	switch len(ipv6CIDRs) {
+	case 1:
+		ipv6Subnets, err = cidr.SplitIntoSubnetsIPv6(ipv6CIDRs[0].String(), 2)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to split IPv6 CIDR into subnets")
+		}
+	default:
+		for _, ipv6Cidr := range ipv6CIDRs {
+			ipv6Subnets = append(ipv6Subnets, &net.IPNet{
+				IP:   ipv6Cidr.IP,
+				Mask: ipv6Cidr.Mask,
+			})
+		}
+	}
+
+	securityRule := capz.SecurityRule{
+		Name:             "apiserver_in",
+		Protocol:         capz.SecurityGroupProtocolTCP,
+		Direction:        capz.SecurityRuleDirectionInbound,
+		Priority:         100,
+		SourcePorts:      ptr.To("*"),
+		DestinationPorts: ptr.To("6443"),
+		Source:           ptr.To("*"),
+		Destination:      ptr.To("*"),
+		Action:           capz.SecurityRuleActionAllow,
+	}
+
+	// If we are using Internal publishing, we need a security rule for each CIDR
+	var securityRules []capz.SecurityRule
+	var securityRulePriority int32 = 100
+	if len(ipv4CIDRs) > 0 && installConfig.Config.Publish == types.InternalPublishingStrategy {
+		for i, ipv4CIDR := range ipv4CIDRs {
+			ipv4SecurityRule := securityRule
+			ipv4SecurityRule.Name = fmt.Sprintf("apiserver_in_ipv4_%02d", i)
+			ipv4SecurityRule.Source = to.Ptr(ipv4CIDR.String())
+			ipv4SecurityRule.Priority = securityRulePriority
+			securityRules = append(securityRules, ipv4SecurityRule)
+			securityRulePriority += 10
+		}
+	}
+	if len(ipv6CIDRs) > 0 && installConfig.Config.Publish == types.InternalPublishingStrategy {
+		for i, ipv6CIDR := range ipv6CIDRs {
+			ipv6SecurityRule := securityRule
+			ipv6SecurityRule.Name = fmt.Sprintf("apiserver_in_ipv6_%02d", i)
+			ipv6SecurityRule.Source = to.Ptr(ipv6CIDR.String())
+			ipv6SecurityRule.Priority = securityRulePriority
+			securityRules = append(securityRules, ipv6SecurityRule)
+			securityRulePriority += 10
+		}
+	}
+	if len(securityRules) == 0 {
+		securityRules = append(securityRules, securityRule)
+	}
+	securityGroup.SecurityGroupClass.SecurityRules = securityRules
+	logrus.Debugf("XXX: len(securityRules)=%d", len(securityRules))
+
+	subnetsPtr := &azureCluster.Spec.NetworkSpec.Subnets
+
+	// Create additional subnets if necessary
+	subnetsCount := max(2, len(ipv4Subnets), len(ipv6Subnets))
+	logrus.Debugf("XXX: subnetsCount=%d", subnetsCount)
+	if subnetsCount > 2 {
+		for i := 2; i < subnetsCount; i++ {
+			newSubnet := capz.SubnetSpec{
+				SubnetClassSpec: capz.SubnetClassSpec{
+					Name: fmt.Sprintf("subnet_%02d", i),
+					Role: capz.SubnetNode,
+				},
+			}
+			*subnetsPtr = append(*subnetsPtr, newSubnet)
+		}
+	}
+
+	var vnetCIDRblocks, controlPlaneCIDRblocks, computeCIDRblocks []string
+
+	// Set the VNet CIDR blockCIDs
+	for _, CIDR := range CIDRs {
+		logrus.Debugf("XXX: CIDR=%s", CIDR.String())
+		vnetCIDRblocks = append(vnetCIDRblocks, CIDR.String())
+	}
+
+	// XXX: 10.1.0.0 is node CIDR, where does 10.2.0.0 come from?
+	vnetCIDRblocks = append(vnetCIDRblocks, "10.1.0.0/16")
+	vnetCIDRblocks = append(vnetCIDRblocks, "10.2.0.0/16")
+
+	logrus.Debugf("XXX: len(ipv4Subnets)=%d", len(ipv4Subnets))
+	logrus.Debugf("XXX: len(ipv6Subnets)=%d", len(ipv6Subnets))
+
+	// Set control plane and compute network
+	for i, ipv4Subnet := range ipv4Subnets {
+		switch i {
+		case 0:
+			controlPlaneCIDRblocks = append(controlPlaneCIDRblocks, ipv4Subnet.String())
+		case 1:
+			computeCIDRblocks = append(computeCIDRblocks, ipv4Subnet.String())
+		}
+	}
+
+	// Set control plane and compute network IPv6 CIDR blocks
+	for i, ipv6Subnet := range ipv6Subnets {
+		switch i {
+		case 0:
+			controlPlaneCIDRblocks = append(controlPlaneCIDRblocks, ipv6Subnet.String())
+		case 1:
+			computeCIDRblocks = append(computeCIDRblocks, ipv6Subnet.String())
+		}
+	}
+
+	azureCluster.Spec.NetworkSpec.Vnet.VnetClassSpec.CIDRBlocks = vnetCIDRblocks
+	azureCluster.Spec.NetworkSpec.Subnets[0].SubnetClassSpec.CIDRBlocks = controlPlaneCIDRblocks
+	azureCluster.Spec.NetworkSpec.Subnets[1].SubnetClassSpec.CIDRBlocks = computeCIDRblocks
+
+	logrus.Debugf("XXX: control plane subnet CIDR blocks = %+v", azureCluster.Spec.NetworkSpec.Subnets[0].SubnetClassSpec.CIDRBlocks)
+
+	// Set the security group for each subnet
+	for i := 0; i < subnetsCount; i++ {
+		azureCluster.Spec.NetworkSpec.Subnets[i].SecurityGroup = securityGroup
 	}
 
 	// We are maintaining a fork of CAPZ for azurestack. The only API difference
@@ -443,4 +661,18 @@ func deepCopy(src, dst interface{}) error {
 		return err
 	}
 	return json.Unmarshal(bytes, dst)
+}
+
+func apiserverSecurityRule(name string, priority int32) capz.SecurityRule {
+	return capz.SecurityRule{
+		Name:             name,
+		Protocol:         capz.SecurityGroupProtocolTCP,
+		Direction:        capz.SecurityRuleDirectionInbound,
+		Priority:         priority,
+		SourcePorts:      ptr.To("*"),
+		DestinationPorts: ptr.To("6443"),
+		Source:           ptr.To("*"),
+		Destination:      ptr.To("*"),
+		Action:           capz.SecurityRuleActionAllow,
+	}
 }
