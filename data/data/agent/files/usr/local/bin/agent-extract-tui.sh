c3ebbe3e25abcdcbfc5ef520bc77f7a3e490c22a
Richard Su <rwsu@redhat.com>
2025-09-08 16:58:49 -0400

    OCPBUGS-61373: Fix agent shellcheck issues
    Disable shellcheck on agent-image.env The file is generated at
    runtime after get-container-images.sh is executed.
    
    Added missing double quotes and switch echo to printf in
    install-status.sh

--- data/data/agent/files/usr/local/bin/agent-extract-tui.sh
+++ data/data/agent/files/usr/local/bin/agent-extract-tui.sh
@@ -3,17 +3,18 @@
 set -xeuo pipefail
 
 /usr/local/bin/get-container-images.sh
+# shellcheck disable=SC1091
 source /usr/local/share/assisted-service/agent-images.env
 
 echo "Extracting agent-tui and libnmstate from agent-installer-utils image $AGENT_INSTALLER_UTILS_IMAGE"
 
-container_id=$(podman create $AGENT_INSTALLER_UTILS_IMAGE)
-mnt=$(podman mount $container_id)
+container_id=$(podman create "$AGENT_INSTALLER_UTILS_IMAGE")
+mnt=$(podman mount "$container_id")
 
-cp ${mnt}/usr/bin/agent-tui /usr/local/bin
-cp ${mnt}/usr/lib64/libnmstate.so.* /usr/local/lib
+cp "${mnt}/usr/bin/agent-tui" /usr/local/bin
+cp "${mnt}/usr/lib64/libnmstate.so.*" /usr/local/lib
 
-podman unmount $container_id
-podman rm $container_id
+podman unmount "$container_id"
+podman rm "$container_id"
 
 restorecon -FRv /usr/local/bin
