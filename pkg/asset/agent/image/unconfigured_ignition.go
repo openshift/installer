e0a8a83c52186120d8f65f85a3270089e37c8d58
Zane Bitter <zbitter@redhat.com>
2025-09-12 22:16:32 +1200

    Add rendezvous IP to unconfigured-ignition
    If the rendezvous IP is known, always create the rendezvous-host.env
    environment file in the unconfigured-ignition.

--- pkg/asset/agent/image/unconfigured_ignition.go
+++ pkg/asset/agent/image/unconfigured_ignition.go
@@ -170,6 +170,19 @@ func (a *UnconfiguredIgnition) Generate(_ context.Context, dependencies asset.Pa
 	rendezvousHostTemplateFile := ignition.FileFromString(fmt.Sprintf("%s.template", rendezvousHostEnvPath), "root", 0644, rendezvousHostTemplateData)
 	config.Storage.Files = append(config.Storage.Files, rendezvousHostTemplateFile)
 
+	var rendezvousIP string
+	if agentConfig.Config != nil {
+		rendezvousIP = agentConfig.Config.RendezvousIP
+	}
+	if rendezvousIP != "" {
+		rendezvousHostData, err := getRendezvousHostEnvFromTemplate(rendezvousHostTemplateData, rendezvousIP)
+		if err != nil {
+			return err
+		}
+		rendezvousHostFile := ignition.FileFromString(rendezvousHostEnvPath, "root", 0644, rendezvousHostData)
+		config.Storage.Files = append(config.Storage.Files, rendezvousHostFile)
+	}
+
 	switch agentWorkflow.Workflow {
 	case workflow.AgentWorkflowTypeInstall:
 		agentTemplateData.ConfigImageFiles = strings.Join(GetConfigImageFiles(), ",")
@@ -180,20 +193,10 @@ func (a *UnconfiguredIgnition) Generate(_ context.Context, dependencies asset.Pa
 	case workflow.AgentWorkflowTypeInstallInteractiveDisconnected:
 		// Add the rendezvous host file. Agent TUI will interact with that file in case
 		// the rendezvous IP wasn't previously configured, by managing it as a template file.
-		var rendezvousIP, rendezvousHostData string
-		if agentConfig.Config != nil {
-			rendezvousIP = agentConfig.Config.RendezvousIP
+		if rendezvousIP == "" {
+			rendezvousHostFile := ignition.FileFromString(rendezvousHostEnvPath, "root", 0644, rendezvousHostTemplateData)
+			config.Storage.Files = append(config.Storage.Files, rendezvousHostFile)
 		}
-		if rendezvousIP != "" {
-			rendezvousHostData, err := getRendezvousHostEnvFromTemplate(rendezvousHostTemplateData, rendezvousIP)
-			if err != nil {
-				return err
-			}
-		} else {
-			rendezvousHostData = rendezvousHostTemplateData
-		}
-		rendezvousHostFile := ignition.FileFromString(rendezvousHostEnvPath, "root", 0644, rendezvousHostData)
-		config.Storage.Files = append(config.Storage.Files, rendezvousHostFile)
 
 		// Explicitly disable the load-config-iso service, not required in the current flow
 		// (even though disabled by default, the udev rule may require it).
