ae49b022eba410dcee47d96a36b5170f57631931
Brandon Palm <bpalm@redhat.com>
2025-11-21 14:32:39 -0600

    Upgrade GolangCI-lint to v2

--- hack/go-lint.sh
+++ hack/go-lint.sh
@@ -5,5 +5,5 @@ podman run --rm \
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/go/src/github.com/openshift/installer:z" \
     --workdir /go/src/github.com/openshift/installer \
-    docker.io/golangci/golangci-lint:v1.64.8 \
+    docker.io/golangci/golangci-lint:v2.3.1 \
     golangci-lint run -v --new-from-rev=dcf8122 "${@}"
