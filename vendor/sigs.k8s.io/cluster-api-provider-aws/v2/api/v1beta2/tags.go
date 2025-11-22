40f228811454565f3122f1cacbf1d34653842023
Thuan Vo <thvo@redhat.com>
2025-11-21 16:14:41 -0800

    deps: top-level: go mod vendor

--- vendor/sigs.k8s.io/cluster-api-provider-aws/v2/api/v1beta2/tags.go
+++ vendor/sigs.k8s.io/cluster-api-provider-aws/v2/api/v1beta2/tags.go
@@ -23,7 +23,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 
-	clusterv1beta1 "sigs.k8s.io/cluster-api/api/core/v1beta1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 )
 
 // Tags defines a map of tags.
@@ -239,7 +239,7 @@ type BuildParams struct {
 
 // WithMachineName tags the namespaced machine name
 // The machine name will be tagged with key "MachineName".
-func (b BuildParams) WithMachineName(m *clusterv1beta1.Machine) BuildParams {
+func (b BuildParams) WithMachineName(m *clusterv1.Machine) BuildParams {
 	machineNamespacedName := types.NamespacedName{Namespace: m.Namespace, Name: m.Name}
 	b.Additional[MachineNameTagKey] = machineNamespacedName.String()
 	return b
