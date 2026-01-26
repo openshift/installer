524811bbaef6d1e72828c5ab4566a2b2ba829cd9
Pawan Pinjarkar <ppinjark@redhat.com>
2026-01-26 09:26:46 -0500

    AGENT-1309: Increase disk size requirements for master and SNO

--- data/data/agent/files/usr/local/bin/configure-assisted-hw-requirements.sh
+++ data/data/agent/files/usr/local/bin/configure-assisted-hw-requirements.sh
@@ -2,14 +2,19 @@
 set -e
 
 ASSISTED_SERVICE_ENV_FILE="/usr/local/share/assisted-service/assisted-service.env"
-# default value
-HW_VALIDATOR_REQUIREMENTS="[{\"version\":\"default\",\"master\":{\"cpu_cores\":4,\"ram_mib\":16384,\"disk_size_gb\":100,\"installation_disk_speed_threshold_ms\":10,\"network_latency_threshold_ms\":100,\"packet_loss_percentage\":0},\"arbiter\":{\"cpu_cores\":2,\"ram_mib\":8192,\"disk_size_gb\":50,\"installation_disk_speed_threshold_ms\":10,\"network_latency_threshold_ms\":1000,\"packet_loss_percentage\":0},\"worker\":{\"cpu_cores\":2,\"ram_mib\":8192,\"disk_size_gb\":100,\"installation_disk_speed_threshold_ms\":10,\"network_latency_threshold_ms\":1000,\"packet_loss_percentage\":10},\"sno\":{\"cpu_cores\":8,\"ram_mib\":16384,\"disk_size_gb\":100,\"installation_disk_speed_threshold_ms\":10}}]"
+
+# default
+MASTER_DISK_SIZE=100
+SNO_DISK_SIZE=100
 
 # Increase disk size requirements for NoRegistryClusterInstall
 if [ -f /etc/assisted/extra-manifests/internalreleaseimage.yaml ]; then
-    # shellcheck disable=SC2001
-    HW_VALIDATOR_REQUIREMENTS=$(echo "$HW_VALIDATOR_REQUIREMENTS" |sed 's/"disk_size_gb":[0-9]\+/"disk_size_gb":220/g')
+    MASTER_DISK_SIZE=220
+    SNO_DISK_SIZE=220
 fi
+
+# Build requirements with variables
+HW_VALIDATOR_REQUIREMENTS="[{\"version\":\"default\",\"master\":{\"cpu_cores\":4,\"ram_mib\":16384,\"disk_size_gb\":${MASTER_DISK_SIZE},\"installation_disk_speed_threshold_ms\":10,\"network_latency_threshold_ms\":100,\"packet_loss_percentage\":0},\"arbiter\":{\"cpu_cores\":2,\"ram_mib\":8192,\"disk_size_gb\":50,\"installation_disk_speed_threshold_ms\":10,\"network_latency_threshold_ms\":1000,\"packet_loss_percentage\":0},\"worker\":{\"cpu_cores\":2,\"ram_mib\":8192,\"disk_size_gb\":100,\"installation_disk_speed_threshold_ms\":10,\"network_latency_threshold_ms\":1000,\"packet_loss_percentage\":10},\"sno\":{\"cpu_cores\":8,\"ram_mib\":16384,\"disk_size_gb\":${SNO_DISK_SIZE},\"installation_disk_speed_threshold_ms\":10}}]"
 
 # Replace the final value in the env file
 sed -i "s|^HW_VALIDATOR_REQUIREMENTS=.*|HW_VALIDATOR_REQUIREMENTS=$HW_VALIDATOR_REQUIREMENTS|" "$ASSISTED_SERVICE_ENV_FILE"
