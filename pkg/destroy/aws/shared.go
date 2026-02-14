f093d45bf659d16eb82e481971020a3d2771f937
Thuan Vo <thvo@redhat.com>
2026-02-13 20:21:16 -0800

    destroy: skip shared resources that AWS do not support tagging
    Untagging hosted zone in region "eusc-de-east-1" is not supported via
    resourcetagging api. If attempting to do so, the api returns the
    following error:
    
    UntagResources operation: Invocation of UntagResources for this resource
    is not supported in this region
    
    This causes the bulk untagging operation to fail and leave other
    resources with the shared tag on.

--- pkg/destroy/aws/shared.go
+++ pkg/destroy/aws/shared.go
@@ -110,10 +110,14 @@ func (o *ClusterUninstaller) removeSharedTag(ctx context.Context, tagClients []*
 				continue
 			}
 
+			// Some regions may not support untag operations for certain resources
+			arns = o.filterUnsupportedUntagResources(arns)
+
 			if len(arns) == 0 {
 				o.Logger.Debugf("No matches in %s for %s: shared, removing client", tagClient.Options().Region, key)
 				continue
 			}
+
 			// appending the tag client here but it needs to be removed if there is a InvalidParameterException when trying to
 			// untag below since that only leads to an infinite loop error.
 			nextTagClients = append(nextTagClients, tagClient)
@@ -299,4 +303,39 @@ func deleteMatchingRecordSetInPublicZone(ctx context.Context, client *route53.Cl
 		return nil
 	}
 	return deleteRoute53RecordSet(ctx, client, zoneID, &matchingRecordSet, logger)
+}
+
+// filterUnsupportedUntagResources filters out ARNs that cannot be untagged due to AWS limitation.
+// For example, hosted zones cannot be untagged in region "eusc-de-east-1".
+func (o *ClusterUninstaller) filterUnsupportedUntagResources(arns []string) []string {
+	filtered := make([]string, 0, len(arns))
+	skipped := make([]string, 0)
+	switch o.Region {
+	case endpointEUSCDeEast1:
+		for _, arnString := range arns {
+			parsedARN, err := arn.Parse(arnString)
+			if err != nil {
+				filtered = append(filtered, arnString)
+				continue
+			}
+			resourceType, _, err := splitSlash("resource", parsedARN.Resource)
+			if err != nil {
+				filtered = append(filtered, arnString)
+				continue
+			}
+			if parsedARN.Service == "route53" && resourceType == "hostedzone" {
+				skipped = append(skipped, arnString)
+				continue
+			}
+			filtered = append(filtered, arnString)
+		}
+	default:
+		filtered = arns
+	}
+
+	for _, arnString := range skipped {
+		o.Logger.WithField("arn", arnString).Warnf("Untagging this resource via resourcetagging api is not supported by AWS in region %s. Please use the AWS Route 53 APIs, CLI, or console", o.Region)
+	}
+
+	return filtered
 }
