1a891eb2f38875b97787a6061ee147000a32058b
Pablo Rodriguez Nava <git@amail.pablintino.eu>
2026-02-27 17:57:17 +0100

    MCO-2116: Generate OSImageStream manifest
    When the OSStreams feature gate is enabled and generate the
    OSImageStream CR in the openshift manifests so the cluster
    can boot with the desired stream.
    If not stream was selected the default one is picked.
    
    Signed-off-by: Pablo Rodriguez Nava <git@amail.pablintino.eu>

--- pkg/rhcos/stream.go
+++ pkg/rhcos/stream.go
@@ -2,6 +2,12 @@
 
 package rhcos
 
+import "github.com/openshift/installer/pkg/types"
+
+// DefaultOSImageStream is the OS image stream used when the install-config
+// does not specify one.
+const DefaultOSImageStream = types.OSImageStreamRHCOS9
+
 func getStreamFileName() string {
 	return "coreos/rhcos.json"
 }
