92162c9cb00bf8dd105e9c0b39d5d1fba26c09cb
Thuan Vo <thvo@redhat.com>
2026-03-10 15:51:06 -0700

    validations: dualstack is only supported in byo subnets in local zones
    In dualstack networking, edge compute pool is only valid if:
    - using existing subnets; and
    - using local zones
    
    Wavelength zones do not support IPv6.

--- pkg/asset/installconfig/aws/validation.go
+++ pkg/asset/installconfig/aws/validation.go
@@ -365,17 +365,32 @@ func validateMachinePool(ctx context.Context, meta *Metadata, fldPath *field.Pat
 	// Edge Compute Pool / AWS Local Zones:
 	// - is valid when installing in existing VPC; or
 	// - is valid in new VPC when Local Zone name is defined
+	// - in dualstack networking: is valid when using local zones and installing in existing VPC
 	if poolName == types.MachinePoolEdgeRoleName {
 		if len(platform.VPC.Subnets) > 0 {
+			subnetFp := field.NewPath("platform", "aws", "vpc", "subnets")
+
 			edgeSubnets, err := meta.EdgeSubnets(ctx)
 			if err != nil {
 				errMsg := fmt.Sprintf("%s pool. %v", poolName, err.Error())
-				return append(allErrs, field.Invalid(field.NewPath("platform", "aws", "vpc", "subnets"), platform.VPC.Subnets, errMsg))
+				return append(allErrs, field.Invalid(subnetFp, platform.VPC.Subnets, errMsg))
 			}
 			if len(edgeSubnets) == 0 {
 				return append(allErrs, field.Required(fldPath, "the provided subnets must include valid subnets for the specified edge zones"))
 			}
+
+			if platform.IPFamily.DualStackEnabled() {
+				for _, sn := range edgeSubnets {
+					if sn.Zone.Type == awstypes.WavelengthZoneType {
+						allErrs = append(allErrs, field.Invalid(subnetFp, platform.VPC.Subnets, fmt.Sprintf("ipFamily %s is not supported for subnets in wavelength zones", platform.IPFamily)))
+					}
+				}
+			}
 		} else {
+			if platform.IPFamily.DualStackEnabled() {
+				return append(allErrs, field.Forbidden(fldPath, fmt.Sprintf("ipFamily %s is only supported with user-provided subnets for edge machine pools", string(platform.IPFamily))))
+			}
+
 			if pool.Zones == nil || len(pool.Zones) == 0 {
 				return append(allErrs, field.Required(fldPath, "zone is required when using edge machine pools"))
 			}
