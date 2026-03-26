fb82f1e8714c18d57a944cf87da8271f7f062058
barbacbd <barbacbd@gmail.com>
2026-03-26 14:42:23 -0400

    1. Update the base go.mod file
    - Update the CAPG reference to match the latest CAPG commit on March 23) to include all firewall information

--- cluster-api/providers/ibmcloud/vendor/google.golang.org/grpc/grpclog/internal/grpclog.go
+++ cluster-api/providers/ibmcloud/vendor/google.golang.org/grpc/grpclog/internal/grpclog.go
@@ -1,6 +1,10 @@
 /*
  *
+<<<<<<<< HEAD:cluster-api/providers/ibmcloud/vendor/google.golang.org/grpc/grpclog/internal/grpclog.go
  * Copyright 2024 gRPC authors.
+========
+ * Copyright 2025 gRPC authors.
+>>>>>>>> 8a4761186a (1. Update the base go.mod file):vendor/google.golang.org/grpc/encoding/internal/internal.go
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +20,7 @@
  *
  */
 
+<<<<<<<< HEAD:cluster-api/providers/ibmcloud/vendor/google.golang.org/grpc/grpclog/internal/grpclog.go
 // Package internal contains functionality internal to the grpclog package.
 package internal
 
@@ -24,3 +29,15 @@ var LoggerV2Impl LoggerV2
 
 // DepthLoggerV2Impl is the logger used for the depth log functions.
 var DepthLoggerV2Impl DepthLoggerV2
+========
+// Package internal contains code internal to the encoding package.
+package internal
+
+// RegisterCompressorForTesting registers a compressor in the global compressor
+// registry. It returns a cleanup function that should be called at the end
+// of the test to unregister the compressor.
+//
+// This prevents compressors registered in one test from appearing in the
+// encoding headers of subsequent tests.
+var RegisterCompressorForTesting any // func RegisterCompressor(c Compressor) func()
+>>>>>>>> 8a4761186a (1. Update the base go.mod file):vendor/google.golang.org/grpc/encoding/internal/internal.go
