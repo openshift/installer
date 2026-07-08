98de92a4feb15724558a83eaff2b20e33b3ba851
Roman Feldman <rofeldma@redhat.com>
2026-07-08 15:41:00 -0400

    vendor changes

--- vendor/github.com/containers/storage/pkg/mount/mounter_unsupported.go
+++
@@ -1,9 +0,0 @@
-//go:build !linux && !(freebsd && cgo)
-// +build !linux
-// +build !freebsd !cgo
-
-package mount
-
-func mount(device, target, mType string, flag uintptr, data string) error {
-	panic("Not implemented")
-}
